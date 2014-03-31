27333a7f423d8653e8b1ec4820db3039200d6512
latkin <latkin@microsoft.com>
2014-03-31 12:24:56 -0700

    Various cleanup of RunTests.cmd, in particular setting all suites to write similarly-named log files directly to \tests.  Also updating the docs and adding .gitignore for test logs. (changeset 1219334)

--- tests/RunTests.cmd
+++ tests/RunTests.cmd
@@ -9,11 +9,10 @@ if not "%_tmp%" == "" set TTAGS_ARG=-ttags:%_tmp:"=%
 rem "nottags" indicates which test areas/test cases will NOT be run, based on the tags in the test.lst and env.lst files
 set NO_TTAGS_ARG=-nottags:ReqPP
 set _tmp=%3
-if not "%3" == "" set NO_TTAGS_ARG=-nottags:ReqPP,%_tmp:"=%
+if not "%_tmp%" == "" set NO_TTAGS_ARG=-nottags:ReqPP,%_tmp:"=%
 
 rem Use commented line to enable parallel execution of tests
-set PARALLEL_ARG=
-set PARALLEL_ARG=-procs:3
+rem set PARALLEL_ARG=-procs:3
 
 rem This can be set to 1 to reduce the number of permutations used and avoid some of the extra-time-consuming tests
 set REDUCED_RUNTIME=1
@@ -26,18 +25,29 @@ rem set HOSTED_COMPILER=1
 
 if /I "%1" == "fsharp" (goto :FSHARP)
 if /I "%1" == "fsharpqa" (goto :FSHARPQA)
+if /I "%1" == "coreunit" (goto :COREUNIT)
 
 echo Usage:
 echo.
-echo RunTests.cmd ^<fsharp^|fsharpqa^>
+echo RunTests.cmd ^<fsharp^|fsharpqa^|coreunit^>
 echo.
 exit /b 1
 
 
 :FSHARP
 
-echo perl %~dp0\fsharpqa\testenv\bin\runall.pl %TTAGS_ARG% %NO_TTAGS_ARG% %PARALLEL_ARG%
-     perl %~dp0\fsharpqa\testenv\bin\runall.pl %TTAGS_ARG% %NO_TTAGS_ARG% %PARALLEL_ARG%
+set RESULTFILE=FSharp_Results.log
+set FAILFILE=FSharp_Failures.log
+set FAILENV=FSharp_Failures
+
+where.exe perl > NUL 2> NUL 
+if errorlevel 1 (
+  echo Error: perl is not in the PATH
+  exit /b 1
+)
+
+echo perl %~dp0\fsharpqa\testenv\bin\runall.pl -resultsroot %~dp0 -results %RESULTFILE% -log %FAILFILE% -fail %FAILENV% -cleanup:yes %TTAGS_ARG% %NO_TTAGS_ARG% %PARALLEL_ARG%
+     perl %~dp0\fsharpqa\testenv\bin\runall.pl -resultsroot %~dp0 -results %RESULTFILE% -log %FAILFILE% -fail %FAILENV% -cleanup:yes %TTAGS_ARG% %NO_TTAGS_ARG% %PARALLEL_ARG%
 goto :EOF
 
 
@@ -70,10 +80,36 @@ set PATH=%PATH%;%CORDIR%
 
 if not exist %WINDIR%\Microsoft.NET\Framework\v2.0.50727\mscorlib.dll set NO_TTAGS_ARG=%NO_TTAGS_ARG%,Req20 
 
-pushd %~dp0\fsharpqa\source
+set RESULTFILE=FSharpQA_Results.log
+set FAILFILE=FSharpQA_Failures.log
+set FAILENV=FSharpQA_Failures
 
-echo perl %~dp0\fsharpqa\testenv\bin\runall.pl %TTAGS_ARG% %NO_TTAGS_ARG% %PARALLEL_ARG%
-     perl %~dp0\fsharpqa\testenv\bin\runall.pl %TTAGS_ARG% %NO_TTAGS_ARG% %PARALLEL_ARG%
+where.exe perl > NUL 2> NUL 
+if errorlevel 1 (
+  echo Error: perl is not in the PATH
+  exit /b 1
+)
+
+pushd %~dp0\fsharpqa\source
+echo perl %~dp0\fsharpqa\testenv\bin\runall.pl -resultsroot %~dp0 -results %RESULTFILE% -log %FAILFILE% -fail %FAILENV% -cleanup:yes %TTAGS_ARG% %NO_TTAGS_ARG% %PARALLEL_ARG%
+     perl %~dp0\fsharpqa\testenv\bin\runall.pl -resultsroot %~dp0 -results %RESULTFILE% -log %FAILFILE% -fail %FAILENV% -cleanup:yes %TTAGS_ARG% %NO_TTAGS_ARG% %PARALLEL_ARG%
 
 popd
+goto :EOF
+
+:COREUNIT
+
+set XMLFILE=CoreUnit_Xml.xml
+set OUTPUTFILE=CoreUnit_Output.log
+set ERRORFILE=CoreUnit_Error.log
+set FILEDIR=%~dp0
+
+where.exe nunit-console.exe > NUL 2> NUL 
+if errorlevel 1 (
+  echo Error: nunit-console.exe is not in the PATH
+  exit /b 1
+)
+echo nunit-console.exe /nologo /result=%XMLFILE% /output=%OUTPUTFILE% /err=%ERRORFILE% /work=%FILEDIR% %~dp0..\Debug\net40\bin\FSharp.Core.Unittests.dll 
+     nunit-console.exe /nologo /result=%XMLFILE% /output=%OUTPUTFILE% /err=%ERRORFILE% /work=%FILEDIR% %~dp0..\Debug\net40\bin\FSharp.Core.Unittests.dll 
+
 goto :EOF
