00804562210ef21a6e4081ec3e7a58509e382b0c
latkin <latkin@microsoft.com>
2014-04-24 13:27:13 -0700

    Update param array test case to build C# component on-the-fly instead of using a checked-in binary.  Also fill in the test case a bit more. (changeset 1239498)

--- tests/fsharpqa/Source/Misc/ParamArray.cs
+++ tests/fsharpqa/Source/Misc/ParamArray.cs
@@ -2,38 +2,27 @@
 
 namespace CSharpAssembly
 {
-	[AttributeUsage(AttributeTargets.All)]
-	public class AttributeWithParamArray : Attribute
-	{
-		public object[] Parameters;
+    [AttributeUsage(AttributeTargets.All)]
+    public class AttributeWithParamArray : Attribute
+    {
+        public object[] Parameters;
 
-		public AttributeWithParamArray(params object[] x)
-		{
+        public AttributeWithParamArray(params object[] x)
+        {
 
-			Parameters = x;
-		}
-	}
+            Parameters = x;
+        }
+    }
 
-	public class Foo
-	{
-		static int Main(string[] args)
-		{
-			Console.WriteLine(Foo.Method());
-			Console.WriteLine(Foo.Method(1));
-			Console.WriteLine(Foo.Method(1, 2));
-			Console.WriteLine(Foo.Method(1, 2, 3));
-			Console.WriteLine(Foo.Method(1, 2, 3, 4));
+    public class CSParamArray
+    {
+        public static int Method(params int[] allArgs)
+        {
+            int total = 0;
+            foreach (int i in allArgs)
+                total += i;
 
-			return 0;
-		}
-
-		static int Method(params int[] allArgs)
-		{
-			int total = 0;
-			foreach (int i in allArgs)
-				total += i;
-
-			return total;
-		}
-	}
+            return total;
+        }
+    }
 }
