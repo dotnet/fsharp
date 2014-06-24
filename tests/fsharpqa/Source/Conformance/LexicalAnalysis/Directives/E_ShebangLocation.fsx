1dfe31978bec6f74c4ec09dca9f7dab7080710d4
Robert Jeppesen <robert.jeppesen@durius.se>
2014-06-24 07:25:42 +0200

    Add test for invalid shebang location

---
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/Directives/E_ShebangLocation.fsx
@@ -0,0 +1,4 @@
+// #Regression #Conformance #LexicalAnalysis 
+//<Expects id="FS0000" span="(3,1)" status="error">#! must appear at the start of the file.</Expects>
+#!/bin/usr/env fsharpi --exec
+exit 0
