dfb883b2afb907771468d80d56bdf4873dbe91b7
KevinRansom <codecutter@hotmail.com>
2014-06-24 17:56:07 -0700

    Fix expected error message to match text.

--- tests/fsharpqa/Source/Conformance/LexicalAnalysis/Directives/E_ShebangLocation.fsx
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/Directives/E_ShebangLocation.fsx
@@ -1,4 +1,4 @@
 // #Regression #Conformance #LexicalAnalysis 
-//<Expects id="FS0000" span="(3,1)" status="error">#! must appear at the start of the file.</Expects>
+//<Expects id="FS0000" span="(3,1)" status="error">#! may only appear as the first line at the start of a file</Expects>
 #!/bin/usr/env fsharpi --exec
 exit 0
