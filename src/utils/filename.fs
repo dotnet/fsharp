7785a90dbbaa69640c660aa7f7c6067e074edddb
Jack Pappas <jack-pappas@users.noreply.github.com>
2014-07-12 20:55:22 -0400

    Optimized the implementation of the 'checkPathForIllegalChars' function, which was showing up in performance profiling as an allocation hotspot. This function is called suprisingly many times during compilation, and the function was allocating a closure each time it was called.

--- src/utils/filename.fs
+++ src/utils/filename.fs
@@ -7,19 +7,35 @@ open Microsoft.FSharp.Compiler.AbstractIL.Internal.Library
 
 exception IllegalFileNameChar of string * char
 
-let illegalPathChars = Path.GetInvalidPathChars()
+/// The set of characters which may not be used in a path.
+/// This is saved here because Path.GetInvalidPathChars() allocates and returns
+/// a new array each time it's called (by necessity, for security reasons).
+/// This is only used within `checkPathForIllegalChars`, and is only read from.
+let illegalPathChars =
+    let chars = Path.GetInvalidPathChars ()
+    Array.sortInPlace chars
+    chars
 
-let checkPathForIllegalChars (path:string) = 
-    for c in path do
-        if illegalPathChars |> Array.exists(fun c1->c1=c) then 
-            raise(IllegalFileNameChar(path,c))
+let checkPathForIllegalChars (path:string) =
+    let len = path.Length
+    for i = 0 to len - 1 do
+        // The current character in the string.
+        let c = path.[i]
+        
+        // Determine if this character is disallowed within a path by
+        // attempting to find it in the array of illegal path characters.
+        for badChar in illegalPathChars do
+            if c = badChar then
+                // The character is not allowed to be used within a path, raise an exception.
+                raise(IllegalFileNameChar(path, c))
 
 // Case sensitive (original behaviour preserved).
 let checkSuffix (x:string) (y:string) = x.EndsWith(y,System.StringComparison.Ordinal) 
 
 let hasExtension (s:string) = 
     checkPathForIllegalChars s
-    (s.Length >= 1 && s.[s.Length - 1] = '.' && s <> ".." && s <> ".") 
+    let sLen = s.Length
+    (sLen >= 1 && s.[sLen - 1] = '.' && s <> ".." && s <> ".") 
     || Path.HasExtension(s)
 
 let chopExtension (s:string) =
