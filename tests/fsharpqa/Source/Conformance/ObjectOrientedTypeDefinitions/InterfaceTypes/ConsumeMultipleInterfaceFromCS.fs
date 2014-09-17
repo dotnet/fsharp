e655395d7a83de36208f737498e7f35c6c98400b
Don Syme <dsyme@microsoft.com>
2014-09-17 16:54:04 +0100

    update tests for multiple instantiations of interfaces

--- tests/fsharpqa/Source/Conformance/ObjectOrientedTypeDefinitions/InterfaceTypes/ConsumeMultipleInterfaceFromCS.fs
+++ tests/fsharpqa/Source/Conformance/ObjectOrientedTypeDefinitions/InterfaceTypes/ConsumeMultipleInterfaceFromCS.fs
@@ -1,5 +1,5 @@
 // #Conformance #ObjectOrientedTypes #InterfacesAndImplementations #ReqNOMT 
-#light
+#light	
 
 let mutable res = true
 let t = new T()
@@ -15,6 +15,7 @@ if (t.Home(0) <> 0) then
   System.Console.WriteLine("t.Home failed")
   res <- false
 
+// Check we can use an object expression implementing a C# interface type inheriting multiple instantiations of an interface
 if ( {new I_003<int> with 
                member xxx.Home(i) = i
                member xxx.Me(c:char) = 0
@@ -26,6 +27,37 @@ if ( {new I_003<int> with
             System.Console.WriteLine("I_003.Home failed")         
             res <- false
   
+// Check we can use an object expression inheriting from a C# type implementing multiple instantiations of an interface
+if (({new T() with 
+               member x.ToString() = "a"
+      interface I_003<int> with 
+               member xxx.Home(i) = i
+               member xxx.Me(c:char) = 0
+               member xxx.Me(s:string) = 0
+      } :> I_003<int>).Home (
+        {new I_002<int> with 
+          member x.Me (s) = s
+        }.Me(0) )  <> 0 ) then
+            System.Console.WriteLine("T.Home obj expr failed")         
+            res <- false
+  
+// Check we can create an object of a C# type implementing multiple instantiations of an interface
+if T().Home(4) <> 0 then
+            System.Console.WriteLine("T.Home failed")         
+            res <- false
+
+
+// Check we can inherit from a C# type implementing multiple instantiations of an interface
+type D() = 
+    inherit T()
+    interface I_003<int> with
+       member xxx.Home(i) = i
+
+if (D() :> I_003<int>).Home(5) <> 5 then
+            System.Console.WriteLine("D.Home failed")         
+            res <- false
+
+
 if (res = true) then
     exit 0
     
