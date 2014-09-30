c3667942d2ec71fc662f7c365b743c9b6f4002c4
latkin <latkin@microsoft.com>
2014-09-30 11:24:32 -0700

    Elevation method previously used in internal tests no longer works.  Adding our own elevation script to replace it. (changeset 1344811)

---
+++ tests/fsharpqa/testenv/bin/elevate.fsx
@@ -0,0 +1,18 @@
+﻿// simple elevation script - runs the specified command line as admin
+// usage: fsi --exec elevate.fsx <program> [args]
+
+open System
+open System.Diagnostics
+
+let program = fsi.CommandLineArgs.[1]
+let args = fsi.CommandLineArgs |> Seq.skip 2 |> String.concat " "
+let startInfo = ProcessStartInfo(FileName = program, Arguments = args, Verb = "runas")
+let proc = new Process(StartInfo = startInfo)
+
+printfn "Elevating: %s %s" program args
+try
+    proc.Start() |> ignore
+    if proc.WaitForExit(30*60*1000) then
+        Environment.Exit(proc.ExitCode)
+with e -> eprintfn "%s" (e.ToString())
+Environment.Exit(1)
