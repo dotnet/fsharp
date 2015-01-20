5307067b7e5b940ae51416062a405170088d7886
Don Syme <dsyme@microsoft.com>
2015-01-19 17:59:46 -0800

    fix for private val set
    fixes #27
    resolves #28
    
    commit bf3c9b3ff6151a09dd9319e5e54d21a0edd17c31
    Merge: 5eb0657 7393d92
    Author: latkin <latkin@microsoft.com>
    Date:   Mon Jan 19 16:57:58 2015 -0800
    
        Merge branch 'private-val-fix' of https://github.com/dsyme/visualfsharp into dsyme-private-val-fix
    
    commit 7393d923dde34478c1ba0be91e7220b948caaadd
    Author: Don Syme <dsyme@microsoft.com>
    Date:   Sat Jan 17 11:50:54 2015 +0000
    
        add tests for val sets
    
    commit 68555a398f8fc2881b32679236640d3163b0ccb7
    Author: Don Syme <dsyme@microsoft.com>
    Date:   Sat Jan 17 11:38:18 2015 +0000
    
        adjust range reported in error
    
    commit bff938f22094c17c16ebe25309ee0f6ad9cf9358
    Author: Don Syme <dsyme@microsoft.com>
    Date:   Sat Jan 17 11:35:08 2015 +0000
    
        fix compile
    
    commit d2f12e44527a0685dee57dcfe445e720696401b8
    Author: Don Syme <dsyme@microsoft.com>
    Date:   Sat Jan 17 11:33:41 2015 +0000
    
        fix for private val set

--- tests/fsharp/typecheck/sigs/neg91.fs
+++ tests/fsharp/typecheck/sigs/neg91.fs
@@ -12,3 +12,44 @@ module T2 =
     type Enum2
         with
         member this.Foo() = 1 // not ok
+
+module TestPrivateSet = 
+    // See https://github.com/Microsoft/visualfsharp/issues/27
+    module A =
+        let mutable private x = 0
+
+    module B = 
+        let test () =
+            // let _ = A.x  // accessibility error on read, as expected
+            A.x <- 1     // but write works!
+
+module TestObsoleteSet = 
+    // See https://github.com/Microsoft/visualfsharp/issues/27
+    module A =
+        [<System.ObsoleteAttribute("Don't touch me")>]
+        let mutable x = 0
+
+    module B = 
+        let test () =
+            A.x <- 1     
+
+module TestCompilerMessgeSet = 
+    // See https://github.com/Microsoft/visualfsharp/issues/27
+    module A =
+        [<CompilerMessageAttribute("Don't touch me", 3003)>]
+        let mutable x = 0
+
+    module B = 
+        let test () =
+            A.x <- 1     
+
+module TestExperimentalSet = 
+    // See https://github.com/Microsoft/visualfsharp/issues/27
+    module A =
+        [<ExperimentalAttribute("It was just an experiment!")>]
+        let mutable x = 0
+
+    module B = 
+        let test () =
+            A.x <- 1     
+
