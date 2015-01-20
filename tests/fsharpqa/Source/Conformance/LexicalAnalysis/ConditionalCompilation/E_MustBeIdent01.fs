438eae21c91d8648f88ea8b343640b1b60460fad
mrange <marten_range@hotmail.com>
2015-01-20 14:35:07 -0800

    Implements extended if grammar
    closes #55
    
    commit 677a7e799fd5350fd166411004a734f56e9c4e4b
    Author: latkin <latkin@microsoft.com>
    Date:   Tue Jan 20 11:34:49 2015 -0800
    
        Update DEVGUIDE and TESTGUIDE to include compiler unit test suite
    
    commit 90f33737381c0932a478746fc566532da862f3c3
    Author: latkin <latkin@microsoft.com>
    Date:   Tue Jan 20 11:20:56 2015 -0800
    
        Simplifying ExtendedIfGrammar tests
    
    commit 78b8ff4e325dc37a6d4ca0777b05a2abe85f961c
    Author: latkin <latkin@microsoft.com>
    Date:   Tue Jan 20 11:20:10 2015 -0800
    
        Adding compilerunit to RunTests.cmd usage
    
    commit ba861296fe847faa217ab76ec1cb76aef7bc4d02
    Merge: 28da5c0 42e0766
    Author: latkin <latkin@microsoft.com>
    Date:   Tue Jan 20 11:05:01 2015 -0800
    
        Merge branch 'pr/extended_if_grammar' of https://github.com/mrange/visualfsharp into mrange-pr/extended_if_grammar
    
        Conflicts:
        	tests/fsharpqa/Source/Conformance/LexicalAnalysis/ConditionalCompilation/ExtendedIfGrammar.fs
    
    commit 28da5c0800a28cc2044a8d3bb4a22751803433aa
    Author: latkin <latkin@microsoft.com>
    Date:   Mon Jan 19 18:38:11 2015 -0800
    
        Removing generated .fs/.fsi files from repo
    
    commit 47b61d5460f4c84c1b985d50ce96d944c8791efa
    Author: latkin <latkin@microsoft.com>
    Date:   Mon Jan 19 18:36:15 2015 -0800
    
        Use dedicated fsharp-compiler-unittests-build.proj so portable/net20 build of library unit tests is not broken
    
    commit 95e1af8c8bac8b4bc6312a171e675392bff67626
    Merge: c5e6699 5f8880c
    Author: latkin <latkin@microsoft.com>
    Date:   Mon Jan 19 18:14:12 2015 -0800
    
        Merge branch 'pr/extended_if_grammar' of https://github.com/mrange/visualfsharp into mrange-pr/extended_if_grammar
    
    commit 5f8880c151f9d027ed31e4d1d0d860ea75a759b3
    Author: mrange <marten_range@hotmail.com>
    Date:   Sun Jan 18 14:50:47 2015 +0100
    
        Implements extended if grammar
    
        Origin: https://fslang.uservoice.com/forums/245727-f-language/suggestions/6079342-allow-extended-if-grammar
    
        This commit adds the possibility to write condtional compilation expressions like this:
         #if SILVERLIGHT || NETFX
         #endif
    
        or a bit more advanced:
         #if (SILVERLIGHT || NETFX) && COMPILED || !DEBUG
         #endif
    
        This commit doesn't add #elif which is an orthogonal problem.

--- tests/fsharpqa/Source/Conformance/LexicalAnalysis/ConditionalCompilation/E_MustBeIdent01.fs
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/ConditionalCompilation/E_MustBeIdent01.fs
@@ -1,6 +1,7 @@
-// #Regression #Conformance #LexicalAnalysis 
+// #Regression #Conformance #LexicalAnalysis
 // Verify error if preprocessor directive isn't a valid identifier
-//<Expects id="FS1169" status="error">#if directive should be immediately followed by an identifier</Expects>
+//<Expects id="FS3182" status="error">Unexpected character '\*' in preprocessor expression</Expects>
+//<Expects id="FS3184" status="error">Incomplete preprocessor expression</Expects>
 
 #if *COMPILED*
 exit 0
