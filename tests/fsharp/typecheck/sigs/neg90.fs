fa5e972adf113af0dc379a4962a549467d58b3b2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-01-26 16:03:38 +0100

    Add test for DU which is accessed without type but RequiredQualifiedAccess was set - relates to #95

--- tests/fsharp/typecheck/sigs/neg90.fs
+++ tests/fsharp/typecheck/sigs/neg90.fs
@@ -6,3 +6,11 @@ let _ = foo<Recd>()
 // See https://github.com/Microsoft/visualfsharp/issues/38
 type [<Measure>] N = foo // foo is undefined
 type M2 = float<N>
+
+
+// See https://github.com/Microsoft/visualfsharp/issues/95
+module First = 
+  [<RequireQualifiedAccess>]
+  type DU = Member of int
+
+let _ = First.Member(0)
