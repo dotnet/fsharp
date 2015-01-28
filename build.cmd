140d0b344390b1611d460f03e255b792d818e27b
Don Syme <dsyme@microsoft.com>
2015-01-28 23:19:40 +0000

    update appveyor support

--- build.cmd
+++ build.cmd
@@ -1,5 +1,7 @@
 @echo on
 
+set APPVEYOR_CI=1
+
 :: Check prerequisites
 set _msbuildexe="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 if not exist %_msbuildexe% set _msbuildexe="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
@@ -19,39 +21,67 @@ if not exist %_gacutilexe% echo Error: Could not find gacutil.exe.  && goto :eof
 
 %_msbuildexe% src\fsharp-proto-build.proj
 @if ERRORLEVEL 1 echo Error: compiler proto build failed && goto :eof
+
 ngen install lib\proto\fsc-proto.exe
+
 %_msbuildexe% src/fsharp-library-build.proj /p:UseNugetPackages=true 
 @if ERRORLEVEL 1 echo Error: library debug build failed && goto :eof
+
 %_msbuildexe% src/fsharp-compiler-build.proj /p:UseNugetPackages=true 
 @if ERRORLEVEL 1 echo Error: compile debug build failed && goto :eof
-%_msbuildexe% src/fsharp-library-build.proj /p:UseNugetPackages=true /p:TargetFramework=net20
-@if ERRORLEVEL 1 echo Error: library net20 debug build failed && goto :eof
+
+REM We don't build new net20 FSharp.Core anymore
+REM %_msbuildexe% src/fsharp-library-build.proj /p:UseNugetPackages=true /p:TargetFramework=net20
+REM @if ERRORLEVEL 1 echo Error: library net20 debug build failed && goto :eof
+
 %_msbuildexe% src/fsharp-library-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable47
 @if ERRORLEVEL 1 echo Error: library portable47 debug build failed && goto :eof
-%_msbuildexe% src/fsharp-library-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable7
-@if ERRORLEVEL 1 echo Error: library portable7 debug build failed && goto :eof
+
+REM Dropped for faster build
+REM %_msbuildexe% src/fsharp-library-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable7
+REM @if ERRORLEVEL 1 echo Error: library portable7 debug build failed && goto :eof
+
+
 %_msbuildexe% src/fsharp-library-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable78
 @if ERRORLEVEL 1 echo Error: library portable78 debug build failed && goto :eof
+
+REM Dropped for faster build
+REM %_msbuildexe% src/fsharp-library-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable259
+REM @if ERRORLEVEL 1 echo Error: library portable259 debug build failed && goto :eof
+
 %_msbuildexe% src/fsharp-library-unittests-build.proj /p:UseNugetPackages=true
 @if ERRORLEVEL 1 echo Error: library unittests debug build failed && goto :eof
-%_msbuildexe% src/fsharp-library-unittests-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable47
-@if ERRORLEVEL 1 echo Error: library unittests debug build failed portable47 && goto :eof
-%_msbuildexe% src/fsharp-library-unittests-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable7
-@if ERRORLEVEL 1 echo Error: library unittests debug build failed portable7 && goto :eof
-%_msbuildexe% src/fsharp-library-unittests-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable78
-@if ERRORLEVEL 1 echo Error: library unittests debug build failed portable78 && goto :eof
+
+
+REM Dropped for faster build
+REM %_msbuildexe% src/fsharp-library-unittests-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable47
+@REM if ERRORLEVEL 1 echo Error: library unittests debug build failed portable47 && goto :eof
+
+REM Dropped for faster build
+REM %_msbuildexe% src/fsharp-library-unittests-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable7
+REM @if ERRORLEVEL 1 echo Error: library unittests debug build failed portable7 && goto :eof
+
+REM Dropped for faster build
+REM %_msbuildexe% src/fsharp-library-unittests-build.proj /p:UseNugetPackages=true /p:TargetFramework=portable78
+REM @if ERRORLEVEL 1 echo Error: library unittests debug build failed portable78 && goto :eof
+
+
 @echo on
 call src\update.cmd debug -ngen
+
 @echo on
 call tests\BuildTestTools.cmd debug 
 REM @if ERRORLEVEL 1 echo Error: 'tests\BuildTestTools.cmd debug' failed && goto :eof
+
 @echo on
-set PATH=%PATH%;%~dp0%packages\NUnit.Runners.2.6.3\tools\
-tests\RunTests.cmd debug coreunit
-@if ERRORLEVEL 1 echo Error: 'RunTests.cmd debug coreunit' failed && goto :eof
+
 tests\RunTests.cmd debug fsharp Smoke
 @if ERRORLEVEL 1 echo Error: 'RunTests.cmd debug fsharpqa Smoke' failed && goto :eof
 
-REM Disabled for now
-REM tests\RunTests.cmd debug fsharpqa Smoke
-REM @if ERRORLEVEL 1 echo Error: 'RunTests.cmd debug fsharpqa Smoke' failed && goto :eof
+tests\RunTests.cmd debug fsharpqa Smoke
+@if ERRORLEVEL 1 echo Error: 'RunTests.cmd debug fsharpqa Smoke' failed && goto :eof
+
+set PATH=%PATH%;%~dp0%packages\NUnit.Runners.2.6.3\tools\
+tests\RunTests.cmd debug coreunit
+@if ERRORLEVEL 1 echo Error: 'RunTests.cmd debug coreunit' failed && goto :eof
+
