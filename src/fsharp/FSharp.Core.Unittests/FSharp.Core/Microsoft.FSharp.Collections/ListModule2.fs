40fc139ce39ded4630a05581d352443e2658e6d9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-01-29 14:57:06 +0100

    Extracting obsolete List and Seq functions into separate test module

--- src/fsharp/FSharp.Core.Unittests/FSharp.Core/Microsoft.FSharp.Collections/ListModule2.fs
+++ src/fsharp/FSharp.Core.Unittests/FSharp.Core/Microsoft.FSharp.Collections/ListModule2.fs
@@ -3,7 +3,6 @@
 // Various tests for the:
 // Microsoft.FSharp.Collections.List module
 
-#nowarn "44" // This construct is deprecated. please use List.item
 namespace FSharp.Core.Unittests.FSharp_Core.Microsoft_FSharp_Collections
 
 open System
@@ -317,21 +316,6 @@ type ListModule02() =
         let funcEpt () = 1
         CheckThrowsArgumentException ( fun() -> List.minBy funcEpt List.empty)
        
-        ()
-        
-    [<Test>]
-    member this.Nth() = 
-        // integer List 
-        let resultInt = List.nth [3;7;9;4;8;1;1;2] 3        
-        Assert.AreEqual(4, resultInt)
-        
-        // string List
-        let resultStr = List.nth   ["a";"b";"c";"d"] 3        
-        Assert.AreEqual("d", resultStr)
-        
-        // empty List 
-        CheckThrowsArgumentException ( fun() -> List.nth List.empty 1)
-
         ()
 
     [<Test>]
