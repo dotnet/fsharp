f1c70a0e911831ade1c3e6c6beb641c473aaf61f
KevinRansom <kevinr@microsoft.com>
2015-02-02 19:28:29 -0800

    Remove BUILDING_PROTO #ifdefs when building proto ensure that FX_ATLEAST_45 is not set

--- src/fsharp/ReferenceResolution.fs
+++ src/fsharp/ReferenceResolution.fs
@@ -91,19 +91,13 @@ module internal MSBuildResolver =
     let private Net40 = "v4.0"
     [<Literal>]    
     let private Net45 = "v4.5"
-#if BUILDING_PROTO
-#else
     [<Literal>]    
     let private Net451 = "v4.5.1"
-#endif
 
-#if BUILDING_PROTO
-    let SupportedNetFrameworkVersions = set [ Net20; Net30; Net35; Net40; Net45; (*SL only*) "v5.0" ]
-#else
     let SupportedNetFrameworkVersions = set [ Net20; Net30; Net35; Net40; Net45; Net451; (*SL only*) "v5.0" ]
-#endif    
-    let GetPathToDotNetFramework(v) =
+
 #if FX_ATLEAST_45
+    let GetPathToDotNetFramework(v) =
         let v =
             match v with
             | Net11 ->  Some TargetDotNetFrameworkVersion.Version11
@@ -112,10 +106,7 @@ module internal MSBuildResolver =
             | Net35 ->  Some TargetDotNetFrameworkVersion.Version35
             | Net40 ->  Some TargetDotNetFrameworkVersion.Version40
             | Net45 ->  Some TargetDotNetFrameworkVersion.Version45
-#if BUILDING_PROTO
-#else
             | Net451 -> Some TargetDotNetFrameworkVersion.Version451
-#endif
             | _ -> assert false; None
         match v with
         | Some v -> 
@@ -124,21 +115,19 @@ module internal MSBuildResolver =
             | x -> [x]
         | _ -> []
 #else
+    let GetPathToDotNetFramework(_) =
         // FX_ATLEAST_45 is not defined for step when we build compiler with proto compiler.
         []
 #endif        
 
-    let DeriveTargetFrameworkDirectoriesFor40Plus(version) = 
 #if FX_ATLEAST_45
+    let DeriveTargetFrameworkDirectoriesFor40Plus(version) = 
         // starting with .Net 4.0, the runtime dirs (WindowsFramework) are never used by MSBuild RAR
         let v =
             match version with
             | Net40 -> Some TargetDotNetFrameworkVersion.Version40
             | Net45 -> Some TargetDotNetFrameworkVersion.Version45
-#if BUILDING_PROTO
-#else
             | Net451 -> Some TargetDotNetFrameworkVersion.Version451
-#endif
             | _ -> assert false; None // unknown version - some parts in the code are not synced
         match v with
         | Some v -> 
@@ -147,6 +136,7 @@ module internal MSBuildResolver =
             | x -> [x]
         | None -> []        
 #else
+    let DeriveTargetFrameworkDirectoriesFor40Plus(_) = 
         // FX_ATLEAST_45 is not defined for step when we build compiler with proto compiler.
         []
 #endif
@@ -154,14 +144,10 @@ module internal MSBuildResolver =
     /// Determine the default "frameworkVersion" (which is passed into MSBuild resolve).
     /// This code uses MSBuild to determine version of the highest installed framework.
     let HighestInstalledNetFrameworkVersionMajorMinor() =
-#if FX_ATLEAST_45    
-#if BUILDING_PROTO
-#else
-        if box (ToolLocationHelper.GetPathToDotNetFramework(TargetDotNetFrameworkVersion.Version451)) <> null then 4, Net451
-        else
-#endif
-            if box (ToolLocationHelper.GetPathToDotNetFramework(TargetDotNetFrameworkVersion.Version45)) <> null then 4, Net45
-            else 4, Net40 // version is 4.0 assumed since this code is running.
+#if FX_ATLEAST_45
+        if box (ToolLocationHelper.GetPathToDotNetFramework(TargetDotNetFrameworkVersion.Version451)) <> null then 4, Net451 
+        elif box (ToolLocationHelper.GetPathToDotNetFramework(TargetDotNetFrameworkVersion.Version45)) <> null then 4, Net45 
+        else 4, Net40 // version is 4.0 assumed since this code is running. 
 #else
         // FX_ATLEAST_45 is not defined is required for step when we build compiler with proto compiler and this branch should not be hit
         4, Net40
