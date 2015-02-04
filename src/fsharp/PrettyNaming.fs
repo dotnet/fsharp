f46558cf5606740084b0c1c957135d0bca78f38d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-04 15:40:58 -0800

    Fix name-mangling of provided types
    fixes #102
    closes #203
    
    commit 6cc322727c1b7d0ae6a0034c2b9018e64d038794
    Author: Steffen Forkmann <steffen.forkmann@msu-solutions.de>
    Date:   Tue Feb 3 15:30:23 2015 +0100
    
        Do not generate logical type names like "MyNamespace.Test,"
    
    commit b40d937a383c1715c3f052d9674d3bd4f89333a6
    Author: Steffen Forkmann <steffen.forkmann@msu-solutions.de>
    Date:   Sun Jan 25 15:08:36 2015 +0100
    
        Fix demangling of provided types with only default values - fixes #98
    
    commit 515501a9765ab617e2837d7ce36ba06a3cbda1cd
    Author: Steffen Forkmann <steffen.forkmann@msu-solutions.de>
    Date:   Sun Jan 25 14:56:45 2015 +0100
    
        Create test case for #98
    
    commit 55695804d6af78a10ce9543764fec0656687156b
    Author: Steffen Forkmann <steffen.forkmann@msu-solutions.de>
    Date:   Sun Jan 25 14:50:04 2015 +0100
    
        Added some tests to describe current behaviour of NameMangling of provided types
    
    commit be2d1570f925d52f1992324553db1f67f7544057
    Author: Steffen Forkmann <steffen.forkmann@msu-solutions.de>
    Date:   Sun Jan 25 14:22:49 2015 +0100
    
        Extract computeMangledNameWithoutDefaultArgValues to make it testable
    
    commit bbdd2a30fd2637c709eb94ce0e064521341bc9f9
    Author: Steffen Forkmann <steffen.forkmann@msu-solutions.de>
    Date:   Sun Jan 25 13:13:00 2015 +0100
    
        Cleanup: removed commented code which uses deleted functions

--- src/fsharp/PrettyNaming.fs
+++ src/fsharp/PrettyNaming.fs
@@ -445,16 +445,15 @@ module internal Microsoft.FSharp.Compiler.PrettyNaming
 
     let demangleProvidedTypeName (typeLogicalName:string) = 
         if typeLogicalName.Contains "," then 
-            let pieces = splitAroundQuotation typeLogicalName ',' 
-            if pieces.[1..] |> Array.forall (fun x -> tryDemangleStaticStringArg x |> Option.isSome) then
-                let argNamesAndValues = 
-                    pieces.[1..] |> Array.map (fun piece -> 
-                        match tryDemangleStaticStringArg piece with 
-                        | None -> raise (InvalidMangledStaticArg piece)
-                        | Some v -> v)
-                pieces.[0], argNamesAndValues
-            else 
-                typeLogicalName, [| |]
+            let pieces = splitAroundQuotation typeLogicalName ','
+            match pieces with
+            | [| x; "" |] -> x, [| |]
+            | _ ->
+                let argNamesAndValues = pieces.[1..] |> Array.choose tryDemangleStaticStringArg
+                if argNamesAndValues.Length = (pieces.Length - 1) then
+                    pieces.[0], argNamesAndValues
+                else
+                    typeLogicalName, [| |]
         else 
             typeLogicalName, [| |]
 
@@ -463,8 +462,20 @@ module internal Microsoft.FSharp.Compiler.PrettyNaming
             nonDefaultArgs
             |> Array.map mangleStaticStringArg
             |> String.concat ","
-        typeLogicalName+","+nonDefaultArgsText
 
-    //let testDemangleStaticStringArg() = 
-    //   for x in [ ""; "\""; "\"\""; "a"; "\\"; "\\\\"; "\\\""; "_"; "\"\"" ] do 
-    //      if demangleStaticStringArg (mangleStaticStringArg x) <> x then printfn "failed for <<%s>>" x
+        if nonDefaultArgsText = "" then
+            typeLogicalName
+        else
+            typeLogicalName + "," + nonDefaultArgsText
+
+
+    let computeMangledNameWithoutDefaultArgValues(nm,staticArgs,defaultArgValues) =
+        let nonDefaultArgs = 
+            (staticArgs,defaultArgValues) 
+            ||> Array.zip 
+            |> Array.choose (fun (staticArg, (defaultArgName, defaultArgValue)) -> 
+                let actualArgValue = string staticArg 
+                match defaultArgValue with 
+                | Some v when v = actualArgValue -> None
+                | _ -> Some (defaultArgName, actualArgValue))
+        mangleProvidedTypeName (nm, nonDefaultArgs)
