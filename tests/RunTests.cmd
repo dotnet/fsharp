0b7bee8d6982361828eae6d717f03364fa364ca1
KevinRansom <kevinr@microsoft.com>
2015-02-10 12:42:24 -0800

    Update nunit to V. 2.6.4
    fixes #3
    closes #221
    
    commit d0a610d5a51b40a6bb07be326b8cc40c0410c864
    Author: latkin <latkin@microsoft.com>
    Date:   Tue Feb 10 12:41:15 2015 -0800
    
        Revert stats file
    
    commit 41176f7ff0a09228348b23d7fcb9d5d119bb33cb
    Author: latkin <latkin@microsoft.com>
    Date:   Tue Feb 10 12:40:21 2015 -0800
    
        Small modifications to fit into recent updates
    
    commit 1da7c7d002d124b7e6961e2c1319e20b097166eb
    Merge: 95bfc9e 19d6f8f
    Author: latkin <latkin@microsoft.com>
    Date:   Tue Feb 10 12:36:33 2015 -0800
    
        Merge branch 'updatenunit' of https://github.com/KevinRansom/visualfsharp into KevinRansom-updatenunit
    
        Conflicts:
        	src/FSharpSource.targets
        	tests/RunTests.cmd
    
    commit 19d6f8f39b3808874c5fbb95af1a9783ff4fdbab
    Author: KevinRansom <kevinr@microsoft.com>
    Date:   Sat Feb 7 09:43:22 2015 -0800
    
        merge upstream

--- tests/RunTests.cmd
+++ tests/RunTests.cmd
@@ -10,7 +10,7 @@ goto :USAGE
 
 :flavor_ok
 
-set NUNITPATH=%~dp0%..\packages\NUnit.Runners.2.6.3\tools\
+set NUNITPATH=%~dp0%..\packages\NUnit.Runners.2.6.4\tools\
 if not exist "%NUNITPATH%" (
     pushd %~dp0..
     .\.nuget\nuget.exe restore packages.config -PackagesDirectory packages
