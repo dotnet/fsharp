8e18c9827d07bc0fb2e2ef1dfe36d8ba096dfe56
KevinRansom <kevinr@microsoft.com>
2015-02-21 15:21:28 -0800

    Move backticks tests to fsharpqa

--- tests/fsharpqa/Source/Conformance/LexicalAnalysis/IdentifiersAndKeywords/backtickmoduleandtypenames.fsx
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/IdentifiersAndKeywords/backtickmoduleandtypenames.fsx
@@ -1,9 +1,5 @@
-// #Regression #Conformance #Accessibility #SignatureFiles #Regression #Records #Unions 
-#if Portable
-module Core_access
-#endif
+//<Expects status=success></Expects>
 
-#light
 let failures = ref false
 let report_failure () = 
   stderr.WriteLine " NO"; failures := true
@@ -67,18 +63,18 @@ module ``Lots of comma's and other characters in type name`` =
 
 do
     if ``Comma in type name``.``,``.x() <> 13 then failures := true
-    printfn "%d" (``Comma in type name``.``,``.x());;
+    printfn "%d" (``Comma in type name``.``,``.x())
 
     if ``Lots of comma's in type name``.``,,,,,,,``.x() <> 15 then failures := true
-    printfn "%d" (``Lots of comma's in type name``.``,,,,,,,``.x());;
+    printfn "%d" (``Lots of comma's in type name``.``,,,,,,,``.x())
 
     if ``Lots of comma's and other characters in type name``.``One, 2, Three, 4, Five``.x() <> 17 then failures := true
-    printfn "%d" (``Lots of comma's and other characters in type name``.``One, 2, Three, 4, Five``.x());;
+    printfn "%d" (``Lots of comma's and other characters in type name``.``One, 2, Three, 4, Five``.x())
 
 
 let aa =
   if !failures then (stdout.WriteLine "Test Failed"; exit 1) 
 
-do  (stdout.WriteLine "Test Passed"; 
-    System.IO.File.WriteAllText("test.ok","ok"); 
+do  (stdout.WriteLine "Test Passed";
+    System.IO.File.WriteAllText("test.ok","ok");
     exit 0)
