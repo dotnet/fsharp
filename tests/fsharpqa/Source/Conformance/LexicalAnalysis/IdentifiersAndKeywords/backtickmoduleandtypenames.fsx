2b2f97b546e35b57137e52a54e273bf2eed05226
KevinRansom <kevinr@microsoft.com>
2015-02-21 22:16:21 -0800

    Move backticks tests to fsharpqa

--- tests/fsharpqa/Source/Conformance/LexicalAnalysis/IdentifiersAndKeywords/backtickmoduleandtypenames.fsx
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/IdentifiersAndKeywords/backtickmoduleandtypenames.fsx
@@ -1,4 +1,10 @@
 //<Expects status=success></Expects>
+open System
+open System.Reflection
+
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+open Microsoft.FSharp.Quotations.DerivedPatterns
 
 let failures = ref false
 let report_failure () = 
@@ -17,7 +23,6 @@ module ``,`` =
 module ``,,,,,,,,,,,`` =
     let x = 5
 
-
 // Lots of comma's and other characters in module name
 module ``One, 2, Three, 4, Five`` =
     let x = 7
@@ -29,52 +34,46 @@ module ``Nested modules with commas`` =
       module ``Three, `` =
         let x = 9
 
-do 
-    printfn "%d" ``,``.x
-    if ``,``.x <> 3 then failures := true
-
-    printfn "%d" ``,,,,,,,,,,,``.x
-    if ``,,,,,,,,,,,``.x <> 5 then failures := true
-
-    printfn "%d" ``One, 2, Three, 4, Five``.x
-    if ``One, 2, Three, 4, Five``.x <> 7 then failures := true
-
-    if ``Nested modules with commas``.``One, ``.``Two, ``.``Three, ``.x <> 9 then  failures := true
-    printfn "%d" ``Nested modules with commas``.``One, ``.``Two, ``.``Three, ``.x
-
-    (*--------------------*)  
-
-// Test cases for Github Issue # 10  deeper look typenames cannot contain ', in fsi
-
-// Comma in type name
 module ``Comma in type name`` =
-    type ``,`` () =
-        static member x() = 13
+    type ``,`` =
+        static member x = 13
 
 // Lots of comma's in type name
 module ``Lots of comma's in type name`` =
-    type ``,,,,,,,`` () =
-        static member x() = 15
+    type ``,,,,,,,`` =
+        static member x = 15
 
 // Lots of comma's and other characters in type name
 module ``Lots of comma's and other characters in type name`` =
-    type ``One, 2, Three, 4, Five`` () =
-        static member x() = 17
+    type ``One, 2, Three, 4, Five`` =
+        static member x = 17
 
-do
-    if ``Comma in type name``.``,``.x() <> 13 then failures := true
-    printfn "%d" (``Comma in type name``.``,``.x())
+do 
+    let eval expr value = if (expr) = value then true else false
 
-    if ``Lots of comma's in type name``.``,,,,,,,``.x() <> 15 then failures := true
-    printfn "%d" (``Lots of comma's in type name``.``,,,,,,,``.x())
+    test "Comma in module name" (eval ``,``.x 3)
+    test "Lots of comma's in module name" (eval ``,,,,,,,,,,,``.x 5)
+    test "Lots of comma's and other characters in module name" (eval ``One, 2, Three, 4, Five``.x 7)
+    test "Nested modules with commas" (eval ``Nested modules with commas``.``One, ``.``Two, ``.``Three, ``.x 9)
 
-    if ``Lots of comma's and other characters in type name``.``One, 2, Three, 4, Five``.x() <> 17 then failures := true
-    printfn "%d" (``Lots of comma's and other characters in type name``.``One, 2, Three, 4, Five``.x())
+    test "Comma in type name" (eval ``Comma in type name``.``,``.x 13)
+    test "Lots of comma's in type name" (eval ``Lots of comma's in type name``.``,,,,,,,``.x 15)
+    test "Lots of comma's and other characters in type name" (eval ``Lots of comma's and other characters in type name``.``One, 2, Three, 4, Five``.x 17)
 
+    let eval expr value = (match expr with | PropertyGet (a, b, c) -> (if (b.GetGetMethod(true) :> MethodBase).Invoke(null, null) :?>int = value then true else false) | _ -> false)
+
+    test "Comma in module name from quotation"                                (eval <@@ ``,``.x @@> 3)
+    test "Lots of comma's in module name from quotation"                      (eval <@@ ``,,,,,,,,,,,``.x @@> 5)
+    test "Lots of comma's and other characters in module name from quotation" (eval <@@ ``One, 2, Three, 4, Five``.x @@> 7)
+    test "Nested modules with commas from quotation"                          (eval <@@ ``Nested modules with commas``.``One, ``.``Two, ``.``Three, ``.x @@> 9)
+
+    test "Comma in type name from quotation"                                  (eval <@@ ``Comma in type name``.``,``.x @@> 13)
+    test "Lots of comma's in type name from quotation"                        (eval <@@ ``Lots of comma's in type name``.``,,,,,,,``.x @@> 15)
+    test "Lots of comma's and other characters in type name from quotation"   (eval <@@ ``Lots of comma's and other characters in type name``.``One, 2, Three, 4, Five``.x @@> 17)
 
 let aa =
   if !failures then (stdout.WriteLine "Test Failed"; exit 1) 
 
-do  (stdout.WriteLine "Test Passed";
+do (stdout.WriteLine "Test Passed";
     System.IO.File.WriteAllText("test.ok","ok");
     exit 0)
