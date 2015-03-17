a8201de33cff1df8fdaacd6c2a871bc414dcecc3
latkin <latkin@microsoft.com>
2015-03-17 10:49:44 -0700

    Expand testing to cover FSC and other scenarios

--- tests/fsharpqa/Source/InteractiveSession/Misc/aaa/bbb/RelativeHashRResolution05_1.fsx
+++ tests/fsharpqa/Source/InteractiveSession/Misc/aaa/bbb/RelativeHashRResolution05_1.fsx
@@ -1,4 +1,8 @@
-#load "..\RelativeHashRResolution05_2.fsx"
+#if PIPED_FROM_TEST_DIR
+#load "./aaa/RelativeHashRResolution05_2.fsx"
+#else
+#load "../RelativeHashRResolution05_2.fsx"
+#endif
 
 printfn "%O" (Lib.X())
 printfn "%O" RelativeHashRResolution05_2.Foo.Y
--- tests/fsharpqa/Source/InteractiveSession/Misc/ccc/RelativeHashRResolution01_1.fsx
+++ tests/fsharpqa/Source/InteractiveSession/Misc/ccc/RelativeHashRResolution01_1.fsx
@@ -1,4 +1,8 @@
+#if PIPED_FROM_TEST_DIR
+#load "aaa/bbb/RelativeHashRResolution01_2.fsx"
+#else
 #load "../aaa/bbb/RelativeHashRResolution01_2.fsx"
+#endif
 
 printfn "%O" (Lib.X())
 printfn "%O" RelativeHashRResolution01_2.Foo.Y
