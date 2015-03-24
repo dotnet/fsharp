450d9451837e0654977908c76103c3efc3cbaa5f
latkin <latkin@microsoft.com>
2015-03-24 13:36:11 -0700

    Remove a bunch of code and libraries that aren't used

--- tests/fsharp/tools/FSharp.PowerPack/fsppack/src/FSharp.PowerPack/math/lapack/lapack_service.fs
+++
@@ -1,4 +0,0 @@
-namespace Microsoft.FSharp.Math.Experimental
-// Code abstracted and moved to service.fs.
-// This file will either be removed, or ILapack move here, splitting up from native utilities.
-
--- tests/fsharp/tools/FSharp.PowerPack/fsppack/src/assemblyinfo.Common.fs
+++
@@ -1,8 +0,0 @@
-module Test.PowerPack.BuildSettings
-
-[<Literal>]
-let Version = "0.0.0.1"
-
-[<assembly:System.Reflection.AssemblyVersion(Version)>]
-[<assembly:System.Reflection.AssemblyFileVersion(Version)>]
-do()
