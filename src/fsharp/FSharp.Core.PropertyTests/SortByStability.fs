662618cc97c4045305d7fd109f9d2842a0ff405b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-04-18 19:16:50 +0200

    Array sort is not stable. This is expected

--- src/fsharp/FSharp.Core.PropertyTests/SortByStability.fs
+++ src/fsharp/FSharp.Core.PropertyTests/SortByStability.fs
@@ -13,30 +13,17 @@ let sortByStableSeq<'a when 'a : comparison> (xs : 'a []) =
     let sorted = indexed |> Seq.sortBy snd
     sorted |> Seq.pairwise |> Seq.forall (fun ((ia, a),(ib, b)) -> if a = b then ia < ib else true)
 
-let sortByStableArray<'a when 'a : comparison> (xs : 'a []) =
-    let indexed = xs |> Seq.indexed |> Seq.toArray
-    let sorted = indexed |> Array.sortBy snd
-    sorted |> Seq.pairwise |> Seq.forall (fun ((ia, a),(ib, b)) -> if a = b then ia < ib else true)
-
 let sortByStableList<'a when 'a : comparison> (xs : 'a []) =
     let indexed = xs |> Seq.indexed |> Seq.toList
     let sorted = indexed |> List.sortBy snd
     sorted |> Seq.pairwise |> Seq.forall (fun ((ia, a),(ib, b)) -> if a = b then ia < ib else true)
 
-type Properties = 
-    
-    static member SortByStableInt xs = sortByStableSeq<int> xs
-    static member SortByStableArrayInt xs = sortByStableArray<int> xs
-    static member SortByStableListInt xs = sortByStableList<int> xs
-
 [<Test>]
 let ``Seq.sortBy is stable`` () =
-    Check.QuickThrowOnFailure(fun xs -> sortByStableSeq<int> xs)
+    Check.QuickThrowOnFailure sortByStableSeq<int>
+    Check.QuickThrowOnFailure sortByStableSeq<string>
     
 [<Test>]
-let ``Array.sortBy is stable`` () =
-    Check.QuickThrowOnFailure(fun xs -> sortByStableArray<int> xs)
-
-[<Test>]
 let ``List.sortBy is stable`` () =
-    Check.QuickThrowOnFailure(fun xs -> sortByStableList<int> xs)
+    Check.QuickThrowOnFailure sortByStableList<int>
+    Check.QuickThrowOnFailure sortByStableSeq<string>
