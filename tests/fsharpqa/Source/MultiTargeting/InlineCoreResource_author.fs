9cf81de567c9dc92f8321b66dca2924b3a8421a3
latkin <latkin@microsoft.com>
2015-06-11 11:52:11 -0700

    Add extra test case for fixed non-inline APIs

--- tests/fsharpqa/Source/MultiTargeting/InlineCoreResource_author.fs
+++ tests/fsharpqa/Source/MultiTargeting/InlineCoreResource_author.fs
@@ -2,3 +2,6 @@ module Test
 
 let inline init1 n f = Array.init n f
 let init2 n f = Array.init n f
+
+let inline reduce1 f = Array.reduce f
+let reduce2 f = Array.reduce f
--- tests/fsharpqa/Source/MultiTargeting/InlineCoreResource_consumer.fsx
+++ tests/fsharpqa/Source/MultiTargeting/InlineCoreResource_consumer.fsx
@@ -7,6 +7,9 @@ module Foo
 Test.init1 4 (fun _ -> 4.) |> printfn "%A"
 Test.init2 4 (fun _ -> 4.) |> printfn "%A"
 
+Test.reduce1 (fun acc n -> acc + n) [| 1 .. 10 |] |> printfn "%A"
+Test.reduce2 (fun acc n -> acc + n) [| 1 .. 10 |] |> printfn "%A"
+
 #if INTERACTIVE
 #q ;;
 #endif
