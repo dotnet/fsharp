e6ef27687e9eb57139ee2adfe6fc38058ad02103
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-06-17 10:02:33 +0200

    fix sortby test

--- src/fsharp/FSharp.Core.PropertyTests/ListProperties.fs
+++ src/fsharp/FSharp.Core.PropertyTests/ListProperties.fs
@@ -8,13 +8,17 @@ open System.Collections.Generic
 open NUnit.Framework
 open FsCheck
 
-let sort_and_sortby<'a when 'a : equality> (xs : list<'a>) (xs2 : list<'a>) =
-    let s = xs |> Seq.append xs2 
-    let l = xs |> List.append xs2
-    let a = xs |> Seq.toArray |> Array.append (Seq.toArray xs2)
-    Seq.toArray s = a && List.toArray l = a
 
+let sort_and_sortby (xs : list<float>) (xs2 : list<float>) =
+    let a = List.sortBy id xs |> Seq.toArray 
+    let b = List.sort xs |> Seq.toArray
+    let result = ref true
+    for i in 0 .. a.Length - 1 do
+        if a.[i] <> b.[i] then
+            if System.Double.IsNaN a.[i] <> System.Double.IsNaN b.[i] then
+                result := false
+    !result 
 
 [<Test>]
 let ``sort behaves like sortby id`` () =   
-    Check.QuickThrowOnFailure sort_and_sortby<int>
+    Check.QuickThrowOnFailure sort_and_sortby
