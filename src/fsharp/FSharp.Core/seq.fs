0063d9ecb2376af7a68ae53dc92162696fbbd653
Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
2015-08-12 13:07:57 -0700

    Split Seq.groupBy for ValueType/RefType
    The StructBox makes code that contains "hard" tail calls, which means
    that performance suffers under the 64 bit JIT
    
    closes https://github.com/Microsoft/visualfsharp/pull/549
    
    commit 36f10b6214d8b73140b481e391f7999b9b8be8a3
    Author: latkin <latkin@microsoft.com>
    Date:   Wed Aug 12 12:40:46 2015 -0700
    
        Proper ref/val type checking for all portable profiles
    
    commit 037a5e1de652ac1873faddbecf0e89a19b0e869f
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Thu Aug 13 05:50:29 2015 +1000
    
        Using default constructor for ResizeArray
    
        Initially this had been set to 1, I had changed it to 4, but after
        discussion it was decided that the default is probably the correct
        choice. As per
        https://github.com/Microsoft/visualfsharp/pull/549#issuecomment-129954146
    
    commit 3796a552ddb98e8fa23d60616c9689fe8a62c636
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Thu Aug 13 05:45:38 2015 +1000
    
        Renamed key to safeKey where appropriate
    
        As per
        https://github.com/Microsoft/visualfsharp/pull/549#discussion-diff-36704422
    
    commit b7884f8409d2708cd26d992bc7b0d77f788a360b
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Wed Jul 22 17:12:30 2015 +1000
    
        Restored null arg exception as lazy
    
        I don't think this is a good way to structure exceptions, but it's to
        match current functionality
    
    commit 23cc1564df7b9df4449ee419fd457c9f8f629564
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Wed Jul 22 05:53:33 2015 +1000
    
        Split dict by ValueType/RefType
    
    commit d4b6861c065ab4ca8775a62f28d8446a79c15058
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Wed Jul 22 05:10:39 2015 +1000
    
        Split Array.countBy/groupBy by ValueType/RefType
    
    commit 02e6d424ea885785dd49f0e1beeb98519a142ba4
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Wed Jul 22 04:55:42 2015 +1000
    
        Split List.groupBy for ValueType/RefType
    
    commit d80e6162abd76980abc4a3cfa260188402e2f352
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Wed Jul 22 04:43:54 2015 +1000
    
        Split List.countBy by RefType/ValueType
    
    commit 202e12e6313f2059fd307b290204113a269ddbe9
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Wed Jul 22 04:27:45 2015 +1000
    
        "Fixing" Reflection issues with Profile builds
    
        There must be some other way to check if a type is a Value Type? I doubt
        if it has been removed?
    
    commit c06d8e6f6762e9f42dd9dad9b81daba415838aa4
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Tue Jul 21 16:07:33 2015 +1000
    
        Split Seq.countBy for ValueType/RefType
    
    commit 1c5ce382eed705af3a44ddcfdeb1e799dde9eed3
    Author: Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
    Date:   Tue Jul 21 15:42:25 2015 +1000
    
        Split Seq.groupBy for ValueType/RefType
    
        The StructBox makes code that contains "hard" tail calls, which means
        that performance suffers under the 64 bit JIT

--- src/fsharp/FSharp.Core/seq.fs
+++ src/fsharp/FSharp.Core/seq.fs
@@ -847,6 +847,7 @@ namespace Microsoft.FSharp.Collections
     open System.Diagnostics
     open System.Collections
     open System.Collections.Generic
+    open System.Reflection
     open Microsoft.FSharp.Core
     open Microsoft.FSharp.Core.LanguagePrimitives.IntrinsicOperators
     open Microsoft.FSharp.Core.Operators
@@ -1444,32 +1445,50 @@ namespace Microsoft.FSharp.Collections
             checkNonNull "source" source
             mkSeq (fun () -> source.GetEnumerator())
 
+        let inline groupByImpl (comparer:IEqualityComparer<'SafeKey>) (keyf:'T->'SafeKey) (getKey:'SafeKey->'Key) (seq:seq<'T>) =
+            checkNonNull "seq" seq
 
+            let dict = Dictionary<_,ResizeArray<_>> comparer
+
+            // Previously this was 1, but I think this is rather stingy, considering that we are alreadying paying
+            // for at least a key, the ResizeArray reference, which includes an array reference, an Entry in the
+            // Dictionary, plus any empty space in the Dictionary of unfilled hash buckets.
+            let minimumBucketSize = 4
+
+            // Build the groupings
+            seq |> iter (fun v -> 
+                let safeKey = keyf v
+                let mutable prev = Unchecked.defaultof<_>
+                match dict.TryGetValue (safeKey, &prev) with
+                | true -> prev.Add v
+                | false ->
+                    let prev = ResizeArray ()
+                    dict.[safeKey] <- prev
+                    prev.Add v)
+
+            // Trim the size of each result group, don't trim very small buckets, as excessive work, and garbage for 
+            // minimal gain 
+            dict |> iter (fun group -> if group.Value.Count > minimumBucketSize then group.Value.TrimExcess())
+                         
+            // Return the sequence-of-sequences. Don't reveal the 
+            // internal collections: just reveal them as sequences
+            dict |> map (fun group -> (getKey group.Key, readonly group.Value))
+
+        // We avoid wrapping a StructBox, because under 64 JIT we get some "hard" tailcalls which affect performance
+        let groupByValueType (keyf:'T->'Key) (seq:seq<'T>) = seq |> groupByImpl HashIdentity.Structural<'Key> keyf id 
+
+        // Wrap a StructBox around all keys in case the key type is itself a type using null as a representation
+        let groupByRefType   (keyf:'T->'Key) (seq:seq<'T>) = seq |> groupByImpl StructBox<'Key>.Comparer (fun t -> StructBox (keyf t)) (fun sb -> sb.Value)
 
         [<CompiledName("GroupBy")>]
-        let groupBy keyf seq =
-
-            mkDelayedSeq (fun () -> 
-                // Wrap a StructBox(_) around all keys in case the key type is itself a type using null as a representation
-                let dict = new Dictionary<StructBox<'Key>,ResizeArray<'T>>(StructBox<'Key>.Comparer)
-
-                // Build the groupings
-                seq |> iter (fun v -> 
-                    let key = StructBox (keyf v)
-                    let ok,prev = dict.TryGetValue(key)
-                    if ok then 
-                        prev.Add(v)
-                    else 
-                        let prev = new ResizeArray<'T>(1)
-                        dict.[key] <- prev
-                        prev.Add(v))
-
-                // Trim the size of each result group.
-                dict |> iter (fun group -> group.Value.TrimExcess())
-                         
-                // Return the sequence-of-sequences. Don't reveal the 
-                // internal collections: just reveal them as sequences
-                dict |> map (fun group -> (group.Key.Value, readonly group.Value)))
+        let groupBy (keyf:'T->'Key) (seq:seq<'T>) =
+#if FX_RESHAPED_REFLECTION
+            if (typeof<'Key>).GetTypeInfo().IsValueType
+#else
+            if typeof<'Key>.IsValueType
+#endif
+                then mkDelayedSeq (fun () -> groupByValueType keyf seq)
+                else mkDelayedSeq (fun () -> groupByRefType   keyf seq)
 
         [<CompiledName("Distinct")>]
         let distinct source =
@@ -1523,19 +1542,38 @@ namespace Microsoft.FSharp.Collections
             let inline compareDescending a b = compare b a
             sortWith compareDescending source
 
-        [<CompiledName("CountBy")>]
-        let countBy keyf source =
+        let inline countByImpl (comparer:IEqualityComparer<'SafeKey>) (keyf:'T->'SafeKey) (getKey:'SafeKey->'Key) (source:seq<'T>) =
             checkNonNull "source" source
-            mkDelayedSeq (fun () -> 
-                let dict = new Dictionary<StructBox<'Key>,int>(StructBox<'Key>.Comparer)
 
-                // Build the groupings
-                source |> iter (fun v -> 
-                    let key = StructBox (keyf v )
-                    let mutable prev = Unchecked.defaultof<_>
-                    if dict.TryGetValue(key, &prev) then dict.[key] <- prev + 1 else dict.[key] <- 1)
+            let dict = Dictionary comparer
 
-                dict |> map (fun group -> (group.Key.Value, group.Value)))
+            // Build the groupings
+            source |> iter (fun v -> 
+                let safeKey = keyf v
+                let mutable prev = Unchecked.defaultof<_>
+                if dict.TryGetValue(safeKey, &prev)
+                    then dict.[safeKey] <- prev + 1
+                    else dict.[safeKey] <- 1)
+
+            dict |> map (fun group -> (getKey group.Key, group.Value))
+
+        // We avoid wrapping a StructBox, because under 64 JIT we get some "hard" tailcalls which affect performance
+        let countByValueType (keyf:'T->'Key) (seq:seq<'T>) = seq |> countByImpl HashIdentity.Structural<'Key> keyf id 
+
+        // Wrap a StructBox around all keys in case the key type is itself a type using null as a representation
+        let countByRefType   (keyf:'T->'Key) (seq:seq<'T>) = seq |> countByImpl StructBox<'Key>.Comparer (fun t -> StructBox (keyf t)) (fun sb -> sb.Value)
+
+        [<CompiledName("CountBy")>]
+        let countBy (keyf:'T->'Key) (source:seq<'T>) =
+            checkNonNull "source" source
+
+#if FX_RESHAPED_REFLECTION
+            if (typeof<'Key>).GetTypeInfo().IsValueType
+#else
+            if typeof<'Key>.IsValueType
+#endif
+                then mkDelayedSeq (fun () -> countByValueType keyf source)
+                else mkDelayedSeq (fun () -> countByRefType   keyf source)
 
         [<CompiledName("Sum")>]
         let inline sum (source: seq< (^a) >) : ^a = 
