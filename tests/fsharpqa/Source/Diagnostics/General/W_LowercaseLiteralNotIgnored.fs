9a902389035043eea427dc655fb2781f1b882f53
dungpa <phananhdung309@yahoo.com>
2015-10-05 06:42:47 +0200

    Use lowecase module names so that the test case is more obvious

--- tests/fsharpqa/Source/Diagnostics/General/W_LowercaseLiteralNotIgnored.fs
+++ tests/fsharpqa/Source/Diagnostics/General/W_LowercaseLiteralNotIgnored.fs
@@ -2,13 +2,13 @@
 //<Expects status="warning" span="(13,7-13,8)" id="FS0026">This rule will never be matched$</Expects>
 module M0
 
-module M1 =
+module m1 =
   let [<Literal>] lowerCase = "lowerCase"
   let [<Literal>] UpperCase = "UpperCase"
 
 module M2 =
   let f = function
-    | M1.lowerCase -> "LowerCase"
+    | m1.lowerCase -> "LowerCase"
     | lowerCase2 -> "LowerCase2"
     | _ -> "Don't know"
 
