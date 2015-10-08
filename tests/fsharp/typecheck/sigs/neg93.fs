2b01f41d84e43b3e217c5137090c9f3a85cb52c1
Don Syme <donsyme@fastmail.fm>
2015-10-09 00:46:44 +0100

    Fix 528 - error on literal patterns with args

--- tests/fsharp/typecheck/sigs/neg93.fs
+++ tests/fsharp/typecheck/sigs/neg93.fs
@@ -21,3 +21,30 @@ module Repro3 =
  let rec foo< > c = bar c
  and bar c = 0
 
+module Bug528_Ex1 = 
+    module Color =
+        let [<Literal>] Yellow = "Yellow"
+
+    let isRed inp = 
+        match inp with
+        | Color.Yellow arg ->   // should get an error here
+            failwith "Don't know this color"
+        | _ -> ""
+
+module Bug528_Ex2 = 
+    type Color = | Yellow = 1 | Red = 2
+
+    let isRed inp = 
+        match inp with
+        | Color.Yellow arg ->   // should get an error here
+            failwith "Don't know this color"
+        | _ -> ""            
+
+module Bug528_Ex3 = 
+
+    let isRed inp = 
+        match inp with
+        | System.DayOfWeek.Monday arg ->   // should get an error here
+            failwith "Don't know this day"
+        | _ -> ""
+
