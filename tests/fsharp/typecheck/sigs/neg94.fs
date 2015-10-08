f0669a1a66f1faf91d30ebfec49f6db361fcb8bd
Don Syme <donsyme@fastmail.fm>
2015-10-08 18:16:16 +0100

    Fix 592 - bad metadata for fake event properties

--- tests/fsharp/typecheck/sigs/neg94.fs
+++ tests/fsharp/typecheck/sigs/neg94.fs
@@ -6,5 +6,7 @@ module Repro1 =
   let v2 = undefined  
   // We're expecting the compile of this file to fail, but the point is that the 
   // reference of neg94-pre.dll shouldn't give extra warnings about bad definitions.
-  // See https://github.com/Microsoft/visualfsharp/issues/631
+  // See 
+  //    https://github.com/Microsoft/visualfsharp/issues/631
+  //    https://github.com/Microsoft/visualfsharp/issues/592
 
