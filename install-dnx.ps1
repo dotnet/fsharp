5ca266308f938db6b6d080c4dfbcec83287c2a25
Cameron Taggart <cameron@tachyus.com>
2015-10-17 15:44:15 -0700

    $env:path debug

--- install-dnx.ps1
+++ install-dnx.ps1
@@ -1,7 +1,9 @@
 # http://dotnet.readthedocs.org/en/latest/getting-started/installing-core-windows.html
 &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
 $bin = ls ~/.dnx/bin | select -first 1 -ExpandProperty FullName
+echo "bin is $bin"
 $env:path = "$bin;$env:path"
+echo "path is $env:path"
 dnvm version
 dnvm install -r coreclr -arch x64 1.0.0-beta8
 dnx --version
