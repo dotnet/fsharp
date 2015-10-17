63d2a66e32787fefd913a7c47dd3560a44bdfd1a
Cameron Taggart <cameron.taggart@gmail.com>
2015-10-17 01:23:47 -0700

    1.0.0-beta8

--- install-dnx.ps1
+++ install-dnx.ps1
@@ -1,5 +1,5 @@
 # http://dotnet.readthedocs.org/en/latest/getting-started/installing-core-windows.html
 &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
 dnvm version
-dnvm install -r coreclr -arch x64 latest -u
+dnvm install -r coreclr -arch x64 1.0.0-beta8
 dnx --version
