7b45801ccf3b554e00470ec91bca332904fa3b9a
Cameron Taggart <cameron@tachyus.com>
2015-10-17 15:55:54 -0700

    ps variables

--- install-dnx.ps1
+++ install-dnx.ps1
@@ -1,7 +1,8 @@
 # http://dotnet.readthedocs.org/en/latest/getting-started/installing-core-windows.html
 &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
 $dnvm = ls ~/.dnx/bin/dnvm.cmd
-$env:path = "$(dnvm.Parent.FullName);$env:path"
+$bin = $dnvm.Parent.FullName
+$env:path = "$bin;$env:path"
 echo "path is $env:path"
 dnvm version
 dnvm install -r coreclr -arch x64 1.0.0-beta8
