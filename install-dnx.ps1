d062fd92a472e17e111b77a706cdabffa3bea949
Cameron Taggart <cameron@tachyus.com>
2015-10-17 15:50:42 -0700

    git parent

--- install-dnx.ps1
+++ install-dnx.ps1
@@ -1,8 +1,8 @@
 # http://dotnet.readthedocs.org/en/latest/getting-started/installing-core-windows.html
 &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
-$bin = ls ~/.dnx/bin | select -first 1 -ExpandProperty FullName
-echo "bin is $bin"
-$env:path = "$bin;$env:path"
+$dnvm = ls ~/.dnx/bin/dnvm.cmd
+echo "dnvm is $dnvm"
+$env:path = "$dnvm.Parent.FullName;$env:path"
 echo "path is $env:path"
 dnvm version
 dnvm install -r coreclr -arch x64 1.0.0-beta8
