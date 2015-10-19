c2cd87f134677522c27bd8696be952fe740a5c0d
Cameron Taggart <cameron@tachyus.com>
2015-10-18 21:42:23 -0700

    added runtimes back in project.json files

--- install-dnx.ps1
+++ install-dnx.ps1
@@ -1,8 +1,3 @@
-# http://dotnet.readthedocs.org/en/latest/getting-started/installing-core-windows.html
-&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
-$dnvm = ls ~/.dnx/bin/dnvm.cmd | select -first 1 -ExpandProperty FullName
-$bin = [IO.Path]::GetDirectoryName($dnvm)
-$env:path = "$bin;$env:path"
 dnvm version
 dnvm install -r coreclr -arch x64 1.0.0-beta8
 dnx --version
