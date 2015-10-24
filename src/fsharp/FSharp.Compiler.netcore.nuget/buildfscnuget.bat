23a8ebe5a8e3708ea4e67f017a1f97e5aa59e1c1
KevinRansom <kevinr@microsoft.com>
2015-10-24 02:18:43 -0700

    add nuget package for fsharp compiler.  Add test nuget packages to easily deploy fsharp to supported platforms

---
+++ src/fsharp/FSharp.Compiler.netcore.nuget/buildfscnuget.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set pkgversion="1.0.0-alpha-0001"
+
+:: Check prerequisites
+if not '%VisualStudioVersion%' == '' goto vsversionset
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\common7\ide\devenv.exe" set VisualStudioVersion=14.0
+if exist "%ProgramFiles%\Microsoft Visual Studio 14.0\common7\ide\devenv.exe" set VisualStudioVersion=14.0
+if exist "%VS140COMNTOOLS%" set VisualStudioVersion=14.0
+
+if not '%VisualStudioVersion%' == '' goto vsversionset
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\common7\ide\devenv.exe" set VisualStudioVersion=12.0
+if exist "%ProgramFiles%\Microsoft Visual Studio 12.0\common7\ide\devenv.exe" set VisualStudioVersion=12.0
+if exist "%VS120COMNTOOLS%" set VisualStudioVersion=12.0
+
+:vsversionset
+if '%VisualStudioVersion%' == '' echo Error: Could not find an installation of Visual Studio && goto :eof
+if '%VisualStudioVersion%' == '14.0' (
+    if exist "%ProgramFiles(x86)%\Microsoft SDKs\F#\4.0\Framework\v4.0\fsi.exe" set _fsiexe="%ProgramFiles(x86)%\Microsoft SDKs\F#\4.0\Framework\v4.0\fsi.exe"
+)
+
+if '%VisualStudioVersion%' == '12.0' (
+    if exist "%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe" set _fsiexe="%ProgramFiles(x86)%\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe"
+)
+
+if not exist "%~dp0..\..\..\release\coreclr\nuget" md "%~dp0..\..\..\release\coreclr\nuget"
+%_fsiexe% --exec "%~dp0..\..\buildtools\buildnugets.fsx" %pkgversion% "%~dp0Microsoft.FSharp.Compiler.netcore.nuspec" "%~dp0..\..\..\release\coreclr\bin" "%~dp0..\..\..\release\coreclr\nuget"
