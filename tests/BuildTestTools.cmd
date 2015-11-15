c5d671df83c576068e5e5c07b4159b7680a08ccf
KevinRansom <kevinr@microsoft.com>
2015-11-14 23:04:40 -0800

    Update some test scripts

--- tests/BuildTestTools.cmd
+++ tests/BuildTestTools.cmd
@@ -40,25 +40,25 @@ if exist "%ProgramFiles(x86)%\MSBuild\%VisualStudioVersion%\Bin\MSBuild.exe" set
 if exist "%ProgramFiles%\MSBuild\%VisualStudioVersion%\Bin\MSBuild.exe"      set _msbuildexe="%ProgramFiles%\MSBuild\%VisualStudioVersion%\Bin\MSBuild.exe"
 if not exist %_msbuildexe% echo Error: Could not find MSBuild.exe. && goto :eof
 
-if not exist "%~dp0\fsharpqa\testenv\bin" mkdir "%~dp0\fsharpqa\testenv\bin"  || goto :error
-%_msbuildexe% %~dp0\fsharpqa\testenv\src\ILComparer\ILComparer.fsproj /p:Configuration=%1 /t:Build  || goto :error
-xcopy /Y %~dp0\fsharpqa\testenv\src\ILComparer\bin\%1\* %~dp0\fsharpqa\testenv\bin  || goto :error
+if not exist "%~dp0fsharpqa\testenv\bin" mkdir "%~dp0fsharpqa\testenv\bin"  || goto :error
+%_msbuildexe% %~dp0fsharpqa\testenv\src\ILComparer\ILComparer.fsproj /p:Configuration=%1 /t:Build  || goto :error
+xcopy /Y %~dp0fsharpqa\testenv\src\ILComparer\bin\%1\* %~dp0fsharpqa\testenv\bin  || goto :error
 
-%_msbuildexe% %~dp0\fsharpqa\testenv\src\diff\diff.fsproj /p:Configuration=%1 /t:Build  || goto :error
-xcopy /Y %~dp0\fsharpqa\testenv\src\diff\bin\%1\* %~dp0\fsharpqa\testenv\bin  || goto :error
+%_msbuildexe% %~dp0fsharpqa\testenv\src\diff\diff.fsproj /p:Configuration=%1 /t:Build  || goto :error
+xcopy /Y %~dp0fsharpqa\testenv\src\diff\bin\%1\* %~dp0fsharpqa\testenv\bin  || goto :error
 
-%_msbuildexe% %~dp0\fsharpqa\testenv\src\HostedCompilerServer\HostedCompilerServer.fsproj /p:Configuration=%1 /t:Build  || goto :error
-xcopy /Y %~dp0\fsharpqa\testenv\src\HostedCompilerServer\bin\%1\* %~dp0\fsharpqa\testenv\bin  || goto :error
+%_msbuildexe% %~dp0fsharpqa\testenv\src\HostedCompilerServer\HostedCompilerServer.fsproj /p:Configuration=%1 /t:Build  || goto :error
+xcopy /Y %~dp0fsharpqa\testenv\src\HostedCompilerServer\bin\%1\* %~dp0fsharpqa\testenv\bin  || goto :error
 
-%_msbuildexe% %~dp0\fsharpqa\testenv\src\ExecAssembly\ExecAssembly.fsproj /p:Configuration=%1 /t:Build /p:Platform=x86  || goto :error
-xcopy /IY %~dp0\fsharpqa\testenv\src\ExecAssembly\bin\%1\* %~dp0\fsharpqa\testenv\bin\x86  || goto :error
+%_msbuildexe% %~dp0fsharpqa\testenv\src\ExecAssembly\ExecAssembly.fsproj /p:Configuration=%1 /t:Build /p:Platform=x86  || goto :error
+xcopy /IY %~dp0fsharpqa\testenv\src\ExecAssembly\bin\%1\* %~dp0fsharpqa\testenv\bin\x86  || goto :error
 
-%_msbuildexe% %~dp0\fsharpqa\testenv\src\ExecAssembly\ExecAssembly.fsproj /p:Configuration=%1 /t:Build /p:Platform=x64  || goto :error
-xcopy /IY %~dp0\fsharpqa\testenv\src\ExecAssembly\bin\%1\* %~dp0\fsharpqa\testenv\bin\AMD64  || goto :error
+%_msbuildexe% %~dp0fsharpqa\testenv\src\ExecAssembly\ExecAssembly.fsproj /p:Configuration=%1 /t:Build /p:Platform=x64  || goto :error
+xcopy /IY %~dp0fsharpqa\testenv\src\ExecAssembly\bin\%1\* %~dp0fsharpqa\testenv\bin\AMD64  || goto :error
 
-if exist %~dp0\..\%1\net40\bin (
-    xcopy /Y %~dp0\..\%1\net40\bin\FSharp.Core.sigdata %~dp0\fsharpqa\testenv\bin  || goto :error
-    xcopy /Y %~dp0\..\%1\net40\bin\FSharp.Core.optdata %~dp0\fsharpqa\testenv\bin  || goto :error
+if exist %~dp0..\%1\net40\bin (
+    xcopy /Y %~dp0..\%1\net40\bin\FSharp.Core.sigdata %~dp0fsharpqa\testenv\bin  || goto :error
+    xcopy /Y %~dp0..\%1\net40\bin\FSharp.Core.optdata %~dp0fsharpqa\testenv\bin  || goto :error
 )
 
 set NUNITPATH=%~dp0%..\packages\NUnit.Runners.2.6.4\tools\
@@ -67,8 +67,8 @@ if not exist "%NUNITPATH%" (
     .\.nuget\nuget.exe restore packages.config -PackagesDirectory packages
     popd
 )    
-xcopy "%NUNITPATH%*.*"  "%~dp0\fsharpqa\testenv\bin\nunit\*.*" /S /Q /Y
-xcopy "%~dp0\fsharpqa\testenv\src\nunit\*.*" "%~dp0\fsharpqa\testenv\bin\nunit\*.*" /S /Q /Y
+xcopy "%NUNITPATH%*.*"  "%~dp0fsharpqa\testenv\bin\nunit\*.*" /S /Q /Y
+xcopy "%~dp0fsharpqa\testenv\src\nunit\*.*" "%~dp0fsharpqa\testenv\bin\nunit\*.*" /S /Q /Y
 
 
 set NUNITPATH=%~dp0%..\packages\NUnit.Runners.2.6.4\tools\
@@ -77,25 +77,25 @@ if not exist "%NUNITPATH%" (
     .\.nuget\nuget.exe restore packages.config -PackagesDirectory packages
     popd
 )    
-xcopy "%NUNITPATH%*.*"  "%~dp0\fsharpqa\testenv\bin\nunit\*.*" /S /Q /Y
-xcopy "%~dp0\fsharpqa\testenv\src\nunit\*.*" "%~dp0\fsharpqa\testenv\bin\nunit\*.*" /S /Q /Y
+xcopy "%NUNITPATH%*.*"  "%~dp0fsharpqa\testenv\bin\nunit\*.*" /S /Q /Y
+xcopy "%~dp0fsharpqa\testenv\src\nunit\*.*" "%~dp0fsharpqa\testenv\bin\nunit\*.*" /S /Q /Y
 
 echo %_fsiexe%
 rem deploy x86 version of compiler and dependencies
-%_fsiexe% --exec %~dp0\fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0\fsharp\project.json --projectJsonLock:%~dp0\fsharp\project.lock.json --packagesDir:%~dp0\..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x86 --fsharpCore:%~dp0\..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0\testbin\%1\coreclr\fsc\win7-x86 --nugetPath:%~dp0\..\.nuget\nuget.exe --nugetConfig:%~dp0\..\.nuget\nuget.config --copyCompiler:yes --v:quiet
-%_fsiexe% --exec %~dp0\fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0\fsharp\project.json --projectJsonLock:%~dp0\fsharp\project.lock.json --packagesDir:%~dp0\..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x86 --fsharpCore:%~dp0\..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0\testbin\%1\coreclr\win7-x86 --nugetPath:%~dp0\..\.nuget\nuget.exe --nugetConfig:%~dp0\..\.nuget\nuget.config --copyCompiler:no --v:quiet
+%_fsiexe% --exec %~dp0fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0fsharp\project.json --projectJsonLock:%~dp0fsharp\project.lock.json --packagesDir:%~dp0..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x86 --fsharpCore:%~dp0..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0testbin\%1\coreclr\fsc\win7-x86 --nugetPath:%~dp0..\.nuget\nuget.exe --nugetConfig:%~dp0..\.nuget\nuget.config --copyCompiler:yes --v:quiet
+%_fsiexe% --exec %~dp0fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0fsharp\project.json --projectJsonLock:%~dp0fsharp\project.lock.json --packagesDir:%~dp0..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x86 --fsharpCore:%~dp0..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0testbin\%1\coreclr\win7-x86 --nugetPath:%~dp0..\.nuget\nuget.exe --nugetConfig:%~dp0..\.nuget\nuget.config --copyCompiler:no --v:quiet
 
 rem deploy x64 version of compiler
-%_fsiexe% --exec %~dp0\fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0\fsharp\project.json --projectJsonLock:%~dp0\fsharp\project.lock.json --packagesDir:%~dp0\..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x64 --fsharpCore:%~dp0\..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0\testbin\%1\coreclr\fsc\win7-x64 --nugetPath:%~dp0\..\.nuget\nuget.exe --nugetConfig:%~dp0\..\.nuget\nuget.config --copyCompiler:yes --v:quiet
-%_fsiexe% --exec %~dp0\fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0\fsharp\project.json --projectJsonLock:%~dp0\fsharp\project.lock.json --packagesDir:%~dp0\..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x64 --fsharpCore:%~dp0\..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0\testbin\%1\coreclr\win7-x64 --nugetPath:%~dp0\..\.nuget\nuget.exe --nugetConfig:%~dp0\..\.nuget\nuget.config --copyCompiler:no --v:quiet
+%_fsiexe% --exec %~dp0fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0fsharp\project.json --projectJsonLock:%~dp0fsharp\project.lock.json --packagesDir:%~dp0..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x64 --fsharpCore:%~dp0..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0testbin\%1\coreclr\fsc\win7-x64 --nugetPath:%~dp0..\.nuget\nuget.exe --nugetConfig:%~dp0..\.nuget\nuget.config --copyCompiler:yes --v:quiet
+%_fsiexe% --exec %~dp0fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0fsharp\project.json --projectJsonLock:%~dp0fsharp\project.lock.json --packagesDir:%~dp0..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x64 --fsharpCore:%~dp0..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0testbin\%1\coreclr\win7-x64 --nugetPath:%~dp0..\.nuget\nuget.exe --nugetConfig:%~dp0..\.nuget\nuget.config --copyCompiler:no --v:quiet
 
 rem deploy linux version of built compiler
-%_fsiexe% --exec %~dp0\fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0\fsharp\project.json --projectJsonLock:%~dp0\fsharp\project.lock.json --packagesDir:%~dp0\..\packages --targetPlatformName:DNXCore,Version=v5.0/ubuntu.14.04-x64 --fsharpCore:%~dp0\..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0\testbin\%1\coreclr\fsc\ubuntu.14.04-x64 --nugetPath:%~dp0\..\.nuget\nuget.exe --nugetConfig:%~dp0\..\.nuget\nuget.config --copyCompiler:yes --v:quiet
-%_fsiexe% --exec %~dp0\fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0\fsharp\project.json --projectJsonLock:%~dp0\fsharp\project.lock.json --packagesDir:%~dp0\..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x64 --fsharpCore:%~dp0\..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0\testbin\%1\coreclr\ubuntu.14.04-x64 --nugetPath:%~dp0\..\.nuget\nuget.exe --nugetConfig:%~dp0\..\.nuget\nuget.config --copyCompiler:no --v:quiet
+%_fsiexe% --exec %~dp0fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0fsharp\project.json --projectJsonLock:%~dp0fsharp\project.lock.json --packagesDir:%~dp0..\packages --targetPlatformName:DNXCore,Version=v5.0/ubuntu.14.04-x64 --fsharpCore:%~dp0..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0testbin\%1\coreclr\fsc\ubuntu.14.04-x64 --nugetPath:%~dp0..\.nuget\nuget.exe --nugetConfig:%~dp0..\.nuget\nuget.config --copyCompiler:yes --v:quiet
+%_fsiexe% --exec %~dp0fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0fsharp\project.json --projectJsonLock:%~dp0fsharp\project.lock.json --packagesDir:%~dp0..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x64 --fsharpCore:%~dp0..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0testbin\%1\coreclr\ubuntu.14.04-x64 --nugetPath:%~dp0..\.nuget\nuget.exe --nugetConfig:%~dp0..\.nuget\nuget.config --copyCompiler:no --v:quiet
 
 rem deploy osx version of built compiler
-%_fsiexe% --exec %~dp0\fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0\fsharp\project.json --projectJsonLock:%~dp0\fsharp\project.lock.json --packagesDir:%~dp0\..\packages --targetPlatformName:DNXCore,Version=v5.0/ubuntu.14.04-x64 --fsharpCore:%~dp0\..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0\testbin\%1\coreclr\fsc\osx.10.10-x64 --nugetPath:%~dp0\..\.nuget\nuget.exe --nugetConfig:%~dp0\..\.nuget\nuget.config --copyCompiler:yes --v:quiet
-%_fsiexe% --exec %~dp0\fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0\fsharp\project.json --projectJsonLock:%~dp0\fsharp\project.lock.json --packagesDir:%~dp0\..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x64 --fsharpCore:%~dp0\..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0\testbin\%1\coreclr\osx.10.10-x64 --nugetPath:%~dp0\..\.nuget\nuget.exe --nugetConfig:%~dp0\..\.nuget\nuget.config --copyCompiler:no --v:quiet
+%_fsiexe% --exec %~dp0fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0fsharp\project.json --projectJsonLock:%~dp0fsharp\project.lock.json --packagesDir:%~dp0..\packages --targetPlatformName:DNXCore,Version=v5.0/ubuntu.14.04-x64 --fsharpCore:%~dp0..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0testbin\%1\coreclr\fsc\osx.10.10-x64 --nugetPath:%~dp0..\.nuget\nuget.exe --nugetConfig:%~dp0..\.nuget\nuget.config --copyCompiler:yes --v:quiet
+%_fsiexe% --exec %~dp0fsharpqa\testenv\src\DeployProj\DeployProj.fsx --projectJson:%~dp0fsharp\project.json --projectJsonLock:%~dp0fsharp\project.lock.json --packagesDir:%~dp0..\packages --targetPlatformName:DNXCore,Version=v5.0/win7-x64 --fsharpCore:%~dp0..\%1\coreclr\bin\fsharp.core.dll --output:%~dp0testbin\%1\coreclr\osx.10.10-x64 --nugetPath:%~dp0..\.nuget\nuget.exe --nugetConfig:%~dp0..\.nuget\nuget.config --copyCompiler:no --v:quiet
 
 goto :EOF
 
