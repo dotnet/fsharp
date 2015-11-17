1ac670f43de7cc3c996346bcb59328f2afe37ec7
enricosada <enrico@sada.io>
2015-11-17 13:53:23 +0100

    bubble up to main2

--- src/fsharp/fsc.fs
+++ src/fsharp/fsc.fs
@@ -1907,13 +1907,31 @@ let main2(Args(tcConfig, tcImports, frameworkTcImports: TcImports, tcGlobals, er
             [], []
         else
             sigDataResources, optDataResources
+
+
+    let mainModuleName = Filename.fileNameOfPath outfile
+
+    let outfilePath = tcConfig.MakePathAbsolute outfile
+
+    let pdbfilePath = pdbfile |> Option.map (tcConfig.MakePathAbsolute >> Path.GetFullPath)
+
+    let pdbfileOpt = if not runningOnMono then pdbfilePath |> Option.map ILBinaryWriter.EmitTo.File else None
+    let mdbfileOpt = if runningOnMono then pdbfilePath |> Option.map ILBinaryWriter.EmitTo.File else None
+
+    let dumpDebugInfoOpt =
+        if tcConfig.dumpDebugInfo then 
+            Some(ILBinaryWriter.EmitTo.File(outfilePath + ".debuginfo")) 
+        else 
+            None
+
+    let outfileP = ILBinaryWriter.EmitTo.File(outfilePath)
     
     // Pass on only the minimum information required for the next phase to ensure GC kicks in.
     // In principle the JIT should be able to do good liveness analysis to clean things up, but the
     // data structures involved here are so large we can't take the risk.
-    Args(tcConfig,tcImports,tcGlobals,errorLogger,generatedCcu,outfile,optimizedImpls,topAttrs,pdbfile,assemblyName, (sigDataAttributes, sigDataResources), optDataResources,assemVerFromAttrib,signingInfo,metadataVersion,exiter)
+    Args(tcConfig,tcImports,tcGlobals,errorLogger,generatedCcu,outfileP,optimizedImpls,topAttrs,pdbfileOpt,mdbfileOpt,dumpDebugInfoOpt,mainModuleName,assemblyName, (sigDataAttributes, sigDataResources), optDataResources,assemVerFromAttrib,signingInfo,metadataVersion,exiter)
 
-let main2b(Args(tcConfig: TcConfig, tcImports, tcGlobals, errorLogger, generatedCcu: CcuThunk, outfile, optimizedImpls, topAttrs, pdbfile, assemblyName, idata, optDataResources, assemVerFromAttrib, signingInfo, metadataVersion, exiter: Exiter)) = 
+let main2b(Args(tcConfig: TcConfig, tcImports, tcGlobals, errorLogger, generatedCcu: CcuThunk, outfile, optimizedImpls, topAttrs, pdbfile, mdbfile, dumpDebugInfo, mainModuleName, assemblyName, idata, optDataResources, assemVerFromAttrib, signingInfo, metadataVersion, exiter: Exiter)) = 
   
     // Compute a static linker. 
     let ilGlobals = tcGlobals.ilg
@@ -1937,28 +1955,11 @@ let main2b(Args(tcConfig: TcConfig, tcImports, tcGlobals, errorLogger, generated
     let permissionSets = ilxGenerator.CreatePermissionSets securityAttrs
     let secDecls = if securityAttrs.Length > 0 then mkILSecurityDecls permissionSets else emptyILSecurityDecls
 
-    let mainModuleName = Filename.fileNameOfPath outfile
-
     let ilxMainModule = MainModuleBuilder.CreateMainModule (tcConfig,tcGlobals,pdbfile,assemblyName,mainModuleName,topAttrs,idata,optDataResources,codegenResults,assemVerFromAttrib,metadataVersion,secDecls)
 
     AbortOnError(errorLogger,tcConfig,exiter)
-
-    let outfilePath = tcConfig.MakePathAbsolute outfile
-
-    let pdbfilePath = pdbfile |> Option.map (tcConfig.MakePathAbsolute >> Path.GetFullPath)
-
-    let pdbfileOpt = if not runningOnMono then pdbfilePath |> Option.map ILBinaryWriter.EmitTo.File else None
-    let mdbfileOpt = if runningOnMono then pdbfilePath |> Option.map ILBinaryWriter.EmitTo.File else None
-
-    let dumpDebugInfoOpt =
-        if tcConfig.dumpDebugInfo then 
-            Some(ILBinaryWriter.EmitTo.File(outfilePath + ".debuginfo")) 
-        else 
-            None
-
-    let outfileP = ILBinaryWriter.EmitTo.File(outfilePath)
     
-    Args (tcConfig,errorLogger,staticLinker,ilGlobals,outfileP,pdbfileOpt,mdbfileOpt,dumpDebugInfoOpt,ilxMainModule,signingInfo,exiter)
+    Args (tcConfig,errorLogger,staticLinker,ilGlobals,outfile,pdbfile,mdbfile,dumpDebugInfo,ilxMainModule,signingInfo,exiter)
 
 let main2c(Args(tcConfig, errorLogger, staticLinker, ilGlobals, outfile, pdbfile, mdbfile, dumpDebugInfo, ilxMainModule, signingInfo, exiter: Exiter)) = 
       
