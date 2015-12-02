b75f85391d945a05c6496d486af298c58bafc0ef
enricosada <enrico@sada.io>
2015-12-02 17:49:56 +0100

    use full path for tests result files

--- tests/RunTests.cmd
+++ tests/RunTests.cmd
@@ -245,12 +245,12 @@ goto :EOF
 
 :COREUNIT
 
-set XMLFILE=CoreUnit_%coreunitsuffix%_Xml.xml
-set OUTPUTFILE=CoreUnit_%coreunitsuffix%_Output.log
-set ERRORFILE=CoreUnit_%coreunitsuffix%_Error.log
+set XMLFILE=%RESULTSDIR%\CoreUnit_%coreunitsuffix%_Xml.xml
+set OUTPUTFILE=%RESULTSDIR%\CoreUnit_%coreunitsuffix%_Output.log
+set ERRORFILE=%RESULTSDIR%\CoreUnit_%coreunitsuffix%_Error.log
 
-echo "%NUNIT3_CONSOLE%" /framework:V4.0 /result=%XMLFILE% /output=%OUTPUTFILE% /err=%ERRORFILE% /work=%FSCBINPATH% %FSCBINPATH%\..\..\%coreunitsuffix%\bin\FSharp.Core.Unittests.dll
-     "%NUNIT3_CONSOLE%" /framework:V4.0 /result=%XMLFILE% /output=%OUTPUTFILE% /err=%ERRORFILE% /work=%FSCBINPATH% %FSCBINPATH%\..\..\%coreunitsuffix%\bin\FSharp.Core.Unittests.dll
+echo "%NUNIT3_CONSOLE%" /framework:V4.0 /result="%XMLFILE%" /output="%OUTPUTFILE%" /err="%ERRORFILE%" /work="%FSCBINPATH%" "%FSCBINPATH%\..\..\%coreunitsuffix%\bin\FSharp.Core.Unittests.dll"
+     "%NUNIT3_CONSOLE%" /framework:V4.0 /result="%XMLFILE%" /output="%OUTPUTFILE%" /err="%ERRORFILE%" /work="%FSCBINPATH%" "%FSCBINPATH%\..\..\%coreunitsuffix%\bin\FSharp.Core.Unittests.dll"
 
 call :UPLOAD_XML
 
@@ -258,12 +258,12 @@ goto :EOF
 
 :COMPILERUNIT
 
-set XMLFILE=CompilerUnit_%compilerunitsuffix%_Xml.xml
-set OUTPUTFILE=CompilerUnit_%compilerunitsuffix%_Output.log
-set ERRORFILE=CompilerUnit_%compilerunitsuffix%_Error.log
+set XMLFILE=%RESULTSDIR%\CompilerUnit_%compilerunitsuffix%_Xml.xml
+set OUTPUTFILE=%RESULTSDIR%\CompilerUnit_%compilerunitsuffix%_Output.log
+set ERRORFILE=%RESULTSDIR%\CompilerUnit_%compilerunitsuffix%_Error.log
 
-echo "%NUNIT3_CONSOLE%" /framework:V4.0 /result=%XMLFILE% /output=%OUTPUTFILE% /err=%ERRORFILE% /work=%FSCBINPATH% %FSCBINPATH%\..\..\%compilerunitsuffix%\bin\FSharp.Compiler.Unittests.dll
-     "%NUNIT3_CONSOLE%" /framework:V4.0 /result=%XMLFILE% /output=%OUTPUTFILE% /err=%ERRORFILE% /work=%FSCBINPATH% %FSCBINPATH%\..\..\%compilerunitsuffix%\bin\FSharp.Compiler.Unittests.dll
+echo "%NUNIT3_CONSOLE%" /framework:V4.0 /result="%XMLFILE%" /output="%OUTPUTFILE%" /err="%ERRORFILE%" /work="%FSCBINPATH%" "%FSCBINPATH%\..\..\%compilerunitsuffix%\bin\FSharp.Compiler.Unittests.dll"
+     "%NUNIT3_CONSOLE%" /framework:V4.0 /result="%XMLFILE%" /output="%OUTPUTFILE%" /err="%ERRORFILE%" /work="%FSCBINPATH%" "%FSCBINPATH%\..\..\%compilerunitsuffix%\bin\FSharp.Compiler.Unittests.dll"
 
 call :UPLOAD_XML
 
@@ -271,13 +271,13 @@ goto :EOF
 
 :IDEUNIT
 
-set XMLFILE=IDEUnit_Xml.xml
-set OUTPUTFILE=IDEUnit_Output.log
-set ERRORFILE=IDEUnit_Error.log
+set XMLFILE=%RESULTSDIR%\IDEUnit_Xml.xml
+set OUTPUTFILE=%RESULTSDIR%\IDEUnit_Output.log
+set ERRORFILE=%RESULTSDIR%\IDEUnit_Error.log
 
 pushd %FSCBINPATH%
-echo "%NUNIT3_CONSOLE%" --x86 /framework:V4.0 /result=%XMLFILE% /output=%OUTPUTFILE% /err=%ERRORFILE% /work=%FSCBINPATH% %FSCBINPATH%\Unittests.dll
-     "%NUNIT3_CONSOLE%" --x86 /framework:V4.0 /result=%XMLFILE% /output=%OUTPUTFILE% /err=%ERRORFILE% /work=%FSCBINPATH% %FSCBINPATH%\Unittests.dll
+echo "%NUNIT3_CONSOLE%" --x86 /framework:V4.0 /result="%XMLFILE%" /output="%OUTPUTFILE%" /err="%ERRORFILE%" /work="%FSCBINPATH%" "%FSCBINPATH%\Unittests.dll"
+     "%NUNIT3_CONSOLE%" --x86 /framework:V4.0 /result="%XMLFILE%" /output="%OUTPUTFILE%" /err="%ERRORFILE%" /work="%FSCBINPATH%" "%FSCBINPATH%\Unittests.dll"
 popd
 call :UPLOAD_XML
 
