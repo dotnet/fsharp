267331c6e02881cad9ac2fdc128c33400cd79119
Omar Tawfik <otawfik@microsoft.com>
2016-01-07 11:12:01 -0800

    Fixed commas in build paths

--- netci.groovy
+++ netci.groovy
@@ -3,18 +3,18 @@ import jobs.generation.Utilities;
 def project = GithubProject
 
 def testProfiles = [
-	'net40,portable7,portable47,portable78,portable259,vs',
-	'cambridge_suite,smoke_only',
-	'qa_suite,smoke_only'
+	'UnitTests': 'net40,portable7,portable47,portable78,portable259,vs',
+	'CambridgeTests': 'cambridge_suite,smoke_only',
+	'QASuite': 'qa_suite,smoke_only'
 ]
 
 // TODO: run both debug/release builds
 	
-testProfiles.each { profile ->
+testProfiles.each { profileName, testSuite ->
 	[true, false].each { isPullRequest ->
 	        
-        def newJobName = Utilities.getFullJobName(project, profile, isPullRequest)
-        def buildString = """call appveyor-build.cmd ${profile}"""
+        def newJobName = Utilities.getFullJobName(project, profileName, isPullRequest)
+        def buildString = """call appveyor-build.cmd ${testSuite}"""
 
         def newJob = job(newJobName) {
             label('windows')
@@ -23,7 +23,7 @@ testProfiles.each { profile ->
             }
         }
         
-        Utilities.simpleInnerLoopJobSetup(newJob, project, isPullRequest, "Jenkins ${profile}")
+        Utilities.simpleInnerLoopJobSetup(newJob, project, isPullRequest, "Jenkins ${profileName}")
         Utilities.addXUnitDotNETResults(newJob, 'tests/TestResults/**/*_Xml.xml')
         Utilities.addArchival(newJob, "Release/**")
     }
