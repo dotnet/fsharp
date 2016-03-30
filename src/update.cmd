8511c0acbfec2b897d42a8bdb52bd37ec51bdc9a
Omar Tawfik <otawfik@microsoft.com>
2016-03-30 16:13:48 -0700

    Added FSharp.Data.TypeProviders to sn -Vr list

--- src/update.cmd
+++ src/update.cmd
@@ -45,6 +45,7 @@ rem Disable strong-name validation for F# binaries built from open source that a
 %SN32% -Vr FSharp.Build,b03f5f7f11d50a3a
 %SN32% -Vr FSharp.Compiler.Interactive.Settings,b03f5f7f11d50a3a
 %SN32% -Vr FSharp.Compiler.Hosted,b03f5f7f11d50a3a
+%SN32% -Vr FSharp.Data.TypeProviders,b03f5f7f11d50a3a
 
 %SN32% -Vr fsc,b03f5f7f11d50a3a
 %SN32% -Vr fsi,b03f5f7f11d50a3a
@@ -68,6 +69,7 @@ if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
     %SN64% -Vr FSharp.Build,b03f5f7f11d50a3a
     %SN64% -Vr FSharp.Compiler.Interactive.Settings,b03f5f7f11d50a3a
     %SN64% -Vr FSharp.Compiler.Hosted,b03f5f7f11d50a3a
+    %SN64% -Vr FSharp.Data.TypeProviders,b03f5f7f11d50a3a
 
     %SN64% -Vr fsc,b03f5f7f11d50a3a
     %SN64% -Vr fsi,b03f5f7f11d50a3a
