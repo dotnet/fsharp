29a8271c762bf1cbb2a18a19c24ca4e45e363b03
Avi.Avni <avi.avni@gmail.com>
2016-04-19 07:52:07 +0300

    fix expected test error message

--- tests/fsharpqa/Source/Conformance/SpecialAttributesAndTypes/Imported/CallerInfo/E_CallerFilePath.fs
+++ tests/fsharpqa/Source/Conformance/SpecialAttributesAndTypes/Imported/CallerInfo/E_CallerFilePath.fs
@@ -1,5 +1,5 @@
-//<Expects id="FS1234" span="(8,19-8,45)" status="error">Bogus caller info definition</Expects>
-//<Expects id="FS1234" span="(11,19-11,47)" status="error">Bogus caller info definition</Expects>
+//<Expects id="FS1246" span="(8,19-8,45)" status="error">'CallerFilePath' must be applied to an argument of type 'string', but has been applied to an argument of type 'int'</Expects>
+//<Expects id="FS1247" span="(11,19-11,47)" status="error">'CallerFilePath' can only be applied to optional arguments</Expects>
 namespace Test
 
 open System.Runtime.CompilerServices
--- tests/fsharpqa/Source/Conformance/SpecialAttributesAndTypes/Imported/CallerInfo/E_CallerLineNumber.fs
+++ tests/fsharpqa/Source/Conformance/SpecialAttributesAndTypes/Imported/CallerInfo/E_CallerLineNumber.fs
@@ -1,5 +1,5 @@
-//<Expects id="FS1234" span="(8,19-8,44)" status="error">Bogus caller info definition</Expects>
-//<Expects id="FS1234" span="(11,19-11,49)" status="error">Bogus caller info definition</Expects>
+//<Expects id="FS1246" span="(8,19-8,44)" status="error">'CallerLineNumber' must be applied to an argument of type 'int', but has been applied to an argument of type 'string'</Expects>
+//<Expects id="FS1247" span="(11,19-11,49)" status="error">'CallerLineNumber' can only be applied to optional arguments</Expects>
 namespace Test
 
 open System.Runtime.CompilerServices
