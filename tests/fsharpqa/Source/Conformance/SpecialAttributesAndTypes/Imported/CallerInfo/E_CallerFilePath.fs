7185fad30afa082c6443937891d543d2869fd1c6
Lincoln Atkinson <latkin@users.noreply.github.com>
2016-04-24 17:40:56 -0700

    Add additional negative tests for non-optional args with option types

--- tests/fsharpqa/Source/Conformance/SpecialAttributesAndTypes/Imported/CallerInfo/E_CallerFilePath.fs
+++ tests/fsharpqa/Source/Conformance/SpecialAttributesAndTypes/Imported/CallerInfo/E_CallerFilePath.fs
@@ -1,5 +1,6 @@
-//<Expects id="FS1246" span="(8,19-8,45)" status="error">'CallerFilePath' must be applied to an argument of type 'string', but has been applied to an argument of type 'int'</Expects>
-//<Expects id="FS1247" span="(11,19-11,47)" status="error">'CallerFilePath' can only be applied to optional arguments</Expects>
+//<Expects id="FS1246" span="(9,19-9,45)" status="error">'CallerFilePath' must be applied to an argument of type 'string', but has been applied to an argument of type 'int'</Expects>
+//<Expects id="FS1247" span="(12,19-12,47)" status="error">'CallerFilePath' can only be applied to optional arguments</Expects>
+//<Expects id="FS1247" span="(15,19-15,47)" status="error">'CallerFilePath' can only be applied to optional arguments</Expects>
 namespace Test
 
 open System.Runtime.CompilerServices
@@ -9,4 +10,7 @@ type MyTy() =
         path
 
     static member GetCallerFilePathNotOptional([<CallerFilePath>] path : string) =
+        path
+
+    static member GetCallerFilePathNotOptional([<CallerFilePath>] path : string option) =
         path
