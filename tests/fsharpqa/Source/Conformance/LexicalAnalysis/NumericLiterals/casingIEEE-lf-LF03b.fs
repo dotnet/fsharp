44b29e4838d675350da9beccd962a7f952a0f1b8
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-05-03 15:32:48 +0200

    Improve numeric literal error - fixes #1129

--- tests/fsharpqa/Source/Conformance/LexicalAnalysis/NumericLiterals/casingIEEE-lf-LF03b.fs
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/NumericLiterals/casingIEEE-lf-LF03b.fs
@@ -1,6 +1,6 @@
 // #Regression #Conformance #LexicalAnalysis #Constants 
 // Number type specifier
 // IEEE32/IEEE64 - Lf is illegal
-//<Expects status="error" id="FS1156" span="(6,9-6,14)">This is not a valid numeric literal\. Sample formats include 4, 0x4, 0b0100, 4L, 4UL, 4u, 4s, 4us, 4y, 4uy, 4\.0, 4\.0f, 4I</Expects>
+//<Expects status="error" id="FS1156" span="(6,9-6,14)">This is not a valid numeric literal. Valid numeric literals include</Expects>
 
 let y = 0X5Lf
