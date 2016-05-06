ead67c7d74ce7e7dc6509b95c5da134291708ec6
Kevin Ransom (msft) <codecutter.fsharp@hotmail.com>
2016-05-06 10:23:55 -0700

    Reword message slightly

--- tests/fsharpqa/Source/Warnings/AssingmentOnImmutable.fs
+++ tests/fsharpqa/Source/Warnings/AssingmentOnImmutable.fs
@@ -1,5 +1,5 @@
 // #Warnings
-//<Expects status="Error" span="(5,1)" id="FS0027">This value is not mutable. If you intend to mutate this value, declare it using the mutable keyword, e.g. 'let mutable x = expression'.</Expects>
+//<Expects status="Error" span="(5,1)" id="FS0027">This value is not mutable. Consider using the mutable keyword, e.g. 'let mutable x = expression'.</Expects>
 
 let x = 10
 x <- 20
