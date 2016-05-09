0acc7b32b576fd492825093fff34d8cccd373d30
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-05-09 11:33:24 +0200

    Improve error reporting: Let as last line in block - fixes #1162

--- tests/fsharpqa/Source/Conformance/LexicalFiltering/HashLight/E_UnclosedLetBlock01.fs
+++ tests/fsharpqa/Source/Conformance/LexicalFiltering/HashLight/E_UnclosedLetBlock01.fs
@@ -2,7 +2,7 @@
 // Verify error on unclosed let-block
 // Regression for FSB 1616
 
-//<Expects id="FS0588" status="error" span="(10,5)">Block following this 'let' is unfinished\. Expect an expression</Expects>
+//<Expects id="FS0588" status="error" span="(10,5)">The block following this 'let' is unfinished. Every code block is an expression and must have a result. 'let' cannot be the final code element in a block. Consider giving this block an explicit result.</Expects>
 
 // Note how the compiler is left hanging for what completes the function...
 
