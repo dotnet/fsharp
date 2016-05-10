a8b716bf69910b3ee1720a99ea3b38431d2ec760
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-05-10 17:19:05 +0200

    Better warning when comma is used instead of semicolon - fixes #1122

---
+++ tests/fsharpqa/Source/Warnings/CommaInRecCtor.fs
@@ -0,0 +1,7 @@
+// #Warnings
+//<Expects status="Error" span="(5,18)" id="FS0001">This expression was expected to have type.    string    .but here has type.    'a * 'b * 'c    A ';' is used to separate field values in records. Consider replacing ',' with ';'.</Expects>
+
+type Person = { Name : string; Age : int; City : string }
+let x = { Name = "Isaac", Age = 21, City = "London" }
+    
+exit 0
