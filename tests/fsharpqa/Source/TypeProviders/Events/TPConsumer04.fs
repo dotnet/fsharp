019b93d8e70dae312bd46417db3419a9d3b8bb22
Omar Tawfik <otawfik@microsoft.com>
2016-05-10 19:11:25 -0700

    Removed FSharpQA Type Providers tests

--- tests/fsharpqa/Source/TypeProviders/Events/TPConsumer04.fs
+++
@@ -1,5 +0,0 @@
-// Regression test for DevDiv#124017
-// "TypeProvider: poor error message for malformed type providers that expose events with no Add/Remove methods"
-// Case4: both GetAddMethod and GetReturnMethod return non-null => all is fine, no errors
-//<Expects status="success"></Expects>
-let _ = new N.T()
