f0b506738b4cce4c1c40f71c000ddec89da2aafb
Avi.Avni <avi.avni@gmail.com>
2016-05-12 20:57:07 +0300

    implement the CS7080 warning and add test

--- tests/fsharpqa/Source/Conformance/SpecialAttributesAndTypes/Imported/CallerInfo/E_CallerMemberName.fs
+++ tests/fsharpqa/Source/Conformance/SpecialAttributesAndTypes/Imported/CallerInfo/E_CallerMemberName.fs
@@ -1,4 +1,4 @@
-//<Expects id="FS1246" span="(9,19-9,48)" status="error">'CallerMemberName' must be applied to an argument of type 'string', but has been applied to an argument of type 'int'</Expects>
+//<Expects id="FS1246" span="(9,19-9,47)" status="error">'CallerMemberName' must be applied to an argument of type 'string', but has been applied to an argument of type 'int'</Expects>
 //<Expects id="FS1247" span="(12,19-12,49)" status="error">'CallerMemberName' can only be applied to optional arguments</Expects>
 //<Expects id="FS1247" span="(15,19-15,49)" status="error">'CallerMemberName' can only be applied to optional arguments</Expects>
 namespace Test
@@ -6,7 +6,7 @@ namespace Test
 open System.Runtime.CompilerServices
 
 type MyTy() =
-    static member GetCallerMemberNamehNotString([<CallerMemberName>] ?name : int) =
+    static member GetCallerMemberNameNotString([<CallerMemberName>] ?name : int) =
         name
 
     static member GetCallerMemberNameNotOptional([<CallerMemberName>] name : string) =
