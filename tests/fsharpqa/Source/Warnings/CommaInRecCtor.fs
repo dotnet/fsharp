2203e25cf14915b689ae670dff2bedd7e3a3c276
Kevin Ransom (msft) <codecutter.fsharp@hotmail.com>
2016-05-12 14:51:32 -0700

    Fix multi-line error message in CommaInRecCtor.fs

--- tests/fsharpqa/Source/Warnings/CommaInRecCtor.fs
+++ tests/fsharpqa/Source/Warnings/CommaInRecCtor.fs
@@ -1,5 +1,10 @@
 // #Warnings
-//<Expects status="Error" span="(5,18)" id="FS0001">This expression was expected to have type.    'string'    .but here has type.    ''a * 'b * 'c'    A ';' is used to separate field values in records. Consider replacing ',' with ';'.</Expects>
+//<Expects status="Error" span="(10,18)" id="FS0001">This expression was expected to have type</Expects>
+//<Expects status="success">    'string'</Expects>
+//<Expects status="success">but here has type</Expects>
+//<Expects status="success">    ''a \* 'b \* 'c'</Expects>
+//<Expects status="success">A ';' is used to separate field values in records. Consider replacing ',' with ';'.</Expects>
+
 
 type Person = { Name : string; Age : int; City : string }
 let x = { Name = "Isaac", Age = 21, City = "London" }
