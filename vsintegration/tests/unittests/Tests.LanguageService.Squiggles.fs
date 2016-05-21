7a8d1075db5dbe3cd8ba4f882eed3c79d2703d71
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-05-21 11:31:05 +0200

    fix error messages

--- vsintegration/tests/unittests/Tests.LanguageService.Squiggles.fs
+++ vsintegration/tests/unittests/Tests.LanguageService.Squiggles.fs
@@ -498,9 +498,10 @@ type X() =
             """     
         let (sln, proj, file) = this.CreateSingleFileProject(fileContents)  
         let errors = GetErrors(proj)
-        let desiredError = errors |> List.tryFind (fun e -> e.Message = "Unexpected floating point literal in pattern. Expected identifier, '(', '(*)' or other token.")
+        let expected = "Unexpected floating point literal in pattern. Expected '(', '(*)', identifier or other token."
+        let desiredError = errors |> List.tryFind (fun e -> e.Message.StartsWith expected)
         match desiredError with
-        | None -> Assert.Fail(sprintf "did not find expected error in %A" errors)
+        | None -> Assert.Fail(sprintf "did not find expected error %s in %A" expected errors)
         | Some(e) -> 
             Assert.IsTrue(e.Context = TextSpan(iStartLine=5, iStartIndex=31, iEndLine=5, iEndIndex=34), "error had wrong location")   
   
