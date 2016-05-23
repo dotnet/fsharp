b96bd13aebe84412eba241502c7ed08c80714554
Kevin Ransom (msft) <codecutter.fsharp@hotmail.com>
2016-05-23 12:58:36 -0700

    Add correct error message patterns

--- tests/fsharpqa/Source/Warnings/ReturnInsteadOfReturnBang.fs
+++ tests/fsharpqa/Source/Warnings/ReturnInsteadOfReturnBang.fs
@@ -1,5 +1,9 @@
 // #Warnings
-//<Expects status="Error" span="(4,32)" id="FS0001">Type mismatch. Expecting a.+''a'.+but given a.+'Async<'a>'.+The types ''a' and 'Async<'a>' cannot be unified. Consider using 'return!' instead of 'return'.*</Expects>
+//<Expects status="Error" span="(8,32)" id="FS0001">Type mismatch. Expecting a</Expects>
+//<Expects>''a'</Expects>
+//<Expects>but given a</Expects>
+//<Expects>'Async<'a>'</Expects>
+//<Expects>The types ''a' and 'Async<'a>' cannot be unified. Consider using 'return!' instead of 'return'.</Expects>
 
 let rec foo() = async { return foo() }
     
