265b2beb6363b31de5e47e15af59d3939343ccd7
Taylor Wood <taylorwood@me.com>
2016-06-01 23:16:06 -0500

    Expand unit-type-expected compiler warning

--- tests/fsharpqa/Source/Conformance/Expressions/BindingExpressions/Binding/in01.fs
+++ tests/fsharpqa/Source/Conformance/Expressions/BindingExpressions/Binding/in01.fs
@@ -5,7 +5,7 @@
 // Eventually, we will deprecated them - and the specs will be updated.
 //
 //<Expects status="error" span="(11,5-11,6)" id="FS0039">The value or constructor 'a' is not defined$</Expects>
-//<Expects status="error" span="(11,5-11,10)" id="FS0020">This expression should have type 'unit', but has type 'bool'\. Use 'ignore' to discard the result of the expression, or 'let' to bind the result to a name\.$</Expects>
+//<Expects status="error" span="(11,5-11,10)" id="FS0020">This expression has a value of type 'bool' that is implicitly ignored\. Use the 'ignore' function to discard this value explicitly, e\.g\. 'expr \|> ignore', or bind it to a name to refer to it later, e\.g\. 'let result = expr'\.$</Expects>
 module A =
     let a = 3 in a + 1 |> ignore;;
     a > 4;;
--- tests/fsharpqa/Source/Diagnostics/NONTERM/quoteExpr01.fs
+++ tests/fsharpqa/Source/Diagnostics/NONTERM/quoteExpr01.fs
@@ -1,5 +1,5 @@
 // #Regression #Diagnostics 
 // Regression test for FSHARP1.0:2391, FSHARP1.0:1479
-//<Expects id="FS0020" span="(5,1-5,24)" status="warning">This expression should have type 'unit', but has type 'seq.Quotations.Var.'</Expects>
+//<Expects id="FS0020" span="(5,1-5,24)" status="warning">This expression has a value of type 'seq.Quotations\.Var.' that is implicitly ignored</Expects>
 #light "off"
 <@@ 1 @@>.GetFreeVars()
