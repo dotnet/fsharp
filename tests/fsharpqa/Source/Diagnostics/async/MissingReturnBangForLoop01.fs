4d2563e3dadf1e0c34b25e3d5e74a949fc6b5fb9
Taylor Wood <taylorwood@me.com>
2016-06-01 23:16:06 -0500

    Expand unit-type-expected compiler warning

--- tests/fsharpqa/Source/Diagnostics/async/MissingReturnBangForLoop01.fs
+++ tests/fsharpqa/Source/Diagnostics/async/MissingReturnBangForLoop01.fs
@@ -1,5 +1,5 @@
 // #Regression #Diagnostics #Async 
 // Regression tests for FSHARP1.0:4394
 // common mistake: forgetting the return! For a loop
-//<Expects status="error" span="(5,39-5,45)" id="FS0020">This expression should have type 'unit', but has type 'Async<'a>'\. Use 'ignore' to discard the result of the expression, or 'let' to bind the result to a name\.$</Expects>
+//<Expects status="error" span="(5,39-5,45)" id="FS0020">This expression has a value of type 'Async<'a>' that is implicitly ignored\. Use the 'ignore' function to discard this value explicitly, e\.g\. 'expr \|> ignore', or bind it to a name to refer to it later, e\.g\. 'let result = expr'\.$</Expects>
 let rec loop() = async { let x = 1 in loop() }
--- tests/fsharpqa/Source/Misc/Parsing02.fs
+++ tests/fsharpqa/Source/Misc/Parsing02.fs
@@ -1,8 +1,8 @@
 // #Regression #Misc 
 // Verify warnings associated with top level expressions getting discarded
 
-//<Expects id="FS0020" status="warning">This expression should have type 'unit', but has type '\( \^a -> unit\) \* int'</Expects>
-//<Expects id="FS0020" status="warning">This expression should have type 'unit', but has type '\('a \[\] -> unit \[\]\) \* string \[\]'</Expects>
+//<Expects id="FS0020" status="warning">This expression has a value of type '\( \^a -> unit\) \* int' that is implicitly ignored</Expects>
+//<Expects id="FS0020" status="warning">This expression has a value of type '\('a \[\] -> unit \[\]\) \* string \[\]' that is implicitly ignored</Expects>
 
 // Note the comma between printf "%A", this results in a tuple expr which probabaly wasn't intended.
 let arr = [|"Foo"; "Bar"|]
