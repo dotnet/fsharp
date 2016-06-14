54f5ffa34c24b15ee445440f701541971ba3f56c
enricosada <enrico@sada.io>
2016-06-14 23:08:01 +0200

    fix and normalize xml

--- tests/fsharpqa/Source/Diagnostics/General/W_Keyword_tailcall01.fs
+++ tests/fsharpqa/Source/Diagnostics/General/W_Keyword_tailcall01.fs
@@ -1,13 +1,13 @@
 // #Regression #Diagnostics 
 // Regression test for FSHARP1.0:2204
-//<Expects>\(13,11-13,19\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
-//<Expects>\(14,12-14,20\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
-//<Expects>\(15,14-15,22\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
-//<Expects>\(15,27-15,35\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
-//<Expects>\(17,13-17,21\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
-//<Expects>\(17,24-17,32\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
-//<Expects>\(17,33-17,41\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
-//<Expects>\(18,22-18,30\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
+//<Expects status="success">\(13,11-13,19\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
+//<Expects status="success">\(14,12-14,20\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
+//<Expects status="success">\(15,14-15,22\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
+//<Expects status="success">\(15,27-15,35\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
+//<Expects status="success">\(17,13-17,21\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
+//<Expects status="success">\(17,24-17,32\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
+//<Expects status="success">\(17,33-17,41\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
+//<Expects status="success">\(18,22-18,30\).+warning FS0046: The keyword 'tailcall' is reserved for future use by F#</Expects>
 
 #light
 
