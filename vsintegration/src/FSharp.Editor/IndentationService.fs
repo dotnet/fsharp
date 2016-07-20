feea9b63fdd9698a355db82f828565c6ed506e6d
Omar Tawfik <otawfik@microsoft.com>
2016-07-20 15:11:36 -0700

    Added Roslyn IndentationService + tests

---
+++ vsintegration/src/FSharp.Editor/IndentationService.fs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.VisualStudio.FSharp.Editor
+
+open System
+open System.Composition
+open System.Collections.Concurrent
+open System.Collections.Generic
+open System.Threading
+open System.Threading.Tasks
+open System.Linq
+
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.Classification
+open Microsoft.CodeAnalysis.Editor
+open Microsoft.CodeAnalysis.Editor.Implementation.BraceMatching
+open Microsoft.CodeAnalysis.Editor.Shared.Utilities
+open Microsoft.CodeAnalysis.Formatting
+open Microsoft.CodeAnalysis.Host.Mef
+open Microsoft.CodeAnalysis.Text
+
+open Microsoft.VisualStudio.FSharp.LanguageService
+open Microsoft.VisualStudio.Text
+open Microsoft.VisualStudio.Text.Tagging
+
+open Microsoft.FSharp.Compiler.Parser
+open Microsoft.FSharp.Compiler.SourceCodeServices
+
+[<Shared>]
+[<ExportLanguageService(typeof<IIndentationService>, FSharpCommonConstants.FSharpLanguageName)>]
+type internal FSharpIndentationService() =
+
+    static member GetDesiredIndentation(sourceText: SourceText, lineNumber: int, tabSize: int): Option<int> =
+        if lineNumber = 0 then
+            // No indentation on the first line of a document
+            None
+        else
+            // Match indentation with previous line
+            let previousLine = sourceText.Lines.[lineNumber - 1]
+            let rec loop column spaces =
+                if previousLine.Start + column >= previousLine.End then
+                    spaces
+                else match previousLine.Text.[previousLine.Start + column] with
+                     | ' ' -> loop (column + 1) (spaces + 1)
+                     | '\t' -> loop (column + 1) (((spaces / tabSize) + 1) * tabSize)
+                     | _ -> spaces
+            Some(loop 0 0)
+
+    interface IIndentationService with
+        member this.GetDesiredIndentationAsync(document: Document, lineNumber: int, cancellationToken: CancellationToken): Task<Nullable<IndentationResult>> =
+            document.GetTextAsync(cancellationToken).ContinueWith(
+                fun (sourceTextTask: Task<SourceText>) ->
+                    let tabSize = document.Project.Solution.Workspace.Options.GetOption(FormattingOptions.TabSize, FSharpCommonConstants.FSharpLanguageName)
+                    try match FSharpIndentationService.GetDesiredIndentation(sourceTextTask.Result, lineNumber, tabSize) with
+                        | None -> Nullable<IndentationResult>()
+                        | Some(indentation) -> Nullable<IndentationResult>(IndentationResult(sourceTextTask.Result.Lines.[lineNumber].Start, indentation))
+                    with ex -> 
+                        Assert.Exception(ex)
+                        reraise()
+                , TaskContinuationOptions.OnlyOnRanToCompletion)
---
+++ vsintegration/tests/unittests/Tests.Roslyn.IndentationService.fs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+namespace Microsoft.VisualStudio.FSharp.Editor.Tests.Roslyn
+
+open System
+open System.Threading
+
+open NUnit.Framework
+
+open Microsoft.CodeAnalysis.Classification
+open Microsoft.CodeAnalysis.Editor
+open Microsoft.CodeAnalysis.Text
+open Microsoft.VisualStudio.FSharp.Editor
+
+[<TestFixture>]
+type IndentationServiceTests()  =
+
+    static let tabSize = 4
+
+    static let consoleProjectTemplate = "
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn \"%A\" argv
+    0 // return an integer exit code"
+
+    static let libraryProjectTemplate = "
+namespace ProjectNamespace
+
+type Class1() = 
+    member this.X = \"F#\""
+
+    static let nestedTypesTemplate = "
+namespace testspace
+    type testtype
+        static member testmember = 1"
+    
+    static member private testCases: Object[][] = [|
+        [| None;     0; consoleProjectTemplate |]
+        [| Some(0);  1; consoleProjectTemplate |]
+        [| Some(0);  2; consoleProjectTemplate |]
+        [| Some(0);  3; consoleProjectTemplate |]
+        [| Some(0);  4; consoleProjectTemplate |]
+        [| Some(0);  5; consoleProjectTemplate |]
+        [| Some(0);  6; consoleProjectTemplate |]
+        [| Some(4);  7; consoleProjectTemplate |]
+        [| Some(4);  8; consoleProjectTemplate |]
+        
+        [| None;     0; libraryProjectTemplate |]
+        [| Some(0);  1; libraryProjectTemplate |]
+        [| Some(0);  2; libraryProjectTemplate |]
+        [| Some(0);  3; libraryProjectTemplate |]
+        [| Some(0);  4; libraryProjectTemplate |]
+        [| Some(4);  5; libraryProjectTemplate |]
+        
+        [| None;     0; nestedTypesTemplate |]
+        [| Some(0);  1; nestedTypesTemplate |]
+        [| Some(0);  2; nestedTypesTemplate |]
+        [| Some(4);  3; nestedTypesTemplate |]
+        [| Some(8);  4; nestedTypesTemplate |]
+    |]
+
+    [<TestCaseSource("testCases")>]
+    member this.TestIndentation(expectedIndentation: Option<int>, lineNumber: int, template: string) = 
+        let actualIndentation = FSharpIndentationService.GetDesiredIndentation(SourceText.From(template), lineNumber, tabSize)
+        match expectedIndentation with
+        | None -> Assert.IsTrue(actualIndentation.IsNone, "No indentation was expected at line {0}", lineNumber)
+        | Some(indentation) -> Assert.AreEqual(expectedIndentation.Value, actualIndentation.Value, "Indentation on line {0} doesn't match", lineNumber)
+    
