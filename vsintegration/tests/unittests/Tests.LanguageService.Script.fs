c761a7e6b74b0bb0c23d3dcfdfa89cb8e2a9da4b
Kevin Ransom (msft) <codecutter.fsharp@hotmail.com>
2016-08-08 13:52:25 -0700

    remove these commented out tests

--- vsintegration/tests/unittests/Tests.LanguageService.Script.fs
+++ vsintegration/tests/unittests/Tests.LanguageService.Script.fs
@@ -977,24 +977,6 @@ type UsingMSBuild() as this =
             """#r "mscorcfg" """        // 'mscorcfg' is loaded from the GAC _and_ it is available on XP and above.
             "#r \"mscor" "Global Assembly Cache"
 
-    // // Disabled because it seems Microsoft.VisualStudio.QualityTools.Common.dll is no longer always available on CI installs in the same way
-    // // as it used to be.
-    // [<Test;Category("fsx closure"); Category("NO_CI")>] 
-    // member public this.``Fsx.HashR_QuickInfo.ResolveFromAssemblyFoldersEx``() = 
-    //     let fileContent = """#r "Microsoft.VisualStudio.QualityTools.Common.dll" """     // 'Microsoft.VisualStudio.QualityTools.Common.dll' is located via AssemblyFoldersEx
-    //     let marker = "#r \"Microsoft.Vis"
-    //     this.AssertQuickInfoContainsAtEndOfMarkerInFsxFile fileContent marker "Microsoft.VisualStudio.QualityTools.Common, Version="
-    //     this.AssertQuickInfoContainsAtEndOfMarkerInFsxFile fileContent marker "Microsoft.VisualStudio.QualityTools.Common.dll"
-
-    // // Disabled because it seems Microsoft.SqlServer.SString.dll is no longer always available on CI installs in the same way
-    //[<Test>]
-    //[<Category("fsx closure")>]
-    //member public this.``Fsx.HashR_QuickInfo.ResolveFromAssemblyFolders``() =
-    //    let fileContent = """#r "Microsoft.SqlServer.SString" """       // Can be any assembly that is in AssemblyFolders but not AssemblyFoldersEx
-    //    let marker = "#r \"Microsoft.SqlSe"
-    //    this.AssertQuickInfoContainsAtEndOfMarkerInFsxFile fileContent marker "Microsoft.SqlServer.SString.dll"
-    //    this.AssertQuickInfoContainsAtEndOfMarkerInFsxFile fileContent marker "Found by AssemblyFolders registry key"
-        
     [<Test>]
     [<Category("fsx closure")>]
     member public this.``Fsx.HashR_QuickInfo.ResolveFromFullyQualifiedPath``() = 
