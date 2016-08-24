20de0281cb6806053f0381507edcf13069bc14fb
liboz <ringil.sword@gmail.com>
2016-08-24 19:17:00 -0400

    escaped braces

--- tests/fsharpqa/Source/InteractiveSession/Misc/PublicField.fsx
+++ tests/fsharpqa/Source/InteractiveSession/Misc/PublicField.fsx
@@ -1,6 +1,6 @@
 // #Regression #NoMT #FSI 
 // Public fields did not print.
-//<Expects status="success">val it : PublicField = FSI_0002+PublicField {X = 2;\n Y = 1;}</Expects>
+//<Expects status="success">val it : PublicField = FSI_0002+PublicField \{X = 2;\n Y = 1;\}</Expects>
 [<Struct>]
 type PublicField = 
     val X : int
