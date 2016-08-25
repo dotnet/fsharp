4691faa7129a737ab5c2019954d3612b5e2b20d9
Omar Tawfik <otawfik@microsoft.com>
2016-08-25 12:34:21 -0700

    Draft version of document diagnostics

---
+++ vsintegration/src/FSharp.Editor/DocumentDifferenceService.fs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Microsoft.VisualStudio.FSharp.Editor
+
+open System
+open System.Composition
+open System.Collections.Immutable
+open System.Threading
+open System.Threading.Tasks
+
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.Diagnostics
+open Microsoft.CodeAnalysis.Host.Mef
+open Microsoft.CodeAnalysis.Text
+open Microsoft.CodeAnalysis.SolutionCrawler
+
+open Microsoft.FSharp.Compiler
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open Microsoft.FSharp.Compiler.Range
+
+open Microsoft.VisualStudio.FSharp.LanguageService
+
+[<ExportLanguageService(typeof<IDocumentDifferenceService>, FSharpCommonConstants.FSharpLanguageName)>]
+type FSharpDocumentDifferenceService() =
+    interface IDocumentDifferenceService with
+        member this.GetDifferenceAsync(_,_,_) =
+            // No incremental anaylsis for now.
+            Task.FromResult(new DocumentDifferenceResult(InvocationReasons.DocumentChanged))
