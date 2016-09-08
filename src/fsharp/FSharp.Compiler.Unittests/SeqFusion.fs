4c42233cf1cbad8def50668ec83dc2f69240ff78
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-09-08 17:16:26 +0200

    Test order of evaluation

--- src/fsharp/FSharp.Compiler.Unittests/SeqFusion.fs
+++ src/fsharp/FSharp.Compiler.Unittests/SeqFusion.fs
@@ -4,6 +4,7 @@ namespace FSharp.Compiler.Unittests
 
 open System
 open NUnit.Framework
+open System.Collections.Generic
 
 [<TestFixture>]
 type SeqFusionTestsModule() =
@@ -12,16 +13,30 @@ type SeqFusionTestsModule() =
     member this.FuseTwoMapsWithSameType() =
         let data = [3; 1; 2]
         let result = Seq.map (fun x -> x * 2) (Seq.map (fun x -> x + 2) data)
-        Assert.areEqual ([12; 6; 8]) (Seq.toList result)
+        Assert.areEqual [12; 6; 8] (Seq.toList result)
 
     [<Test>]
     member this.FuseTwoMapsWithSameType_String() =
         let data = ["hello"; "world"; "!"]
         let result = Seq.map (fun x -> "hello" + x) (Seq.map (fun (y:string) -> " " + y) data)
-        Assert.areEqual (["hello hello"; "hello world"; "hello !"]) (Seq.toList result)
+        Assert.areEqual ["hello hello"; "hello world"; "hello !"] (Seq.toList result)
         
     [<Test>]
     member this.FuseTwoMapsWithDifferentType() =
         let data = ["hello"; "world"; "!"]
         let result = Seq.map (fun x -> x * 3) (Seq.map (fun (y:string) -> y.Length) data)
-        Assert.areEqual ([15; 15; 3]) (Seq.toList result)
+        Assert.areEqual [15; 15; 3] (Seq.toList result)
+
+    [<Test>]
+    member this.FusisonOfTwoMapsKeepsSideEffectOrder() =
+        let list = List<string>()
+        let data = ["hello"; "world"; "!"]
+        let result = Seq.map (fun x -> x * 3) (Seq.map (fun y -> list.Add y; y.Length) data)
+        
+        // seq is not evaluated yet
+        Assert.areEqual 0 list.Count
+
+        // evaluate it
+        Assert.areEqual [15; 15; 3] (Seq.toList result)
+
+        Assert.areEqual data (Seq.toList list)
