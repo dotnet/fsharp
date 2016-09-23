ea1b3b0512442df5eea461242e89da8596da23b8
Omar Tawfik <otawfik@microsoft.com>
2016-09-23 11:59:46 -0700

    Upgrade to Preview5 Roslyn nugets

--- vsintegration/src/FSharp.Editor/GoToDefinitionService.fs
+++ vsintegration/src/FSharp.Editor/GoToDefinitionService.fs
@@ -29,12 +29,15 @@ open Microsoft.FSharp.Compiler.Range
 open Microsoft.FSharp.Compiler.SourceCodeServices
 
 type internal FSharpNavigableItem(document: Document, textSpan: TextSpan, displayString: string) =
+    member this.DisplayString = displayString
+
     interface INavigableItem with
         member this.Glyph = Glyph.BasicFile
         member this.DisplayFileLocation = true
-        member this.DisplayString = displayString
+        member this.IsImplicitlyDeclared = false
         member this.Document = document
         member this.SourceSpan = textSpan
+        member this.DisplayTaggedParts = Unchecked.defaultof<ImmutableArray<TaggedText>>
         member this.ChildItems = ImmutableArray<INavigableItem>.Empty
 
 [<Shared>]
@@ -124,7 +127,7 @@ type internal FSharpGoToDefinitionService [<ImportingConstructor>] ([<ImportMany
             
             if definitionTask.Status = TaskStatus.RanToCompletion then
                 if definitionTask.Result.Any() then
-                    let navigableItem = definitionTask.Result.First() // F# API provides only one INavigableItem
+                    let navigableItem = definitionTask.Result.First() :?> FSharpNavigableItem // F# API provides only one INavigableItem
                     for presenter in presenters do
                         presenter.DisplayResult(navigableItem.DisplayString, definitionTask.Result)
                     true
