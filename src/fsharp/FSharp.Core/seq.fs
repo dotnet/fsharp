e7d7d059e5b395f89a07c88515ed4e259e0d9fe6
Paul Westcott <p.westcott@gmail.com>
2016-10-12 20:23:43 +1100

    bug fix: "truncate 0" was causing MoveNext on underlying seq

--- src/fsharp/FSharp.Core/seq.fs
+++ src/fsharp/FSharp.Core/seq.fs
@@ -1731,6 +1731,7 @@ namespace Microsoft.FSharp.Collections
 
         [<CompiledName("Truncate")>]
         let truncate n (source: seq<'T>) =
+            if n <= 0 then empty else
             source |> seqFactory (SeqComposer.TruncateFactory n)
 
         [<CompiledName("Pairwise")>]
