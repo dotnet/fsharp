68e786bde5e7d83e20890f9b3d771a4661342ca9
Don Syme <donsyme@fastmail.fm>
2016-11-04 17:27:31 +0000

    don't use C# task

---
+++ src/buildtools/subst.fsx
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information,
+//
+//=========================================================================================
+
+#load "scriptlib.fsx"
+
+open System.IO
+open System.Text.RegularExpressions
+
+try 
+    let input = getCmdLineArgReqd    "--in:"  
+    let output = getCmdLineArgReqd    "--out:"  
+    let pattern = getCmdLineArgReqd    "--pattern:"  
+    let replacement = getCmdLineArgReqd    "--replacement:"  
+
+    File.WriteAllText(output,Regex.Replace(File.ReadAllText(input), pattern, replacement))
+    exit 0
+with e -> 
+    eprintfn "%A" e
+    exit 1
