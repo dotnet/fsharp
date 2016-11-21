5f2e843289b6726dff7c878c4245b072ed3750e5
Kevin Ransom <kevinr@microsoft.com>
2016-11-21 15:15:16 -0800

    Update desktop components to use the latest published System.ValueTuple package

--- src/fsharp/FSharp.Core.Unittests/TypeForwarding.fs
+++ src/fsharp/FSharp.Core.Unittests/TypeForwarding.fs
@@ -18,10 +18,10 @@ type TypeForwardingModule() =
         let currentRuntimeVersion = System.Runtime.InteropServices.RuntimeEnvironment.GetSystemVersion()
         let currentFSharpCoreTargetRuntime = typeof<int list>.Assembly.ImageRuntimeVersion
         let tupleAssemblyName = typeof<System.Tuple<int,int>>.Assembly.FullName
-        
+
         let mscorlib4AssemblyName = "mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
         let fsharpCore2AssemblyName = "FSharp.Core, Version=2.3.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
-        
+
         printfn "currentRuntimeVersion = %s; currentFSharpCoreTargetRuntime=%s tupleAssemblyName=%s" currentRuntimeVersion currentFSharpCoreTargetRuntime tupleAssemblyName
         match (currentRuntimeVersion, currentFSharpCoreTargetRuntime) with
         | ("v2.0.50727", _)           
