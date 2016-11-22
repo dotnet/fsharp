243979f247a5c9db04bc01531a2d478875e092b8
Josh Varty <joshvarty@gmail.com>
2016-11-21 17:29:07 -0800

    Add FSharpEditorFactory

---
+++ vsintegration/src/FSharp.ProjectSystem.Base/Project/FSharpEditorFactory.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
+
+namespace Microsoft.VisualStudio.FSharp.ProjectSystem
+{
+    public class FSharpEditorFactory : IVsEditorFactory
+    {
+        private Package _parentPackage;
+        private IOleServiceProvider _serviceProvider;
+
+        public FSharpEditorFactory(Package parentPackage)
+        {
+            _parentPackage = parentPackage;
+        }
+
+        public int Close()
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int CreateEditorInstance(uint grfCreateDoc, string pszMkDocument, string pszPhysicalView, IVsHierarchy pvHier, uint itemid, IntPtr punkDocDataExisting, out IntPtr ppunkDocView, out IntPtr ppunkDocData, out string pbstrEditorCaption, out Guid pguidCmdUI, out int pgrfCDW)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MapLogicalView(ref Guid rguidLogicalView, out string pbstrPhysicalView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SetSite(Microsoft.VisualStudio.OLE.Interop.IServiceProvider packageServiceProvider)
+        {
+            _serviceProvider = packageServiceProvider;
+            return VSConstants.S_OK;
+        }
+    }
+}
