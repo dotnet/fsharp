c1d14c8f55c6d20936ce385125011a558372d27b
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2016-11-25 21:44:10 +0000

    Add more tests.

--- tests/fsharpqa/Source/Conformance/DeclarationElements/MemberDefinitions/OptionalDefaultParameterValueArguments/ConsumeFromCS.fs
+++ tests/fsharpqa/Source/Conformance/DeclarationElements/MemberDefinitions/OptionalDefaultParameterValueArguments/ConsumeFromCS.fs
@@ -1,5 +1,7 @@
 ﻿// #Conformance #DeclarationElements #MemberDefinitions #OptionalDefaultParameterValueArguments 
 
+namespace ConsumeFromCS
+
 open System.Runtime.InteropServices
 open System
 
@@ -33,36 +35,3 @@ type Class() =
     //Insanity checks - basically make sure the compiler does not crash.
     static member OnlyOptional([<Optional>]a: int) = a
     static member OnlyDefault([<DefaultParameterValueAttribute(1)>]a: int) = a
-
-let checkMethod f value defaultFun defaultValue =
-    if f value <> value then exit 1
-    if defaultFun() <> defaultValue then exit 1
-
-do checkMethod (fun v -> Class.Method1 (v)) 1y       (fun () -> Class.Method1 ()) 42y
-do checkMethod (fun v -> Class.Method2 (v)) 1uy      (fun () -> Class.Method2 ()) 42uy
-do checkMethod (fun v -> Class.Method3 (v)) 1s       (fun () -> Class.Method3 ()) 42s
-do checkMethod (fun v -> Class.Method4 (v)) 1us      (fun () -> Class.Method4 ()) 42us
-do checkMethod (fun v -> Class.Method5 (v)) 1        (fun () -> Class.Method5 ()) 42
-do checkMethod (fun v -> Class.Method6 (v)) 1u       (fun () -> Class.Method6 ()) 42u
-do checkMethod (fun v -> Class.Method7 (v)) 1L       (fun () -> Class.Method7 ()) 42L
-do checkMethod (fun v -> Class.Method8 (v)) 1UL      (fun () -> Class.Method8 ()) 42UL
-do checkMethod (fun v -> Class.Method9 (v)) 1.1f     (fun () -> Class.Method9 ()) 42.42f
-do checkMethod (fun v -> Class.Method10(v)) 1.1      (fun () -> Class.Method10()) 42.42
-do checkMethod (fun v -> Class.Method11(v)) false    (fun () -> Class.Method11()) true
-do checkMethod (fun v -> Class.Method12(v)) 'c'      (fun () -> Class.Method12()) 'q'
-do checkMethod (fun v -> Class.Method13(v)) "noo"    (fun () -> Class.Method13()) "ono"
-do checkMethod (fun v -> Class.Method14(v)) (obj())  (fun () -> Class.Method14()) null
-do checkMethod (fun v -> Class.Method15(v)) (new DateTime(12300L))  (fun () -> Class.Method15()) (new DateTime())
-
-do checkMethod (fun v -> Class.Mix1(1, "1", v)) 100 (fun () -> Class.Mix1(2, "2")) -12
-do checkMethod (fun v -> Class.Mix2(1, "1", v, 101)) 100 (fun () -> Class.Mix2(2, "2", d=12)) -12
-do if (Class.Mix3(1, "1")) <> ("1",-12,-123) then exit 1
-do if (Class.Mix3(1)) <> ("str",-12,-123) then exit 1
-do if (Class.Mix3(1, c=1, b="123")) <> ("123",1,-123) then exit 1
-
-//can't omit the argument, but can still call the method.
-do if Class.OnlyOptional(5) <> 5 then exit 1
-//can't omit the argument, but can still call the method.
-do if Class.OnlyDefault(2) <> 2 then exit 1
-
-exit 0
---
+++ tests/fsharpqa/Source/Conformance/DeclarationElements/MemberDefinitions/OptionalDefaultParameterValueArguments/WrongDefaultType_Fail.fs
@@ -0,0 +1,12 @@
+﻿// #Conformance #DeclarationElements #MemberDefinitions #OptionalDefaultParameterValueArguments
+
+//this currently fails the PEVerify step - need to fix by creating appropriate compile error.
+
+open System.Runtime.InteropServices
+
+type Class() =
+    static member WrongType([<Optional;DefaultParameterValue(1)>]b:string) = b
+
+let r = Class.WrongType()
+
+do ()
