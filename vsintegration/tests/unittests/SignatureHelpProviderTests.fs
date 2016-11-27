284082cc192be5e7c4038804da357483997df27b
dsyme <dsyme@MSRC-4079076>
2016-11-27 13:14:27 +0000

    fix disposal test

--- vsintegration/tests/unittests/SignatureHelpProviderTests.fs
+++ vsintegration/tests/unittests/SignatureHelpProviderTests.fs
@@ -154,6 +154,7 @@ type foo5 = N1.T<Param1=1,ParamIgnored= >
 
             let triggerChar = if marker = "," then Some ',' elif marker = "(" then Some '(' elif marker = "<" then Some '<' else None
             let triggered = FSharpSignatureHelpProvider.ProvideMethodsAsyncAux(documentationProvider, SourceText.From(fileContents), caretPosition, options, triggerChar, filePath, 0) |> Async.RunSynchronously
+            FSharpLanguageService.Checker.ClearLanguageServiceRootCachesAndCollectAndFinalizeAllTransients()
             let actual = 
                 match triggered with 
                 | None -> None
