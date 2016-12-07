71d3093fb1d1ced5f386ef667cd1ace4008f688a
Vasily Kirichenko <vaskir@gmail.com>
2016-12-07 15:59:36 +0300

    indexing and caching NavigateTo results

--- vsintegration/src/FSharp.Editor/NavigateToSearchService.fs
+++ vsintegration/src/FSharp.Editor/NavigateToSearchService.fs
@@ -10,17 +10,11 @@ open System.Collections.Generic
 open System.Collections.Immutable
 open System.Threading
 open System.Threading.Tasks
-open System.Linq
 open System.Runtime.CompilerServices
-open System.Windows
-open System.Windows.Controls
-open System.Windows.Media
+open System.Globalization
 
 open Microsoft.CodeAnalysis
-open Microsoft.CodeAnalysis.Classification
 open Microsoft.CodeAnalysis.Editor
-open Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense.QuickInfo
-open Microsoft.CodeAnalysis.Editor.Shared.Utilities
 open Microsoft.CodeAnalysis.Host.Mef
 open Microsoft.CodeAnalysis.Options
 open Microsoft.CodeAnalysis.Text
@@ -29,19 +23,14 @@ open Microsoft.CodeAnalysis.Navigation
 
 open Microsoft.VisualStudio.FSharp.LanguageService
 open Microsoft.VisualStudio.Text
-open Microsoft.VisualStudio.Text.Classification
-open Microsoft.VisualStudio.Text.Tagging
-open Microsoft.VisualStudio.Text.Formatting
 open Microsoft.VisualStudio.Shell
 open Microsoft.VisualStudio.Shell.Interop
 
 open Microsoft.FSharp.Compiler
-open Microsoft.FSharp.Compiler.Parser
 open Microsoft.FSharp.Compiler.Range
 open Microsoft.FSharp.Compiler.SourceCodeServices
-open System.Windows.Documents
 
-type internal NavigableItem(document: Document, sourceSpan: TextSpan, glyph: Glyph, name: string) =
+type internal NavigableItem(document: Document, sourceSpan: TextSpan, glyph: Glyph, name: string, kind: string, additionalInfo: string) =
     interface INavigableItem with
         member __.Glyph = glyph
         /// The tagged parts to display for this item. If default, the line of text from <see cref="Document"/> is used.
@@ -55,17 +44,89 @@ type internal NavigableItem(document: Document, sourceSpan: TextSpan, glyph: Gly
         member __.Document = document
         member __.SourceSpan = sourceSpan
         member __.ChildItems = ImmutableArray<INavigableItem>.Empty
+    member __.Name = name
+    member __.Kind = kind
+    member __.AdditionalInfo = additionalInfo
 
-type internal NavigateToSearchResult(name: string, item: INavigableItem, kind: string, additionalInfo: string) =
+type internal NavigateToSearchResult(item: NavigableItem, matchKind: NavigateToMatchKind) =
     interface INavigateToSearchResult with
-        member __.AdditionalInformation = additionalInfo
-        member __.Kind = kind
-        member __.MatchKind = NavigateToMatchKind.Substring
+        member __.AdditionalInformation = item.AdditionalInfo
+        member __.Kind = item.Kind
+        member __.MatchKind = matchKind
         member __.IsCaseSensitive = false
-        member __.Name = name
+        member __.Name = item.Name
         member __.SecondarySort = null
         member __.Summary = null
-        member __.NavigableItem = item
+        member __.NavigableItem = upcast item
+
+module private Index =
+    [<System.Diagnostics.DebuggerDisplay("{DebugString()}")>]
+    type private IndexEntry(str: string, offset: int, item: NavigableItem, isOperator: bool) =
+        member __.String = str
+        member __.Offset = offset
+        member __.Length = str.Length - offset
+        member __.Item = item
+        member __.IsOperator = isOperator
+        member x.StartsWith (s: string) = 
+            if s.Length > x.Length then false
+            else CultureInfo.CurrentCulture.CompareInfo.IndexOf(str, s, offset, s.Length, CompareOptions.IgnoreCase) = offset
+        member private __.DebugString() = sprintf "%s (offset %d) (%s)" (str.Substring offset) offset str
+
+    let private indexEntryComparer =
+        { new IComparer<IndexEntry> with
+            member __.Compare(a, b) = 
+                let res = CultureInfo.CurrentCulture.CompareInfo.Compare(a.String, a.Offset, b.String, b.Offset, CompareOptions.IgnoreCase)
+                if res = 0 then a.Offset.CompareTo(b.Offset) else res }
+
+    type IIndexedNavigableItems =
+        abstract Find: searchValue: string -> ImmutableArray<INavigateToSearchResult>
+
+    let build (items: seq<NavigableItem>) =
+        let entries = ResizeArray()
+
+        for item in items do
+            let isOperator, name = 
+                if PrettyNaming.IsMangledOpName item.Name then 
+                    true, PrettyNaming.DecompileOpName item.Name 
+                else 
+                    false, item.Name
+            for i = 0 to name.Length - 1 do
+                entries.Add(IndexEntry(name, i, item, isOperator))
+
+        entries.Sort(indexEntryComparer)
+        { new IIndexedNavigableItems with
+              member __.Find (searchValue) =
+                  let result = ResizeArray()
+                  if entries.Count > 0 then 
+                     let entryToFind = IndexEntry(searchValue, 0, Unchecked.defaultof<_>, Unchecked.defaultof<_>)
+                     
+                     let initial = 
+                         let p = entries.BinarySearch(entryToFind, indexEntryComparer)
+                         if p < 0 then ~~~p else p
+                     
+                     let handle index =
+                         let entry = entries.[index]
+                         let matchKind = 
+                             if entry.Offset = 0 then
+                                 if entry.Length = searchValue.Length then NavigateToMatchKind.Exact
+                                 else NavigateToMatchKind.Prefix
+                             else NavigateToMatchKind.Substring
+                         let item = entry.Item
+                         result.Add (NavigateToSearchResult(item, matchKind) :> INavigateToSearchResult)
+                    
+                     // in case if there are multiple matching items binary search might return not the first one.
+                     // in this case we'll walk backwards searching for the applicable answers
+                     let mutable pos = initial
+                     while pos >= 0 && pos < entries.Count && entries.[pos].StartsWith searchValue do
+                         handle pos
+                         pos <- pos - 1
+                    
+                     // value of 'initial' position was already handled on the previous step so here we'll bump it
+                     let mutable pos = initial + 1
+                     while pos < entries.Count && entries.[pos].StartsWith searchValue do
+                         handle pos
+                         pos <- pos + 1
+                  result.ToImmutableArray() }
 
 module private Utils =
     let navigateToItemKindToRoslynKind = function
@@ -117,43 +178,59 @@ type internal FSharpNavigateToSearchService
         projectInfoManager: ProjectInfoManager
     ) =
 
-    static member GetNavigateToSearchResult(filePath: string, sourceText: SourceText, checker: FSharpChecker, options: FSharpProjectOptions) =
+    let itemsByDocumentId = ConcurrentDictionary<DocumentId, (int * NavigableItem [])>()
+
+    let getNavigableItems(document: Document, options: FSharpProjectOptions, cancellationToken: CancellationToken) =
         async {
-            let! parseResults = checker.ParseFileInProject(filePath, sourceText.ToString(), options)
-            match parseResults.ParseTree with
-            | Some parsedInput -> return NavigateTo.getNavigableItems parsedInput
-            | None -> return [||]
+            let! sourceText = document.GetTextAsync(cancellationToken) |> Async.AwaitTask
+            let! parseResults = checkerProvider.Checker.ParseFileInProject(document.FilePath, sourceText.ToString(), options)
+            return 
+                match parseResults.ParseTree |> Option.map NavigateTo.getNavigableItems with
+                | Some items ->
+                    [| for item in items do
+                         let sourceSpan = CommonRoslynHelpers.FSharpRangeToTextSpan(sourceText, item.Range)
+                         let glyph = Utils.navigateToItemKindToGlyph item.Kind
+                         let kind = Utils.navigateToItemKindToRoslynKind item.Kind
+                         let additionalInfo = Utils.containerToString item.Container document.Project
+                         yield NavigableItem(document, sourceSpan, glyph, item.Name, kind, additionalInfo) |]
+                | None -> [||]
+        }
+
+    let getCachedNavigableItems(document: Document, options: FSharpProjectOptions, cancellationToken: CancellationToken) =
+        async {
+            let! textVersion = document.GetTextVersionAsync(cancellationToken) |> Async.AwaitTask
+            let textVersionHash = hash textVersion
+            match itemsByDocumentId.TryGetValue document.Id with
+            | true, (oldTextVersionHash, items) when oldTextVersionHash = textVersionHash ->
+                return items
+            | _ ->
+                let! items = getNavigableItems(document, options, cancellationToken)
+                itemsByDocumentId.[document.Id] <- (textVersionHash, items)
+                return items
         }
 
     interface INavigateToSearchService with
-        member __.SearchProjectAsync(project, _searchPattern, cancellationToken) : Task<ImmutableArray<INavigateToSearchResult>> =
+        member __.SearchProjectAsync(project, searchPattern, cancellationToken) : Task<ImmutableArray<INavigateToSearchResult>> =
             async {
                 match projectInfoManager.TryGetOptionsForProject(project.Id) with
                 | Some options ->
                     let! items =
                         project.Documents
-                        |> Seq.map (fun document -> 
-                            async {
-                                let! sourceText = document.GetTextAsync(cancellationToken) |> Async.AwaitTask
-                                let! results = FSharpNavigateToSearchService.GetNavigateToSearchResult(document.FilePath, sourceText, checkerProvider.Checker, options)
-                                return document, sourceText, results
-                            })
+                        |> Seq.map (fun document -> getCachedNavigableItems(document, options, cancellationToken))
                         |> Async.Parallel
-                    
-                    return
-                        [| for document, sourceText, items in items do
-                             for item in items do
-                                 let sourceSpan = CommonRoslynHelpers.FSharpRangeToTextSpan(sourceText, item.Range)
-                                 let glyph = Utils.navigateToItemKindToGlyph item.Kind
-                                 let navigableItem = NavigableItem(document, sourceSpan, glyph, item.Name)
-                                 let kind = Utils.navigateToItemKindToRoslynKind item.Kind
-                                 let additionalInfo = Utils.containerToString item.Container project
-                                 yield NavigateToSearchResult(item.Name, navigableItem, kind, additionalInfo) :> INavigateToSearchResult 
-                        |].ToImmutableArray()
 
-                | None -> return [].ToImmutableArray()
+                    return (items |> Array.concat |> Index.build).Find(searchPattern).ToImmutableArray()
+
+                | None -> return ImmutableArray<INavigateToSearchResult>.Empty
             } |> CommonRoslynHelpers.StartAsyncAsTask(cancellationToken)
 
 
-        member __.SearchDocumentAsync(_document, _searchPattern, _cancellationToken) : Task<ImmutableArray<INavigateToSearchResult>> =
-            Task.FromResult([].ToImmutableArray())
+        member __.SearchDocumentAsync(document, searchPattern, cancellationToken) : Task<ImmutableArray<INavigateToSearchResult>> =
+            async {
+                let! options = projectInfoManager.TryGetOptionsForDocumentOrProject(document)
+                match options with
+                | Some options ->
+                    let! items = getCachedNavigableItems(document, options, cancellationToken)
+                    return (Index.build items).Find(searchPattern).ToImmutableArray()
+                | None -> return ImmutableArray<INavigateToSearchResult>.Empty
+            } |> CommonRoslynHelpers.StartAsyncAsTask(cancellationToken)
