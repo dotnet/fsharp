79385707d8f47e1d1c75012febb360eff7722c7e
Vasily Kirichenko <vaskir@gmail.com>
2016-12-12 17:19:43 +0300

    one file -> many Documents
    fix FSharpSymbol.IsPrivateToFile

--- vsintegration/src/FSharp.Editor/FindReferencesService.fs
+++ vsintegration/src/FSharp.Editor/FindReferencesService.fs
@@ -41,56 +41,68 @@ type internal FSharpFindReferencesService
                     | FSharpCheckFileAnswer.Aborted -> failwith "Compilation isn't complete yet"
                     | FSharpCheckFileAnswer.Succeeded(results) -> results
                 
-                let textLine = sourceText.Lines.GetLineFromPosition(position)
-                let textLineNumber = textLine.LineNumber + 1 // Roslyn line numbers are zero-based
-                let textLinePos = sourceText.Lines.GetLinePosition(position)
-                let fcsTextLineNumber = textLinePos.Line + 1
+                let textLine = sourceText.Lines.GetLineFromPosition(position).ToString()
+                let lineNumber = sourceText.Lines.GetLinePosition(position).Line + 1
                 let defines = CompilerEnvironment.GetCompilationDefinesForEditing(document.FilePath, options.OtherOptions |> Seq.toList)
                 
-                let rangeToDocumentSpan (range: range) =
+                // File can be included in more than one project, hence single `range` may results with multiple `Document`s.
+                let rangeToDocumentSpans (range: range) =
                     async {
-                        match document.Project.Solution.GetDocumentIdsWithFilePath(range.FileName) |> Seq.tryHead with
-                        | Some docId ->
-                            let doc = document.Project.Solution.GetDocument(docId)
-                            let! sourceText = doc.GetTextAsync(context.CancellationToken) |> Async.AwaitTask
-                            let span = CommonHelpers.fixupSpan(sourceText, CommonRoslynHelpers.FSharpRangeToTextSpan(sourceText, range))
-                            return Some (DocumentSpan(doc, span))
-                        | None -> return None
+                        if range.Start = range.End then return [||]
+                        else 
+                            let! spans =
+                                document.Project.Solution.GetDocumentIdsWithFilePath(range.FileName)
+                                |> Seq.map (fun documentId ->
+                                    async {
+                                        let doc = document.Project.Solution.GetDocument(documentId)
+                                        let! sourceText = doc.GetTextAsync(context.CancellationToken) |> Async.AwaitTask
+                                        match CommonRoslynHelpers.TryFSharpRangeToTextSpan(sourceText, range) with
+                                        | Some span ->
+                                           let span = CommonHelpers.fixupSpan(sourceText, span)
+                                           return Some (DocumentSpan(doc, span))
+                                        | None -> return None
+                                    })
+                                |> Async.Parallel
+                            return spans |> Array.choose id
                     }
                 
                 match CommonHelpers.tryClassifyAtPosition(document.Id, sourceText, document.FilePath, defines, position, context.CancellationToken) with
                 | Some (islandColumn, qualifiers, _) -> 
-                    let! symbolUse = checkFileResults.GetSymbolUseAtLocation(textLineNumber, islandColumn, textLine.ToString(), qualifiers)
+                    let! symbolUse = checkFileResults.GetSymbolUseAtLocation(lineNumber, islandColumn, textLine, qualifiers)
                     match symbolUse with
                     | Some symbolUse ->
-                        let! declaration = checkFileResults.GetDeclarationLocationAlternate (fcsTextLineNumber, islandColumn, textLine.ToString(), qualifiers, false)
+                        let! declaration = checkFileResults.GetDeclarationLocationAlternate (lineNumber, islandColumn, textLine, qualifiers, false)
                         let declarationRange = 
                             match declaration with
                             | FSharpFindDeclResult.DeclFound range -> Some range
                             | _ -> None
                         
-                        let! declarationSpan =
+                        let! declarationSpans =
                             match declarationRange with
-                            | Some range -> rangeToDocumentSpan range
-                            | None -> async.Return None
+                            | Some range -> rangeToDocumentSpans range
+                            | None -> async.Return [||]
                 
-                        let definitionItem =
-                            match declarationSpan with 
-                            | Some span ->
-                                DefinitionItem.Create(
-                                    ImmutableArray<string>.Empty, 
-                                    [TaggedText(TextTags.Text, symbolUse.Symbol.FullName)].ToImmutableArray(), 
-                                    span)
-                            | None -> 
-                                DefinitionItem.CreateNonNavigableItem(
-                                    ImmutableArray<string>.Empty, 
-                                    [TaggedText(TextTags.Text, symbolUse.Symbol.FullName)].ToImmutableArray())    
-                
-                        do! context.OnDefinitionFoundAsync(definitionItem) |> Async.AwaitTask
+                        let definitionItems =
+                            match declarationSpans with 
+                            | [||] -> 
+                                [| DefinitionItem.CreateNonNavigableItem(
+                                       ImmutableArray<string>.Empty, 
+                                       [TaggedText(TextTags.Text, symbolUse.Symbol.FullName)].ToImmutableArray(),
+                                       [TaggedText(TextTags.Assembly, symbolUse.Symbol.Assembly.SimpleName)].ToImmutableArray()) |]
+                            | _ ->
+                                declarationSpans
+                                |> Array.map (fun span ->
+                                    DefinitionItem.Create(
+                                        ImmutableArray<string>.Empty, 
+                                        [TaggedText(TextTags.Text, symbolUse.Symbol.FullName)].ToImmutableArray(), 
+                                        span))
+                        
+                        for definitionItem in definitionItems do
+                            do! context.OnDefinitionFoundAsync(definitionItem) |> Async.AwaitTask
                         
                         let! symbolUses =
                             async {
-                                if symbolUse.Symbol.IsPrivateToFile then
+                                if symbolUse.IsPrivateToFile then
                                     return! checkFileResults.GetUsesOfSymbolInFile(symbolUse.Symbol)
                                 else
                                     let! projectCheckResults = checker.ParseAndCheckProject(options)
@@ -101,12 +113,14 @@ type internal FSharpFindReferencesService
                             match declarationRange with
                             | Some declRange when declRange = symbolUse.RangeAlternate -> ()
                             | _ ->
-                                let! referenceDocSpan = rangeToDocumentSpan symbolUse.RangeAlternate
-                                match referenceDocSpan with
-                                | Some span -> 
-                                    let referenceItem = SourceReferenceItem(definitionItem, span)
-                                    do! context.OnReferenceFoundAsync(referenceItem) |> Async.AwaitTask
-                                | None -> ()
+                                let! referenceDocSpans = rangeToDocumentSpans symbolUse.RangeAlternate
+                                match referenceDocSpans with
+                                | [||] -> ()
+                                | _ ->
+                                    for referenceDocSpan in referenceDocSpans do
+                                        for definitionItem in definitionItems do
+                                            let referenceItem = SourceReferenceItem(definitionItem, referenceDocSpan)
+                                            do! context.OnReferenceFoundAsync(referenceItem) |> Async.AwaitTask
                     | None -> ()
                 | None -> ()
             | None -> ()
