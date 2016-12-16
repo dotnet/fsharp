1c97a2a23bea2e02c920684d92e906576bd54777
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-12-16 15:56:31 +0100

    fix tests

--- tests/fsharpqa/Source/Warnings/ExtraArgumentInCtor.fs
+++ tests/fsharpqa/Source/Warnings/ExtraArgumentInCtor.fs
@@ -1,5 +1,5 @@
 // #Warnings
-//<Expects status="Error" id="FS0501">The object constructor 'Person' takes 0 argument(s) but is here given 1. The required signature is 'new : unit -> Person'.$</Expects>
+//<Expects status="Error" id="FS0501">The object constructor 'Person' takes 0 argument\(s\) but is here given 1. The required signature is 'new : unit -> Person'.$</Expects>
 
 type Person() =
     member val Name = "" with get,set
--- tests/fsharpqa/Source/Warnings/MissingCtorValue.fs
+++ tests/fsharpqa/Source/Warnings/MissingCtorValue.fs
@@ -1,5 +1,5 @@
 // #Warnings
-//<Expects status="Error" id="FS0496">The member or object constructor 'Person' requires 1 argument(s). The required signature is 'new : x:int -> Person'.</Expects>
+//<Expects status="Error" id="FS0496">The member or object constructor 'Person' requires 1 argument\(s\). The required signature is 'new : x:int -> Person'.</Expects>
 
 type Person(x:int) =
     member val Name = "" with get,set
