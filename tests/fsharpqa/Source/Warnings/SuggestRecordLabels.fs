a77a71ee32224d134e40b0693a713057a27ab9f7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-12-28 09:46:27 +0100

    Show that we can't suggest record labels

---
+++ tests/fsharpqa/Source/Warnings/SuggestRecordLabels.fs
@@ -0,0 +1,7 @@
+// #Warnings
+//<Expects status="Error" id="FS0039">The value, constructor, namespace or type 'DateTie' is not defined</Expects>
+//<Expects>Maybe you want one of the following: DateTime, DateTimeKind, DateTimeOffset</Expects>
+
+let x = System.DateTie.MaxValue
+
+exit 0
