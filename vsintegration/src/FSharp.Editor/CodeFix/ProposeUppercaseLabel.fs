83fd4a164708ea679bbdfe7a7e3592a646901322
Vasily Kirichenko <vaskir@gmail.com>
2016-12-28 16:59:05 +0300

    refactor with asyncMaybe

--- vsintegration/src/FSharp.Editor/CodeFix/ProposeUppercaseLabel.fs
+++ vsintegration/src/FSharp.Editor/CodeFix/ProposeUppercaseLabel.fs
@@ -36,60 +36,51 @@ type internal FSharpProposeUpperCaseLabelCodeFixProvider
     override __.FixableDiagnosticIds = fixableDiagnosticIds.ToImmutableArray()
 
     override __.RegisterCodeFixesAsync context : Task =
-        async {
-            if context.Span.Length > 0 then
-                let document = context.Document
-                let! sourceText = document.GetTextAsync(context.CancellationToken)
-                let originalText = sourceText.ToString(context.Span)
-                if originalText.Length > 0 then
-                    match projectInfoManager.TryGetOptionsForEditingDocumentOrProject context.Document with 
-                    | Some options ->
-                        let! sourceText = context.Document.GetTextAsync(context.CancellationToken)
-                        let defines = CompilerEnvironment.GetCompilationDefinesForEditing(document.Name, options.OtherOptions |> Seq.toList)
-                        match CommonHelpers.getSymbolAtPosition(document.Id, sourceText, context.Span.Start, document.FilePath, defines, SymbolLookupKind.Fuzzy) with 
-                        | Some symbol -> 
-                            let! textVersion = document.GetTextVersionAsync(context.CancellationToken)
-                            let checker = checkerProvider.Checker
-                            let! _, checkFileAnswer = checker.ParseAndCheckFileInProject(context.Document.FilePath, textVersion.GetHashCode(), sourceText.ToString(), options)
-                            match checkFileAnswer with
-                            | FSharpCheckFileAnswer.Aborted -> ()
-                            | FSharpCheckFileAnswer.Succeeded checkFileResults ->
-                                let textLine = sourceText.Lines.GetLineFromPosition(context.Span.Start)
-                                let textLinePos = sourceText.Lines.GetLinePosition(context.Span.Start)
-                                let fcsTextLineNumber = textLinePos.Line + 1
-                                let! symbolUse = checkFileResults.GetSymbolUseAtLocation(fcsTextLineNumber, symbol.RightColumn, textLine.Text.ToString(), [symbol.Text])
-                                match symbolUse with
-                                | Some symbolUse ->
-                                    match symbolUse.GetDeclarationLocation(document) with
-                                    | None -> ()
-                                    | Some declLoc ->
-                                        let newText = originalText.[0].ToString().ToUpper() + originalText.Substring(1)
-                                        let title = FSComp.SR.replaceWithSuggestion newText
-                                        // defer finding all symbol uses throughout the solution until the code fix action is executed
-                                        let codeFix = 
-                                            CodeAction.Create(
-                                                title,
-                                                (fun (cancellationToken: CancellationToken) ->
-                                                    async {
-                                                        let! symbolUsesByDocumentId = 
-                                                            SymbolHelpers.getSymbolUsesInSolution(symbolUse.Symbol, declLoc, checkFileResults, projectInfoManager, checker, document.Project.Solution)
+        asyncMaybe {
+            do! Option.guard (context.Span.Length > 0)
+            let document = context.Document
+            let! sourceText = document.GetTextAsync(context.CancellationToken)
+            let originalText = sourceText.ToString(context.Span)
+            do! Option.guard (originalText.Length > 0)
+            let! options = projectInfoManager.TryGetOptionsForEditingDocumentOrProject context.Document
+            let! sourceText = context.Document.GetTextAsync(context.CancellationToken)
+            let defines = CompilerEnvironment.GetCompilationDefinesForEditing(document.Name, options.OtherOptions |> Seq.toList)
+            let! symbol = CommonHelpers.getSymbolAtPosition(document.Id, sourceText, context.Span.Start, document.FilePath, defines, SymbolLookupKind.Fuzzy)
+            let! textVersion = document.GetTextVersionAsync(context.CancellationToken)
+            let checker = checkerProvider.Checker
+            let! _, checkFileAnswer = checker.ParseAndCheckFileInProject(context.Document.FilePath, textVersion.GetHashCode(), sourceText.ToString(), options) |> liftAsync
+            match checkFileAnswer with
+            | FSharpCheckFileAnswer.Aborted -> ()
+            | FSharpCheckFileAnswer.Succeeded checkFileResults ->
+                let textLine = sourceText.Lines.GetLineFromPosition(context.Span.Start)
+                let textLinePos = sourceText.Lines.GetLinePosition(context.Span.Start)
+                let fcsTextLineNumber = textLinePos.Line + 1
+                let! symbolUse = checkFileResults.GetSymbolUseAtLocation(fcsTextLineNumber, symbol.RightColumn, textLine.Text.ToString(), [symbol.Text])
+                let! declLoc = symbolUse.GetDeclarationLocation(document)
+                let newText = originalText.[0].ToString().ToUpper() + originalText.Substring(1)
+                let title = FSComp.SR.replaceWithSuggestion newText
+                // defer finding all symbol uses throughout the solution until the code fix action is executed
+                let codeFix = 
+                    CodeAction.Create(
+                        title,
+                        (fun (cancellationToken: CancellationToken) ->
+                            async {
+                                let! symbolUsesByDocumentId = 
+                                    SymbolHelpers.getSymbolUsesInSolution(symbolUse.Symbol, declLoc, checkFileResults, projectInfoManager, checker, document.Project.Solution)
+                
+                                let mutable solution = document.Project.Solution
                                 
-                                                        let mutable solution = document.Project.Solution
-                                                        
-                                                        for KeyValue(documentId, symbolUses) in symbolUsesByDocumentId do
-                                                            let document = document.Project.Solution.GetDocument(documentId)
-                                                            let! sourceText = document.GetTextAsync(cancellationToken)
-                                                            let mutable sourceText = sourceText
-                                                            for symbolUse in symbolUses do
-                                                                let textSpan = CommonHelpers.fixupSpan(sourceText, CommonRoslynHelpers.FSharpRangeToTextSpan(sourceText, symbolUse.RangeAlternate))
-                                                                sourceText <- sourceText.Replace(textSpan, newText)
-                                                                solution <- solution.WithDocumentText(documentId, sourceText)
-                                                        return solution
-                                                    } |> CommonRoslynHelpers.StartAsyncAsTask(cancellationToken)),
-                                                title)
-                                        let diagnostics = (context.Diagnostics |> Seq.filter (fun x -> fixableDiagnosticIds |> List.contains x.Id)).ToImmutableArray()
-                                        context.RegisterCodeFix(codeFix, diagnostics)
-                                | None -> ()
-                        | None -> ()
-                    | _ -> ()
-        } |> CommonRoslynHelpers.StartAsyncUnitAsTask(context.CancellationToken)
+                                for KeyValue(documentId, symbolUses) in symbolUsesByDocumentId do
+                                    let document = document.Project.Solution.GetDocument(documentId)
+                                    let! sourceText = document.GetTextAsync(cancellationToken)
+                                    let mutable sourceText = sourceText
+                                    for symbolUse in symbolUses do
+                                        let textSpan = CommonHelpers.fixupSpan(sourceText, CommonRoslynHelpers.FSharpRangeToTextSpan(sourceText, symbolUse.RangeAlternate))
+                                        sourceText <- sourceText.Replace(textSpan, newText)
+                                        solution <- solution.WithDocumentText(documentId, sourceText)
+                                return solution
+                            } |> CommonRoslynHelpers.StartAsyncAsTask(cancellationToken)),
+                        title)
+                let diagnostics = (context.Diagnostics |> Seq.filter (fun x -> fixableDiagnosticIds |> List.contains x.Id)).ToImmutableArray()
+                context.RegisterCodeFix(codeFix, diagnostics)
+        } |> Async.ignore |> CommonRoslynHelpers.StartAsyncUnitAsTask(context.CancellationToken)
