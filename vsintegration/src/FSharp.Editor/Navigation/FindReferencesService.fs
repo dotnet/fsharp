f8e579fcf57a043d63471795af9490b79d3a94d9
Vasily Kirichenko <vaskir@gmail.com>
2017-01-06 08:11:25 +0300

    Fix find all references (#2167)
    * add glyph for declarations in Find All References result window
    
    * fixed: ColorizationService may return duplicated spans which caused FindReferencesService to crash
    
    * remove #nowarn "1182"
    
    * fixed: FindReferencesService does not work on external symbols

--- vsintegration/src/FSharp.Editor/Navigation/FindReferencesService.fs
+++ vsintegration/src/FSharp.Editor/Navigation/FindReferencesService.fs
@@ -14,6 +14,7 @@ open Microsoft.CodeAnalysis.Editor.Host
 open Microsoft.CodeAnalysis.Navigation
 open Microsoft.CodeAnalysis.FindSymbols
 open Microsoft.CodeAnalysis.FindReferences
+open Microsoft.CodeAnalysis.Completion
 
 open Microsoft.VisualStudio.FSharp.LanguageService
 
@@ -62,31 +63,32 @@ type internal FSharpFindReferencesService
             let! symbol = CommonHelpers.getSymbolAtPosition(document.Id, sourceText, position, document.FilePath, defines, SymbolLookupKind.Fuzzy)
             let! symbolUse = checkFileResults.GetSymbolUseAtLocation(lineNumber, symbol.RightColumn, textLine, [symbol.Text])
             let! declaration = checkFileResults.GetDeclarationLocationAlternate (lineNumber, symbol.RightColumn, textLine, [symbol.Text], false) |> liftAsync
+            let tags = GlyphTags.GetTags(CommonRoslynHelpers.GetGlyphForSymbol symbolUse.Symbol)
             
             let declarationRange = 
                 match declaration with
                 | FSharpFindDeclResult.DeclFound range -> Some range
                 | _ -> None
             
-            let! declarationSpans =
-                match declarationRange with
-                | Some range -> rangeToDocumentSpans(document.Project.Solution, range, context.CancellationToken) |> liftAsync
-                | None -> async.Return None
-            
-            let definitionItems =
-                match declarationSpans with 
-                | [] -> 
-                    [ DefinitionItem.CreateNonNavigableItem(
-                          ImmutableArray<string>.Empty, 
-                          [TaggedText(TextTags.Text, symbolUse.Symbol.FullName)].ToImmutableArray(),
-                          [TaggedText(TextTags.Assembly, symbolUse.Symbol.Assembly.SimpleName)].ToImmutableArray()) ]
-                | _ ->
-                    declarationSpans
-                    |> List.map (fun span ->
-                        DefinitionItem.Create(
-                            ImmutableArray<string>.Empty, 
-                            [TaggedText(TextTags.Text, symbolUse.Symbol.FullName)].ToImmutableArray(), 
-                            span))
+            let! definitionItems =
+                async {
+                    let! declarationSpans =
+                        match declarationRange with
+                        | Some range -> rangeToDocumentSpans(document.Project.Solution, range, context.CancellationToken)
+                        | None -> async.Return []
+                    
+                    return 
+                        match declarationSpans with 
+                        | [] -> 
+                            [ DefinitionItem.CreateNonNavigableItem(
+                                tags,
+                                ImmutableArray.Create(TaggedText(TextTags.Text, symbol.Text)),
+                                ImmutableArray.Create(TaggedText(TextTags.Assembly, symbolUse.Symbol.Assembly.SimpleName))) ]
+                        | _ ->
+                            declarationSpans
+                            |> List.map (fun span ->
+                                DefinitionItem.Create(tags, ImmutableArray.Create(TaggedText(TextTags.Text, symbol.Text)), span))
+                } |> liftAsync
             
             for definitionItem in definitionItems do
                 do! context.OnDefinitionFoundAsync(definitionItem) |> Async.AwaitTask |> liftAsync
