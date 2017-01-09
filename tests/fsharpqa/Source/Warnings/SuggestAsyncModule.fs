16fc35cd8c661b2776d3044e1c1e795fb94f57d3
Steffen Forkmann <sforkmann@gmail.com>
2017-01-09 07:19:27 +0100

    Suggestions for spelling mistakes needs to be filtered much stronger (#2175)
    * Use EditDistance for threshold
    
    * Don't throw too specific error message
    
    * Suggest more types
    
    * Suggest vals and exns
    
    * Oupsi

---
+++ tests/fsharpqa/Source/Warnings/SuggestAsyncModule.fs
@@ -0,0 +1,8 @@
+// #Warnings
+//<Expects status="Error" id="FS0039"> The value, namespace, type or module 'Asnc' is not defined. Maybe you want one of the following: Async, async, asin$</Expects>
+
+let f =
+    Asnc.Sleep 1000
+
+
+exit 0
