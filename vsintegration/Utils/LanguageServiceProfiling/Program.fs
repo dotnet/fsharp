cce06880b20b4dd1c9f0ffdb6c05b4984349e015
Vasily Kirichenko <vaskir@gmail.com>
2017-01-22 17:24:35 +0300

    it can check TypeChecker.fs

--- vsintegration/Utils/LanguageServiceProfiling/Program.fs
+++ vsintegration/Utils/LanguageServiceProfiling/Program.fs
@@ -1,15 +1,18 @@
-﻿open Microsoft.FSharp.Compiler.SourceCodeServices
+﻿open Microsoft.FSharp.Compiler
+open Microsoft.FSharp.Compiler.SourceCodeServices
 open System
+open System.IO
 open LanguageServiceProfiling
 open System.Diagnostics
 open System.Collections.Generic
 
 [<EntryPoint>]
 let main argv = 
-    let options = Options.FCS argv.[0]
+    let rootDir = argv.[0]
+    let options = Options.FCS rootDir
     let checker = FSharpChecker.Create()
     let waste = new ResizeArray<int array>()
-    let check() =
+    let checkProject() =
         printfn "ParseAndCheckProject(FCS)..."
         let sw = Stopwatch.StartNew()
         let result = checker.ParseAndCheckProject(options) |> Async.RunSynchronously
@@ -17,27 +20,50 @@ let main argv =
             printfn "Finished with ERRORS: %+A" result.Errors
         else printfn "Finished successfully in %O" sw.Elapsed
         ()
+
+    let checkFile (relativePath: string) (fileVersion: int) =
+        printfn "ParseAndCheckFileInProject(%s)..." relativePath
+        let filePath = Path.Combine(rootDir, relativePath)
+        let sw = Stopwatch.StartNew()
+        let _, answer = checker.ParseAndCheckFileInProject(filePath, fileVersion, File.ReadAllText filePath, options) |> Async.RunSynchronously
+        match answer with
+        | FSharpCheckFileAnswer.Aborted ->
+            printfn "Abortedin %O!" sw.Elapsed
+        | FSharpCheckFileAnswer.Succeeded results ->
+            if results.Errors |> Array.exists (fun x -> x.Severity = FSharpErrorSeverity.Error) then
+                printfn "Finished with ERRORS in %O: %+A" sw.Elapsed results.Errors
+            else printfn "Finished successfully in %O" sw.Elapsed
     
     let wasteMemory () =
         waste.Add(Array.zeroCreate (1024 * 1024 * 25))
 
-    printfn "Press <C> for check the project, <G> for GC.Collect(2), <W> for wasting memory, <M> to reclaim waste <Enter> for exit."
-    let rec loop() =
+    printfn """Press:
+
+<C> for check the project
+<G> for GC.Collect(2)
+<F> for check TypeChecker.fs (you need to change it before rechecking)
+<W> for wasting 100M of memory
+<M> to reclaim waste <Enter> for exit."""
+
+    let rec loop (fileVersion: int) =
         match Console.ReadKey().Key with
         | ConsoleKey.C -> 
-            check()
-            loop()
+            checkProject()
+            loop fileVersion
         | ConsoleKey.G -> 
             printfn "GC is running..."
             let sw = Stopwatch.StartNew()
             GC.Collect 2
             printfn "GC is done in %O" sw.Elapsed
-            loop()
+            loop fileVersion
+        | ConsoleKey.F ->
+            checkFile @"src\fsharp\TypeChecker.fs" fileVersion
+            loop (fileVersion + 1)
         | ConsoleKey.W ->
             wasteMemory()
         | ConsoleKey.M ->
             waste.Clear()
         | ConsoleKey.Enter -> ()
-        | _ -> loop()
-    loop()
+        | _ -> loop fileVersion
+    loop 0
     0
