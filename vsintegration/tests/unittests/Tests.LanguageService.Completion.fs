aa8f0e813387eb08e0ee69731f1064ce9780daf5
Don Syme <dsyme@users.noreply.github.com>
2017-02-08 11:51:35 +0000

    don't keep parse results in background builder (#2378)
    Like #2377 this eliminates a source of unnecessarily retained memory - the parse trees from the background build. These are unused in the Visual F# Tools and even if they were needed it is easy to recreate them on-demand.
    
    The original code is being kept #if because the FSharp.Compiler.Service buget package component might choose to optionally allow the storage of these, so there's no need to delete the original code path yet
    
    * don't keep parse results in background builder
    
    * fix memory capture by TcSymbolUses
    
    * use struct tuples
    
    * fix test

--- vsintegration/tests/unittests/Tests.LanguageService.Completion.fs
+++ vsintegration/tests/unittests/Tests.LanguageService.Completion.fs
@@ -4218,7 +4218,11 @@ let x = query { for bbbb in abbbbc(*D0*) do
         SaveFileToDisk file2      
         TakeCoffeeBreak(this.VS)
         
+#if FCS_RETAIN_BACKGROUND_PARSE_RESULTS
         gpatcc.AssertExactly(notAA[file2], notAA[file2;file3])
+#else
+        gpatcc.AssertExactly(notAA[file2; file3], notAA[file2;file3])
+#endif
 
     /// FEATURE: References added to the project bring corresponding new .NET and F# items into scope.
     [<Test;Category("ReproX")>]
