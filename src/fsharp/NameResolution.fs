cea1aac316803cd0960f845ec12b0cbc70ac953d
Don Syme <dsyme@users.noreply.github.com>
2017-02-08 11:51:35 +0000

    don't keep parse results in background builder (#2378)
    Like #2377 this eliminates a source of unnecessarily retained memory - the parse trees from the background build. These are unused in the Visual F# Tools and even if they were needed it is easy to recreate them on-demand.
    
    The original code is being kept #if because the FSharp.Compiler.Service buget package component might choose to optionally allow the storage of these, so there's no need to delete the original code path yet
    
    * don't keep parse results in background builder
    
    * fix memory capture by TcSymbolUses
    
    * use struct tuples
    
    * fix test

--- src/fsharp/NameResolution.fs
+++ src/fsharp/NameResolution.fs
@@ -1393,17 +1393,17 @@ type TcResolutions
 type TcSymbolUses(g, capturedNameResolutions : ResizeArray<CapturedNameResolution>, formatSpecifierLocations: range[]) = 
     
     // Make sure we only capture the information we really need to report symbol uses
-    let cnrs = [| for cnr in capturedNameResolutions  -> cnr.Item, cnr.ItemOccurence, cnr.DisplayEnv, cnr.Range |]
+    let cnrs = [| for cnr in capturedNameResolutions  -> struct (cnr.Item, cnr.ItemOccurence, cnr.DisplayEnv, cnr.Range) |]
     let capturedNameResolutions = () 
     do ignore capturedNameResolutions // don't capture this!
 
     member this.GetUsesOfSymbol(item) = 
-        [| for (cnrItem,occ,denv,m) in cnrs do
+        [| for (struct (cnrItem,occ,denv,m)) in cnrs do
                if protectAssemblyExploration false (fun () -> ItemsAreEffectivelyEqual g item cnrItem) then
                   yield occ, denv, m |]
 
     member this.GetAllUsesOfSymbols() = 
-        [| for (cnrItem,occ,denv,m) in cnrs do
+        [| for (struct (cnrItem,occ,denv,m)) in cnrs do
               yield (cnrItem, occ, denv, m) |]
 
     member this.GetFormatSpecifierLocations() =  formatSpecifierLocations
