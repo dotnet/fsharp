529d6ba9a0ca49333522a634142afcae8b4ad2bf
Don Syme <donsyme@fastmail.fm>
2017-02-08 21:39:15 +0000

    fix build

--- src/fsharp/vs/IncrementalBuild.fs
+++ src/fsharp/vs/IncrementalBuild.fs
@@ -1606,7 +1606,7 @@ type IncrementalBuilder(frameworkTcImportsCache: FrameworkImportsCache, tcConfig
     let parseTreesNode              = Vector.Map "ParseTrees" ParseTask stampedFileNamesNode
     let tcStatesNode                = Vector.ScanLeft "TypeCheckingStates" TypeCheckTask initialTcAccNode stampedFileNamesNode
 #else
-    let tcStatesNode                = Vector.ScanLeft "TypeCheckingStates" (fun tcAcc n -> TypeCheckTask tcAcc (ParseTask n)) initialTcAccNode stampedFileNamesNode
+    let tcStatesNode                = Vector.ScanLeft "TypeCheckingStates" (fun ctok tcAcc n -> TypeCheckTask ctok tcAcc (ParseTask ctok n)) initialTcAccNode stampedFileNamesNode
 #endif
     let finalizedTypeCheckNode      = Vector.Demultiplex "FinalizeTypeCheck" FinalizeTypeCheckTask tcStatesNode
 
