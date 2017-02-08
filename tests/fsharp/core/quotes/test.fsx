7c92d12611e27d0b099f6271cb3ef93ed01c6592
Libo Zeng <ringil.sword@gmail.com>
2017-02-08 16:17:07 -0500

    fix typo

--- tests/fsharp/core/quotes/test.fsx
+++ tests/fsharp/core/quotes/test.fsx
@@ -1647,7 +1647,7 @@ module FlexibleUnionConstructorTests =
     test "check struct flexible union constructor"   
         (<@ testList |> A @> |> testFunction "A")
     
-    test "check struct flexible union constructor"   
+    test "check flexible union constructor"   
         (<@ testList |> B @> |> testFunction "B")
 
 
