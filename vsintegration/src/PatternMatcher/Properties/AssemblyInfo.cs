5eff2bca2b25a64db7940700993236efb2c8f732
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2017-02-15 15:17:16 +0300

    add ProvideCodeBase attribute

--- vsintegration/src/PatternMatcher/Properties/AssemblyInfo.cs
+++ vsintegration/src/PatternMatcher/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
 
 [assembly: AssemblyTitle("PatternMatcher")]
 [assembly: AssemblyDescription("")]
@@ -17,3 +18,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 //[assembly: AssemblyKeyFile(@"..\..\..\tests\fsharp\core\internalsvisible\key.snk")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("FSharp.Editor, PublicKey=002400000480000094000000060200000024000052534131000400000100010007D1FA57C4AED9F0A32E84AA0FAEFD0DE9E8FD6AEC8F87FB03766C834C99921EB23BE79AD9D5DCC1DD9AD236132102900B723CF980957FC4E177108FC607774F29E8320E92EA05ECE4E821C0A5EFE8F1645C4C0C93C1AB99285D622CAA652C1DFAD63D745D6F2DE5F17E5EAF0FC4963D261C8A12436518206DC093344D5AD293")]
+[assembly: ProvideCodeBase(CodeBase = @"$PackageFolder$\PatternMatcher.dll")]
