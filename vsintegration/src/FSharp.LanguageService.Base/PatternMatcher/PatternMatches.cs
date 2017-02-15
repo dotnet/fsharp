9d3d7ec6d73559b32acb1695717736fbafb92470
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2017-02-15 15:46:31 +0300

    move PatternMatcher code to FSharp.LanguageService.Base

---
+++ vsintegration/src/FSharp.LanguageService.Base/PatternMatcher/PatternMatches.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+
+namespace Microsoft.CodeAnalysis.PatternMatching
+{
+    /// <summary>
+    /// Pattern matching results returned when calling <see cref="PatternMatcher.GetMatches(string, string)"/>
+    /// Specifically, this type individually provides the matches produced when matching against the
+    /// 'candidate' text and the 'container' text.
+    /// </summary>
+    internal struct PatternMatches
+    {
+        public static readonly PatternMatches Empty = new PatternMatches(null, null);
+
+        public readonly PatternMatch[] CandidateMatches;
+        public readonly PatternMatch[] ContainerMatches;
+
+        public PatternMatches(PatternMatch[] candidateMatches,
+                              PatternMatch[] containerMatches = null)
+        {
+            CandidateMatches = candidateMatches ?? new PatternMatch[0];
+            ContainerMatches = containerMatches ?? new PatternMatch[0];
+        }
+
+        public bool IsEmpty => CandidateMatches.Length == 0 && ContainerMatches.Length == 0;
+
+        internal bool All(Func<PatternMatch, bool> predicate)
+        {
+            return CandidateMatches.All(predicate) && ContainerMatches.All(predicate);
+        }
+
+        internal bool Any(Func<PatternMatch, bool> predicate)
+        {
+            return CandidateMatches.Any(predicate) || ContainerMatches.Any(predicate);
+        }
+    }
+}
