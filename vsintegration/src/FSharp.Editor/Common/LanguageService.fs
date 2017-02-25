dotnet/fsharp

commit 1e7bb92d06c60f8cd642be499f755225b5c71cf0

fix for #2346 (#2408)
* Disconnect() project only after document closed

* naming

vsintegration/src/FSharp.Editor/Common/LanguageService.fs
@@ -341,11 +341,15 @@ and
             projectContext.AddSourceFile(fileName)
             
             let project = projectContext :?> AbstractProject
-            let document = project.GetCurrentDocumentFromPath(fileName)
+            let documentId = project.GetCurrentDocumentFromPath(fileName).Id
 
-            document.Closing.Add(fun _ ->  
-                projectInfoManager.RemoveSingleFileProject(projectId)
-                project.Disconnect())
+            let rec onDocumentClosed = EventHandler<DocumentEventArgs> (fun _ args ->
+                if args.Document.Id = documentId then
+                    projectInfoManager.RemoveSingleFileProject(projectId)
+                    project.Disconnect()
+                    workspace.DocumentClosed.RemoveHandler(onDocumentClosed)
+            )
+            workspace.DocumentClosed.AddHandler(onDocumentClosed)
 
     override this.ContentTypeName = FSharpCommonConstants.FSharpContentTypeName
     override this.LanguageName = FSharpCommonConstants.FSharpLanguageName

commit 61642ec5c14c2340d2d5abc5301a1f38ceb41030

fix bug introduced in #2408

vsintegration/src/FSharp.Editor/Common/LanguageService.fs
@@ -248,11 +248,26 @@ and
         if String.IsNullOrWhiteSpace projectFileName then projectFileName
         else Path.GetFileNameWithoutExtension projectFileName
 
+    let singleFileProjects = ConcurrentDictionary<_, AbstractProject>()
+
+    let tryRemoveSingleFileProject projectId =
+        match singleFileProjects.TryRemove(projectId) with
+        | true, project ->
+            projectInfoManager.RemoveSingleFileProject(projectId)
+            project.Disconnect()       
+        | _ -> ()
+
     override this.Initialize() =
         base.Initialize()
  
         this.Workspace.Options <- this.Workspace.Options.WithChangedOption(Completion.CompletionOptions.BlockForCompletionItems, FSharpCommonConstants.FSharpLanguageName, false)
         this.Workspace.Options <- this.Workspace.Options.WithChangedOption(Shared.Options.ServiceFeatureOnOffOptions.ClosedFileDiagnostic, FSharpCommonConstants.FSharpLanguageName, Nullable false)
+
+        this.Workspace.DocumentClosed.Add <| fun args ->
+            tryRemoveSingleFileProject args.Document.Project.Id
+        //because DocumentClosed doesn't fire when closing solution, we need also this:
+        Events.SolutionEvents.OnAfterCloseSolution.Add <| fun _ ->
+            for id in singleFileProjects.Keys do tryRemoveSingleFileProject id |> ignore
             
         Events.SolutionEvents.OnAfterOpenProject.Add <| fun args ->
             match args.Hierarchy with
@@ -333,15 +348,7 @@ and
             projectContext.AddSourceFile(fileName)
             
             let project = projectContext :?> AbstractProject
-            let documentId = project.GetCurrentDocumentFromPath(fileName).Id
-
-            let rec onDocumentClosed = EventHandler<DocumentEventArgs> (fun _ args ->
-                if args.Document.Id = documentId then
-                    projectInfoManager.RemoveSingleFileProject(projectId)
-                    project.Disconnect()
-                    workspace.DocumentClosed.RemoveHandler(onDocumentClosed)
-            )
-            workspace.DocumentClosed.AddHandler(onDocumentClosed)
+            singleFileProjects.[projectId] <- project
 
     override this.ContentTypeName = FSharpCommonConstants.FSharpContentTypeName
     override this.LanguageName = FSharpCommonConstants.FSharpLanguageName