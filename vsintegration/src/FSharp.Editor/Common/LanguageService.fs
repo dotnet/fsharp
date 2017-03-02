dotnet/fsharp

commit 1e7bb92d06c60f8cd642be499f755225b5c71cf0

fix for #2346 (#2408)
* Disconnect() project only after document closed

* naming

vsintegration/src/FSharp.Editor/Common/LanguageService.fs
@@ -341,11 +341,15 @@ and
             projectContext.AddSourceFile(fileName)
             
             let project = projectContext :?> AbstractProject
-            let document = project.GetCurrentDocumentFromPath(fileName)
+            let documentId = project.GetCurrentDocumentFromPath(fileName).Id
 
-            document.Closing.Add(fun _ ->  
-                projectInfoManager.RemoveSingleFileProject(projectId)
-                project.Disconnect())
+            let rec onDocumentClosed = EventHandler<DocumentEventArgs> (fun _ args ->
+                if args.Document.Id = documentId then
+                    projectInfoManager.RemoveSingleFileProject(projectId)
+                    project.Disconnect()
+                    workspace.DocumentClosed.RemoveHandler(onDocumentClosed)
+            )
+            workspace.DocumentClosed.AddHandler(onDocumentClosed)
 
     override this.ContentTypeName = FSharpCommonConstants.FSharpContentTypeName
     override this.LanguageName = FSharpCommonConstants.FSharpLanguageName

commit 0bb19968609a0747a8c3f04d93e4170b8ed45d19

Setup F# projects sooner after solution load (#2469)
* setup projects as soon as possible

* checker no longer used here

* mailbox added

* StartImmediate

* no async

* threadsafe ProjectInfoManager

* fix bug introduced in #2408

* setup projects after solution load

vsintegration/src/FSharp.Editor/Common/LanguageService.fs
@@ -125,10 +125,9 @@ type internal ProjectInfoManager
 
     /// Get the options for a project
     member this.TryGetOptionsForProject(projectId: ProjectId) = 
-        if projectTable.ContainsKey(projectId) then
-            Some(projectTable.[projectId])
-        else
-            None
+        match projectTable.TryGetValue(projectId) with
+        | true, options -> Some options 
+        | _ -> None
 
     /// Get the exact options for a document or project
     member this.TryGetOptionsForDocumentOrProject(document: Document) = async { 
@@ -137,9 +136,9 @@ type internal ProjectInfoManager
         // The options for a single-file script project are re-requested each time the file is analyzed.  This is because the
         // single-file project may contain #load and #r references which are changing as the user edits, and we may need to re-analyze
         // to determine the latest settings.  FCS keeps a cache to help ensure these are up-to-date.
-        if singleFileProjectTable.ContainsKey(projectId) then 
+        match singleFileProjectTable.TryGetValue(projectId) with
+        | true, (loadTime,_) ->
           try
-            let loadTime,_ = singleFileProjectTable.[projectId]
             let fileName = document.FilePath
             let! cancellationToken = Async.CancellationToken
             let! sourceText = document.GetTextAsync(cancellationToken)
@@ -149,18 +148,16 @@ type internal ProjectInfoManager
           with ex -> 
             Assert.Exception(ex)
             return None
-        else return this.TryGetOptionsForProject(projectId) 
+        | _ -> return this.TryGetOptionsForProject(projectId) 
      }
 
     /// Get the options for a document or project relevant for syntax processing.
     /// Quicker then TryGetOptionsForDocumentOrProject as it doesn't need to recompute the exact project options for a script.
     member this.TryGetOptionsForEditingDocumentOrProject(document: Document) = 
         let projectId = document.Project.Id
-        if singleFileProjectTable.ContainsKey(projectId) then 
-            let _loadTime, originalOptions = singleFileProjectTable.[projectId]
-            Some originalOptions
-        else 
-            this.TryGetOptionsForProject(projectId) 
+        match singleFileProjectTable.TryGetValue(projectId) with 
+        | true, (_loadTime, originalOptions) -> Some originalOptions
+        | _ -> this.TryGetOptionsForProject(projectId) 
 
 // Used to expose FSharpChecker/ProjectInfo manager to diagnostic providers
 // Diagnostic providers can be executed in environment that does not use MEF so they can rely only
@@ -245,32 +242,59 @@ and
     internal FSharpLanguageService(package : FSharpPackage) =
     inherit AbstractLanguageService<FSharpPackage, FSharpLanguageService>(package)
 
-    let checkerProvider = package.ComponentModel.DefaultExportProvider.GetExport<FSharpCheckerProvider>().Value
     let projectInfoManager = package.ComponentModel.DefaultExportProvider.GetExport<ProjectInfoManager>().Value
 
     let projectDisplayNameOf projectFileName = 
         if String.IsNullOrWhiteSpace projectFileName then projectFileName
         else Path.GetFileNameWithoutExtension projectFileName
 
-    let openedProjects = Queue<IProvideProjectSite>()
+    let singleFileProjects = ConcurrentDictionary<_, AbstractProject>()
 
-    do
-      Events.SolutionEvents.OnAfterOpenProject.Add (fun (args: Events.OpenProjectEventArgs) -> 
-        match args.Hierarchy with
-        | :? IProvideProjectSite as siteProvider ->
-            openedProjects.Enqueue(siteProvider)            
-        | _ -> ())
+    let tryRemoveSingleFileProject projectId =
+        match singleFileProjects.TryRemove(projectId) with
+        | true, project ->
+            projectInfoManager.RemoveSingleFileProject(projectId)
+            project.Disconnect()
+        | _ -> ()
+
+    override this.Initialize() =
+        base.Initialize()
+
+        this.Workspace.Options <- this.Workspace.Options.WithChangedOption(Completion.CompletionOptions.BlockForCompletionItems, FSharpCommonConstants.FSharpLanguageName, false)
+        this.Workspace.Options <- this.Workspace.Options.WithChangedOption(Shared.Options.ServiceFeatureOnOffOptions.ClosedFileDiagnostic, FSharpCommonConstants.FSharpLanguageName, Nullable false)
+
+        this.Workspace.DocumentClosed.Add <| fun args ->
+            tryRemoveSingleFileProject args.Document.Project.Id 
+            
+        Events.SolutionEvents.OnAfterCloseSolution.Add <| fun _ ->
+            singleFileProjects.Keys |> Seq.iter tryRemoveSingleFileProject
+
+        let ctx = System.Threading.SynchronizationContext.Current
         
-    member this.BatchSetupProjects() =    
-        if openedProjects.Count > 0 then    
-            let workspace = package.ComponentModel.GetService<VisualStudioWorkspaceImpl>()
-            for siteProvider in openedProjects do
-                this.SetupProjectFile(siteProvider, workspace)
-            openedProjects.Clear()
+        let rec setupProjectsAfterSolutionOpen() =
+            async {
+                use openedProjects = MailboxProcessor.Start <| fun inbox ->
+                    async { 
+                        // waits for AfterOpenSolution and then starts projects setup
+                        do! Async.AwaitEvent Events.SolutionEvents.OnAfterOpenSolution |> Async.Ignore
+                        while true do
+                            let! siteProvider = inbox.Receive()
+                            do! Async.SwitchToContext ctx
+                            this.SetupProjectFile(siteProvider, this.Workspace) }
+
+                use _ = Events.SolutionEvents.OnAfterOpenProject |> Observable.subscribe ( fun args ->
+                    match args.Hierarchy with
+                    | :? IProvideProjectSite as siteProvider -> openedProjects.Post(siteProvider)
+                    | _ -> () )
+
+                do! Async.AwaitEvent Events.SolutionEvents.OnAfterCloseSolution |> Async.Ignore
+                do! setupProjectsAfterSolutionOpen() 
+            }
+        setupProjectsAfterSolutionOpen() |> Async.StartImmediate
         
     /// Sync the information for the project 
     member this.SyncProject(project: AbstractProject, projectContext: IWorkspaceProjectContext, site: IProjectSite, forceUpdate) =
-
+      async {
         let hashSetIgnoreCase x = new HashSet<string>(x, StringComparer.OrdinalIgnoreCase)
         let updatedFiles = site.SourceFilesOnDisk() |> hashSetIgnoreCase
         let workspaceFiles = project.GetCurrentDocuments() |> Seq.map(fun file -> file.FilePath) |> hashSetIgnoreCase
@@ -290,6 +314,7 @@ and
         // update the cached options
         if updated then
             projectInfoManager.UpdateProjectInfo(project.Id, site, project.Workspace)
+      } |> Async.Start
 
     member this.SetupProjectFile(siteProvider: IProvideProjectSite, workspace: VisualStudioWorkspaceImpl) =
         let  rec setup (site: IProjectSite) =
@@ -341,15 +366,7 @@ and
             projectContext.AddSourceFile(fileName)
             
             let project = projectContext :?> AbstractProject
-            let documentId = project.GetCurrentDocumentFromPath(fileName).Id
-
-            let rec onDocumentClosed = EventHandler<DocumentEventArgs> (fun _ args ->
-                if args.Document.Id = documentId then
-                    projectInfoManager.RemoveSingleFileProject(projectId)
-                    project.Disconnect()
-                    workspace.DocumentClosed.RemoveHandler(onDocumentClosed)
-            )
-            workspace.DocumentClosed.AddHandler(onDocumentClosed)
+            singleFileProjects.[projectId] <- project
 
     override this.ContentTypeName = FSharpCommonConstants.FSharpContentTypeName
     override this.LanguageName = FSharpCommonConstants.FSharpLanguageName
@@ -362,11 +379,8 @@ and
 
     override this.SetupNewTextView(textView) =
         base.SetupNewTextView(textView)
-        let workspace = package.ComponentModel.GetService<VisualStudioWorkspaceImpl>()
+
         let textViewAdapter = package.ComponentModel.GetService<IVsEditorAdaptersFactoryService>()
-        
-        workspace.Options <- workspace.Options.WithChangedOption(Completion.CompletionOptions.BlockForCompletionItems, FSharpCommonConstants.FSharpLanguageName, false)
-        workspace.Options <- workspace.Options.WithChangedOption(Shared.Options.ServiceFeatureOnOffOptions.ClosedFileDiagnostic, FSharpCommonConstants.FSharpLanguageName, Nullable false)
                
         match textView.GetBuffer() with
         | (VSConstants.S_OK, textLines) ->
@@ -375,38 +389,11 @@ and
             | Some (hier, _) ->
                 match hier with
                 | :? IProvideProjectSite as siteProvider when not (IsScript(filename)) -> 
-                    this.SetupProjectFile(siteProvider, workspace)
+                    this.SetupProjectFile(siteProvider, this.Workspace)
                 | _ -> 
                     let fileContents = VsTextLines.GetFileContents(textLines, textViewAdapter)
-                    this.SetupStandAloneFile(filename, fileContents, workspace, hier)
+                    this.SetupStandAloneFile(filename, fileContents, this.Workspace, hier)
             | _ -> ()
         | _ -> ()
 
-        // This is the second half of the bridge between the F# IncrementalBuild analysis engine and the Roslyn analysis
-        // engine.  When a document gets the focus, we call FSharpLanguageService.Checker.StartBackgroundCompile for the
-        // project containing that file. This ensures the F# IncrementalBuild engine starts analyzing the project.
-        let wpfTextView = textViewAdapter.GetWpfTextView(textView)
-        match wpfTextView.TextBuffer.Properties.TryGetProperty<ITextDocument>(typeof<ITextDocument>) with
-        | true, textDocument ->
-            let filePath = textDocument.FilePath
-            let onGotFocus = new EventHandler(fun _ _ ->
-                for documentId in workspace.CurrentSolution.GetDocumentIdsWithFilePath(filePath) do 
-                    let document = workspace.CurrentSolution.GetDocument(documentId)
-                    match projectInfoManager.TryGetOptionsForEditingDocumentOrProject(document) with 
-                    | Some options -> checkerProvider.Checker.StartBackgroundCompile(options)
-                    | None -> ()
-            )
-            let rec onViewClosed = new EventHandler(fun _ _ -> 
-                wpfTextView.GotAggregateFocus.RemoveHandler(onGotFocus)
-                wpfTextView.Closed.RemoveHandler(onViewClosed)
-            )
-            wpfTextView.GotAggregateFocus.AddHandler(onGotFocus)
-            wpfTextView.Closed.AddHandler(onViewClosed)
-        | _ -> ()
-
-        // When the text view is opened, setup all remaining projects on the background.
-        async {
-            do this.BatchSetupProjects()
-        }
-        |> Async.StartImmediate
-            
+      