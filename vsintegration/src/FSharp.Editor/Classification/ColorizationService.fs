b854d90f68bd6b84a1514db189494bae2ff0eab3
Vasily Kirichenko <vaskir@gmail.com>
2017-03-04 01:14:13 +0300

    Fix wrong FSX colorization when #r directives change (#2521)
    * add logging
    
    * use TryGetOptionsForDocumentOrProject instead of TryGetOptionsForEditingDocumentOrProject in Classification Service to avoid passing stale options to FCS
    
    * Revert "add logging"
    
    This reverts commit 72b84216a7bdcf4be45f8ae8da7f1d31f1f0b086.

--- vsintegration/src/FSharp.Editor/Classification/ColorizationService.fs
+++ vsintegration/src/FSharp.Editor/Classification/ColorizationService.fs
@@ -35,7 +35,7 @@ type internal FSharpColorizationService
 
         member this.AddSemanticClassificationsAsync(document: Document, textSpan: TextSpan, result: List<ClassifiedSpan>, cancellationToken: CancellationToken) =
             asyncMaybe {
-                let! options = projectInfoManager.TryGetOptionsForEditingDocumentOrProject(document)
+                let! options = projectInfoManager.TryGetOptionsForDocumentOrProject(document)
                 let! sourceText = document.GetTextAsync(cancellationToken)
                 let! _, _, checkResults = checkerProvider.Checker.ParseAndCheckDocument(document, options, sourceText = sourceText, allowStaleResults = false) 
                 // it's crucial to not return duplicated or overlapping `ClassifiedSpan`s because Find Usages service crashes.
