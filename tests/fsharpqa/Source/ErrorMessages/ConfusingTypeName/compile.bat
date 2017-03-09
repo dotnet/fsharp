e95d0530621333703ca1b173fdfcbaf3b015db79
Gauthier Segay <gauthier.segay@gmail.com>
2017-03-09 15:39:04 +0100

    try to fix fsharpqa

--- tests/fsharpqa/Source/ErrorMessages/ConfusingTypeName/compile.bat
+++ tests/fsharpqa/Source/ErrorMessages/ConfusingTypeName/compile.bat
@@ -1,6 +1,6 @@
 rem if you want to try it out without running the whole suite
-csc liba-and-b.cs -t:library -out:liba.dll
-csc liba-and-b.cs -t:library -out:libb.dll
-fsc libc.fs --target:library -r:liba.dll --out:libc.dll
-fsc libd.fs --target:library -r:libb.dll --out:libd.dll
-fsc app.fs -r:liba.dll -r:libb.dll -r:libc.dll -r:libd.dll
+csc -t:library -out:liba.dll liba-and-b.cs
+csc -t:library -out:libb.dll liba-and-b.cs
+fsc --target:library -r:liba.dll --out:libc.dll libc.fs
+fsc --target:library -r:libb.dll --out:libd.dll libd.fs
+fsc -r:liba.dll -r:libb.dll -r:libc.dll -r:libd.dll app.fs
