8db3f52430969cb29b5662e21023ac6c0f22f8b8
Paul Westcott <p.westcott@gmail.com>
2017-03-15 20:38:33 +1100

    Copied Seq tests and converted to ISeq
    Due to missing functionality, this process will probably be done again
    some time, but this is just a rough sketch to get some tests up and
    running

---
+++ src/fsharp/FSharp.Core.Unittests/FSharp.Core/Microsoft.FSharp.Collections/ISeqProperties.fs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+module FSharp.Core.Unittests.FSharp_Core.Microsoft_FSharp_Collections.ISeqProperties
+
+open System
+open System.Collections.Generic
+open NUnit.Framework
+open FsCheck
+open Utils
+
+let sortByStable<'a when 'a : comparison> (xs : 'a []) =
+    let indexed = xs|> ISeq.ofSeq |> ISeq.indexed
+    let sorted = indexed |> ISeq.sortBy snd
+    isStable sorted
+
+[<Test>]
+let ``ISeq.sortBy is stable`` () =
+    Check.QuickThrowOnFailure sortByStable<int>
+    Check.QuickThrowOnFailure sortByStable<string>
+
+let sortWithStable<'a when 'a : comparison> (xs : 'a []) =
+    let indexed = xs |> ISeq.ofSeq |> ISeq.indexed |> Seq.toList
+    let sorted = indexed |> ISeq.ofSeq |> ISeq.sortWith (fun x y -> compare (snd x) (snd y))
+    isStable sorted
+    
+[<Test>]
+let ``ISeq.sortWithStable is stable`` () =
+    Check.QuickThrowOnFailure sortWithStable<int>
+    Check.QuickThrowOnFailure sortWithStable<string>
+    
+let distinctByStable<'a when 'a : comparison> (xs : 'a []) =
+    let indexed = xs|> ISeq.ofSeq |> ISeq.indexed
+    let sorted = indexed |> ISeq.distinctBy snd
+    isStable sorted
+    
+[<Test>]
+let ``ISeq.distinctBy is stable`` () =
+    Check.QuickThrowOnFailure distinctByStable<int>
+    Check.QuickThrowOnFailure distinctByStable<string>
