7a611eaf32c732bf59634331ffc8973a18db7a5b
Vasily Kirichenko <vaskir@gmail.com>
2017-03-17 21:20:04 +0300

    fix compilation

--- vsintegration/src/FSharp.LanguageService/Intellisense.fs
+++ vsintegration/src/FSharp.LanguageService/Intellisense.fs
@@ -517,7 +517,7 @@ type internal FSharpIntellisenseInfo
                                 let oldTextSnapshot = oldTextSnapshotInfo :?> ITextSnapshot
                                 hasTextChangedSinceLastTypecheck (textSnapshot, oldTextSnapshot, Range.Range.toZ range)
 
-                            let! decls = typedResults.GetDeclarationListInfo(untypedParseInfoOpt, Range.Line.fromZ line, col, lineText, fst plid, snd plid, detectTextChange) 
+                            let! decls = typedResults.GetDeclarationListInfo(untypedParseInfoOpt, Range.Line.fromZ line, col, lineText, fst plid, snd plid, [], detectTextChange) 
                             return (new FSharpDeclarations(documentationBuilder, decls.Items, reason) :> Declarations) 
                     else
                         // no TypeCheckInfo in ParseResult.
