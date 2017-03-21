6d6c8f3aa4d73edf8d72d3fa3c85ade3b9942148
Cameron Taggart <cameron@tachyus.com>
2017-03-21 17:40:43 -0500

    2017

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
-echo "VS150COMNTOOLS: %VS150COMNTOOLS%"
-dir "%VS150COMNTOOLS%\..\..\MSBuild"
+REM echo "VS150COMNTOOLS: %VS150COMNTOOLS%"
+REM dir "%VS150COMNTOOLS%\..\..\MSBuild"
 
 REM dir "C:\Program Files (x86)\"
-dir "C:\Program Files (x86)\Microsoft Visual Studio"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2017"
