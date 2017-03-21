6f2d698b265607d5288cc8f01e4046a7a14a789b
Cameron Taggart <cameron@tachyus.com>
2017-03-21 17:47:32 -0500

    Enterprise

--- build.cmd
+++ build.cmd
@@ -2,4 +2,4 @@ REM echo "VS150COMNTOOLS: %VS150COMNTOOLS%"
 REM dir "%VS150COMNTOOLS%\..\..\MSBuild"
 
 REM dir "C:\Program Files (x86)\"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2017"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise"
