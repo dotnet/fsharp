f0c689dda06fa0b1fd64f2e6645db59e9c12d031
Kevin Ransom <kevinr@microsoft.com>
2017-03-22 05:00:22 -0500

    rework build to work with bare command window

--- netci.groovy
+++ netci.groovy
@@ -68,8 +68,6 @@ def static getBuildJobName(def configuration, def os) {
 echo *** Build Visual F# Tools ***
 
 SET VS150COMNTOOLS=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\Common7\\Tools\\
-SET VSSDK150Install=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\VSSDK\\
-SET VSSDKInstall=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\VSSDK\\
 
 .\\build.cmd ${buildFlavor} ${build_args}""")
                     }
