d23aa7222a7eef676d069c175b68ccde7fba00f3
Cameron Taggart <cameron@tachyus.com>
2017-03-21 21:59:31 -0500

    where msbuild

--- build.cmd
+++ build.cmd
@@ -1,15 +1,16 @@
+where msbuild
 echo "VS150COMNTOOLS: %VS150COMNTOOLS%"
 REM https://github.com/fsharp/FAKE/blob/master/src/app/FakeLib/MSBuildHelper.fs
-if "%VS150COMNTOOLS%" EQU "" if exists "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" (
+if "%VS150COMNTOOLS%" EQU "" if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" (
     call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 )
-if "%VS150COMNTOOLS%" EQU "" if exists "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" (
+if "%VS150COMNTOOLS%" EQU "" if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" (
     call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
 )
-if "%VS150COMNTOOLS%" EQU "" if exists "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" (
+if "%VS150COMNTOOLS%" EQU "" if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" (
     call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 )
-if "%VS150COMNTOOLS%" EQU "" if exists "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" (
+if "%VS150COMNTOOLS%" EQU "" if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" (
     call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 )
 if "%VS150COMNTOOLS%" EQU "" (
@@ -17,5 +18,5 @@ if "%VS150COMNTOOLS%" EQU "" (
     exit /B 1
 )
 echo "VS150COMNTOOLS: %VS150COMNTOOLS%"
-msbuild /version
 where msbuild
+msbuild /version
