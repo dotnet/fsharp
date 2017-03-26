ab2feffb1c3ce11c2f1b966f3f36ff6bb9ac1fef
Cameron Taggart <cameron@tachyus.com>
2017-03-26 09:07:11 -0500

    remove debugging

--- build.cmd
+++ build.cmd
@@ -640,15 +640,10 @@ echo WHERE_ARG_NUNIT=!WHERE_ARG_NUNIT!
 set NUNITPATH=%~dp0tests\fsharpqa\testenv\bin\nunit\
 set NUNIT3_CONSOLE=%~dp0packages\NUnit.Console.3.0.0\tools\nunit3-console.exe
 
-echo aaa
-if "" == "%link_exe%" (if exist "%VCToolsInstallDir%bin\HostX64\x86\link.exe" (
-    echo bbb
+if "%link_exe%" == "" (if exist "%VCToolsInstallDir%bin\HostX64\x86\link.exe" (
     set link_exe="%VCToolsInstallDir%bin\HostX64\x86\link.exe"
-    echo ccc
 ))
-echo ddd
-echo link_exe=%link_exe%
-if "" == "%link_exe%" (
+if "%link_exe%" == "" (
     set link_exe="%~dp0packages\VisualCppTools.14.0.24519-Pre\lib\native\bin\link.exe"
     if not exist "%~dp0packages\VisualCppTools.14.0.24519-Pre\lib\native\bin\link.exe" (
         %_nugetexe% install -source https://www.myget.org/F/fsharp-daily/api/v3/index.json VisualCppTools -version 14.0.24519-Pre -out packages
@@ -658,7 +653,6 @@ if not exist "%link_exe%" (
     echo Error: failed to find link.exe
     goto :failure
 )
-echo link_exe=%link_exe%
 
 if /I not "%single_threaded%" == "true" (set PARALLEL_ARG=-procs:%NUMBER_OF_PROCESSORS%) else set PARALLEL_ARG=-procs:0
 
@@ -668,6 +662,7 @@ if not exist "%RESULTSDIR%" (mkdir "%RESULTSDIR%")
 
 ECHO FSCBINPATH=%FSCBINPATH%
 ECHO RESULTSDIR=%RESULTSDIR%
+echo link_exe=%link_exe%
 ECHO NUNIT3_CONSOLE=%NUNIT3_CONSOLE%
 ECHO NUNITPATH=%NUNITPATH%
 
