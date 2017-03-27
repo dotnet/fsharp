b4f90fb6d2350d947dd07cff4c474bfaab61526a
Vasily Kirichenko <vaskir@gmail.com>
2017-03-28 01:07:24 +0300

    RemoveUnusedOpens Code Fix removes all occurrences in entire document (#2706)

--- vsintegration/src/FSharp.Editor/CodeFix/RemoveUnusedOpens.fs
+++ vsintegration/src/FSharp.Editor/CodeFix/RemoveUnusedOpens.fs
@@ -12,29 +12,47 @@ open Microsoft.CodeAnalysis.Text
 open Microsoft.CodeAnalysis.CodeFixes
 open Microsoft.CodeAnalysis.CodeActions
 
+open Microsoft.FSharp.Compiler.Range
+
 [<ExportCodeFixProvider(FSharpCommonConstants.FSharpLanguageName, Name = "RemoveUnusedOpens"); Shared>]
-type internal FSharpRemoveUnusedOpensCodeFixProvider() =
+type internal FSharpRemoveUnusedOpensCodeFixProvider
+    [<ImportingConstructor>]
+    (
+        checkerProvider: FSharpCheckerProvider, 
+        projectInfoManager: ProjectInfoManager
+    ) =
     inherit CodeFixProvider()
     let fixableDiagnosticIds = [IDEDiagnosticIds.RemoveUnnecessaryImportsDiagnosticId]
         
-    let createCodeFix (title: string, context: CodeFixContext, textChange: TextChange) =
+    let createCodeFix (title: string, context: CodeFixContext) =
         CodeAction.Create(
             title,
             (fun (cancellationToken: CancellationToken) ->
-                async {
-                    let! sourceText = context.Document.GetTextAsync()
-                    return context.Document.WithText(sourceText.WithChanges(textChange))
-                } |> CommonRoslynHelpers.StartAsyncAsTask(cancellationToken)),
+                asyncMaybe {
+                    let document = context.Document
+                    let! sourceText = document.GetTextAsync()
+                    let checker = checkerProvider.Checker
+                    let! options = projectInfoManager.TryGetOptionsForEditingDocumentOrProject(document)
+                    let! unusedOpens = UnusedOpensDiagnosticAnalyzer.GetUnusedOpenRanges(document, options, checker)
+                    let changes =
+                        unusedOpens
+                        |> List.map (fun m ->
+                            let span = sourceText.Lines.[Line.toZ m.StartLine].SpanIncludingLineBreak
+                            TextChange(span, ""))
+                        |> List.toArray
+
+                    return document.WithText(sourceText.WithChanges(changes))
+                }
+                |> Async.map (Option.defaultValue context.Document)
+                |> CommonRoslynHelpers.StartAsyncAsTask(cancellationToken)),
             title)
 
     override __.FixableDiagnosticIds = Seq.toImmutableArray fixableDiagnosticIds
 
     override __.RegisterCodeFixesAsync context : Task =
         async {
-            let! sourceText = context.Document.GetTextAsync()
-            let line = sourceText.Lines.GetLineFromPosition context.Span.Start
             let diagnostics = context.Diagnostics |> Seq.filter (fun x -> fixableDiagnosticIds |> List.contains x.Id) |> Seq.toImmutableArray
-            context.RegisterCodeFix(createCodeFix(SR.RemoveUnusedOpens.Value, context, TextChange(line.SpanIncludingLineBreak, "")), diagnostics)
+            context.RegisterCodeFix(createCodeFix(SR.RemoveUnusedOpens.Value, context), diagnostics)
         } |> CommonRoslynHelpers.StartAsyncUnitAsTask(context.CancellationToken)
 
     override __.GetFixAllProvider() = WellKnownFixAllProviders.BatchFixer
