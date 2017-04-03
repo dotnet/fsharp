caa828b8949ae2859611d7107ea802a47e3af79c
Kevin Ransom (msft) <codecutter.fsharp@hotmail.com>
2017-04-02 19:03:56 -0700

    Update Add-VisualStudioComponents.ps1
    Review comments

--- scripts/Add-VisualStudioComponents.ps1
+++ scripts/Add-VisualStudioComponents.ps1
@@ -7,25 +7,6 @@ if (-not (Get-Module -ListAvailable -Name VSSetup)) {
 }
 $vs = Get-VSSetupInstance
 
-# Microsoft.Net.Component.4.6.2.SDK: .NET Framework 4.6.2 SDK
-# Microsoft.Net.Core.Component.SDK: .NET Core 1.0 - 1.1 development tools
-# Microsoft.Component.NetFX.Core.Runtime: .NET Core runtime
-# Microsoft.VisualStudio.Component.FSharp: F# language support
-# Microsoft.VisualStudio.Component.VC.ATL: Visual C++ ATL support
-# Microsoft.VisualStudio.Component.VSSDK: Visual Studio SDK
-# Microsoft.Component.MSBuild: MSBuild
-# Microsoft.VisualStudio.Component.NuGet: NuGet package manager
-# Microsoft.VisualStudio.Component.VC.Tools.x86.x64: VC++ 2017 v141 toolset (x86,x64)
-# Microsoft.VisualStudio.Component.Windows10SDK.14393: Windows 10 SDK (10.0.14393.0)
-# Microsoft.VisualStudio.Component.Windows81SDK: Windows 8.1 SDK
-# Microsoft.VisualStudio.Component.PortableLibrary: .NET Framework PortableLibrary targeting pack
-# Microsoft.Net.Component.4.TargetingPack: .NET Framework 4 targeting pack
-# Microsoft.Net.Component.4.5.TargetingPack: .NET Framework 4.5 targeting pack
-# Microsoft.Net.Component.4.5.1.TargetingPack: .NET Framework 4.5.1 targeting pack
-# Microsoft.Net.Component.4.6.TargetingPack: .NET Framework 4.6 targeting pack
-# Microsoft.Net.Component.4.5.1.TargetingPack: .NET Framework 4.5.1 targeting pack
-# Microsoft.Net.Component.4.6.2.TargetingPack: .NET Framework 4.6.2 targeting pack
-
 & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vs_installershell.exe" modify `
     --installPath $vs.InstallationPath `
     --add Microsoft.Net.Component.4.6.2.SDK `
@@ -39,7 +20,7 @@ $vs = Get-VSSetupInstance
     --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
     --add Microsoft.VisualStudio.Component.Windows10SDK.14393 `
     --add Microsoft.VisualStudio.Component.Windows81SDK `
-	  --add Microsoft.VisualStudio.Component.PortableLibrary `
+    --add Microsoft.VisualStudio.Component.PortableLibrary `
     --add Microsoft.Net.Component.4.TargetingPack `
     --add Microsoft.Net.Component.4.5.TargetingPack `
     --add Microsoft.Net.Component.4.5.1.TargetingPack `
