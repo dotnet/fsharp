20a367adc8747b53ebf053e5bb9152b305d3a017
Paul Westcott <p.westcott@gmail.com>
2017-04-20 17:55:02 +1000

    Used LanguagePrimitives.IntrinsicOperators.(&&)

--- src/fsharp/FSharp.Core/prim-types.fs
+++ src/fsharp/FSharp.Core/prim-types.fs
@@ -3709,6 +3709,7 @@ namespace Microsoft.FSharp.Collections.SeqComposition
     open Microsoft.FSharp.Core
     open Microsoft.FSharp.Collections
     open BasicInlinedOperations
+    open LanguagePrimitives.IntrinsicOperators
 
     type PipeIdx = int
 
@@ -3745,11 +3746,10 @@ namespace Microsoft.FSharp.Collections.SeqComposition
             member this.StopFurtherProcessing pipeIdx = 
                 let currentIdx = haltedIdx
                 haltedIdx <- pipeIdx
-                if currentIdx = 0 then
-                    if haltedIdx <> 0 then
-                        match listeners with
-                        | null -> ()
-                        | a -> a.Invoke pipeIdx
+                if currentIdx = 0 && haltedIdx <> 0 then
+                    match listeners with
+                    | null -> ()
+                    | a -> a.Invoke pipeIdx
 
             member this.ListenForStopFurtherProcessing action =
                 listeners <- Delegate.Combine (listeners, action) :?> Action<PipeIdx>
