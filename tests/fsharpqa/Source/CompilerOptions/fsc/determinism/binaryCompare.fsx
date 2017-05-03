468e0d5ab73ce23016b8ab44954a51fd119a0e98
David Glassborow <dave@conceptfirst.com>
2017-05-03 17:27:53 +0100

    Allow PRECMD to call FSI more than once, and use this for copying files and pausing to prevent race-condition

--- tests/fsharpqa/Source/CompilerOptions/fsc/determinism/binaryCompare.fsx
+++ tests/fsharpqa/Source/CompilerOptions/fsc/determinism/binaryCompare.fsx
@@ -1,19 +1,16 @@
-// Will check all filetypes are exactly the same for to staring names, except for source code
-// so given 'dummy' and 'dummy2', and 'dummy.exe' and 'dummy.pdb' exist in the current directory
+// sgiven 'dummy' and 'dummy2', and 'dummy.exe' and 'dummy.pdb' exist in the current directory
 // this will check 'dummy.exe' and 'dummy2.exe are exactly the same, and the same for the 'pdb' files
-// expects 3 args, first filename, second filename, whether files should be exactly the same
-let firstFilename = fsi.CommandLineArgs.[1]
-let secondFilename = fsi.CommandLineArgs.[2]
-let shouldBeSame = bool.Parse fsi.CommandLineArgs.[3]
+// expects 1 arg: whether files should be exactly the same
+let shouldBeSame = bool.Parse fsi.CommandLineArgs.[1]
 
 let areSame (first,second) = 
     let load = System.IO.File.ReadAllBytes
     (load first) = (load second)
 
 let filePairsToCheck = 
-    System.IO.Directory.EnumerateFiles(__SOURCE_DIRECTORY__, firstFilename + ".*") 
-    |> Seq.filter (fun (s:string) -> s.EndsWith(".fs") |> not )                              // Don't check the source code
-    |> Seq.map (fun (f:string) -> f, f.Replace(firstFilename,secondFilename))
+    System.IO.Directory.EnumerateFiles(__SOURCE_DIRECTORY__, "dummy.*") 
+    |> Seq.filter (fun s -> s.EndsWith(".fs") |> not )                              // Don't check the source code
+    |> Seq.map (fun f -> f, f.Replace("dummy","dummy2"))
 
 let compareFiles pair =
     let correct = areSame pair = shouldBeSame
