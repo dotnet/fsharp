fe086b3b2508177426f1f8ba8e89ae8f72cf1c2b
dsyme <dsyme@MSRC-4079076>
2017-05-11 00:25:26 +0100

    Fix configuration changes

--- src/fsharp/CompileOps.fs
+++ src/fsharp/CompileOps.fs
@@ -2702,8 +2702,6 @@ type TcConfig private (data : TcConfigBuilder,validate:bool) =
         | Some(fslibFilename) ->
             let filename = ComputeMakePathAbsolute data.implicitIncludeDir fslibFilename
             try 
-                use ilReader = OpenILBinary(filename,data.optimizeForMemory,data.openBinariesInMemory,None,None, data.shadowCopyReferences)
-                checkFSharpBinaryCompatWithMscorlib filename ilReader.ILAssemblyRefs ilReader.ILModuleDef.ManifestOfAssembly.Version rangeStartup;
                 let fslibRoot = Path.GetDirectoryName(FileSystem.GetFullPathShim(filename))
                 fslibRoot (* , sprintf "v%d.%d" v1 v2 *)
             with e -> 
