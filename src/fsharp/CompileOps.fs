50a4575e9df57e1a913c4e475c711945f786585f
dsyme <dsyme@MSRC-4079076>
2017-05-24 15:06:18 +0100

    fix tests

--- src/fsharp/CompileOps.fs
+++ src/fsharp/CompileOps.fs
@@ -3555,7 +3555,7 @@ type TcAssemblyResolutions(results : AssemblyResolution list, unresolved : Unres
             let assumeDotNetFramework = primaryReference.SimpleAssemblyNameIs("mscorlib")
             if tcConfig.framework then 
                 for s in DefaultReferencesForScriptsAndOutOfProjectSources(assumeDotNetFramework) do 
-                    yield AssemblyReference(rangeStartup,s+".dll",None)
+                    yield AssemblyReference(rangeStartup,(if s.EndsWith(".dll") then s else s+".dll"),None)
 
             if tcConfig.useFsiAuxLib then
                 let name = Path.Combine(tcConfig.fsharpBinariesDir, GetFsiLibraryName() + ".dll")
