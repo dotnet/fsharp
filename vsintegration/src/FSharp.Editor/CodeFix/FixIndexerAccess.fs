07e08e20a0972f72df878e634c3381158184b790
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-06-01 10:42:52 +0200

    Add FixIndexerAccessCodeFix

---
+++ vsintegration/src/FSharp.Editor/CodeFix/FixIndexerAccess.fs
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace rec Microsoft.VisualStudio.FSharp.Editor
+
+open System
+open System.Composition
+open System.Collections.Immutable
+open System.Threading
+open System.Threading.Tasks
+
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.Text
+open Microsoft.CodeAnalysis.CodeFixes
+open Microsoft.CodeAnalysis.CodeActions
+
+[<ExportCodeFixProvider(FSharpConstants.FSharpLanguageName, Name = "FixIndexerAccess"); Shared>]
+type internal FSharpFixIndexerAccessCodeFixProvider() =
+    inherit CodeFixProvider()
+    let fixableDiagnosticIds = set ["FS3217"]
+        
+    let createCodeFix (title: string, context: CodeFixContext, textChange: TextChange) =
+        CodeAction.Create(
+            title,
+            (fun (cancellationToken: CancellationToken) ->
+                async {
+                    let! cancellationToken = Async.CancellationToken
+                    let! sourceText = context.Document.GetTextAsync(cancellationToken) |> Async.AwaitTask
+                    return context.Document.WithText(sourceText.WithChanges(textChange))
+                } |> RoslynHelpers.StartAsyncAsTask(cancellationToken)),
+            title)
+
+    override __.FixableDiagnosticIds = Seq.toImmutableArray fixableDiagnosticIds
+
+    override __.RegisterCodeFixesAsync context : Task =
+        async {
+            let diagnostics = 
+                context.Diagnostics 
+                |> Seq.filter (fun x -> fixableDiagnosticIds |> Set.contains x.Id)
+                |> Seq.toList
+            if not (List.isEmpty diagnostics) then
+                let! sourceText = context.Document.GetTextAsync() |> Async.AwaitTask
+
+                diagnostics
+                |> Seq.iter (fun diagnostic ->
+                    let diagnostics = ImmutableArray.Create diagnostic
+
+                    let codefix = 
+                        createCodeFix(
+                            FSComp.SR.addIndexerDot(), 
+                            context,
+                            TextChange(context.Span, sourceText.GetSubText(context.Span).ToString() + "."))
+                    context.RegisterCodeFix(codefix, diagnostics))
+        } |> RoslynHelpers.StartAsyncUnitAsTask(context.CancellationToken)
