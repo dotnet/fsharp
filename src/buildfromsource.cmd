f3689b92ed148ad26cd06609e72dfadc7ec775ef
Kevin Ransom <kevinr@microsoft.com>
2017-06-26 21:52:00 -0700

    script

--- src/buildfromsource.cmd
+++ src/buildfromsource.cmd
@@ -1,18 +1,20 @@
-@echo off
-rem build tools
-dotnet restore buildtools\fssrgen\fssrgen.fsproj
-dotnet publish buildtools\fssrgen\fssrgen.fsproj -o %~dp0..\Tools\fssrgen
-dotnet restore buildtools\fslex\fslex.fsproj
-dotnet publish buildtools\fslex\fslex.fsproj -o %~dp0..\Tools\fslex
-dotnet restore buildtools\fsyacc\fsyacc.fsproj
-dotnet publish buildtools\fsyacc\fsyacc.fsproj -o %~dp0..\Tools\fsyacc
+__scriptpath=$(cd "$(dirname "$0")"; pwd -P)
+
+
+# build tools                               ç
+dotnet restore $__scriptpath/buildtools/fssrgen/fssrgen.fsproj
+dotnet publish $__scriptpath/buildtools/fssrgen/fssrgen.fsproj -o $__scriptpath/../Tools/fssrgen
+dotnet restore $__scriptpath/buildtools/fslex/fslex.fsproj
+dotnet publish $__scriptpath/buildtools/fslex/fslex.fsproj -o $__scriptpath/../Tools/fslex
+dotnet restore $__scriptpath/buildtools/fsyacc/fsyacc.fsproj
+dotnet publish $__scriptpath/buildtools/fsyacc/fsyacc.fsproj -o $__scriptpath/../Tools/fsyacc
 
 rem build tools
-dotnet restore fsharp\FSharp.Build\FSharp.Build.BuildFromSource.fsproj
-dotnet publish fsharp\FSharp.Build\FSharp.Build.BuildFromSource.fsproj
+dotnet restore $__scriptpath/fsharp/FSharp.Build/FSharp.Build.BuildFromSource.fsproj
+dotnet publish $__scriptpath/fsharp/FSharp.Build/FSharp.Build.BuildFromSource.fsproj
 
-dotnet restore fsharp\fsi\Fsi.BuildFromSource.fsproj
-dotnet publish fsharp\fsi\Fsi.BuildFromSource.fsproj
+dotnet restore $__scriptpath/fsharp/fsi/Fsi.BuildFromSource.fsproj
+dotnet publish fsharp/fsi/Fsi.BuildFromSource.fsproj
 
-dotnet restore fsharp\Fsc\Fsc.BuildFromSource.fsproj
-dotnet publish fsharp\Fsc\Fsc.BuildFromSource.fsproj
+dotnet restore $__scriptpath/fsharp/Fsc/Fsc.BuildFromSource.fsproj
+dotnet publish $__scriptpath/fsharp/Fsc/Fsc.BuildFromSource.fsproj
