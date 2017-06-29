f4b5d8426ed9f846a591a4838df9cbd2ee0857c1
Don Syme <donsyme@fastmail.fm>
2017-06-29 04:43:42 +0100

    fix 719 - traverse abstract slots for unsolved

--- tests/fsharp/core/attributes/testlib.fs
+++ tests/fsharp/core/attributes/testlib.fs
@@ -118,3 +118,9 @@ module TypeParamAttributesDifferent =
 type ThisLibAssembly = X | Y
 
 
+module Bug719 = 
+
+    open System.Runtime.InteropServices
+
+    type IFoo =
+        abstract Test : [<DefaultParameterValueAttribute(null)>]?value:int -> string
