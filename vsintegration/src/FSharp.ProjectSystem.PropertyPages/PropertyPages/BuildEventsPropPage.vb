1080f8d120e5e612ff4502bc3df6a8a6c247d592
Kevin Ransom <kevinr@microsoft.com>
2017-07-27 13:44:03 -0700

    Revert "Correctly fetch the IVsBuildMacroInfo to use for the build events page (#3388)"
    This reverts commit 213f38d320f2c788291476c41cc902358d7a9dab.

--- vsintegration/src/FSharp.ProjectSystem.PropertyPages/PropertyPages/BuildEventsPropPage.vb
+++ vsintegration/src/FSharp.ProjectSystem.PropertyPages/PropertyPages/BuildEventsPropPage.vb
@@ -335,16 +335,8 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
         Private Function GetTokenValue(ByVal MacroName As String) As String
             Dim MacroEval As IVsBuildMacroInfo
             Dim MacroValue As String = Nothing
-            Dim Hier As IVsHierarchy = Nothing
-            Dim ItemId As UInteger
-            Dim ThisObj As Object = m_Objects(0)
 
-            If TypeOf ThisObj Is IVsBrowseObject Then
-                VSErrorHandler.ThrowOnFailure(CType(ThisObj, IVsBrowseObject).GetProjectItem(Hier, ItemId))
-            ElseIf TypeOf ThisObj Is IVsCfgBrowseObject Then
-                VSErrorHandler.ThrowOnFailure(CType(ThisObj, IVsCfgBrowseObject).GetProjectItem(Hier, ItemId))
-            End If
-            MacroEval = CType(Hier, IVsBuildMacroInfo)
+            MacroEval = CType(m_Objects(0), IVsBuildMacroInfo)
             VSErrorHandler.ThrowOnFailure(MacroEval.GetBuildMacroValue(MacroName, MacroValue))
 
             Return MacroValue
