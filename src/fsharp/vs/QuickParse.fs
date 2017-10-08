5a457f03811d9dd54a55e9c898b0444b848ea207
Vasily Kirichenko <vaskir@gmail.com>
2017-10-08 20:46:07 +0300

    fix QuickParse

--- src/fsharp/vs/QuickParse.fs
+++ src/fsharp/vs/QuickParse.fs
@@ -220,7 +220,7 @@ module QuickParse =
             let rec SkipWhitespaceBeforeDotIdentifier(pos, ident, current, throwAwayNext, lastDotPos) =
                 if pos > index then PartialLongName.Default  // we're in whitespace after an identifier, if this is where the cursor is, there is no PLID here
                 elif IsWhitespace pos then SkipWhitespaceBeforeDotIdentifier(pos+1,ident,current,throwAwayNext,lastDotPos)
-                elif IsDot pos then AtStartOfIdentifier(pos+1,ident::current,throwAwayNext, match lastDotPos with Some _ -> lastDotPos | _ -> Some pos)
+                elif IsDot pos then AtStartOfIdentifier(pos+1,ident::current,throwAwayNext, Some pos)
                 elif IsStartOfComment pos then EatComment(1, pos + 1, EatCommentCallContext.SkipWhiteSpaces(ident, current, throwAwayNext), lastDotPos)
                 else AtStartOfIdentifier(pos,[],false,None) // Throw away what we have and start over.
 
@@ -257,7 +257,7 @@ module QuickParse =
                     if IsIdentifierPartCharacter pos then InUnquotedIdentifier(left,pos+1,current,throwAwayNext,lastDotPos)
                     elif IsDot pos then 
                         let ident = line.Substring(left,pos-left)
-                        AtStartOfIdentifier(pos+1,ident::current,throwAwayNext, match lastDotPos with Some _ -> lastDotPos | _ -> Some pos)
+                        AtStartOfIdentifier(pos+1,ident::current,throwAwayNext, Some pos)
                     elif IsWhitespace pos || IsStartOfComment pos then 
                         let ident = line.Substring(left,pos-left)
                         SkipWhitespaceBeforeDotIdentifier(pos, ident, current, throwAwayNext, lastDotPos)
@@ -296,7 +296,7 @@ module QuickParse =
                         elif IsDot pos then 
                             if pos = 0 then
                                 // dot on first char of line, currently treat it like empty identifier to the left
-                                AtStartOfIdentifier(pos+1,""::current,throwAwayNext, match lastDotPos with Some _ -> lastDotPos | _ -> Some pos)
+                                AtStartOfIdentifier(pos+1,""::current,throwAwayNext, Some pos)
                             elif not (pos > 0 && (IsIdentifierPartCharacter(pos-1) || IsWhitespace(pos-1))) then
                                 // it's not dots as part.of.a.long.ident, it's e.g. the range operator (..), or some other multi-char operator ending in dot
                                 if line.[pos-1] = ')' then
@@ -308,7 +308,7 @@ module QuickParse =
                                 else
                                     AtStartOfIdentifier(pos+1,[],false,None) // Throw away what we have and start over.
                             else
-                                AtStartOfIdentifier(pos+1,""::current,throwAwayNext, match lastDotPos with Some _ -> lastDotPos | _ -> Some pos)                            
+                                AtStartOfIdentifier(pos+1,""::current,throwAwayNext, Some pos)
                         else AtStartOfIdentifier(pos+1,[],throwAwayNext, lastDotPos)
             let partialLongName = AtStartOfIdentifier(0, [], false, None) 
             
