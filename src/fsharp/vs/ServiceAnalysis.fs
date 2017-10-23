42e528bc0cf6653f423c22c192c840ef9610704a
Vasily Kirichenko <vaskir@gmail.com>
2017-10-23 19:53:28 +0300

    the proper way, finally

--- src/fsharp/vs/ServiceAnalysis.fs
+++ src/fsharp/vs/ServiceAnalysis.fs
@@ -70,118 +70,19 @@ module UnusedOpens =
              | _ -> None // for now
            )
 
-    let getAutoOpenAccessPath (ent:FSharpEntity) =
-        // Some.Namespace+AutoOpenedModule+Entity
-
-        // HACK: I can't see a way to get the EnclosingEntity of an Entity
-        // Some.Namespace + Some.Namespace.AutoOpenedModule are both valid
-        ent.TryFullName |> Option.bind(fun _ ->
-            if (not ent.IsNamespace) && ent.QualifiedName.Contains "+" then 
-                Some ent.QualifiedName.[0..ent.QualifiedName.IndexOf "+" - 1]
-            else
-                None)
-
-    let entityNamespace (entOpt: FSharpEntity option) =
-        match entOpt with
-        | Some ent ->
-            if ent.IsFSharpModule then
-                [ yield Some ent.QualifiedName
-                  yield Some ent.LogicalName
-                  yield Some ent.AccessPath
-                  yield Some ent.FullName
-                  yield Some ent.DisplayName
-                  yield ent.TryGetFullDisplayName()
-                  if ent.HasFSharpModuleSuffix then
-                    yield Some (ent.AccessPath + "." + ent.DisplayName)]
-            else
-                [ yield ent.Namespace
-                  yield Some ent.AccessPath
-                  yield getAutoOpenAccessPath ent
-                  for path in ent.AllCompilationPaths do
-                    yield Some path 
-                ]
-        | None -> []
-
-    type NamespaceUse =
-        { Ident: string
-          ExtraNamespaces: string[] }
-    
-    let getNamespaceInUse (fullIsland: string) (fullName: string) : NamespaceUse option =
-        // given a full island such as `Text.ISegment` and a full name of `MonoDevelop.Core.Text.ISegment`, return `MonoDevelop.Core`
-        let lengthDiff = fullName.Length - fullIsland.Length - 2
-        if lengthDiff <= 0 || lengthDiff > fullName.Length - 1 then None
-        else 
-            let requiredOpenNamespace = fullName.[0..lengthDiff]
-            let rest = fullName.[lengthDiff + 1..]
-            let extraNamespaces =
-                match rest.Split '.' with
-                | [||] | [|_|] -> [||]
-                | rest -> rest.[..rest.Length - 2]
-            Some { Ident = requiredOpenNamespace; ExtraNamespaces = extraNamespaces }
-
-    let getPossibleNamespaces (getSourceLineStr: int -> string) (symbolUse: FSharpSymbolUse) : NamespaceUse[] =
-        let lineStr = getSourceLineStr symbolUse.RangeAlternate.StartLine
-        let partialName = QuickParse.GetPartialLongNameEx (lineStr, symbolUse.RangeAlternate.EndColumn - 1)
-        if partialName.PartialIdent = "" then [||]
-        else
-            let qualifyingIsland = partialName.QualifyingIdents |> String.concat "."
-            let fullIsland = qualifyingIsland + partialName.PartialIdent
-            let isQualified fullName = fullName = fullIsland
-
-            (match symbolUse with
-             | SymbolUse.Entity (ent, cleanFullNames) when not (cleanFullNames |> List.exists isQualified) ->
-                 Some (cleanFullNames, Some ent)
-             | SymbolUse.Field f when not (isQualified f.FullName) ->
-                 Some ([f.FullName], Some f.DeclaringEntity)
-             | SymbolUse.MemberFunctionOrValue mfv when not (isQualified mfv.FullName) ->
-                 Some ([mfv.FullName], mfv.EnclosingEntity)
-             | SymbolUse.Operator op when not (isQualified op.FullName) ->
-                 Some ([op.FullName], op.EnclosingEntity)
-             | SymbolUse.ActivePattern ap when not (isQualified ap.FullName) ->
-                 Some ([ap.FullName], ap.EnclosingEntity)
-             | SymbolUse.ActivePatternCase apc when not (isQualified apc.FullName) ->
-                 Some ([apc.FullName], apc.Group.EnclosingEntity)
-             | SymbolUse.UnionCase uc when not (isQualified uc.FullName) ->
-                 Some ([uc.FullName], Some uc.ReturnType.TypeDefinition)
-             | SymbolUse.Parameter p when not (isQualified p.FullName) && p.Type.HasTypeDefinition ->
-                 Some ([p.FullName], Some p.Type.TypeDefinition)
-             | _ -> None)
-            |> Option.map (fun (fullNames, declaringEntity) ->
-                 [| for name in fullNames do
-                      let partNamespace = getNamespaceInUse fullIsland name
-                      yield partNamespace
-                    yield! 
-                        entityNamespace declaringEntity
-                        |> List.map (Option.bind (getNamespaceInUse qualifyingIsland))
-                 |])
-            |> Option.toArray
-            |> Array.concat
-            |> Array.choose id
-            |> Array.distinct
-
-    type SymbolUseWithFullNames =
-        { SymbolUse: FSharpSymbolUse
-          FullNames: string[][] }
-
-    type SymbolUse =
-        { SymbolUse: FSharpSymbolUse
-          PossibleNamespaces: NamespaceUse[] }
-
-    let getSymbolUses (getSourceLineStr: int -> string) (symbolUses: FSharpSymbolUse[]) : SymbolUse[] =
+    let filterSymbolUses (getSourceLineStr: int -> string) (symbolUses: FSharpSymbolUse[]) : FSharpSymbolUse[] =
         symbolUses
-        |> Array.filter (fun (symbolUse: FSharpSymbolUse) -> 
-             not symbolUse.IsFromDefinition //&& 
-             //match symbolUse.Symbol with
-             //| :? FSharpEntity as e -> not e.IsNamespace
-             //| _ -> true
-           )
-        |> Array.map (fun su ->
-            { SymbolUse = su
-              PossibleNamespaces = getPossibleNamespaces getSourceLineStr su })
+        |> Array.filter (fun su -> not su.IsFromDefinition)
+        |> Array.filter (fun su ->
+             match su.Symbol with
+             | :? FSharpMemberOrFunctionOrValue as fv when fv.IsExtensionMember -> true
+             | _ -> 
+                let partialName = QuickParse.GetPartialLongNameEx (getSourceLineStr su.RangeAlternate.StartLine, su.RangeAlternate.EndColumn - 1)
+                partialName.PartialIdent <> "" && partialName.QualifyingIdents = [])
 
     let getUnusedOpens (checkFileResults: FSharpCheckFileResults, getSourceLineStr: int -> string) : Async<range list> =
         
-        let filter (openStatements: OpenStatement list) (symbolUses: SymbolUse[]) : OpenStatement list =
+        let filter (openStatements: OpenStatement list) (symbolUses: FSharpSymbolUse[]) : OpenStatement list =
             let rec filterInner acc (openStatements: OpenStatement list) (seenOpenStatements: OpenStatement list) = 
                 
                 let isUsed (openStatement: OpenStatement) =
@@ -190,13 +91,13 @@ module UnusedOpens =
                         let usedSomewhere =
                             symbolUses
                             |> Array.exists (fun symbolUse -> 
-                                let inScope = rangeContainsRange openStatement.AppliedScope symbolUse.SymbolUse.RangeAlternate
+                                let inScope = rangeContainsRange openStatement.AppliedScope symbolUse.RangeAlternate
                                 if not inScope then false
-                                elif openStatement.Idents |> Set.intersect symbolUse.PossibleNamespaces |> Set.isEmpty then false
+                                //elif openStatement.Idents |> Set.intersect symbolUse.PossibleNamespaces |> Set.isEmpty then false
                                 else
                                     let moduleSymbols = openStatement.AllChildSymbols |> Seq.toList
                                     moduleSymbols
-                                    |> List.exists (fun x -> x.IsEffectivelySameAs symbolUse.SymbolUse.Symbol))
+                                    |> List.exists (fun x -> x.IsEffectivelySameAs symbolUse.Symbol))
 
                         if not usedSomewhere then false
                         else
@@ -219,8 +120,8 @@ module UnusedOpens =
             filterInner [] openStatements []
 
         async {
-            let! fsharpSymbolUses = checkFileResults.GetAllUsesOfAllSymbolsInFile()
-            let symbolUses = getSymbolUses getSourceLineStr fsharpSymbolUses
+            let! symbolUses = checkFileResults.GetAllUsesOfAllSymbolsInFile()
+            let symbolUses = filterSymbolUses getSourceLineStr symbolUses
             let openStatements = getOpenStatements checkFileResults.OpenDeclarations
             return filter openStatements symbolUses |> List.map (fun os -> os.Range)
         }
