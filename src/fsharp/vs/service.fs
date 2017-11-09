bc497d460eb2f36759d80f0867c918ccf17f3381
Vasily Kirichenko <vaskir@gmail.com>
2017-11-09 09:37:44 +0300

    do not provide completion after ' (#3911)

--- src/fsharp/vs/service.fs
+++ src/fsharp/vs/service.fs
@@ -872,8 +872,17 @@ type TypeCheckInfo
             
             // Other completions
             | cc ->
-                let isInRangeOperator = (match cc with Some (CompletionContext.RangeOperator) -> true | _ -> false)
-                GetDeclaredItems (parseResultsOpt, lineStr, origLongIdentOpt, colAtEndOfNamesAndResidue, residueOpt, lastDotPos, line, loc, filterCtors,resolveOverloads, hasTextChangedSinceLastTypecheck, isInRangeOperator, getAllSymbols)
+                match residueOpt |> Option.bind Seq.tryHead with
+                | Some ''' ->
+                    // The last token in 
+                    //    let x = 'E
+                    // is Ident with text "'E", however it's either unfinished char literal or generic parameter. 
+                    // We should not provide any completion in the former case, and we don't provide it for the latter one for now
+                    // because providing generic parameters list is context aware, which we don't have here (yet).
+                    None
+                | _ ->
+                    let isInRangeOperator = (match cc with Some (CompletionContext.RangeOperator) -> true | _ -> false)
+                    GetDeclaredItems (parseResultsOpt, lineStr, origLongIdentOpt, colAtEndOfNamesAndResidue, residueOpt, lastDotPos, line, loc, filterCtors,resolveOverloads, hasTextChangedSinceLastTypecheck, isInRangeOperator, getAllSymbols)
         
         res |> Option.map (fun (items, denv, m) -> items, denv, completionContext, m)
 
