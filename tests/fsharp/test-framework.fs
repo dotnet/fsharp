537af434a27ca2969765417bbf67f35ced3c3dbd
Brett V. Forsgren <brettfo@microsoft.com>
2017-11-27 14:18:48 -0800

    use csc from a NuGet package (#4023)

--- tests/fsharp/test-framework.fs
+++ tests/fsharp/test-framework.fs
@@ -105,7 +105,6 @@ module Commands =
 
 type TestConfig = 
     { EnvironmentVariables : Map<string, string>
-      CORDIR : string
       CSC : string
       csc_flags : string
       BUILD_CONFIG : string
@@ -131,25 +130,6 @@ module WindowsPlatform =
             [| "PROCESSOR_ARCHITECTURE" |] |> Seq.tryPick (fun s -> find s) |> function None -> "" | Some x -> x
         value = "AMD64"
 
-    let clrPath envVars =
-
-        let windir = 
-            match envVars |> Map.tryFind "windir" with
-            | Some x -> x 
-            | None -> failwithf "environment variable '%s' required " "WINDIR"
-
-        let mutable CORDIR =
-            match Directory.EnumerateDirectories (windir ++ "Microsoft.NET" ++ "Framework", "v4.0.?????") |> List.ofSeq |> List.rev with
-            | x :: _ -> x
-            | [] -> failwith "couldn't determine CORDIR"
-
-        // == Use the same runtime as our architecture
-        // == ASSUMPTION: This could be a good or bad thing.
-        if Is64BitOperatingSystem envVars then 
-            CORDIR <- CORDIR.Replace("Framework", "Framework64")
-
-        CORDIR
-
 type FSLibPaths = 
     { FSCOREDLLPATH : string }
 
@@ -164,10 +144,9 @@ let config configurationName envVars =
     let csc_flags = "/nologo" 
     let fsc_flags = "-r:System.Core.dll --nowarn:20 --define:COMPILED"
     let fsi_flags = "-r:System.Core.dll --nowarn:20 --define:INTERACTIVE --maxerrors:1 --abortonerror"
-    let CORDIR = WindowsPlatform.clrPath envVars
     let Is64BitOperatingSystem = WindowsPlatform.Is64BitOperatingSystem envVars
     let architectureMoniker = if Is64BitOperatingSystem then "x64" else "x86"
-    let CSC = requireFile (CORDIR ++ "csc.exe")
+    let CSC = requireFile (packagesDir ++ "Microsoft.Net.Compilers.2.4.0" ++ "tools" ++ "csc.exe")
     let ILDASM = requireFile (packagesDir ++ ("runtime.win-" + architectureMoniker + ".Microsoft.NETCore.ILDAsm.2.0.3") ++ "runtimes" ++ ("win-" + architectureMoniker) ++ "native" ++ "ildasm.exe")
     let coreclrdll = requireFile (packagesDir ++ ("runtime.win-" + architectureMoniker + ".Microsoft.NETCore.Runtime.CoreCLR.2.0.3") ++ "runtimes" ++ ("win-" + architectureMoniker) ++ "native" ++ "coreclr.dll")
     let PEVERIFY = requireFile (SCRIPT_ROOT ++ ".." ++ "fsharpqa" ++ "testenv" ++ "src" ++ "PEVerify" ++ "bin" ++ configurationName ++ "net46" ++ "PEVerify.exe")
@@ -204,7 +183,6 @@ let config configurationName envVars =
         | false -> "win7-x86"
 
     { EnvironmentVariables = envVars
-      CORDIR = CORDIR |> Commands.pathAddBackslash
       FSCBinPath = FSCBinPath |> Commands.pathAddBackslash
       FSCOREDLLPATH = FSCOREDLLPATH
       ILDASM = ILDASM
@@ -225,7 +203,6 @@ let logConfig (cfg: TestConfig) =
     log "---------------------------------------------------------------"
     log "Executables"
     log ""
-    log "CORDIR              =%s" cfg.CORDIR
     log "CSC                 =%s" cfg.CSC
     log "BUILD_CONFIG        =%s" cfg.BUILD_CONFIG
     log "csc_flags           =%s" cfg.csc_flags
