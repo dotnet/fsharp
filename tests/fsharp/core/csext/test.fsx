80039c378c2c4e1f39fa3dc11f0a60d759cb9220
dsyme <dsyme@MSRC-4079076>
2017-11-29 18:04:39 +0000

    add deactivated tests for struct tuple

--- tests/fsharp/core/csext/test.fsx
+++ tests/fsharp/core/csext/test.fsx
@@ -55,6 +55,7 @@ type Struct(i:int) =
 
 #nowarn "3220"
 
+// See https://github.com/Microsoft/visualfsharp/pull/3729
 module TestsExplicitUseOfTupleProperties = 
     // all give a warning, suppressed in this file
     let x1 = 
@@ -160,6 +161,115 @@ module TestsExplicitUseOfTupleProperties =
     let text = sprintf "%A" x4
     printfn "%s" text
     check "vewjwervwver" text "PropertyGet (Some (PropertyGet (None, tup, [])), Item1, [])"
+
+(*  
+// See https://github.com/Microsoft/visualfsharp/pull/3729, struct tuple access to Item* and Rest* are not supported
+module TestsExplicitUseOfStructTupleProperties = 
+    // all give a warning, suppressed in this file
+    let x1 = 
+       [ (struct (1,2)).Item1 
+         (struct (1,2)).Item2
+         (struct (1,2,3)).Item1
+         (struct (1,2,3)).Item2
+         (struct (1,2,3)).Item3
+         (struct (1,2,3,4)).Item1
+         (struct (1,2,3,4)).Item2
+         (struct (1,2,3,4)).Item3
+         (struct (1,2,3,4)).Item4
+         (struct (1,2,3,4,5)).Item1
+         (struct (1,2,3,4,5)).Item2
+         (struct (1,2,3,4,5)).Item3
+         (struct (1,2,3,4,5)).Item4
+         (struct (1,2,3,4,5)).Item5
+         (struct (1,2,3,4,5,6)).Item1
+         (struct (1,2,3,4,5,6)).Item2
+         (struct (1,2,3,4,5,6)).Item3
+         (struct (1,2,3,4,5,6)).Item4
+         (struct (1,2,3,4,5,6)).Item5
+         (struct (1,2,3,4,5,6)).Item6
+         (struct (1,2,3,4,5,6,7)).Item1
+         (struct (1,2,3,4,5,6,7)).Item2
+         (struct (1,2,3,4,5,6,7)).Item3
+         (struct (1,2,3,4,5,6,7)).Item4
+         (struct (1,2,3,4,5,6,7)).Item5
+         (struct (1,2,3,4,5,6,7)).Item6
+         (struct (1,2,3,4,5,6,7)).Item7
+         (struct (1,2,3,4,5,6,7,8)).Item1
+         (struct (1,2,3,4,5,6,7,8)).Item2
+         (struct (1,2,3,4,5,6,7,8)).Item3
+         (struct (1,2,3,4,5,6,7,8)).Item4
+         (struct (1,2,3,4,5,6,7,8)).Item5
+         (struct (1,2,3,4,5,6,7,8)).Item6
+         (struct (1,2,3,4,5,6,7,8)).Item7
+         (struct (1,2,3,4,5,6,7,8,9)).Item1
+         (struct (1,2,3,4,5,6,7,8,9)).Item2
+         (struct (1,2,3,4,5,6,7,8,9)).Item3
+         (struct (1,2,3,4,5,6,7,8,9)).Item4
+         (struct (1,2,3,4,5,6,7,8,9)).Item5
+         (struct (1,2,3,4,5,6,7,8,9)).Item6
+         (struct (1,2,3,4,5,6,7,8,9)).Item7
+         (struct (1,2)).get_Item1()
+         (struct (1,2)).get_Item2()
+         (struct (1,2,3)).get_Item1()
+         (struct (1,2,3)).get_Item2()
+         (struct (1,2,3)).get_Item3()
+         (struct (1,2,3,4)).get_Item1()
+         (struct (1,2,3,4)).get_Item2()
+         (struct (1,2,3,4)).get_Item3()
+         (struct (1,2,3,4)).get_Item4()
+         (struct (1,2,3,4,5)).get_Item1()
+         (struct (1,2,3,4,5)).get_Item2()
+         (struct (1,2,3,4,5)).get_Item3()
+         (struct (1,2,3,4,5)).get_Item4()
+         (struct (1,2,3,4,5)).get_Item5()
+         (struct (1,2,3,4,5,6)).get_Item1()
+         (struct (1,2,3,4,5,6)).get_Item2()
+         (struct (1,2,3,4,5,6)).get_Item3()
+         (struct (1,2,3,4,5,6)).get_Item4()
+         (struct (1,2,3,4,5,6)).get_Item5()
+         (struct (1,2,3,4,5,6)).get_Item6()
+         (struct (1,2,3,4,5,6,7)).get_Item1()
+         (struct (1,2,3,4,5,6,7)).get_Item2()
+         (struct (1,2,3,4,5,6,7)).get_Item3()
+         (struct (1,2,3,4,5,6,7)).get_Item4()
+         (struct (1,2,3,4,5,6,7)).get_Item5()
+         (struct (1,2,3,4,5,6,7)).get_Item6()
+         (struct (1,2,3,4,5,6,7)).get_Item7()
+         (struct (1,2,3,4,5,6,7,8)).get_Item1()
+         (struct (1,2,3,4,5,6,7,8)).get_Item2()
+         (struct (1,2,3,4,5,6,7,8)).get_Item3()
+         (struct (1,2,3,4,5,6,7,8)).get_Item4()
+         (struct (1,2,3,4,5,6,7,8)).get_Item5()
+         (struct (1,2,3,4,5,6,7,8)).get_Item6()
+         (struct (1,2,3,4,5,6,7,8)).get_Item7()
+         (struct (1,2,3,4,5,6,7,8,9)).get_Item1()
+         (struct (1,2,3,4,5,6,7,8,9)).get_Item2()
+         (struct (1,2,3,4,5,6,7,8,9)).get_Item3()
+         (struct (1,2,3,4,5,6,7,8,9)).get_Item4()
+         (struct (1,2,3,4,5,6,7,8,9)).get_Item5()
+         (struct (1,2,3,4,5,6,7,8,9)).get_Item6()
+         (struct (1,2,3,4,5,6,7,8,9)).get_Item7() ]
+
+    printfn "x1 = %A" x1
+    check "vwhnwrvep01" x1 [1; 2; 1; 2; 3; 1; 2; 3; 4; 1; 2; 3; 4; 5; 1; 2; 3; 4; 5; 6; 1; 2; 3; 4; 5; 6; 7; 1; 2; 3; 4; 5; 6; 7; 1; 2; 3; 4; 5; 6; 7;
+                            1; 2; 1; 2; 3; 1; 2; 3; 4; 1; 2; 3; 4; 5; 1; 2; 3; 4; 5; 6; 1; 2; 3; 4; 5; 6; 7; 1; 2; 3; 4; 5; 6; 7; 1; 2; 3; 4; 5; 6; 7]
+
+    let x2 = (struct (1,2,3,4,5,6,7,8)).Rest // gives a warning, suppressed in this file
+    printfn "x2 = %A" x2
+    check "vwhnwrvep02" x2 (System.Tuple(8))
+
+    let x3 = (struct (1,2,3,4,5,6,7,8,9)).Rest // gives a warning, suppressed in this file
+    printfn "x3 = %A" x3
+    check "vwhnwrvep03" x3 (unbox (box (struct (8,9))))
+    
+    // check a quotation of these
+    let tup = (struct (1,2))
+    let x4 = <@ tup.Item1 @>
+
+    let text = sprintf "%A" x4
+    printfn "%s" text
+    check "vewjwervwver" text "PropertyGet (Some (PropertyGet (None, tup, [])), Item1, [])"
+*)
 
 (*--------------------*)  
 
