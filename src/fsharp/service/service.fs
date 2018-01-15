21993cbc0278611ace9505ed45c9fc2e145b425f
nosami <jasonimison@gmail.com>
2018-01-15 11:38:23 +0000

    Get logical name from ExnCase

--- src/fsharp/service/service.fs
+++ src/fsharp/service/service.fs
@@ -987,6 +987,8 @@ type TypeCheckInfo
                         items |> List.groupBy (fun d ->
                             match d.Item with  
                             | Item.Types (_,(TType_app(tcref,_) :: _))
+                            | Item.ExnCase tcref -> tcref.LogicalName
+                            | Item.UnqualifiedType(tcref :: _)
                             | Item.FakeInterfaceCtor (TType_app(tcref,_)) 
                             | Item.DelegateCtor (TType_app(tcref,_)) -> tcref.CompiledName
                             | Item.CtorGroup (_, (cinfo :: _)) -> (tcrefOfAppTy g cinfo.EnclosingType).CompiledName
