ebb400fdfdedb45f7288744584c1e8edb64a5997
Matt Mitchell <mmitche@microsoft.com>
2018-02-22 13:50:40 -0800

    Use mutli-line string

--- netci.groovy
+++ netci.groovy
@@ -30,13 +30,13 @@ def static getBuildJobName(def configuration, def os) {
             def buildCommand = '';
             def buildOutput= '';
             def buildArgs= '';
-            def getMono = '
+            def getMono = '''
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu stable-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt-get update
 sudo apt-get -my install mono-devel
 mono --version
-'
+'''
 
             if (configuration == "Release_fcs" && branch != "dev15.5") {
                 // Build and test FCS NuGet package
