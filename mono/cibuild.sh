aa219fd1e6cbc8bfce8ea8a23e2d9f18d47f034d
Don Syme <dsyme@users.noreply.github.com>
2018-02-22 23:29:51 +0000

    Allow sudo on CI Ubuntu to install latest Mono (#4382)
    * rejig mono scripts
    
    * upates to ci
    
    * upates to ci
    
    * upates to ci
    
    * use outer-latest for sudo
    
    * fix scripts

--- mono/cibuild.sh
+++ mono/cibuild.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # note: expects to run from top directory
-# suitable for Ubuntu 16.04 - get latest Mono stable
-../mono/latest-mono-stable.sh && \
+./mono/latest-mono-stable.sh && \
 make Configuration=$@ && \
 sudo make install Configuration=$@ && \
-../mono/test-mono.sh
+./mono/test-mono.sh
