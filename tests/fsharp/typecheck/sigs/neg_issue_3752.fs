a360c6c269a91da4714049db2d031a2e1a482f2e
Matthias Dittrich <matthi.d@gmail.com>
2018-03-13 23:24:52 +0100

    Update according to test

--- tests/fsharp/typecheck/sigs/neg_issue_3752.fs
+++ tests/fsharp/typecheck/sigs/neg_issue_3752.fs
@@ -1,5 +1,4 @@
-module Test =
-    let memoizeBy (getKey : 'a -> string) (f: 'a -> 'b) : 'a -> 'b =
-       let cache = System.Collections.Concurrent.ConcurrentDictionary<string, 'b>()
-       fun (x: 'a) -> cache.GetOrAdd(getKey x, f)
-do ()
+module Test
+let memoizeBy (getKey : 'a -> string) (f: 'a -> 'b) : 'a -> 'b =
+   let cache = System.Collections.Concurrent.ConcurrentDictionary<string, 'b>()
+   fun (x: 'a) -> cache.GetOrAdd(getKey x, f)
