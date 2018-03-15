9fd6190d3ed4133448a5e9f02fdc75940dc593e9
Jakub Majocha <majocha@outlook.com>
2018-03-16 00:36:20 +0100

    clarify the comment

--- vsintegration/src/FSharp.Editor/Options/EditorOptions.fs
+++ vsintegration/src/FSharp.Editor/Options/EditorOptions.fs
@@ -14,7 +14,7 @@ module DefaultTuning =
     let SimplifyNameInitialDelay = 2000 (* milliseconds *)
     let SimplifyNameEachItemDelay = 0 (* milliseconds *)
 
-// CLIMutable to make the record work also as a view model
+// CLIMutable attribute is required if we want to use the record also as a view model bound to XAML options page
 [<CLIMutable>]
 type IntelliSenseOptions =
   { ShowAfterCharIsTyped: bool
