16dd8f40fd79d46aa832c0a2417a9fd4dfc8327c
Don Syme <dsyme@users.noreply.github.com>
2018-05-08 06:10:25 +0200

    Fix #3737 - allow inheriting from FSharpFunc (#4804)
    * allow inheriting from FSharpFunc
    
    * allow inheriting from FSharpFunc
    
    * fix #4569
    
    * fix tests
    
    * fix test
    
    * fix error

--- tests/fsharpqa/Source/Diagnostics/General/E_AreYouMissingAnArgumentToAFunction01.fs
+++ tests/fsharpqa/Source/Diagnostics/General/E_AreYouMissingAnArgumentToAFunction01.fs
@@ -1,7 +1,8 @@
 // #Regression #Diagnostics 
 // Regression test for FSHARP1.0:2804
 // Make sure we don't emit ?. (notice that the error message changed a bit since the bug was opened)
-//<Expects status="error" span="(6,32-6,37)" id="FS0001">Expecting a type supporting the operator 'op_Explicit' but given a function type\. You may be missing an argument to a function\.$</Expects>
+//<Expects status="error" span="(7,32-7,33)" id="FS0001">This expression was expected to have type</Expects>
+//<Expects status="error" span="(7,35-7,37)" id="FS0001">This expression was expected to have type</Expects>
 
 let f (x : int list)  = int32 (x>>32)
 
