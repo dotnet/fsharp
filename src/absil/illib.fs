6f80f247044dddf6e8c86a2d300d1a797907ae95
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-05-08 11:40:26 +0200

    TryGetValue should use TryGetValue

--- src/absil/illib.fs
+++ src/absil/illib.fs
@@ -1146,10 +1146,8 @@ type LayeredMap<'Key,'Value  when 'Key : comparison> = Map<'Key,'Value>
 type Map<'Key,'Value when 'Key : comparison> with
     static member Empty : Map<'Key,'Value> = Map.empty
 
-    member m.TryGetValue (key,res:byref<'Value>) = 
-        match m.TryFind key with 
-        | None -> false
-        | Some r -> res <- r; true
+    member m.TryGetValue (key,res:byref<'Value>) =
+        m.TryGetValue(key,&res)
 
     member x.Values = [ for (KeyValue(_,v)) in x -> v ]
     member x.AddAndMarkAsCollapsible (kvs: _[])   = (x,kvs) ||> Array.fold (fun x (KeyValue(k,v)) -> x.Add(k,v))
