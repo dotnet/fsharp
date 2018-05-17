d44e5a3054b42357111e02595720f1cdad6bb3cf
Don Syme <dsyme@microsoft.com>
2018-05-17 15:47:10 +0100

    allow flexible inference (subsumption) at list/sequence/array yield points

--- tests/fsharp/typecheck/sigs/pos31.fs
+++ tests/fsharp/typecheck/sigs/pos31.fs
@@ -1,5 +1,8 @@
 module Pos31
 
-let x1 : list<obj>  = [ yield "" ]
-let x2 : seq<obj>  = seq { yield "" }
-let x3 : seq<obj>  = seq { yield "" :> obj  }
+let x1 : obj list  = [ ""  ]
+let x2 : obj [] = [| ""  |]
+let x3 : obj list  = [ yield ""  ]
+let x4 : obj[]  = [| yield "" |]
+let x5 : seq<obj>  = seq { yield "" }
+let x6 : seq<obj>  = seq { yield "" :> obj  }
