c2a4a594cd9cc54f26970eaf898452a172feed3d
Don Syme <dsyme@microsoft.com>
2018-05-18 12:50:06 +0100

    suppress warnings for compat purposes

--- tests/fsharp/typecheck/sigs/pos31.fs
+++ tests/fsharp/typecheck/sigs/pos31.fs
@@ -6,3 +6,26 @@ let x3 : obj list  = [ yield ""  ]
 let x4 : obj[]  = [| yield "" |]
 let x5 : seq<obj>  = seq { yield "" }
 let x6 : seq<obj>  = seq { yield "" :> obj  }
+
+let g2 () : System.Reflection.MemberInfo[] = 
+    [| yield (Unchecked.defaultof<System.Type> :> _) |]
+
+let g3 () : System.Reflection.MemberInfo[] = 
+    [| yield Unchecked.defaultof<System.Type> |]
+
+let g4 () : System.Reflection.MemberInfo[] = 
+    [| yield! g2()
+       yield Unchecked.defaultof<System.Type> |]
+
+// Still not allowed
+//let g5 () = 
+//    [| yield Unchecked.defaultof<System.Type>
+//       yield Unchecked.defaultof<System.Reflection.MemberInfo> |]
+
+// Still not allowed
+//let g6 () = 
+//    [| 
+//       yield Unchecked.defaultof<System.Reflection.MemberInfo> 
+ //      yield Unchecked.defaultof<System.Type>
+ //   |]
+
