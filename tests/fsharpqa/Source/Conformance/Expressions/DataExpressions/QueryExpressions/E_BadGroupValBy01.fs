cdc765d231fa4b3f39681d0d43cdfeba033009b7
Diego Esmerio <diego.esmerio@fitsolucoes.biz>
2018-05-22 15:37:56 -0300

    New error messages

--- tests/fsharpqa/Source/Conformance/Expressions/DataExpressions/QueryExpressions/E_BadGroupValBy01.fs
+++ tests/fsharpqa/Source/Conformance/Expressions/DataExpressions/QueryExpressions/E_BadGroupValBy01.fs
@@ -1,7 +1,7 @@
 // #Conformance #DataExpressions #Query
 // DevDiv:179889, groupValBy was hard to use with poor diagnostics
-//<Expects status="error" span="(9,9-9,19)" id="FS3099">'groupValBy' is used with an incorrect number of arguments\. This is a custom operation in this query or computation expression\. Expected 2 argument\(s\), but given 0\.$</Expects>
-//<Expects status="error" span="(15,9-15,19)" id="FS3099">'groupValBy' is used with an incorrect number of arguments\. This is a custom operation in this query or computation expression\. Expected 2 argument\(s\), but given 1\.$</Expects>
+//<Expects status="error" span="(8,9-8,28)" id="FS0001">This expression was expected to have type.    'Linq\.QuerySource\<'a,'b\> \* \('a \-\> 'c\) \* \('a \-\> 'd\)'    .but here has type.    'Linq\.QuerySource\<int,System\.Collections\.IEnumerable\>'</Expects>
+//<Expects status="error" span="(15,9-15,21)" id="FS0501">The member or object constructor 'GroupValBy' takes 3 argument\(s\) but is here given 2\. The required signature is 'member Linq\.QueryBuilder\.GroupValBy \: source\:Linq\.QuerySource\<'T,'Q\> \* resultSelector\:\('T \-\> 'Value\) \* keySelector\:\('T \-\> 'Key\) \-\> Linq\.QuerySource\<System\.Linq\.IGrouping\<'Key,'Value\>,'Q\> when 'Key \: equality'\.$</Expects>
 
 let q1 = 
     query {
--- tests/fsharpqa/Source/Conformance/Expressions/DataExpressions/QueryExpressions/E_WhitespaceErrors01.fs
+++ tests/fsharpqa/Source/Conformance/Expressions/DataExpressions/QueryExpressions/E_WhitespaceErrors01.fs
@@ -1,6 +1,6 @@
 // #Conformance #DataExpressions #Query
 // DevDiv:188241, just baselines errors around spacing
-//<Expects status="error" span="(15,13-15,18)" id="FS3099">'where' is used with an incorrect number of arguments\. This is a custom operation in this query or computation expression\. Expected 1 argument\(s\), but given 3\.$</Expects>
+//<Expects status="error" span="(15,13-16,25)" id="FS0501">The member or object constructor 'Where' takes 2 argument\(s\) but is here given 4\. The required signature is 'member Linq\.QueryBuilder\.Where : source:Linq\.QuerySource<'T,'Q> \* predicate:\('T \-> bool\) \-> Linq\.QuerySource<'T,'Q>'\.$</Expects>
 
 let q1 = // no errors
     query {
