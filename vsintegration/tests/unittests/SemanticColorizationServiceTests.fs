c763ebd7ea4dfb8de06fc5178b041776b9dd38a2
Vasily Kirichenko <vaskir@gmail.com>
2018-05-28 20:51:10 +0300

    all the tests are green

--- vsintegration/tests/unittests/SemanticColorizationServiceTests.fs
+++ vsintegration/tests/unittests/SemanticColorizationServiceTests.fs
@@ -6,11 +6,10 @@ open NUnit.Framework
 open Microsoft.VisualStudio.FSharp.Editor
 open Microsoft.FSharp.Compiler.SourceCodeServices
 open Microsoft.FSharp.Compiler
-open Microsoft.CodeAnalysis
 open Microsoft.CodeAnalysis.Text
 
-[<TestFixture>][<Category "Roslyn Services">]
-type SemanticClassificationServiceTests()  =
+[<TestFixture; Category "Roslyn Services">]
+type SemanticClassificationServiceTests() =
     let filePath = "C:\\test.fs"
 
     let projectOptions = { 
@@ -42,24 +41,22 @@ type SemanticClassificationServiceTests()  =
     let verifyColorizerAtEndOfMarker(fileContents: string, marker: string, classificationType: string) =
         let text = SourceText.From fileContents
         let ranges = getRanges fileContents
-        let line = text.Lines.GetLineFromPosition (fileContents.IndexOf(marker) + marker.Length - 1)
-        let col = line.ToString().IndexOf(marker) + marker.Length - 1
-        let markerPos = Range.mkPos (Range.Line.toZ line.LineNumber) col
-        printfn "parker pos: %A, ranges: %A" markerPos ranges
+        let line = text.Lines.GetLinePosition (fileContents.IndexOf(marker) + marker.Length - 1)
+        let markerPos = Range.mkPos (Range.Line.fromZ line.Line) (line.Character + marker.Length - 1)
         match ranges |> List.tryFind (fun (range, _) -> Range.rangeContainsPos range markerPos) with
         | None -> Assert.Fail("Cannot find colorization data for end of marker")
         | Some(_, ty) -> Assert.AreEqual(classificationType, FSharpClassificationTypes.getClassificationTypeName ty, "Classification data doesn't match for end of marker")
 
-    [<TestCase("(*1*)Guid", FSharpClassificationTypes.ReferenceType)>]
-    [<TestCase("(*2*)string", FSharpClassificationTypes.ReferenceType)>]
-    [<TestCase("(*3*)Guid", FSharpClassificationTypes.ValueType)>]
-    [<TestCase("(*4*)string", FSharpClassificationTypes.ReferenceType)>]
-    [<TestCase("(*5*)int", FSharpClassificationTypes.ValueType)>]
-    [<TestCase("(*6*)Guid", FSharpClassificationTypes.ValueType)>]
-    [<TestCase("(*7*)string", FSharpClassificationTypes.ReferenceType)>]
-    member public this.Measured_Types(marker: string, classificationType: string) =
+    [<TestCase("(*1*)", FSharpClassificationTypes.ValueType)>]
+    [<TestCase("(*2*)", FSharpClassificationTypes.ReferenceType)>]
+    [<TestCase("(*3*)", FSharpClassificationTypes.ValueType)>]
+    [<TestCase("(*4*)", FSharpClassificationTypes.ReferenceType)>]
+    [<TestCase("(*5*)", FSharpClassificationTypes.ValueType)>]
+    [<TestCase("(*6*)", FSharpClassificationTypes.ValueType)>]
+    [<TestCase("(*7*)", FSharpClassificationTypes.ReferenceType)>]
+    member __.Measured_Types(marker: string, classificationType: string) =
         verifyColorizerAtEndOfMarker(
-             """#light (*Light*)
+                """#light (*Light*)
                 open System
                 
                 [<MeasureAnnotatedAbbreviation>] type (*1*)Guid<[<Measure>] 'm> = Guid
