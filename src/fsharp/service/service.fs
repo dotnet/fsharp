7d62e29421ce61a5f3daa910b805d395e76cf16e
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2018-05-30 10:45:34 +0300

    refactoring and commenting

--- src/fsharp/service/service.fs
+++ src/fsharp/service/service.fs
@@ -522,7 +522,7 @@ type TypeCheckInfo
     // Return only items with the specified name
     let FilterDeclItemsByResidue (getItem: 'a -> Item) residue (items: 'a list) = 
         let attributedResidue = residue + "Attribute"
-        let nameMatchesResidue name = residue = name || attributedResidue = name
+        let nameMatchesResidue name = (residue = name) || (attributedResidue = name)
 
         items |> List.filter (fun x -> 
             let item = getItem x
@@ -532,11 +532,8 @@ type TypeCheckInfo
             | Item.CtorGroup (_, meths) ->
                 nameMatchesResidue n1 ||
                 meths |> List.exists (fun meth ->
-                    match meth.ApparentEnclosingAppType with
-                    | TType_app (tyref, _) ->
-                        tyref.IsProvided || nameMatchesResidue tyref.DisplayName
-                    | _ -> nameMatchesResidue meth.DisplayName 
-                )
+                    let tcref = meth.ApparentEnclosingTyconRef
+                    tcref.IsProvided || nameMatchesResidue tcref.DisplayName)
             | _ -> residue = n1)
             
     /// Post-filter items to make sure they have precisely the right name
@@ -1135,6 +1132,8 @@ type TypeCheckInfo
                 match GetDeclItemsForNamesAtPosition(ctok, None,namesOpt,None,None,line,lineStr,colAtEndOfNames,ResolveTypeNamesToCtors,ResolveOverloads.No,(fun() -> []),fun _ -> false) with
                 | None -> FSharpMethodGroup("",[| |])
                 | Some (items, denv, _, m) -> 
+                    // GetDeclItemsForNamesAtPosition returns Items.Types and Item.CtorGroup for `new T(|)`, 
+                    // the Item.Types is not needed here as it duplicates (at best) parameterless ctor.
                     let ctors = items |> List.filter (fun x -> match x.Item with Item.CtorGroup _ -> true | _ -> false)
                     let items =
                         match ctors with
