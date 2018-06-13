277f8cf73fccd45cd0592e5732185489b47efe12
Don Syme <donsyme@fastmail.fm>
2018-06-13 21:12:07 +0200

    make sure we fun both kinds of FCS tests

--- fcs/build.fsx
+++ fcs/build.fsx
@@ -98,7 +98,8 @@ Target "Test" (fun _ ->
     runDotnet __SOURCE_DIRECTORY__ "build ../tests/projects/Sample_NETCoreSDK_FSharp_Library_netstandard2_0/Sample_NETCoreSDK_FSharp_Library_netstandard2_0.fsproj -v n"
 
     // Now run the tests
-    runDotnet __SOURCE_DIRECTORY__ "test FSharp.Compiler.Service.Tests/FSharp.Compiler.Service.Tests.fsproj -v n -c Release"
+    runDotnet __SOURCE_DIRECTORY__ "test FSharp.Compiler.Service.Tests/FSharp.Compiler.Service.Tests.fsproj -v n -c Release -f net46"
+    runDotnet __SOURCE_DIRECTORY__ "test FSharp.Compiler.Service.Tests/FSharp.Compiler.Service.Tests.fsproj -v n -c Release -f netcoreapp2.0"
 )
 
 Target "NuGet" (fun _ ->
