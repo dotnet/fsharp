e6233331508d6b219764137091e0da00881f854d
Isaac Abraham <isaac.abraham@gmail.com>
2018-07-23 11:26:46 +0200

    More unit test whack-a-mole.

--- tests/fsharpqa/Source/Warnings/NestedElseBranchHasWrongType.fs
+++ tests/fsharpqa/Source/Warnings/NestedElseBranchHasWrongType.fs
@@ -1,5 +1,5 @@
 // #Warnings
-//<Expects status="Error" id="FS0001">All branches of an 'if' expression must have the same type.</Expects>
+//<Expects status="Error" id="FS0001">All branches of an 'if' expression must return values of the same type as the first branch, which here is 'bool'. This branch returns a value of type 'string'.</Expects>
 
 let x = 1
 if x = 1 then true
