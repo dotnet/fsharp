96b6f0c498776748b334c5bff679bc7aaf972374
Tom Davies <todavies5@gmail.com>
2018-09-04 15:31:38 +0100

    Sketch out example Map and Apply impls

--- scratch/data/SimpleBuilder.fsx
+++ scratch/data/SimpleBuilder.fsx
@@ -2,35 +2,47 @@
 
 [<Struct>]
 type OptionalBuilder =
-  member __.Bind(opt, binder) =
-    match opt with
-    | Some value -> binder value
-    | None -> None
-  member __.Return(value) =
-    Some value
+
+    member __.Bind(opt, f) =
+        match opt with
+        | Some x -> f x
+        | None -> None
     
-let optional = OptionalBuilder()
+    // TODO Make this actually get called when `let! ... and! ...` syntax is used (and automagically if RHSs allow it?)
+    member __.Apply(x : 'a option, f : ('a -> 'b) option) : 'b option =
+        match f, x with
+        | Some f, Some x -> f x
+        | _ -> None
+
+    // TODO Not needed, but for maximum efficiency, we want to use this if it is defined
+    member __.Map(x : 'a option, f : 'a -> 'b) : 'b option =
+        match x with
+        | Some x -> f x
+        | None -> None
+
+    member __.Return(x) =
+        Some x
+    
+let opt = OptionalBuilder()
 
 let x = Some 1
 let y = Some "A"
 let z : float option = None
 
 let foo =
-    optional
-        {
-            let! x' = x
-            let! y' = y
-            let! z' = z
-            return sprintf "x = %d, y = %s, z = %f" x y z
-        }
+    opt {
+        let! x' = x
+        let! y' = y
+        let! z' = z
+        return sprintf "x = %d, y = %s, z = %f" x y z
+    }
 
 (*
 let foo' =
-    optional
-        {
-            let! x' = x
-            and! y' = y
-            and! z' = z
-            return sprintf "x = %d, y = %s, z = %f" x y z
-        }
+    opt {
+        let! x' = x
+        and! y' = y
+        and! z' = z
+        return sprintf "x = %d, y = %s, z = %f" x y z
+    }
 *)
