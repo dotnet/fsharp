33751fca0d35c8791fb229d99e9f0f58c535df67
Tom Davies <todavies5@gmail.com>
2018-09-04 13:22:16 +0100

    Add .fsx with an builder in it as an example

---
+++ scratch/data/SimpleBuilder.fsx
@@ -0,0 +1,125 @@
+// TODO Work towards defining an option applicative builder here
+
+// Computations that can be run step by step
+type Eventually<'T> =
+    | Done of 'T
+    | NotYetDone of (unit -> Eventually<'T>)
+
+module Eventually =
+    // The bind for the computations. Append 'func' to the
+    // computation.
+    let rec bind func expr =
+        match expr with
+        | Done value -> NotYetDone (fun () -> func value)
+        | NotYetDone work -> NotYetDone (fun () -> bind func (work()))
+
+    // Return the final value wrapped in the Eventually type.
+    let result value = Done value
+
+    type OkOrException<'T> =
+        | Ok of 'T
+        | Exception of System.Exception
+
+    // The catch for the computations. Stitch try/with throughout
+    // the computation, and return the overall result as an OkOrException.
+    let rec catch expr =
+        match expr with
+        | Done value -> result (Ok value)
+        | NotYetDone work ->
+            NotYetDone (fun () ->
+                let res = try Ok(work()) with | exn -> Exception exn
+                match res with
+                | Ok cont -> catch cont // note, a tailcall
+                | Exception exn -> result (Exception exn))
+
+    // The delay operator.
+    let delay func = NotYetDone (fun () -> func())
+
+    // The stepping action for the computations.
+    let step expr =
+        match expr with
+        | Done _ -> expr
+        | NotYetDone func -> func ()
+
+    // The rest of the operations are boilerplate.
+    // The tryFinally operator.
+    // This is boilerplate in terms of "result", "catch", and "bind".
+    let tryFinally expr compensation =
+        catch (expr)
+        |> bind (fun res -> 
+            compensation();
+            match res with
+            | Ok value -> result value
+            | Exception exn -> raise exn)
+
+    // The tryWith operator.
+    // This is boilerplate in terms of "result", "catch", and "bind".
+    let tryWith exn handler =
+        catch exn
+        |> bind (function Ok value -> result value | Exception exn -> handler exn)
+
+    // The whileLoop operator.
+    // This is boilerplate in terms of "result" and "bind".
+    let rec whileLoop pred body =
+        if pred() then body |> bind (fun _ -> whileLoop pred body)
+        else result ()
+
+    // The sequential composition operator.
+    // This is boilerplate in terms of "result" and "bind".
+    let combine expr1 expr2 =
+        expr1 |> bind (fun () -> expr2)
+
+    // The using operator.
+    let using (resource: #System.IDisposable) func =
+        tryFinally (func resource) (fun () -> resource.Dispose())
+
+    // The forLoop operator.
+    // This is boilerplate in terms of "catch", "result", and "bind".
+    let forLoop (collection:seq<_>) func =
+        let ie = collection.GetEnumerator()
+        tryFinally 
+            (whileLoop 
+                (fun () -> ie.MoveNext()) 
+                (delay (fun () -> let value = ie.Current in func value)))
+            (fun () -> ie.Dispose())
+
+// The builder class.
+type EventuallyBuilder() =
+    member x.Bind(comp, func) = Eventually.bind func comp
+    member x.Return(value) = Eventually.result value
+    member x.ReturnFrom(value) = value
+    member x.Combine(expr1, expr2) = Eventually.combine expr1 expr2
+    member x.Delay(func) = Eventually.delay func
+    member x.Zero() = Eventually.result ()
+    member x.TryWith(expr, handler) = Eventually.tryWith expr handler
+    member x.TryFinally(expr, compensation) = Eventually.tryFinally expr compensation
+    member x.For(coll:seq<_>, func) = Eventually.forLoop coll func
+    member x.Using(resource, expr) = Eventually.using resource expr
+
+let eventually = new EventuallyBuilder()
+
+let comp = eventually {
+    for x in 1..2 do
+        printfn " x = %d" x
+    return 3 + 4 }
+
+// Try the remaining lines in F# interactive to see how this 
+// computation expression works in practice.
+let step x = Eventually.step x
+
+// returns "NotYetDone <closure>"
+comp |> step
+
+// prints "x = 1"
+// returns "NotYetDone <closure>"
+comp |> step |> step
+
+// prints "x = 1"
+// prints "x = 2"
+// returns "NotYetDone <closure>"
+comp |> step |> step |> step |> step |> step |> step
+
+// prints "x = 1"
+// prints "x = 2"
+// returns "Done 7"
+comp |> step |> step |> step |> step |> step |> step |> step |> step
