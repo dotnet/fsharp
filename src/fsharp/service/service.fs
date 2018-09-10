4f39394da7543fcd0b7e2fddd27cb3f5a320cb8d
TIHan <lol.tihan@gmail.com>
2018-09-10 15:43:13 -0700

    Using instance instead of static errorlogger

--- src/fsharp/service/service.fs
+++ src/fsharp/service/service.fs
@@ -1598,7 +1598,7 @@ module internal Parser =
                 let isExe = options.IsExe
                 try Some (ParseInput(lexfun, errHandler.ErrorLogger, lexbuf, None, fileName, (isLastCompiland, isExe)))
                 with e ->
-                    stopProcessingRecovery e Range.range0 // don't re-raise any exceptions, we must return None.
+                    errHandler.ErrorLogger.StopProcessingRecovery e Range.range0 // don't re-raise any exceptions, we must return None.
                     None)
         errHandler.CollectedDiagnostics, parseResult, errHandler.AnyErrors
 
