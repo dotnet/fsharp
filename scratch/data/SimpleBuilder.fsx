190674c3014832c65054d9e6651cc047468f5e76
Tom Davies <todavies5@gmail.com>
2018-09-18 14:48:03 +0100

    Remove bind from builder in SimpleBuilder.fsx to prove it is not needed

--- scratch/data/SimpleBuilder.fsx
+++ scratch/data/SimpleBuilder.fsx
@@ -1,31 +1,14 @@
-// TODO Work towards defining an option applicative builder here
 
 [<Struct>]
 type OptionalBuilder =
 
-    member __.Bind(opt, f) =
-        match opt with
-        | Some x -> f x
-        | None -> None
-    
-    // TODO Make this actually get called when `let! ... and! ...` syntax is used (and automagically if RHSs allow it?)
     member __.Apply(fOpt : ('a -> 'b) option, xOpt : 'a option) : 'b option =
         match fOpt, xOpt with
         | Some f, Some x -> Some <| f x
         | _ -> None
 
-    // TODO Not needed, but for maximum efficiency, we want to use this if it is defined
-    member __.Map(f : 'a -> 'b, xOpt : 'a option) : 'b option =
-        match xOpt with
-        | Some x -> Some <| f x
-        | None -> None
-
     member __.Return(x) =
         Some x
-
-    //member __.Zero () = // TODO: Remove - just here to explore current compiler limitations
-    //    None
-    
 let opt = OptionalBuilder()
 
 let fOpt = Some (sprintf "f (x = %d) (y = %s) (z = %f)")
