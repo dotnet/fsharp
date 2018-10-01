bd93b046bcf8bf36b13755c1d4dcfa43f4bd8524
Tom Davies <todavies5@gmail.com>
2018-10-01 17:37:07 +0100

    Make new CE tests closer to existing ones

---
+++ tests/fsharpqa/Source/ComputationExpressions/LetBangAndBangDesugarsCorrectly.fsx
@@ -0,0 +1,20 @@
+// #Misc #AppCE
+
+//<Expects status=success>val result : Trace int = Trace 3</Expects>
+//<Expects status=success>val trace : TraceOp list = [ Apply 2 ; Apply 1 ; Return ]</Expects>
+
+open ComputationsExpressions.Test
+
+let result, trace =
+    let tracer = TraceBuilder()
+
+    let ceResult : int Trace =
+        tracer {
+            let! x = Trace 1
+            and! y = Trace 2
+            return 1 + 2
+        }
+
+    ceResult, tracer.GetTrace ()
+;;
+#q;;
