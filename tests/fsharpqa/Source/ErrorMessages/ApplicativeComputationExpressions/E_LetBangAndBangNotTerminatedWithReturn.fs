b845f520071e3ec5198100cb6ce641852c89d792
Tom Davies <todavies5@gmail.com>
2018-10-01 12:03:42 +0100

    Make new applicative CE error msg tests pass locally

--- tests/fsharpqa/Source/ErrorMessages/ApplicativeComputationExpressions/E_LetBangAndBangNotTerminatedWithReturn.fs
+++ tests/fsharpqa/Source/ErrorMessages/ApplicativeComputationExpressions/E_LetBangAndBangNotTerminatedWithReturn.fs
@@ -1,9 +1,9 @@
 // #ErrorMessages
-//<Expects id="FS3243" status="error" span="(16,19)">Expecting 'return' but saw something else. </Expects>
-//<Expects>Applicative computation expressions must be of the form 'let! <pat1> = <expr2> </Expects>
-//<Expects>and! <pat2> = <expr2> and! ... and! <patN> = <exprN> return <exprBody>'.</Expects>
+//<Expects id="FS3243" status="error" span="(11,9)">Expecting 'return' but saw something else. Applicative computation expressions must be of the form 'let! <pat1> = <expr2> and! <pat2> = <expr2> and! ... and! <patN> = <exprN> return <exprBody>'.</Expects>
 
 namespace ApplicativeComputationExpressions
+
+module E_LetBangAndBangNotTerminatedWithReturn =
 
     eventually {
         let! x = Eventually.NotYetDone (fun () -> Eventually.Done 4)
--- tests/fsharpqa/Source/ErrorMessages/ApplicativeComputationExpressions/E_PatternOnLhsOfUseBangAndUseBangBinding.fs
+++ tests/fsharpqa/Source/ErrorMessages/ApplicativeComputationExpressions/E_PatternOnLhsOfUseBangAndUseBangBinding.fs
@@ -1,9 +1,9 @@
 // #ErrorMessages
-//<Expects id="FS3243" status="error" span="(16,19)">Pattern matching is not allowed on the left-hand side of the equals. </Expects>
-//<Expects>'use! ... anduse! ...' bindings must be of the form </Expects>
-//<Expects>'use! <var> = <expr>' or 'anduse! <var> = <expr>'.</Expects>
+//<Expects id="FS3244" status="error" span="(9,14)">Pattern matching is not allowed on the left-hand side of the equals. 'use! ... anduse! ...' bindings must be of the form 'use! <var> = <expr>' or 'anduse! <var> = <expr>'.</Expects>
 
 namespace ApplicativeComputationExpressions
+
+module E_PatternOnLhsOfUseBangAndUseBangBinding =
 
     eventually {
         use! (x,_) = Eventually.Done (FakeDisposable 3, 19)
