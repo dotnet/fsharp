d1460d8604b3ddacb82232c7341571c2b56f3b1e
Grégoire Geis <git@gregoirege.is>
2018-10-26 18:18:37 +0200

    Added test case for #warnon pragma.

---
+++ tests/fsharpqa/Source/Warnings/WarnAfterWarnonPragma.fs
@@ -0,0 +1,10 @@
+﻿// #Warnings
+//<Expects status="Warning" span="(8,11)" id="FS0025">The result of this equality expression has type 'bool' and is implicitly discarded. Consider using 'let' to bind the result to a name, e.g. 'let result = expression'.</Expects>
+
+#nowarn "25"
+let foo = function [] -> true
+#warnon "25"
+
+let bar = function [] -> true
+
+exit 0
