5b9e6ddf34084f399faeb207fb3e6b9ada284c83
TIHan <lol.tihan@gmail.com>
2018-10-31 13:23:06 -0700

    Removing dead code

--- vsintegration/src/FSharp.Editor/LanguageService/ProjectSitesAndFiles.fs
+++ vsintegration/src/FSharp.Editor/LanguageService/ProjectSitesAndFiles.fs
@@ -30,28 +30,3 @@ let projectDisplayNameOf projectFileName =
 /// A value and a function to recompute/refresh the value.  The function is passed a flag indicating if a refresh is happening.
 type Refreshable<'T> = 'T * (bool -> 'T)
 
-/// Convert from FSharpProjectOptions into IProjectSite.
-type private ProjectSiteOfScriptFile(filename:string, referencedProjectFileNames, checkOptions: FSharpProjectOptions) = 
-    interface IProjectSite with
-        override __.Description = sprintf "Script Closure at Root %s" filename
-        override __.CompilationSourceFiles = checkOptions.SourceFiles
-        override __.CompilationOptions = checkOptions.OtherOptions
-        override __.CompilationReferences =
-             checkOptions.OtherOptions 
-             |> Array.choose (fun flag -> if flag.StartsWith("-r:") then Some flag.[3..] else None) 
-        override __.CompilationBinOutputPath = None
-        override __.ProjectFileName = checkOptions.ProjectFileName
-        override __.BuildErrorReporter with get() = None and set _ = ()
-        override __.AdviseProjectSiteChanges(_,_) = ()
-        override __.AdviseProjectSiteCleaned(_,_) = ()
-        override __.AdviseProjectSiteClosed(_,_) = ()
-        override __.IsIncompleteTypeCheckEnvironment = checkOptions.IsIncompleteTypeCheckEnvironment
-        override __.TargetFrameworkMoniker = ""
-        override __.ProjectGuid = ""
-        override __.LoadTime = checkOptions.LoadTime
-        override __.ProjectProvider = None
-
-    interface IHaveCheckOptions with
-        override __.OriginalCheckOptions() = (referencedProjectFileNames, checkOptions)
-
-    override __.ToString() = sprintf "ProjectSiteOfScriptFile(%s)" filename
