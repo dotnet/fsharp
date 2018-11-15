9b4962258f2c4bf2e5a05bf9589da548ac4ff178
Will Smith <lol.tihan@gmail.com>
2018-11-15 07:13:26 -0800

    Update Extensions.fs

--- vsintegration/src/FSharp.Editor/Common/Extensions.fs
+++ vsintegration/src/FSharp.Editor/Common/Extensions.fs
@@ -5,7 +5,6 @@ module internal Microsoft.VisualStudio.FSharp.Editor.Extensions
 
 open System
 open System.IO
-open System.Linq
 open Microsoft.CodeAnalysis
 open Microsoft.CodeAnalysis.Host
 open Microsoft.FSharp.Compiler.Ast
