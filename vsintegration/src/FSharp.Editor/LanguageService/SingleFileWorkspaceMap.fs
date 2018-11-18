44bf2a4a88ffc2157e111bb5d8d5bf09475e4881
TIHan <lol.tihan@gmail.com>
2018-11-18 12:12:01 -0800

    Create project per script file

--- vsintegration/src/FSharp.Editor/LanguageService/SingleFileWorkspaceMap.fs
+++ vsintegration/src/FSharp.Editor/LanguageService/SingleFileWorkspaceMap.fs
@@ -2,24 +2,14 @@
 
 namespace Microsoft.VisualStudio.FSharp.Editor
 
-#nowarn "40"
-
 open System
 open System.Collections.Concurrent
-open System.Collections.Generic
-open System.Diagnostics
-open System.IO
-open System.Linq
-open System.Runtime.CompilerServices
 open Microsoft.CodeAnalysis
 open Microsoft.VisualStudio
 open Microsoft.VisualStudio.FSharp.Editor
 open Microsoft.VisualStudio.LanguageServices.Implementation.ProjectSystem
 open Microsoft.VisualStudio.LanguageServices.ProjectSystem
 open Microsoft.VisualStudio.Shell.Interop
-open Microsoft.VisualStudio.TextManager.Interop
-open Microsoft.VisualStudio.Editor
-open Microsoft.CodeAnalysis.Text
 open Microsoft.VisualStudio.LanguageServices
 
 [<Sealed>]
@@ -30,7 +20,6 @@ type internal SingleFileWorkspaceMap(workspace: VisualStudioWorkspace,
                                      rdt: IVsRunningDocumentTable) as this =
 
     let files = ConcurrentDictionary(StringComparer.OrdinalIgnoreCase)
-    let projectContext = projectContextFactory.CreateProjectContext(FSharpConstants.FSharpLanguageName, FSharpConstants.FSharpMiscellaneousFilesName, null, Guid.NewGuid(), null, null)
 
     let createSourceCodeKind (filePath: string) =
         if isScriptFile filePath then
@@ -38,31 +27,35 @@ type internal SingleFileWorkspaceMap(workspace: VisualStudioWorkspace,
         else
             SourceCodeKind.Regular
 
+    let createProjectContext filePath =
+        let projectContext = projectContextFactory.CreateProjectContext(FSharpConstants.FSharpLanguageName, filePath, filePath, Guid.NewGuid(), null, null)
+        projectContext.DisplayName <- FSharpConstants.FSharpMiscellaneousFilesName
+        projectContext.AddSourceFile(filePath, sourceCodeKind = createSourceCodeKind filePath)
+        projectContext
+
     do
         miscFilesWorkspace.DocumentOpened.Add(fun args ->
             let document = args.Document
             if document.Project.Language = FSharpConstants.FSharpLanguageName && workspace.CurrentSolution.GetDocumentIdsWithFilePath(document.FilePath).Length = 0 then
-                projectContext.AddSourceFile(document.FilePath, sourceCodeKind = createSourceCodeKind document.FilePath)
+                files.[document.FilePath] <- createProjectContext document.FilePath
         )
 
         workspace.DocumentOpened.Add(fun args ->
             let document = args.Document
-            if document.Project.Name = FSharpConstants.FSharpMiscellaneousFilesName then
-                files.[document.FilePath] <- document
-            elif document.Project.Language = FSharpConstants.FSharpLanguageName then
+            if document.Project.Language = FSharpConstants.FSharpLanguageName && document.Project.Name <> FSharpConstants.FSharpMiscellaneousFilesName then
                 match files.TryRemove(document.FilePath) with
-                | true, _ ->
+                | true, projectContext ->
                     optionsManager.ClearSingleFileOptionsCache(document.Id)
-                    projectContext.RemoveSourceFile(document.FilePath)
+                    projectContext.Dispose()
                 | _ -> ()
         )
 
         workspace.DocumentClosed.Add(fun args ->
             let document = args.Document
             match files.TryRemove(document.FilePath) with
-            | true, _ ->
+            | true, projectContext ->
                 optionsManager.ClearSingleFileOptionsCache(document.Id)
-                projectContext.RemoveSourceFile(document.FilePath)
+                projectContext.Dispose()
             | _ -> ()
         )
 
@@ -71,39 +64,47 @@ type internal SingleFileWorkspaceMap(workspace: VisualStudioWorkspace,
 
     interface IVsRunningDocTableEvents with
 
-        member __.OnAfterAttributeChange(_, _) = VSConstants.S_OK
+        member __.OnAfterAttributeChange(_, _) = VSConstants.E_NOTIMPL
 
-        member __.OnAfterDocumentWindowHide(_, _) = VSConstants.S_OK
+        member __.OnAfterDocumentWindowHide(_, _) = VSConstants.E_NOTIMPL
 
-        member __.OnAfterFirstDocumentLock(_, _, _, _) = VSConstants.S_OK
+        member __.OnAfterFirstDocumentLock(_, _, _, _) = VSConstants.E_NOTIMPL
 
-        member __.OnAfterSave(_) = VSConstants.S_OK
+        member __.OnAfterSave(_) = VSConstants.E_NOTIMPL
 
-        member __.OnBeforeDocumentWindowShow(_, _, _) = VSConstants.S_OK
+        member __.OnBeforeDocumentWindowShow(_, _, _) = VSConstants.E_NOTIMPL
 
-        member __.OnBeforeLastDocumentUnlock(_, _, _, _) = VSConstants.S_OK
+        member __.OnBeforeLastDocumentUnlock(_, _, _, _) = VSConstants.E_NOTIMPL
 
     interface IVsRunningDocTableEvents2 with
 
-        member __.OnAfterAttributeChange(_, _) = VSConstants.S_OK
+        member __.OnAfterAttributeChange(_, _) = VSConstants.E_NOTIMPL
 
         member __.OnAfterAttributeChangeEx(_, grfAttribs, _, _, pszMkDocumentOld, _, _, pszMkDocumentNew) = 
             // Handles renaming of a misc file
             if (grfAttribs &&& (uint32 __VSRDTATTRIB.RDTA_MkDocument)) <> 0u && files.ContainsKey(pszMkDocumentOld) then
                 match files.TryRemove(pszMkDocumentOld) with
-                | true, document ->
-                    optionsManager.ClearSingleFileOptionsCache(document.Id)
-                    projectContext.RemoveSourceFile(pszMkDocumentOld)
-                    projectContext.AddSourceFile(pszMkDocumentNew, sourceCodeKind = createSourceCodeKind pszMkDocumentNew)
+                | true, projectContext ->
+                    let project = workspace.CurrentSolution.GetProject(projectContext.Id)
+                    if project <> null then
+                        let documentOpt =
+                            project.Documents 
+                            |> Seq.tryFind (fun x -> String.Equals(x.FilePath, pszMkDocumentOld, StringComparison.OrdinalIgnoreCase))
+                        match documentOpt with
+                        | None -> ()
+                        | Some(document) ->                           
+                            optionsManager.ClearSingleFileOptionsCache(document.Id)
+                            projectContext.Dispose()
+                            files.[pszMkDocumentNew] <- createProjectContext pszMkDocumentNew
                 | _ -> ()
             VSConstants.S_OK
 
-        member __.OnAfterDocumentWindowHide(_, _) = VSConstants.S_OK
+        member __.OnAfterDocumentWindowHide(_, _) = VSConstants.E_NOTIMPL
 
-        member __.OnAfterFirstDocumentLock(_, _, _, _) = VSConstants.S_OK
+        member __.OnAfterFirstDocumentLock(_, _, _, _) = VSConstants.E_NOTIMPL
 
-        member __.OnAfterSave(_) = VSConstants.S_OK
+        member __.OnAfterSave(_) = VSConstants.E_NOTIMPL
 
-        member __.OnBeforeDocumentWindowShow(_, _, _) = VSConstants.S_OK
+        member __.OnBeforeDocumentWindowShow(_, _, _) = VSConstants.E_NOTIMPL
 
-        member __.OnBeforeLastDocumentUnlock(_, _, _, _) = VSConstants.S_OK
+        member __.OnBeforeLastDocumentUnlock(_, _, _, _) = VSConstants.E_NOTIMPL
