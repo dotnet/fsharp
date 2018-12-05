5a54ebd34682f7a57e3b6e5cace269b9d0eebe81
Victor Peter Rouven Müller <mueller.vpr@gmail.com>
2018-12-05 22:22:29 +0100

    Fix #5531 (#5778)
    * Fix #5531
    
    This is a working fix for the example of #5531.
    However there are still some points open before I think it's a good idea to merge this:
    - Discuss the change of this proposal, is it OK to prefer overrides of methods and so ignore base implementations?
    - Extend the check to the complete inheritance graph instead of a single "look-back"
    - Only ignore a methinfo if the signature of both match (so respect different overloads)
    
    @dsyme It would be great if you could have a look whether this check is allowed at this location or should appear earlier.
    
    * Fix left points.
    
    Signed-off-by: realvictorprm <mueller.vpr@gmail.com>
    
    * another approach to fix
    
    * remove old approach
    
    * Remove approach again and apply different approach.
    
    Signed-off-by: realvictorprm <mueller.vpr@gmail.com>
    
    * Adjusting predicate to reflect correct behaviour.
    
    * Revert to old List.merge strategy.
    
    * Apply review and add tests
    
    Signed-off-by: realvictorprm <mueller.vpr@gmail.com>
    
    * Try fixing weird CI failure
    
    Signed-off-by: realvictorprm <mueller.vpr@gmail.com>
    
    * Fix test.
    
    Signed-off-by: realvictorprm <mueller.vpr@gmail.com>

--- tests/fsharp/tests.fs
+++ tests/fsharp/tests.fs
@@ -1756,6 +1756,36 @@ module RegressionTests =
 ////    let ``struct-tuple-bug-1-FSI_BASIC`` () = singleTestBuildAndRun "regression/struct-tuple-bug-1" FSI_BASIC
 
 #if !FSHARP_SUITE_DRIVES_CORECLR_TESTS
+    [<Test>]
+    let ``SRTP doesn't handle calling member hiding hinherited members`` () =
+        let cfg = testConfig "regression/5531"
+
+        let outFile = "compilation.output.test.txt"
+        let expectedFile = "compilation.output.test.bsl"
+        
+        fscBothToOut cfg outFile "%s --nologo -O" cfg.fsc_flags ["test.fs"]
+
+        let diff = fsdiff cfg outFile expectedFile
+
+        match diff with
+        | "" -> ()
+        | _ ->
+            Assert.Fail (sprintf "'%s' and '%s' differ; %A" (getfullpath cfg outFile) (getfullpath cfg expectedFile) diff)
+            
+        let outFile2 = "output.test.txt"
+        let expectedFile2 = "output.test.bsl"
+
+        execBothToOut cfg (cfg.Directory) outFile2 (cfg.Directory ++ "test.exe") ""
+
+        let diff2 = fsdiff cfg outFile2 expectedFile2
+
+        match diff2 with
+        | "" -> ()
+        | _ ->
+            Assert.Fail (sprintf "'%s' and '%s' differ; %A" (getfullpath cfg outFile2) (getfullpath cfg expectedFile2) diff2)
+#endif
+
+#if !FSHARP_SUITE_DRIVES_CORECLR_TESTS
     // This test is disabled in coreclr builds dependent on fixing : https://github.com/Microsoft/visualfsharp/issues/2600
     [<Test>]
     let ``struct-measure-bug-1`` () = 
