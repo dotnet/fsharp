33422b1f4ecd364b7b0e107264cb10c4a2a2fb99
Jakub Majocha <majocha@outlook.com>
2018-12-10 18:44:58 +0100

    add comments

--- vsintegration/src/FSharp.Editor/Options/UIHelpers.fs
+++ vsintegration/src/FSharp.Editor/Options/UIHelpers.fs
@@ -13,6 +13,8 @@ module internal OptionsUIHelpers =
     type AbstractOptionPage<'options>() as this =
         inherit UIElementDialogPage()
 
+        // this replicates the logic used by Roslyn option pages. Some following comments have been copied from
+        // https://github.com/dotnet/roslyn/blob/5b125935f891b3c20405459f8f7e1cdfdc2cfa3d/src/VisualStudio/Core/Impl/Options/AbstractOptionPage.cs
         let mutable needsLoadOnNextActivate = true
 
         let view = lazy this.CreateView()
@@ -29,14 +31,30 @@ module internal OptionsUIHelpers =
 
         override this.OnActivate _ =
             if needsLoadOnNextActivate then
+                // It looks like the bindings do not always pick up new source, unless we cycle the DataContext like this
+                view.Value.DataContext <- DependencyProperty.UnsetValue
                 view.Value.DataContext <- optionService.Value.Read<'options>()
                 needsLoadOnNextActivate <- false
 
         override this.SaveSettingsToStorage() = 
             downcast view.Value.DataContext |> optionService.Value.Write<'options>
+            // Make sure we load the next time the page is activated, in case if options changed
+            // programmatically between now and the next time the page is activated
             needsLoadOnNextActivate <- true
 
-        override this.LoadSettingsFromStorage() = 
+        override this.LoadSettingsFromStorage() =
+            // This gets called in two situations:
+            //
+            // 1) during the initial page load when you first activate the page, before OnActivate
+            //    is called.
+            // 2) during the closing of the dialog via Cancel/close when options don't need to be
+            //    saved. The intent here is the settings get reloaded so the next time you open the
+            //    page they are properly populated.
+            //
+            // This second one is tricky, because we don't actually want to update our controls
+            // right then, because they'd be wrong the next time the page opens -- it's possible
+            // they may have been changed programmatically. Therefore, we'll set a flag so we load
+            // next time         
             needsLoadOnNextActivate <- true
 
     //data binding helpers
