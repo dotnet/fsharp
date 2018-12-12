4291fb164c61e364a84602e712470b5d7052029f
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-12-12 16:57:22 +0300

    Always emit conditional attributes in FCS (fixes #3890)

--- src/fsharp/TypeChecker.fs
+++ src/fsharp/TypeChecker.fs
@@ -10829,8 +10829,10 @@ and TcAttribute canFail cenv (env: TcEnv) attrTgt (synAttr: SynAttribute)  =
     let conditionalCallDefineOpt = TryFindTyconRefStringAttribute cenv.g mAttr cenv.g.attrib_ConditionalAttribute tcref 
 
     match conditionalCallDefineOpt with 
+#if !COMPILER_SERVICE_AS_DLL 
     | Some d when not (List.contains d cenv.conditionalDefines) -> 
         [], false
+#endif
     | _ ->
 
          // REVIEW: take notice of inherited? 
