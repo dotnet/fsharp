a1c503177868a7364ffe05945413dea94f470cad
TIHan <lol.tihan@gmail.com>
2018-12-14 10:08:25 -0800

    Removed addNewLine

--- vsintegration/tests/UnitTests/SemanticColorizationServiceTests.fs
+++ vsintegration/tests/UnitTests/SemanticColorizationServiceTests.fs
@@ -27,18 +27,13 @@ type SemanticClassificationServiceTests() =
         Stamp = None
     }
 
-    // Adding this new-line character at the end of the source seems odd but is required for some unit tests
-    // Todo: fix tests
-    let addNewLine (source: string) =
-        if source.Length = 0 || not (source.[source.Length - 1] = '\n') then source + "\n" else source
-
     let checker = FSharpChecker.Create()
     let perfOptions = { LanguageServicePerformanceOptions.Default with AllowStaleCompletionResults = false }
 
     let getRanges (sourceText: string) : (Range.range * SemanticClassificationType) list =
         asyncMaybe {
 
-            let! _, _, checkFileResults = checker.ParseAndCheckDocument(filePath, 0, SourceText.From(addNewLine sourceText), projectOptions, perfOptions, "")
+            let! _, _, checkFileResults = checker.ParseAndCheckDocument(filePath, 0, SourceText.From(sourceText), projectOptions, perfOptions, "")
             return checkFileResults.GetSemanticClassification(None)
         } 
         |> Async.RunSynchronously
@@ -46,7 +41,7 @@ type SemanticClassificationServiceTests() =
         |> List.collect Array.toList
 
     let verifyClassificationAtEndOfMarker(fileContents: string, marker: string, classificationType: string) =
-        let text = SourceText.From(addNewLine fileContents)
+        let text = SourceText.From(fileContents)
         let ranges = getRanges fileContents
         let line = text.Lines.GetLinePosition (fileContents.IndexOf(marker) + marker.Length - 1)
         let markerPos = Range.mkPos (Range.Line.fromZ line.Line) (line.Character + marker.Length - 1)
@@ -55,7 +50,7 @@ type SemanticClassificationServiceTests() =
         | Some(_, ty) -> Assert.AreEqual(classificationType, FSharpClassificationTypes.getClassificationTypeName ty, "Classification data doesn't match for end of marker")
 
     let verifyNoClassificationDataAtEndOfMarker(fileContents: string, marker: string, classificationType: string) =
-        let text = SourceText.From(addNewLine fileContents)
+        let text = SourceText.From(fileContents)
         let ranges = getRanges fileContents
         let line = text.Lines.GetLinePosition (fileContents.IndexOf(marker) + marker.Length - 1)
         let markerPos = Range.mkPos (Range.Line.fromZ line.Line) (line.Character + marker.Length - 1)
