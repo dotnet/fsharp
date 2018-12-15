3f8bcb2120e75e842c8ab567ab2ee32cd087c11d
Will Smith <lol.tihan@gmail.com>
2018-12-15 15:13:27 -0800

    Initial benchmarking

---
+++ benchmarks/CompilerServiceBenchmarks/Program.fs
@@ -0,0 +1,60 @@
+﻿open System
+open System.IO
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open Microsoft.FSharp.Compiler.ErrorLogger
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open System.Text
+
+[<ClrJob(baseline = true)>]
+type CompilerServiceParsing() =
+
+    let mutable checkerOpt = None
+
+    let mutable sourceOpt = None
+
+    let parsingOptions =
+        {
+            SourceFiles = [|"TypeChecker.fs"|]
+            ConditionalCompilationDefines = []
+            ErrorSeverityOptions = FSharpErrorSeverityOptions.Default
+            IsInteractive = false
+            LightSyntax = None
+            CompilingFsLib = false
+            IsExe = false
+        }
+
+    [<GlobalSetup>]
+    member __.Setup() =
+        match checkerOpt with
+        | None -> checkerOpt <- Some(FSharpChecker.Create())
+        | _ -> ()
+
+        match sourceOpt with
+        | None ->
+            let source = File.ReadAllText("""C:\visualfsharp\src\fsharp\TypeChecker.fs""")
+            sourceOpt <- Some(source)
+        | _ -> ()
+    
+    [<IterationSetup>]
+    member __.ParsingSetup() =
+        match checkerOpt with
+        | None -> failwith "no checker"
+        | Some(checker) ->
+            checker.InvalidateAll()
+            checker.ClearLanguageServiceRootCachesAndCollectAndFinalizeAllTransients()
+            checker.ParseFile("dummy.fs", "dummy", parsingOptions) |> Async.RunSynchronously |> ignore
+
+    [<Benchmark>]
+    member __.Parsing() =
+        match checkerOpt, sourceOpt with
+        | None, _ -> failwith "no checker"
+        | _, None -> failwith "no source"
+        | Some(checker), Some(source) ->
+            let results = checker.ParseFile("TypeChecker.fs", source, parsingOptions) |> Async.RunSynchronously
+            if results.ParseHadErrors then failwithf "parse had errors: %A" results.Errors
+
+[<EntryPoint>]
+let main argv =
+    let _ = BenchmarkRunner.Run<CompilerServiceParsing>()
+    0
