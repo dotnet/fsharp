d19a7787dff92f49208e9ff1d144a02ae3f8ff48
Will Smith <lol.tihan@gmail.com>
2019-02-08 14:15:54 -0800

    Fixed not struct constraint for anon records. Added better way to test source code.

---
+++ tests/FSharp.Compiler.UnitTests/Compiler.fs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.UnitTests
+
+open System
+open Microsoft.FSharp.Compiler.Text
+open Microsoft.FSharp.Compiler.SourceCodeServices
+
+open NUnit.Framework
+
+[<RequireQualifiedAccess>]
+module Compiler =
+
+    let checker = FSharpChecker.Create()
+
+    let private defaultProjectOptions =
+        {
+            ProjectFileName = "Z:\\test.fsproj"
+            ProjectId = None
+            SourceFiles = [|"test.fs"|]
+            OtherOptions = [||]
+            ReferencedProjects = [||]
+            IsIncompleteTypeCheckEnvironment = false
+            UseScriptResolutionRules = false
+            LoadTime = DateTime()
+            UnresolvedReferences = None
+            OriginalLoadReferences = []
+            ExtraProjectInfo = None
+            Stamp = None
+        }
+
+    let AssertPositive (source: string) =
+        let parseResults, fileAnswer = checker.ParseAndCheckFileInProject("test.fs", 0, SourceText.ofString source, defaultProjectOptions) |> Async.RunSynchronously
+
+        Assert.True(parseResults.Errors.Length = 0, sprintf "Parse errors: %A" parseResults.Errors)
+
+        match fileAnswer with
+        | FSharpCheckFileAnswer.Aborted _ -> Assert.Fail("Type Checker Aborted")
+        | FSharpCheckFileAnswer.Succeeded(typeCheckResults) ->
+
+        Assert.True(typeCheckResults.Errors.Length = 0, sprintf "Type Checker errors: %A" typeCheckResults.Errors)
---
+++ tests/FSharp.Compiler.UnitTests/Language/AnonRecords.fs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.UnitTests
+
+open NUnit.Framework
+
+[<TestFixture>]
+module AnonRecords =
+
+    [<Test>]
+    let NotStructConstraintPass() =
+        Compiler.AssertPositive("""
+type RefClass<'a when 'a : not struct>() = class end
+let rAnon = RefClass< {| R: int |}>()""")
+        
