eaf3e539ccf987b72595814fbd55ff8bf399455c
Will Smith <lol.tihan@gmail.com>
2019-02-08 16:15:30 -0800

    Added one more test

--- tests/FSharp.Compiler.UnitTests/Language/AnonRecords.fs
+++ tests/FSharp.Compiler.UnitTests/Language/AnonRecords.fs
@@ -24,6 +24,17 @@ let sAnon = StructClass<struct {| S: int |}>()
             """
 
     [<Test>]
+    let NotStructConstraintFail() =
+        Compiler.AssertSingleErrorTypeCheck 
+            """
+    type RefClass<'a when 'a : not struct>() = class end
+    let rAnon = RefClass<struct {| R: int |}>()
+            """ 
+            1
+            (3, 16, 3, 45)
+            "A generic construct requires that the type 'struct {|R : int|}' have reference semantics, but it does not, i.e. it is a struct"
+
+    [<Test>]
     let StructConstraintFail() =
         Compiler.AssertSingleErrorTypeCheck 
             """
