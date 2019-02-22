ba56217d96b5e9f29a8d4e59d6a444e6d02f479b
Kevin Ransom <codecutter@hotmail.com>
2019-02-22 00:01:09 -0800

    Avoid multiple dotnet cli downloads

--- fcs/build.fsx
+++ fcs/build.fsx
@@ -24,7 +24,14 @@ let isMono = false
 // Utilities
 // --------------------------------------------------------------------------------------
 
-let dotnetExePath = DotNetCli.InstallDotNetSDK "2.1.403"
+let dotnetExePath =
+    // Build.cmd normally downloads a dotnet cli to: <repo-root>\artifacts\toolset\dotnet
+    // check if there is one there to avoid downloading an additional one here
+    let pathToCli = Path.Combine(__SOURCE_DIRECTORY__, @"..\artifacts\toolset\dotnet\dotnet.exe")
+    if File.Exists(pathToCli) then
+        pathToCli
+    else
+        DotNetCli.InstallDotNetSDK "2.1.403"
 
 let runDotnet workingDir args =
     let result =
