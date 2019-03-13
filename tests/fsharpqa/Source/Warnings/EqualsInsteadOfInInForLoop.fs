4b915986709f804b406c128979dc4f6a7202e6a5
Don Syme <dsyme@microsoft.com>
2019-03-13 18:25:09 +0000

    fix test

--- tests/fsharpqa/Source/Warnings/EqualsInsteadOfInInForLoop.fs
+++ tests/fsharpqa/Source/Warnings/EqualsInsteadOfInInForLoop.fs
@@ -1,5 +1,5 @@
 // #Warnings
-//<Expects status="Error" id="FS3216">Unexpected symbol '=' in expression. Did you intend to use 'for x in y .. z do' instead?</Expects>
+//<Expects status="Error" id="FS3215">Unexpected symbol '=' in expression. Did you intend to use 'for x in y .. z do' instead?</Expects>
 
 for i = 0 .. 100 do
     ()
