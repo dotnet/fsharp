467ad883efa1b2a2d59843306ab6c63de66baa46
TIHan <lol.tihan@gmail.com>
2019-04-18 17:57:32 -0700

    Added compiler service memory analysis

---
+++ benchmarks/CompilerServiceMemoryAnalysisRunner/Program.fs
@@ -0,0 +1,116 @@
+﻿open System.Diagnostics
+open System.Collections.Generic
+open System.Threading.Tasks
+open Microsoft.Diagnostics.Runtime
+
+// WARNING: This type name can change.
+let CompilerServiceProjectCacheTypeName = "Internal.Utilities.Collections.MruCache<FSharp.Compiler.AbstractIL.Internal.Library+CompilationThreadToken,FSharp.Compiler.SourceCodeServices.FSharpProjectOptions,System.Tuple<Microsoft.FSharp.Core.FSharpOption<FSharp.Compiler.IncrementalBuilder>,FSharp.Compiler.SourceCodeServices.FSharpErrorInfo[],System.IDisposable>>"
+
+[<RequireQualifiedAccess>]
+type ClrInstance =
+    | Object of ClrObject
+    | ValueClass of ClrValueClass
+
+let rec getFullMemorySize (instance: ClrInstance) (objSet: HashSet<uint64>) =
+    match instance with
+    | ClrInstance.Object(clrInstance) ->
+
+        if clrInstance.IsNull || not (objSet.Add clrInstance.Address) then 0
+        else
+
+        let instanceFieldFullMemorySize =
+            clrInstance.Type.Fields
+            |> Array.ofSeq
+            |> Array.map (fun clrInstanceField ->
+                if clrInstanceField.Type.IsValueClass then
+                    let clrValueClass = clrInstance.GetValueClassField(clrInstanceField.Name)
+                    fun () -> getFullMemorySize (ClrInstance.ValueClass clrValueClass) objSet
+                elif clrInstanceField.Type.IsPrimitive then
+                    fun () -> clrInstanceField.Type.BaseSize
+                else
+                    let clrObject = clrInstance.GetObjectField(clrInstanceField.Name)
+                    fun () -> getFullMemorySize (ClrInstance.Object clrObject) objSet
+            )
+            |> Seq.sumBy (fun f -> f ())
+
+        clrInstance.Type.BaseSize + instanceFieldFullMemorySize
+
+    | ClrInstance.ValueClass(clrInstance) ->
+
+        let instanceFieldFullMemorySize =
+            clrInstance.Type.Fields
+            |> Seq.map (fun clrInstanceField ->
+                if clrInstanceField.Type.IsValueClass then
+                    let clrValueClass = clrInstance.GetValueClassField(clrInstanceField.Name)
+                    fun () -> getFullMemorySize (ClrInstance.ValueClass clrValueClass) objSet
+                elif clrInstanceField.Type.IsPrimitive then
+                    fun () -> clrInstanceField.Type.BaseSize
+                else
+                    let clrObject = clrInstance.GetObjectField(clrInstanceField.Name)
+                    fun () -> getFullMemorySize (ClrInstance.Object clrObject) objSet
+            )
+            |> Seq.sumBy (fun f -> f ())
+
+        clrInstance.Type.BaseSize + instanceFieldFullMemorySize
+
+let analyzeCompilerServiceProjectCache (cacheObject: ClrObject) (runtime: ClrRuntime) =
+    printfn "Analyzing compiler service project cache"
+
+    let totalByteCount = getFullMemorySize (ClrInstance.Object cacheObject) (HashSet())
+    printfn "Cache Size: %.00f MB" (single totalByteCount / 1024.f / 1024.f)
+
+    printfn "Finished analyzing compiler service project cache"
+
+let analyzeTestModule (runtime: ClrRuntime) (info: ModuleInfo) =
+    printfn "Analyzing test module"
+
+    for seg in runtime.Heap.Segments do
+        let mutable objId = seg.FirstObject
+        while objId <> 0UL do
+            let clrObject = runtime.Heap.GetObject(objId)
+            if not clrObject.IsNull && clrObject.Type.Name = CompilerServiceProjectCacheTypeName then
+                analyzeCompilerServiceProjectCache clrObject runtime
+                objId <- 0UL
+            else
+                objId <- seg.NextObject(objId)
+
+    printfn "Finished analyzing test module"
+
+let analyzeDataTarget (dataTarget: DataTarget) =
+    dataTarget.EnumerateModules()
+    |> Seq.iter (fun m ->
+        if m.FileName.Contains("CompilerServiceMemoryAnalysisTest.dll") then
+            analyzeTestModule (dataTarget.ClrVersions.[0].CreateRuntime()) m
+        else
+            ()
+    )
+
+[<EntryPoint>]
+let main _ =
+    use p = new Process()
+    p.StartInfo.FileName <- "dotnet"
+    p.StartInfo.Arguments <- "CompilerServiceMemoryAnalysisTest.dll"
+    p.StartInfo.UseShellExecute <- false
+    p.StartInfo.RedirectStandardOutput <- true
+    p.StartInfo.RedirectStandardInput <- true
+    p.OutputDataReceived.Add(fun args ->
+        if args.Data = "CompilerServiceMemoryAnalysisTest-Finished" then
+            try
+                let dataTarget = DataTarget.AttachToProcess(p.Id, 1000u)
+                analyzeDataTarget dataTarget
+                dataTarget.Dispose()
+                p.StandardInput.WriteLine()
+            with
+            | ex -> 
+                printfn "%A" ex
+                p.StandardInput.WriteLine()
+        else
+            printfn "\nCompilerServiceMemoryAnalysisTest Output: %A\n" args.Data 
+    )
+
+    printfn "Initializing compiler service test"
+    p.Start() |> ignore
+    p.BeginOutputReadLine()
+    p.WaitForExit()
+    printfn "CompilerServiceMemoryAnalysisRunner Finished"
+    0
---
+++ benchmarks/CompilerServiceMemoryAnalysisTest/Program.fs
@@ -0,0 +1,113 @@
+﻿open System
+open System.IO
+open System.Text
+open FSharp.Compiler.ErrorLogger
+open FSharp.Compiler.SourceCodeServices
+open FSharp.Compiler.Text
+open FSharp.Compiler.AbstractIL
+open FSharp.Compiler.AbstractIL.IL
+open FSharp.Compiler.AbstractIL.ILBinaryReader
+
+[<AutoOpen>]
+module Helpers =
+
+    // Hack: Currently a hack to get the runtime assemblies for netcore in order to compile.
+    let assemblies =
+        typeof<obj>.Assembly.Location
+        |> Path.GetDirectoryName
+        |> Directory.EnumerateFiles
+        |> Seq.toArray
+        |> Array.filter (fun x -> x.ToLowerInvariant().Contains("system."))
+        |> Array.map (fun x -> sprintf "-r:%s" x)
+    let netcoreArgs = Array.append [|"--targetprofile:netcore"; "--noframework"|] assemblies
+
+    let createProject name referencedProjects =
+        let tmpPath = Path.GetTempPath()
+        let file = Path.Combine(tmpPath, Path.ChangeExtension(name, ".fs"))
+        {
+            ProjectFileName = Path.Combine(tmpPath, Path.ChangeExtension(name, ".dll"))
+            ProjectId = None
+            SourceFiles = [|file|]
+            OtherOptions = 
+                Array.append [|"--optimize+"; "--target:library" |] (referencedProjects |> Array.ofList |> Array.map (fun x -> "-r:" + x.ProjectFileName))
+                |> Array.append netcoreArgs
+            ReferencedProjects =
+                referencedProjects
+                |> List.map (fun x -> (x.ProjectFileName, x))
+                |> Array.ofList
+            IsIncompleteTypeCheckEnvironment = false
+            UseScriptResolutionRules = false
+            LoadTime = DateTime()
+            UnresolvedReferences = None
+            OriginalLoadReferences = []
+            ExtraProjectInfo = None
+            Stamp = Some 0L (* set the stamp to 0L on each run so we don't evaluate the whole project again *)
+        }
+
+    let generateSourceCode moduleName =
+        sprintf """
+module Benchmark.%s
+
+type %s =
+
+    val X : int
+
+    val Y : int
+
+    val Z : int
+
+let function%s (x: %s) =
+    let x = 1
+    let y = 2
+    let z = x + y
+    z""" moduleName moduleName moduleName moduleName
+
+type Test() =
+
+    let checker = FSharpChecker.Create(projectCacheSize = 200)
+
+    let mainProjectOptions =
+        createProject "MainProject"
+            [ for i = 1 to 100 do
+                yield 
+                    createProject ("ReferencedProject" + string i) []
+            ]
+
+    member this.Run() =
+        mainProjectOptions.SourceFiles
+        |> Seq.iter (fun file ->
+            File.WriteAllText(file, generateSourceCode (Path.GetFileNameWithoutExtension(file)))
+        )
+
+        mainProjectOptions.ReferencedProjects
+        |> Seq.iter (fun (_, referencedProjectOptions) ->
+            referencedProjectOptions.SourceFiles
+            |> Seq.iter (fun file ->
+                File.WriteAllText(file, generateSourceCode (Path.GetFileNameWithoutExtension(file)))
+            )
+        )
+
+        let file = mainProjectOptions.SourceFiles.[0]
+
+        let parseResult, checkResult =                                                                
+            checker.ParseAndCheckFileInProject(file, 0, SourceText.ofString (File.ReadAllText(file)), mainProjectOptions)
+            |> Async.RunSynchronously
+
+        if parseResult.Errors.Length > 0 then
+            failwithf "%A" parseResult.Errors
+
+        match checkResult with
+        | FSharpCheckFileAnswer.Aborted -> failwith "aborted"
+        | FSharpCheckFileAnswer.Succeeded checkFileResult ->
+
+            if checkFileResult.Errors.Length > 0 then
+                failwithf "%A" checkFileResult.Errors
+
+[<EntryPoint>]
+let main _ =
+    let test = Test()
+    test.Run()
+    GC.Collect(2, GCCollectionMode.Forced)
+    printfn "CompilerServiceMemoryAnalysisTest-Finished"
+    Console.ReadLine() |> ignore
+    0
