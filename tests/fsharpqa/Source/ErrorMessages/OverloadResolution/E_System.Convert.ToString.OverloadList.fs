9671f338ec24056771c10a420792888f56f8d775
gauthier <gauthier.segay@gmail.com>
2019-04-20 21:18:43 -0700

    trying to make it green now.

--- tests/fsharpqa/Source/ErrorMessages/OverloadResolution/E_System.Convert.ToString.OverloadList.fs
+++ tests/fsharpqa/Source/ErrorMessages/OverloadResolution/E_System.Convert.ToString.OverloadList.fs
@@ -1,4 +1,3 @@
 System.Convert.ToString('a',0) |> ignore
 
 //<Expects id="FS0039" status="error" span="(1,1-1,1)">is not compatible with type</Expects>
-//<Expects id="FS0039" status="error" span="(1,1-1,1)">testfailfirst</Expects>
