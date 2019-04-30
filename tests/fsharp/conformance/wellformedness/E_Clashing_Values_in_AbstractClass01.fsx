4034f0fd2d1e0003dd833369dd2543230c644f61
Gauthier Segay <smoothdeveloper@users.noreply.github.com>
2019-04-30 10:51:32 -0700

    Migrate a set of fsharpqa tests to new test suite (overload resolution error messages) (#6654)
    * migrate (as a separate copy) fsharpqa tests that are going to be impacted by #6596
    
    the baseline files are empty on purpose, expecting CI failure reporting those tests, I intend to update the baseline and clean up the comments / xml tags from fsharpqa syntax in later commit, and then remove those specific tests altogether from fsharpqa if this is OK with the maintainers.
    
    * update the .bsl files
    
    * remove comments that are handled by baseline files, update baseline files
    
    * remove the migrated tests from fsharpqa tests
    
    * need to be more careful when migrating those
    
    * testing if running the test with .fs instead of .fsx makes them work on .netcore
    
    * exclude migrated fsharpqa from dotnet core run

--- tests/fsharp/conformance/wellformedness/E_Clashing_Values_in_AbstractClass01.fsx
+++ tests/fsharp/conformance/wellformedness/E_Clashing_Values_in_AbstractClass01.fsx
@@ -1,11 +1,6 @@
 // #Regression #Conformance #TypeInference 
 // Regression tests for FSHARP1.0:1348, FSHARP1.0:2949,FSHARP1.0:4927,FSHARP1.0:5939
 // Overloads that differ for the return type only are not allowed (in general)
-
-//<Expects status="error" span="(27,11-27,16)" id="FS0041">A unique overload for method 'X' could not be determined based on type information prior to this program point\. A type annotation may be needed\. Candidates: member Q\.X : unit -> decimal, member Q\.X : unit -> float$</Expects>
-
-
-
 [<AbstractClass>]
 type T() = 
     abstract X : unit -> decimal
