f8e7428b1b283002a95a4c226614c7d5597ac68e
gauthier <gauthier.segay@gmail.com>
2019-04-30 10:44:08 -0700

    merge issue unused variable warning

--- src/fsharp/ConstraintSolver.fs
+++ src/fsharp/ConstraintSolver.fs
@@ -2537,7 +2537,6 @@ and ResolveOverloading
                     UnresolvedConversionOperator (denv, fromTy, toTy, m)
                 | None -> 
                     // Otherwise collect a list of possible overloads
-                    let nl = System.Environment.NewLine
                     let msg =
                         let displayArgType (name , ttype) =
                             let typeDisplay = NicePrint.prettyStringOfTy denv ttype
