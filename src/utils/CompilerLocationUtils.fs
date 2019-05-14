444f436a978399ad9986af0ef9975af609f7af08
Kevin Ransom <codecutter.fsharp@hotmail.com>
2019-05-14 15:24:05 -0700

    Update VisualFSharp for VS2019.3

--- src/utils/CompilerLocationUtils.fs
+++ src/utils/CompilerLocationUtils.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 module internal FSharpEnvironment =
 
     /// The F# version reported in the banner
-    let FSharpBannerVersion = "10.4.0 for F# 4.6"
+    let FSharpBannerVersion = "10.6.0 for F# 4.7"
 
     let versionOf<'t> =
 #if FX_RESHAPED_REFLECTION
