522fc420f396393112b7ad8458f3881724837fb5
Kevin Ransom (msft) <codecutter@hotmail.com>
2019-06-03 13:17:04 -0700

    Ensure that Language Version support flows to Parser (#6891)
    * update testing
    
    * Ensure Lange Feature can be used from the parser
    
    * remove comments on tests
    
    * repair tests.fs

---
+++ src/fsharp/LanguageFeatures.fs
@@ -0,0 +1,90 @@
+// Copyright (c) Microsoft Corporation. All Rights Reserved. See License.txt in the project root for license information.
+
+/// Coordinating compiler operations - configuration, loading initial context, reporting errors etc.
+module internal FSharp.Compiler.Features
+
+open System
+
+//------------------------------------------------------------------------------------------------------------------
+// Language version command line switch
+//------------------------------------------------------------------------------------------------------------------
+// Add your features to this List - in code use languageVersion.SupportsFeature(LanguageFeatures.yourFeature) 
+// a return value of false means your feature is not supported by the user's language selection
+// All new language features added from now on must be protected by this.
+// Note:
+//   *  The fslang design process will require a decision about feature name and whether it is required.
+//   *  When a feature is assigned a release language, we will scrub the code of feature references and apply
+//      the Release Language version.
+
+/// LanguageFeature enumeration
+[<RequireQualifiedAccess>]
+type LanguageFeature =
+    | LanguageVersion46 = 0
+    | LanguageVersion47 = 1
+    | SingleUnderscorePattern = 2
+    | Nullness = 1000
+    | ScriptingPackageManagement = 1001
+
+/// LanguageVersion management
+type LanguageVersion (specifiedVersion) =
+
+    // When we increment language versions here preview is higher than current RTM version
+    static let languageVersion46 = 4.6m
+    static let languageVersion47 = 4.7m
+
+    static let previewVersion = languageVersion47       // Language version when preview specified
+    static let defaultVersion = languageVersion46       // Language version when default specified
+    static let latestVersion = defaultVersion           // Language version when latest specified
+    static let latestMajorVersion = languageVersion46   // Language version when latestmajor specified
+
+    static let validOptions = [| "preview"; "default"; "latest"; "latestmajor" |]
+    static let languageVersions = set [| latestVersion |]
+
+    static let features = dict [|
+        // Add new LanguageVersions here ...
+        LanguageFeature.LanguageVersion47, 4.7m
+        LanguageFeature.LanguageVersion46, 4.6m
+        LanguageFeature.Nullness, previewVersion
+        LanguageFeature.ScriptingPackageManagement, previewVersion
+        LanguageFeature.SingleUnderscorePattern, previewVersion
+
+        // Add new LanguageFeatures here ...
+        |]
+
+    let specified =
+        match specifiedVersion with
+        | "?" -> 0m
+        | "preview" -> previewVersion
+        | "default" -> latestVersion
+        | "latest" -> latestVersion
+        | "latestmajor" -> latestMajorVersion
+        | _ ->
+            match Decimal.TryParse(specifiedVersion) with
+            | true, v -> v
+            | _ -> 0m
+
+    /// Check if this feature is supported by the selected langversion
+    member __.SupportsFeature featureId =
+        match features.TryGetValue featureId with
+        | true, v -> v <= specified
+        | false, _ -> false
+
+    /// Does the languageVersion support this version string
+    member __.ContainsVersion version =
+        match version with
+        | "?" | "preview" | "default" | "latest" | "latestmajor" -> true
+        | _ -> 
+            match Decimal.TryParse(specifiedVersion) with
+            | true, v -> languageVersions.Contains v
+            | _ -> false
+
+    /// Get a list of valid strings for help text
+    member __.ValidOptions = validOptions
+
+    /// Get a list of valid versions for help text
+    member __.ValidVersions = [|
+        for v in languageVersions |> Seq.sort do
+            let label = if v = defaultVersion || v = latestVersion then "(Default)" else ""
+            yield sprintf "%M %s" v label
+            |]
+
--- tests/fsharp/test-framework.fs
+++ tests/fsharp/test-framework.fs
@@ -435,7 +435,8 @@ let execAppendIgnoreExitCode cfg stdoutPath stderrPath p = Command.exec cfg.Dire
 let exec cfg p = Command.exec cfg.Directory cfg.EnvironmentVariables execArgs p >> checkResult
 let execExpectFail cfg p = Command.exec cfg.Directory cfg.EnvironmentVariables execArgs p >> checkErrorLevel1
 let execIn cfg workDir p = Command.exec workDir cfg.EnvironmentVariables execArgs p >> checkResult
-let execBothToOut cfg workDir outFile p = Command.exec workDir  cfg.EnvironmentVariables { execArgs with Output = OutputAndErrorToSameFile(Overwrite(outFile)) } p >> checkResult
+let execBothToOutNoCheck cfg workDir outFile p = Command.exec workDir  cfg.EnvironmentVariables { execArgs with Output = OutputAndErrorToSameFile(Overwrite(outFile)) } p
+let execBothToOut cfg workDir outFile p = execBothToOutNoCheck cfg workDir outFile p >> checkResult
 let execAppendOutIgnoreExitCode cfg workDir outFile p = Command.exec workDir  cfg.EnvironmentVariables { execArgs with Output = Output(Append(outFile)) } p >> alwaysSuccess
 let execAppendErrExpectFail cfg errPath p = Command.exec cfg.Directory cfg.EnvironmentVariables { execArgs with Output = Error(Overwrite(errPath)) } p >> checkErrorLevel1
 let execStdin cfg l p = Command.exec cfg.Directory cfg.EnvironmentVariables { Output = Inherit; Input = Some(RedirectInput(l)) } p >> checkResult
