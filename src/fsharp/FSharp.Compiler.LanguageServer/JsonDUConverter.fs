1ca0d6a0b24e47aad176f78149d701c73cbc268d
Brett V. Forsgren <brettfo@microsoft.com>
2019-06-05 14:15:37 -0700

    enable preview LSP support
    Includes plumbing and a stub for `textDocument/hover` (e.g., QuickInfo).

---
+++ src/fsharp/FSharp.Compiler.LanguageServer/JsonDUConverter.fs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.LanguageServer
+
+open System
+open FSharp.Reflection
+open Newtonsoft.Json
+
+type JsonDUConverter() =
+    inherit JsonConverter()
+    override __.CanConvert(typ) = FSharpType.IsUnion(typ)
+    override __.WriteJson(writer, value, _serializer) =
+        writer.WriteValue(value.ToString().ToLowerInvariant())
+    override __.ReadJson(reader, typ, x, serializer) =
+        let cases = FSharpType.GetUnionCases(typ)
+        let str = serializer.Deserialize(reader, typeof<string>) :?> string
+        let case = cases |> Array.find (fun c -> String.Compare(c.Name, str, StringComparison.OrdinalIgnoreCase) = 0)
+        FSharpValue.MakeUnion(case, [||])
--- src/fsharp/FSharp.Compiler.LanguageServer/TextDocument.fs
+++ src/fsharp/FSharp.Compiler.LanguageServer/TextDocument.fs
@@ -2,26 +2,23 @@
 
 namespace FSharp.Compiler.LanguageServer
 
+open System
+
 module TextDocument =
 
-    let Hover(state: State, args: TextDocumentPositionParams) =
+    let Hover (state: State) (textDocument: TextDocumentIdentifier) (position: Position) =
         async {
-            return {
-                Hover.contents = {
-                    MarkupContent.kind = MarkupKind.PlainText
-                    value = "TODO"
+            Console.Error.WriteLine("hover at " + position.line.ToString() + "," + position.character.ToString())
+            if not state.Options.usePreviewTextHover then return None
+            else
+                let startCol, endCol =
+                    if position.character = 0 then 0, 1
+                    else position.character, position.character + 1
+                return Some { contents = { kind = MarkupKind.PlainText
+                                           value = "servivng textDocument/hover from LSP" }
+                              range = Some { start = { line = position.line; character = startCol }
+                                             ``end`` = { line = position.line; character = endCol } }
                 }
-                range = Some({
-                    Range.start = {
-                        Position.line = 0
-                        character = 0
-                    }
-                    ``end`` = {
-                        Position.line = 0
-                        character = 0
-                    }
-                })
-            }
         }
 
     let PublishDiagnostics(state: State) =
