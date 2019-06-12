8648ce96d1000008631c4e89eebde5f5d36bb419
Brett V. Forsgren <brettfo@microsoft.com>
2019-06-11 17:27:25 -0700

    add workspace and diagnostics to lsp

--- src/fsharp/FSharp.Compiler.LanguageServer/TextDocument.fs
+++ src/fsharp/FSharp.Compiler.LanguageServer/TextDocument.fs
@@ -2,13 +2,15 @@
 
 namespace FSharp.Compiler.LanguageServer
 
-open System
+open System.Threading
 
 module TextDocument =
 
+    let mutable publishDiagnosticsCancellationTokenSource = new CancellationTokenSource()
+
     let Hover (state: State) (textDocument: TextDocumentIdentifier) (position: Position) =
         async {
-            Console.Error.WriteLine("hover at " + position.line.ToString() + "," + position.character.ToString())
+            eprintfn "hover at %d, %d" position.line position.character
             if not state.Options.usePreviewTextHover then return None
             else
                 let startCol, endCol =
@@ -21,10 +23,53 @@ module TextDocument =
                 }
         }
 
-    let PublishDiagnostics(state: State) =
+    let PublishDiagnostics(state: State, projectOptions: FSharp.Compiler.SourceCodeServices.FSharpProjectOptions) =
+        // TODO: honor TextDocumentClientCapabilities.publishDiagnostics.relatedInformation
+        // cancel any existing request to publish diagnostics
+        publishDiagnosticsCancellationTokenSource.Cancel()
+        publishDiagnosticsCancellationTokenSource <- new CancellationTokenSource()
         async {
-            return {
-                PublishDiagnosticsParams.uri = ""
-                diagnostics = [||]
-            }
+            if not state.Options.usePreviewDiagnostics then return ()
+            else
+                eprintfn "starting diagnostics computation"
+                match state.JsonRpc with
+                | None -> eprintfn "state.JsonRpc was null; should not be?"
+                | Some jsonRpc ->
+                    let! results = state.Checker.ParseAndCheckProject projectOptions
+                    let diagnostics = results.Errors
+                    let diagnosticsPerFile =
+                        diagnostics
+                        |> Array.fold (fun state t ->
+                            let existing = Map.tryFind t.FileName state |> Option.defaultValue []
+                            Map.add t.FileName (t :: existing) state) Map.empty
+                    for sourceFile in projectOptions.SourceFiles do
+                        let diagnostics =
+                            Map.tryFind sourceFile diagnosticsPerFile
+                            |> Option.defaultValue []
+                            |> List.map (fun d ->
+                                // F# errors count lines starting at 1, but LSP starts at 0
+                                let range: Range =
+                                    { start = { line = d.StartLineAlternate - 1; character = d.StartColumn }
+                                      ``end`` = { line = d.EndLineAlternate - 1; character = d.EndColumn } }
+                                let severity =
+                                    match d.Severity with
+                                    | FSharp.Compiler.SourceCodeServices.FSharpErrorSeverity.Warning -> Diagnostic.Warning
+                                    | FSharp.Compiler.SourceCodeServices.FSharpErrorSeverity.Error -> Diagnostic.Error
+                                let res: Diagnostic =
+                                    { range = range
+                                      severity = Some severity
+                                      code = "FS" + d.ErrorNumber.ToString("0000")
+                                      source = Some d.FileName
+                                      message = d.Message
+                                      relatedInformation = None }
+                                res)
+                            |> List.toArray
+                        let args: PublishDiagnosticsParams =
+                            { uri = System.Uri(sourceFile).AbsoluteUri
+                              diagnostics = diagnostics }
+
+                        // fire each notification separately
+                        jsonRpc.NotifyAsync(TextDocumentPublishDiagnostics, args) |> Async.AwaitTask |> Async.Start
         }
+        |> (fun computation -> Async.StartAsTask(computation, cancellationToken=publishDiagnosticsCancellationTokenSource.Token))
+        |> Async.AwaitTask
--- vsintegration/src/FSharp.UIResources/Strings.Designer.cs
+++ vsintegration/src/FSharp.UIResources/Strings.Designer.cs
@@ -151,6 +151,15 @@ namespace Microsoft.VisualStudio.FSharp.UIResources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Diagnostics.
+        /// </summary>
+        public static string Diagnostics {
+            get {
+                return ResourceManager.GetString("Diagnostics", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to D_ot underline.
         /// </summary>
         public static string Dot_underline {
