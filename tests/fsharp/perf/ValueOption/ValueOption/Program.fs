5197db8157d7d420beb5ee866460c24504c38910
Christer van der Meeren <cmeeren@protonmail.com>
2019-06-15 15:59:57 +0200

    Add tryChooseV and tryPickV performance tests

---
+++ tests/fsharp/perf/ValueOption/ValueOption/Program.fs
@@ -0,0 +1,95 @@
+﻿open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+type Record = {
+  Int: int
+  String: string
+  Children: Record list
+}
+
+
+[<MemoryDiagnoser>]
+type Choose() =
+
+    [<Params(10, 1000, 100000)>]
+    [<DefaultValue>] val mutable N : int
+
+    [<Params("int", "record")>]
+    [<DefaultValue>] val mutable Type : string
+
+    [<DefaultValue>] val mutable ints : int list
+    [<DefaultValue>] val mutable recs : Record list
+
+    [<GlobalSetup>]
+    member this.Setup () =
+      this.ints <- [1 .. this.N]
+      this.recs <- List.init this.N (fun i -> 
+        { Int = i
+          String = string i
+          Children = [ 
+            { Int = i
+              String = string i
+              Children = [] }
+          ] }
+      )
+
+    [<Benchmark(Baseline=true)>]
+    member this.Option () =
+      match this.Type with
+      | "int" -> this.ints |> List.choose (fun x -> Some x) |> ignore
+      | "record" -> this.recs |> List.choose (fun x -> Some x) |> ignore
+      | _ -> failwith "Should never happen"
+
+    [<Benchmark>]
+    member this.ValueOption () =
+      match this.Type with
+      | "int" -> this.ints |> List.chooseV (fun x -> ValueSome x) |> ignore
+      | "record" -> this.recs |> List.chooseV (fun x -> ValueSome x) |> ignore
+      | _ -> failwith "Should never happen"
+
+
+[<MemoryDiagnoser>]
+type TryPick() =
+
+    [<Params(10, 1000, 100000)>]
+    [<DefaultValue>] val mutable N : int
+
+    [<Params("int", "record")>]
+    [<DefaultValue>] val mutable Type : string
+
+    [<DefaultValue>] val mutable ints : int list
+    [<DefaultValue>] val mutable recs : Record list
+
+    [<GlobalSetup>]
+    member this.Setup () =
+      this.ints <- [1 .. this.N]
+      this.recs <- List.init this.N (fun i -> 
+        { Int = i
+          String = string i
+          Children = [ 
+            { Int = i
+              String = string i
+              Children = [] }
+          ] }
+      )
+
+    [<Benchmark(Baseline=true)>]
+    member this.Option () =
+      match this.Type with
+      | "int" -> this.ints |> List.tryPick (fun x -> Some x) |> ignore
+      | "record" -> this.recs |> List.tryPick (fun x -> Some x) |> ignore
+      | _ -> failwith "Should never happen"
+
+    [<Benchmark>]
+    member this.ValueOption () =
+      match this.Type with
+      | "int" -> this.ints |> List.tryPickV (fun x -> ValueSome x) |> ignore
+      | "record" -> this.recs |> List.tryPickV (fun x -> ValueSome x) |> ignore
+      | _ -> failwith "Should never happen"
+
+
+[<EntryPoint>]
+let main argv =
+    let summaries = BenchmarkRunner.Run(typeof<Choose>.Assembly)
+    printfn "%A" summaries
+    0
