26a71f38418c4d205bc7178f726f7a7932b9bd51
Nelson Wu <wu_nelson@outlook.com>
2019-09-03 14:47:59 -0700

    add test for feature

---
+++ tests/fsharp/Compiler/Language/TypeAttributeTests.fs
@@ -0,0 +1,44 @@
+﻿namespace FSharp.Compiler.UnitTests
+
+open NUnit.Framework
+open FSharp.Compiler.SourceCodeServices
+
+[<TestFixture>]
+module TypeAttributeTests = 
+
+    [<Test>]
+    let ``Attribute can be assigned to type definition``() = 
+        CompilerAssert.Pass
+            """
+[<Struct>]
+type Point = {x:int; y:int}
+            """
+    [<Test>]
+    let ``Attribute cannot be assigned to optional type extension``() = 
+        CompilerAssert.TypeCheckSingleError
+            """
+open System
+
+[<NoEquality>]
+type String with 
+    member this.test = 42
+            """
+            FSharpErrorSeverity.Error
+            3245
+            (4, 1, 4, 15)
+            "Attributes cannot be assigned to type extensions."
+
+    [<Test>]
+    let ``Attribute cannot be assigned to intrinsic type extension``() = 
+        CompilerAssert.TypeCheckSingleError
+            """
+type Point = {x:int; y:int}
+
+[<NoEquality>]
+type Point with 
+    member this.test = 42
+            """
+            FSharpErrorSeverity.Error
+            3245
+            (4, 1, 4, 15)
+            "Attributes cannot be assigned to type extensions."
