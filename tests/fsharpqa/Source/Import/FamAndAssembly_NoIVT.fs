06fe677b6e9780eac95dcf4a6c98e085d79c6b40
Phillip Carter <phcart@microsoft.com>
2019-09-04 16:12:26 -0700

    hope this 'n works

--- tests/fsharpqa/Source/Import/FamAndAssembly_NoIVT.fs
+++ tests/fsharpqa/Source/Import/FamAndAssembly_NoIVT.fs
@@ -1,5 +1,4 @@
 // #Regression #NoMT #Import 
-//<Expects status="error" span="(8,5)" id="FS0509">Method or object constructor 'Accessibility' not found</Expects>
 //<Expects status="error" span="(8,13)" id="FS1092">The type 'Accessibility' is not defined</Expects>
 namespace NS
 
