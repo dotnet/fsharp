f9e5f4a65bb8e35fae15678ec38bc46714036295
Phillip Carter <pcarter@fastmail.com>
2019-09-03 18:25:44 -0700

    updates lol

--- tests/fsharpqa/Source/Conformance/Expressions/ApplicationExpressions/ObjectConstruction/E_ObjectConstruction01.fs
+++ tests/fsharpqa/Source/Conformance/Expressions/ApplicationExpressions/ObjectConstruction/E_ObjectConstruction01.fs
@@ -1,6 +1,6 @@
 // #Regression #Conformance #ApplicationExpressions #ObjectConstructors 
 // Verify error for inaccessible constructor
-//<Expects id="FS0039" status="error" span="(5,37)">The type 'BitArray' does not define a field, constructor or member 'BitArrayEnumeratorSimple'</Expects>
+//<Expects id="FS0039" status="error" span="(5,37)">The type 'BitArray' does not define the field, constructor or member 'BitArrayEnumeratorSimple'</Expects>
 
 let y = System.Collections.BitArray.BitArrayEnumeratorSimple
 ()
