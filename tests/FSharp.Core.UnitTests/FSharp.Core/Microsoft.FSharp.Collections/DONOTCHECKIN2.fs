b2c9bae4509a8cd738f23e85ef2c580e5b15206b
Nelson Wu <wu_nelson@outlook.com>
2019-10-14 13:43:04 -0700

    snapshot

---
+++ tests/FSharp.Core.UnitTests/FSharp.Core/Microsoft.FSharp.Collections/DONOTCHECKIN2.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharp.Core.UnitTests.FSharp_Core.Microsoft_FSharp_Collections
+open NUnit.Framework
+open System
+module DONOTCHECKIN2
+
+[<TestFixture>]
+type DONOTCHECKIN2 = 
+    [<Test>]
+    member this.reverse() = 
+        let arr = [1;2;3]
+        let a = ^1
+        Console.WriteLine(arr.[^1..])
