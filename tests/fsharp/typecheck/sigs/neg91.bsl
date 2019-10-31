2ce2c9e7fc9c49fc43fef51c9c07effb935908f6
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-10-31 03:10:18 +0300

    Fix member declaration ranges (#7676)
    * Fix member declaration ranges
    
    * Fix member declaration ranges
    
    * Update baseline
    
    * Update baseline

--- tests/fsharp/typecheck/sigs/neg91.bsl
+++ tests/fsharp/typecheck/sigs/neg91.bsl
@@ -1,5 +1,5 @@
 
-neg91.fs(7,16,7,30): typecheck error FS0896: Enumerations cannot have members
+neg91.fs(7,9,7,30): typecheck error FS0896: Enumerations cannot have members
 
 neg91.fs(10,10,10,15): typecheck error FS0956: Members that extend interface, delegate or enum types must be placed in a module separate to the definition of the type. This module must either have the AutoOpen attribute or be opened explicitly by client code to bring the extension members into scope.
 
--- tests/service/Common.fs
+++ tests/service/Common.fs
@@ -190,13 +190,19 @@ let parseAndCheckScript (file, input) =
 let parseSourceCode (name: string, code: string) =
     let location = Path.Combine(Path.GetTempPath(),"test"+string(hash (name, code)))
     try Directory.CreateDirectory(location) |> ignore with _ -> ()
-    let projPath = Path.Combine(location, name + ".fsproj")
     let filePath = Path.Combine(location, name + ".fs")
     let dllPath = Path.Combine(location, name + ".dll")
     let args = mkProjectCommandLineArgs(dllPath, [filePath])
     let options, errors = checker.GetParsingOptionsFromCommandLineArgs(List.ofArray args)
     let parseResults = checker.ParseFile(filePath, FSharp.Compiler.Text.SourceText.ofString code, options) |> Async.RunSynchronously
     parseResults.ParseTree
+
+open FSharp.Compiler.Ast
+
+let parseSourceCodeAndGetModule (source: string) =
+    match parseSourceCode ("test", source) with
+    | Some (ParsedInput.ImplFile (ParsedImplFileInput (_, _, _, _, _, [ moduleOrNamespace ], _))) -> moduleOrNamespace
+    | _ -> failwith "Could not get module decls"
 
 /// Extract range info 
 let tups (m:Range.range) = (m.StartLine, m.StartColumn), (m.EndLine, m.EndColumn)
