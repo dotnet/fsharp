d471eb2cf6c7e5b5ae418d0be02f4b3ee7f9fd4b
Nelson Wu <wu_nelson@outlook.com>
2019-11-04 10:21:55 -0800

    overloading tests

--- tests/fsharp/Compiler/Language/HatDesugaringTests.fs
+++ tests/fsharp/Compiler/Language/HatDesugaringTests.fs
@@ -1,11 +1,33 @@
 ﻿namespace FSharp.Compiler.UnitTests
 
 open NUnit.Framework
+open FSharp.Compiler.SourceCodeServices
 
-//[<TestFixture>]
-//module HatDesugaringTests =
+[<TestFixture>]
+module HatDesugaringTests =
 
-//    [<Test>]
-//    let ``Users s
+    [<Test>]
+    let ``Hat operator should be overloadable in infix context``() =
+        CompilerAssert.CompileExeAndRun
+            """
+open System
 
+let (^) (x: int) (y: int) = x + y
+
+if 1 ^ 2 <> 3 then failwith "expected result to be 3"
+            """
+
+
+    [<Test>]
+    let ``Hat operator should not be overloadable in prefix context``() = 
+        CompilerAssert.ParseWithErrors
+            """
+open System
+
+let (^) (x: int) (y:int) = x + y
+
+Console.WriteLine(^1)
+            """
+            [|
+            |]
 
