87a3270e3673d8752a19d9318c6997d0de781c8a
Chet Husk <chusk3@gmail.com>
2019-11-17 14:35:41 -0600

    update runner to netcore runner

--- fcs/build.sh
+++ fcs/build.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-if test "$OS" = "Windows_NT"
-then
-  # use .Net
-  cmd fcs/build.cmd $@ 
-else
-  cd fcs
 
-  # use mono
-  if [[ ! -e ~/.config/.mono/certs ]]; then
-    mozroots --import --sync --quiet
-  fi
-
-  ./download-paket.sh
-  mono .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-    exit $exit_code
-  fi
-  
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
-fi
+dotnet tool restore
+dotnet paket restore
+dotnet fake build -t $@
