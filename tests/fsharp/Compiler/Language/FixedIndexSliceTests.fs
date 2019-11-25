f54bab78f6058a1e22eb16d93b070be8f26ed214
Nelson Wu <wu_nelson@outlook.com>
2019-11-25 15:44:53 -0800

    merge and add test

---
+++ tests/fsharp/Compiler/Language/FixedIndexSliceTests.fs
@@ -0,0 +1,21 @@
+﻿namespace FSharp.Compiler.UnitTests
+
+open NUnit.Framework
+open FSharp.Compiler.SourceCodeServices
+
+[<TestFixture>]
+module FixedIndexSliceTests =
+    
+    [<Test>]
+    let ``Fixed index 3d slicing should not be available in 47``() =
+        CompilerAssert.TypeCheckWithErrorsAndOptions [| "--langversion:4.7"|]
+            """
+open System
+
+let arr3 = Array3D.create 2 2 2 2
+arr3.[1, *, *]
+            """
+            [|
+                FSharpErrorSeverity.Error, 39, (5,1,5,15), "The field, constructor or member 'GetSlice' is not defined."
+            |]
+
