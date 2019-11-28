9dfe9f1ee52f7504c2e5e76f24b146566edcfcfe
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2019-11-28 15:29:16 +0000

    fix ast compilation in netcoreapp

---
+++ tests/fsharp/Compiler/Infrastructure/AstCompiler.fs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.UnitTests.AstCompiler
+
+open FSharp.Compiler.UnitTests
+open NUnit.Framework
+open System.Reflection
+
+[<TestFixture>]
+module ``AST Compiler Smoke Tests`` =
+
+    [<Test>]
+    let ``Simple E2E module compilation``() =
+        let assembly = 
+            CompilerAssert.CompileOfAstToDynamicAssembly
+                """
+module TestModule
+
+    let rec fib n = if n <= 1 then n else fib (n - 2) + fib (n - 1)
+"""
+
+        let method = assembly.GetType("TestModule").GetMethod("fib", BindingFlags.Static ||| BindingFlags.Public)
+        Assert.NotNull(method)
+        Assert.AreEqual(55, method.Invoke(null, [|10|]))
