0809bac22bef2089ded2d0b784042d80166e30ce
Will Smith <lol.tihan@gmail.com>
2019-12-15 04:59:13 -0800

    Removed UnicodeFileAsLexbuf. Replaced it with StreamReaderAsLexbuf. No longer allocating string and chars as a result.

--- src/absil/illib.fs
+++ src/absil/illib.fs
@@ -1361,7 +1361,45 @@ module Shim =
                 directory.Contains("packages\\") || 
                 directory.Contains("lib/mono/")
 
-    let mutable FileSystem = DefaultFileSystem() :> IFileSystem 
+    let mutable FileSystem = DefaultFileSystem() :> IFileSystem
+
+    // The choice of 60 retries times 50 ms is not arbitrary. The NTFS FILETIME structure 
+    // uses 2 second resolution for LastWriteTime. We retry long enough to surpass this threshold 
+    // plus 1 second. Once past the threshold the incremental builder will be able to retry asynchronously based
+    // on plain old timestamp checking.
+    //
+    // The sleep time of 50ms is chosen so that we can respond to the user more quickly for Intellisense operations.
+    //
+    // This is not run on the UI thread for VS but it is on a thread that must be stopped before Intellisense
+    // can return any result except for pending.
+    let retryDelayMilliseconds = 50
+    let numRetries = 60
+
+    let getReader (filename, codePage: int option, retryLocked: bool) =
+        // Retry multiple times since other processes may be writing to this file.
+        let rec getSource retryNumber =
+          try 
+            // Use the .NET functionality to auto-detect the unicode encoding
+            let stream = FileSystem.FileStreamReadShim(filename) 
+            match codePage with 
+            | None -> new  StreamReader(stream,true)
+            | Some n -> new  StreamReader(stream,System.Text.Encoding.GetEncoding(n))
+          with 
+              // We can get here if the file is locked--like when VS is saving a file--we don't have direct
+              // access to the HRESULT to see that this is EONOACCESS.
+              | :? System.IO.IOException as err when retryLocked && err.GetType() = typeof<System.IO.IOException> -> 
+                   // This second check is to make sure the exception is exactly IOException and none of these for example:
+                   //   DirectoryNotFoundException 
+                   //   EndOfStreamException 
+                   //   FileNotFoundException 
+                   //   FileLoadException 
+                   //   PathTooLongException
+                   if retryNumber < numRetries then 
+                       System.Threading.Thread.Sleep (retryDelayMilliseconds)
+                       getSource (retryNumber + 1)
+                   else 
+                       reraise()
+        getSource 0
 
     type File with 
 
@@ -1373,4 +1411,7 @@ module Shim =
             while n < len do 
                 n <- n + stream.Read(buffer, n, len-n)
             buffer
+
+        static member OpenReaderAndRetry (filename, codepage, retryLocked)  =
+            getReader (filename, codepage, retryLocked)
 
