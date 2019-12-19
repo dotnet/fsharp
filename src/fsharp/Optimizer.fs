f8173e5b24ae15a52308914c49cb871b34cc9ed1
Will Smith <lol.tihan@gmail.com>
2019-12-19 11:40:06 -0800

    Small LOH fix for Dictionary
    Dictionary started with 10000 capacity which will be put on the LOH

--- src/fsharp/Optimizer.fs
+++ src/fsharp/Optimizer.fs
@@ -3435,7 +3435,7 @@ let OptimizeImplFile (settings, ccu, tcGlobals, tcVal, importMap, optEnv, isIncr
           g=tcGlobals 
           amap=importMap
           optimizing=true
-          localInternalVals=Dictionary<Stamp, ValInfo>(10000)
+          localInternalVals=Dictionary<Stamp, ValInfo> 1024
           emitTailcalls=emitTailcalls
           casApplied=new Dictionary<Stamp, bool>() }
     let (optEnvNew, _, _, _ as results) = OptimizeImplFileInternal cenv optEnv isIncrementalFragment hidden mimpls  
