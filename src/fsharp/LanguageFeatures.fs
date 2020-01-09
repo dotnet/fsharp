9e51b70930e1accaa76683f0c16f613b2f7678bd
Kevin Ransom <codecutter@hotmail.com>
2020-01-09 15:30:51 -0800

    language features

--- src/fsharp/LanguageFeatures.fs
+++ src/fsharp/LanguageFeatures.fs
@@ -37,12 +37,12 @@ type LanguageVersion (specifiedVersionAsString) =
     static let languageVersion47 = 4.7m
     static let languageVersion50 = 5.0m
     static let previewVersion = 9999m                   // Language version when preview specified
-    static let defaultVersion = languageVersion47       // Language version when default specified
+    static let defaultVersion = languageVersion50       // Language version when default specified
     static let latestVersion = defaultVersion           // Language version when latest specified
     static let latestMajorVersion = languageVersion47   // Language version when latestmajor specified
 
     static let validOptions = [| "preview"; "default"; "latest"; "latestmajor" |]
-    static let languageVersions = set [| languageVersion46; languageVersion47 (*; languageVersion50 *) |]
+    static let languageVersions = set [| languageVersion46; languageVersion47; languageVersion50 |]
 
     static let features =
         dict [
