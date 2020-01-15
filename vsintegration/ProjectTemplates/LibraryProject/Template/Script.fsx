f9e70262e83c032fbe6340dec6064350ad1edb64
Brett V. Forsgren <brettfo@microsoft.com>
2020-01-14 18:31:46 -0800

    restore templates removed in #7379 (#8205)

---
+++ vsintegration/ProjectTemplates/LibraryProject/Template/Script.fsx
@@ -0,0 +1,8 @@
+﻿// @@@LearnMore|Learn more about F# at https://fsharp.org@@@
+// @@@SeeTutorial|See the 'F# Tutorial' project for more help.@@@
+
+#load "Library1.fs"
+open $safeprojectname$
+
+// @@@Define|Define your library scripting code here@@@
+
