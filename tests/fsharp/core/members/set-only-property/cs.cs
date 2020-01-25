b5e59d5c4fe197d88e932d21fcde447232d9374e
Gauthier Segay <gauthier.segay@gmail.com>
2020-01-25 18:14:52 +0100

    Furthering the test, all languages exhibit the same issue.
    Added another property that is expected to fail, exhibiting inconsistencies in how the error is reported depending if the type is defined in F# or not.

--- tests/fsharp/core/members/set-only-property/cs.cs
+++ tests/fsharp/core/members/set-only-property/cs.cs
@@ -2,6 +2,7 @@ namespace csharp
 {
     public class Class
     {
-        public int Prop { set; private get; }
-    }
+        public int Prop1 { set; private get; }
+        public int Prop2 { private set; get; }
+    }    
 }
--- tests/fsharp/core/members/set-only-property/fs.fs
+++ tests/fsharp/core/members/set-only-property/fs.fs
@@ -1,17 +1,8 @@
+namespace fsharp
 type Class () =
   let mutable v = 0
-  member x.Prop with set(value) = v <- value
-                 and private get () = v
+  member x.Prop1 with set(value) = v <- value
+                  and private get () = v
 
-let a = csharp.Class(Prop=1)
-let b = basic.BasicClass(Prop=1)
-let c = Class(Prop=1)
-
-type Maker =
-  static member mkCs () = csharp.Class()
-  static member mkVb () = basic.BasicClass()
-  static member mkFs () = Class()
-
-let aa = Maker.mkCs(Prop=1)
-let bb = Maker.mkVb(Prop=1)
-let cc = Maker.mkFs(Prop=1)
+  member x.Prop2 with private set(value) = v <- value
+                  and public get () = v
