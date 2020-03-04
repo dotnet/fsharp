49c6774ba033b4d2731900b069f6e3719446d3ed
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-03-04 14:07:15 +0300

    Fix fsharpqa baselines

--- tests/fsharpqa/Source/Conformance/DeclarationElements/LetBindings/Basic/E_AttributesOnLet01.fs
+++ tests/fsharpqa/Source/Conformance/DeclarationElements/LetBindings/Basic/E_AttributesOnLet01.fs
@@ -4,7 +4,10 @@
 // Regression test for FSharp1.0:3744 - Unable to apply attributes on individual patterns in a tupled pattern match let binding - Implementation doesn't match spec
 // Test against error emitted when attributes applied within pattern
 
-//<Expects id="FS0683" span="(11,6-11,33)" status="error">Attributes are not allowed within patterns</Expects>
+//<Expects id="FS0683" span="(14,5-14,26)" status="error">Attributes are not allowed within patterns</Expects>
+//<Expects id="FS0842" span="(14,7-14,22)" status="error">This attribute is not valid for use on this language element</Expects>
+//<Expects id="FS0683" span="(14,41-14,62)" status="error">Attributes are not allowed within patterns</Expects>
+//<Expects id="FS0842" span="(14,43-14,58)" status="error">This attribute is not valid for use on this language element</Expects>
 
 open System
 
