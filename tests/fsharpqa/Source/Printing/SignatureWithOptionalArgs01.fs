c6269497111e8ed9b6fc3073794e010661acaa54
stmax82 <stmax82@gmail.com>
2020-04-13 22:43:01 +0200

    * Fix for missing type annotation in SignatureWithOptionalArgs01.fs

--- tests/fsharpqa/Source/Printing/SignatureWithOptionalArgs01.fs
+++ tests/fsharpqa/Source/Printing/SignatureWithOptionalArgs01.fs
@@ -15,7 +15,7 @@ open Microsoft.FSharp.Control
  
 type AsyncTimer(f, ?delay ) =
   let mutable does_again = true
-  let mutable delay = delay
+  let mutable delay:int = delay
   
   member t.Delay 
     with get() = delay
