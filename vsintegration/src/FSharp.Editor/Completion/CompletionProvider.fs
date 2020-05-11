9f2fdd62c1c7d164dea817506e79670ef6060e29
cartermp <pcarter@fastmail.com>
2020-05-10 21:58:29 -0700

    Use roslyn API to show unopened namespace in extended completion

--- vsintegration/src/FSharp.Editor/Completion/CompletionProvider.fs
+++ vsintegration/src/FSharp.Editor/Completion/CompletionProvider.fs
@@ -139,10 +139,10 @@ type internal FSharpCompletionProvider
 
             declarationItems |> Array.iteri (fun number declarationItem ->
                 let glyph = Tokenizer.FSharpGlyphToRoslynGlyph (declarationItem.Glyph, declarationItem.Accessibility)
-                let name =
+                let namespaceName =
                     match declarationItem.NamespaceToOpen with
-                    | Some namespaceToOpen -> sprintf "%s (open %s)" declarationItem.Name namespaceToOpen
-                    | _ -> declarationItem.Name
+                    | Some namespaceToOpen -> namespaceToOpen
+                    | _ -> null // Icky, but this is how roslyn handles it
                     
                 let filterText =
                     match declarationItem.NamespaceToOpen, declarationItem.Name.Split '.' with
@@ -153,8 +153,14 @@ type internal FSharpCompletionProvider
                     | _, idents -> Array.last idents
 
                 let completionItem = 
-                    FSharpCommonCompletionItem.Create(name, null, rules = getRules intellisenseOptions.ShowAfterCharIsTyped, glyph = Nullable glyph, filterText = filterText)
-                                        .AddProperty(FullNamePropName, declarationItem.FullName)
+                    FSharpCommonCompletionItem.Create(
+                        declarationItem.Name,
+                        null,
+                        rules = getRules intellisenseOptions.ShowAfterCharIsTyped,
+                        glyph = Nullable glyph,
+                        filterText = filterText,
+                        inlineDescription = namespaceName)
+                        .AddProperty(FullNamePropName, declarationItem.FullName)
                         
                 let completionItem =
                     match declarationItem.Kind with
@@ -163,7 +169,7 @@ type internal FSharpCompletionProvider
                     | _ -> completionItem
                 
                 let completionItem =
-                    if name <> declarationItem.NameInCode then
+                    if declarationItem.Name <> declarationItem.NameInCode then
                         completionItem.AddProperty(NameInCodePropName, declarationItem.NameInCode)
                     else completionItem
 
