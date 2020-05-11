2d8a2fa913e739843c4873bbb6aab461be901de7
cartermp <pcarter@fastmail.com>
2020-05-10 19:01:32 -0700

    Actually show keyword descriptions in tooltips for completion

--- vsintegration/src/FSharp.Editor/Completion/KeywordCompletionProvider.fs
+++ vsintegration/src/FSharp.Editor/Completion/KeywordCompletionProvider.fs
@@ -2,40 +2,47 @@
 
 open System
 
+open Microsoft.CodeAnalysis
 open Microsoft.CodeAnalysis.Completion
 open Microsoft.CodeAnalysis.ExternalAccess.FSharp.Completion
 
 open FSharp.Compiler.SourceCodeServices
+open System.Collections.Immutable
 
 type internal KeywordCompletionProvider(projectInfoManager: FSharpProjectOptionsManager) =
 
     static let [<Literal>] IsKeywordPropName = "IsKeyword"
 
-    static let keywordCompletionItems =
+    let keywordCompletionItems =
         Keywords.KeywordsWithDescription
         |> List.filter (fun (keyword, _) -> not (PrettyNaming.IsOperatorName keyword))
         |> List.sortBy (fun (keyword, _) -> keyword)
         |> List.mapi (fun n (keyword, description) ->
-             FSharpCommonCompletionItem.Create(keyword, null, CompletionItemRules.Default, Nullable Glyph.Keyword, sortText = sprintf "%06d" (1000000 + n))
-                .AddProperty("description", description)
+            FSharpCommonCompletionItem.Create(
+                displayText = keyword,
+                displayTextSuffix = "",
+                rules = CompletionItemRules.Default,
+                glyph = Nullable Glyph.Keyword,
+                description = ImmutableArray.Create(SymbolDisplayPart(SymbolDisplayPartKind.Text, null, description)),
+                sortText = sprintf "%06d" (1000000 + n))
                 .AddProperty(IsKeywordPropName, ""))
 
     interface IFSharpCommonCompletionProvider with
-         member _.ProvideCompletionsAsync(context) =
-             asyncMaybe {    
-                 let document = context.Document
+        member _.ProvideCompletionsAsync(context) =
+            asyncMaybe {    
+                let document = context.Document
 
-                 let! ct = liftAsync Async.CancellationToken
-                 let! sourceText = document.GetTextAsync(ct)
-                 let defines = projectInfoManager.GetCompilationDefinesForEditingDocument(document)
+                let! ct = liftAsync Async.CancellationToken
+                let! sourceText = document.GetTextAsync(ct)
+                let defines = projectInfoManager.GetCompilationDefinesForEditingDocument(document)
 
-                 do! Option.guard (CompletionUtils.shouldProvideCompletion(document.Id, document.FilePath, defines, sourceText, context.Position))
+                do! Option.guard (CompletionUtils.shouldProvideCompletion(document.Id, document.FilePath, defines, sourceText, context.Position))
 
-                 context.AddItems(keywordCompletionItems)
-             } 
-             |> Async.Ignore
-             |> RoslynHelpers.StartAsyncUnitAsTask context.CancellationToken
+                context.AddItems(keywordCompletionItems)
+            } 
+            |> Async.Ignore
+            |> RoslynHelpers.StartAsyncUnitAsTask context.CancellationToken
  
-         member _.IsInsertionTrigger(text, position, _) = FSharpCommonCompletionUtilities.IsStartingNewWord(text, position, (fun x -> Char.IsLetter x), (fun x -> Char.IsLetterOrDigit x))
+        member _.IsInsertionTrigger(text, position, _) = FSharpCommonCompletionUtilities.IsStartingNewWord(text, position, (fun x -> Char.IsLetter x), (fun x -> Char.IsLetterOrDigit x))
  
-         member _.GetTextChangeAsync(baseGetTextChangeAsync, selectedItem, ch, cancellationToken) = baseGetTextChangeAsync.Invoke(selectedItem, ch, cancellationToken)
+        member _.GetTextChangeAsync(baseGetTextChangeAsync, selectedItem, ch, cancellationToken) = baseGetTextChangeAsync.Invoke(selectedItem, ch, cancellationToken)
