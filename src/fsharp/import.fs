3f9bb57533816ca9de6c60adc5af7045a0535fd9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2020-05-20 20:32:01 +0200

    Do not check genericArgs.Length over and over again

--- src/fsharp/import.fs
+++ src/fsharp/import.fs
@@ -287,14 +287,15 @@ let rec ImportProvidedType (env: ImportMap) (m: range) (* (tinst: TypeInst) *) (
                 tcref, [] 
         
         /// Adjust for the known primitive numeric types that accept units of measure. 
-        let tcref = 
-            if tyconRefEq g tcref g.system_Double_tcref && genericArgs.Length = 1 then g.pfloat_tcr
-            elif tyconRefEq g tcref g.system_Single_tcref && genericArgs.Length = 1 then g.pfloat32_tcr
-            elif tyconRefEq g tcref g.system_Decimal_tcref && genericArgs.Length = 1 then g.pdecimal_tcr
-            elif tyconRefEq g tcref g.system_Int16_tcref && genericArgs.Length = 1 then g.pint16_tcr
-            elif tyconRefEq g tcref g.system_Int32_tcref && genericArgs.Length = 1 then g.pint_tcr
-            elif tyconRefEq g tcref g.system_Int64_tcref && genericArgs.Length = 1 then g.pint64_tcr
-            elif tyconRefEq g tcref g.system_SByte_tcref && genericArgs.Length = 1 then g.pint8_tcr
+        let tcref =
+            if genericArgs.Length <> 1 then tcref
+            elif tyconRefEq g tcref g.system_Double_tcref then g.pfloat_tcr
+            elif tyconRefEq g tcref g.system_Single_tcref then g.pfloat32_tcr
+            elif tyconRefEq g tcref g.system_Decimal_tcref then g.pdecimal_tcr
+            elif tyconRefEq g tcref g.system_Int16_tcref then g.pint16_tcr
+            elif tyconRefEq g tcref g.system_Int32_tcref then g.pint_tcr
+            elif tyconRefEq g tcref g.system_Int64_tcref then g.pint64_tcr
+            elif tyconRefEq g tcref g.system_SByte_tcref then g.pint8_tcr
             else tcref
         
         let tps = tcref.Typars m
