f7c29a587a47b6ec14c878ea4feb1c08959be1a7
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2020-05-21 11:27:45 +0200

    Migrated 'module abbreviations are always private' testcase

---
+++ tests/fsharp/Compiler/ErrorMessages/ModuleAbbreviationTests.fs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.UnitTests
+
+open NUnit.Framework
+open FSharp.Compiler.SourceCodeServices
+
+[<TestFixture>]
+module ``Module Abbreviations`` =
+
+    [<Test>]
+    let ``Downcast Instead Of Upcast``() =
+        CompilerAssert.TypeCheckSingleError
+            """
+module public L1 = List
+            """
+            FSharpErrorSeverity.Error
+            536
+            (2, 1, 2, 7)
+            "The 'Public' accessibility attribute is not allowed on module abbreviation. Module abbreviations are always private."
