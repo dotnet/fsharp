432db0b1d1c893ad088d3c6704c8a13b22637e9d
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2020-05-25 20:44:33 +0200

    Moved some test cased to NUnit

---
+++ tests/fsharp/Compiler/Libraries/Core/CollectionTests.fs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.UnitTests
+
+open NUnit.Framework
+open FSharp.Compiler.SourceCodeServices
+
+[<TestFixture>]
+module ``Collection Tests`` =
+
+    [<Test>]
+    let ``Iter should not throw on non-zero based 2D arrays``() =
+        // Regression for FSHARP1.0: 5919
+        // bug in array2D functions would cause iter to blow up
+
+        let a = Array2D.createBased 1 5 10 10 0.0
+        let testDelegate = TestDelegate (fun _ -> a |> Array2D.iter (printf "%f"))
+
+        Assert.DoesNotThrow testDelegate
+
+    [<Test>]
+    let ``Iteri should not throw on non-zero based 2D arrays``() =
+        // Regression for FSHARP1.0: 5919
+        // bug in array2D functions would cause iteri to blow up
+
+        let a = Array2D.createBased 1 5 10 10 0.0
+        let testDelegate = TestDelegate (fun _ -> a |> Array2D.iteri (fun _ _ x -> printf "%f" x))
+
+        Assert.DoesNotThrow testDelegate
+        
+    [<Test>]
+    let ``Equality should be implemented on map``() =
+        // Dev11:19569 - this used to throw an ArgumentException saying Object didn't implement IComparable
+        
+        let m = Map.ofArray [| 1, obj() |]
+        let testDelegate = TestDelegate (fun _ -> (m = m) |> ignore)
+        
+        Assert.DoesNotThrow testDelegate
