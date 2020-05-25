76c7324e874fb19d7fb2655988d3067d68ead893
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2020-05-25 22:34:29 +0200

    Moved List.head and List.tail test cases to NUnit

--- tests/fsharp/Compiler/Libraries/Core/CollectionTests.fs
+++ tests/fsharp/Compiler/Libraries/Core/CollectionTests.fs
@@ -36,3 +36,53 @@ module ``Collection Tests`` =
         let testDelegate = TestDelegate (fun _ -> (m = m) |> ignore)
         
         Assert.DoesNotThrow testDelegate
+
+    [<Test>]
+    let ``List hd should not exist``() =
+        // Regression test for FSharp1.0:5641
+        // Title: List.hd/tl --> List.head/tail
+
+        CompilerAssert.TypeCheckSingleError
+            """
+List.hd [1] |> ignore
+            """
+            FSharpErrorSeverity.Error
+            39
+            (2, 6, 2, 8)
+            "The value, constructor, namespace or type 'hd' is not defined."
+
+            
+            
+    [<Test>]
+    let ``List tl should not exist``() =
+        // Regression test for FSharp1.0:5641
+        // Title: List.hd/tl --> List.head/tail
+            
+        CompilerAssert.TypeCheckSingleError
+            """
+List.tl [1] |> ignore
+            """
+            FSharpErrorSeverity.Error
+            39
+            (2, 6, 2, 8)
+            "The value, constructor, namespace or type 'tl' is not defined."
+
+    [<Test>]
+    let ``List head of empty list``() =
+        let testDelegate = TestDelegate (fun _ -> (List.head [] |> ignore))
+
+        Assert.Throws<System.ArgumentException> testDelegate |> ignore
+
+    [<Test>]
+    let ``List tail of empty list``() =
+        let testDelegate = TestDelegate (fun _ -> (List.tail [] |> ignore))
+
+        Assert.Throws<System.ArgumentException> testDelegate |> ignore
+
+    [<Test>]
+    let ``List head and tail``() =
+        Assert.areEqual 1 (List.head [1 .. 10])
+        Assert.areEqual "a" (List.head ["a"])
+        Assert.areEqual [2 .. 10] (List.tail [1 .. 10])
+        Assert.areEqual [] (List.tail [1])
+        Assert.areEqual (List.head (List.tail ['a'; 'a'])) (List.head ['a'; 'a'])
