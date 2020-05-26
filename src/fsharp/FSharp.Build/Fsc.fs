ac4a13b97a4273393c7df73bb7440bd785d7d4d3
Saul Rennison <saul.rennison@gmail.com>
2020-05-26 13:54:57 +0100

    Use `String.concat` to concatenate command line arguments

--- src/fsharp/FSharp.Build/Fsc.fs
+++ src/fsharp/FSharp.Build/Fsc.fs
@@ -589,7 +589,7 @@ type public Fsc () as this =
 
     override fsc.GenerateResponseFileCommands() =
         let builder = generateCommandLineBuilder ()
-        builder.GetCapturedArguments() |> Seq.fold(fun acc f -> acc + f + Environment.NewLine) ""
+        builder.GetCapturedArguments() |> String.concat Environment.NewLine
 
     // expose this to internal components (for nunit testing)
     member internal fsc.InternalGenerateCommandLineCommands() =
