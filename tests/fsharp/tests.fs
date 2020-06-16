c38ebd87a76b19ee131e8c6ba7ef84cdd3eafb78
Don Syme <dsyme@microsoft.com>
2020-06-16 14:19:15 +0100

    add test case

--- tests/fsharp/tests.fs
+++ tests/fsharp/tests.fs
@@ -5,7 +5,7 @@
 #load "../FSharp.TestHelpers/TestFramework.fs"
 #load "single-test.fs"
 #else
-module ``FSharp-Tests-Core``
+module FSharp.Tests.Core
 #endif
 
 open System
@@ -2203,6 +2203,15 @@ module TypecheckTests =
         let cfg = testConfig' "typecheck/sigs"
         fsc cfg "%s --target:library -o:pos35.dll --warnaserror" cfg.fsc_flags ["pos35.fs"]
         peverify cfg "pos35.dll"
+
+    [<Test>]
+    let ``sigs pos36-srtp`` () =
+        let cfg = testConfig' "typecheck/sigs"
+        fsc cfg "%s --target:library -o:pos36-srtp-lib.dll --warnaserror" cfg.fsc_flags ["pos36-srtp-lib.fs"]
+        fsc cfg "%s --target:exe -r:pos36-srtp-lib.dll -o:pos36-srtp-app.exe --warnaserror" cfg.fsc_flags ["pos36-srtp-app.fs"]
+        peverify cfg "pos36-srtp-lib.dll"
+        peverify cfg "pos36-srtp-app.exe"
+        exec cfg ("." ++ "pos36-srtp-app.exe") ""
 
     [<Test>]
     let ``sigs pos23`` () =
