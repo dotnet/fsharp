3584698f1fe777c4ba92afed69b424e58c8cdaef
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2020-06-18 17:27:46 +0200

    [WIP] Moving compiler tests to a component suite

--- tests/FSharp.Compiler.ComponentTests/ErrorMessages/ConstructorTests.fs
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/ConstructorTests.fs
@@ -17,8 +17,8 @@ let doSomething (xs) = List.map (fun {field1=x} -> x) xs
 doSomething {Record.field1=0; field2=0}
             """
             [|
-                FSharpErrorSeverity.Error, 1, (5, 13, 5, 40), "This expression was expected to have type\n    'Record list'    \nbut here has type\n    'Record'    "
-                FSharpErrorSeverity.Warning, 20, (5, 1, 5, 40), "The result of this expression has type 'int list' and is implicitly ignored. Consider using 'ignore' to discard this value explicitly, e.g. 'expr |> ignore', or 'let' to bind the result to a name, e.g. 'let result = expr'."
+                FSharpErrorSeverity.Error, 1, (4, 13, 4, 40), "This expression was expected to have type\n    'Record list'    \nbut here has type\n    'Record'    "
+                FSharpErrorSeverity.Warning, 20, (4, 1, 4, 40), "The result of this expression has type 'int list' and is implicitly ignored. Consider using 'ignore' to discard this value explicitly, e.g. 'expr |> ignore', or 'let' to bind the result to a name, e.g. 'let result = expr'."
             |]
 
     [<Fact>]
@@ -29,7 +29,7 @@ type Person = { Name : string; Age : int; City : string }
 let x = { Name = "Isaac", Age = 21, City = "London" }
             """
             [|
-                FSharpErrorSeverity.Error, 1, (3, 18, 3, 52), "This expression was expected to have type\n    'string'    \nbut here has type\n    ''a * 'b * 'c'    \r\nA ';' is used to separate field values in records. Consider replacing ',' with ';'."
+                FSharpErrorSeverity.Error, 1, (3, 18, 3, 52), "This expression was expected to have type\n    'string'    \nbut here has type\n    ''a * 'b * 'c'    " + System.Environment.NewLine + "A ';' is used to separate field values in records. Consider replacing ',' with ';'."
                 FSharpErrorSeverity.Error, 764, (3, 9, 3, 54), "No assignment given for field 'Age' of type 'Test.Person'"
             |]
 
@@ -46,7 +46,7 @@ let p =
            Age = 18)
             """
             [|
-                FSharpErrorSeverity.Error, 39, (7, 12, 7, 16), "The value or constructor 'Name' is not defined. Maybe you want one of the following:\r\n   nan"
+                FSharpErrorSeverity.Error, 39, (7, 12, 7, 16), "The value or constructor 'Name' is not defined. Maybe you want one of the following:" + System.Environment.NewLine + "   nan"
                 FSharpErrorSeverity.Warning, 20, (7, 12, 7, 25), "The result of this equality expression has type 'bool' and is implicitly discarded. Consider using 'let' to bind the result to a name, e.g. 'let result = expression'."
                 FSharpErrorSeverity.Error, 39, (8, 12, 8, 15), "The value or constructor 'Age' is not defined."
                 FSharpErrorSeverity.Error, 501, (7, 5, 8, 21), "The object constructor 'Person' takes 0 argument(s) but is here given 1. The required signature is 'new : unit -> Person'. If some of the arguments are meant to assign values to properties, consider separating those arguments with a comma (',')."
