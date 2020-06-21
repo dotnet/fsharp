e5a9c7276ab0f77c8ac72fab5ff3ab275048665c
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2020-06-21 10:38:59 +0200

    Migrated zerosize01.fs test case

---
+++ tests/fsharp/Compiler/Libraries/Core/NativeInterop/stackalloc/StackallocTests.fs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.UnitTests
+
+open NUnit.Framework
+
+#nowarn "9"
+
+[<TestFixture>]
+module ``Stackalloc Tests`` =
+    
+    [<Test>]
+    let ``Stackalloc zero-size``() =
+        // Regression test for FSHARP1.0:
+        // stackalloc<System.DateTime> 0
+
+        let testDelegate = TestDelegate (fun () -> 
+            // check stackalloc 0 -- ok
+            let data = NativeInterop.NativePtr.stackalloc<System.DateTime> 0
+            
+            // The returned pointer is undefined
+            // No allocation should happen
+            let _ = NativeInterop.NativePtr.toNativeInt data
+            
+            ())
+
+        Assert.DoesNotThrow testDelegate
