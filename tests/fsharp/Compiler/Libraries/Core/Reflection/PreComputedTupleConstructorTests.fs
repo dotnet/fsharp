c20c31791591a2eaf064041b2dbf17e85d70bd21
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2020-07-02 21:52:38 +0200

    Migrated PreComputedTupleConstructor02.fs test case

--- tests/fsharp/Compiler/Libraries/Core/Reflection/PreComputedTupleConstructorTests.fs
+++ tests/fsharp/Compiler/Libraries/Core/Reflection/PreComputedTupleConstructorTests.fs
@@ -16,3 +16,13 @@ module ``PreComputedTupleConstructor Tests`` =
             Reflection.FSharpValue.PreComputeTupleConstructor(typeof<int * string>) [| box 12; box "text" |] |> ignore)
 
         Assert.DoesNotThrow testDelegate |> ignore
+
+    [<Test>]
+    let ``PreComputedTupleConstructor with wrong order of arguments``() =
+        // Regression test for FSHARP1.0:5113
+        // MT DCR: Reflection.FSharpValue.PreComputeTupleConstructor fails when executed for NetFx 2.0 by a Dev10 compiler
+
+        let testDelegate = TestDelegate (fun () -> 
+            Reflection.FSharpValue.PreComputeTupleConstructor(typeof<int * string>) [| box "text"; box 12; |] |> ignore)
+
+        Assert.Throws<System.ArgumentException> testDelegate |> ignore
