c15b5c4ecf0a60a0c672ac39d17b0bbe07f421d3
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2020-07-05 18:44:45 +0200

    Migrated DecimalLiterals01.fs test case

--- tests/FSharp.Compiler.ComponentTests/ErrorMessages/InvalidNumericLiteralTests.fs
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/InvalidNumericLiteralTests.fs
@@ -18,3 +18,11 @@ let foo = 1up // int
             1156
             (2, 11, 2, 14)
             "This is not a valid numeric literal. Valid numeric literals include 1, 0x1, 0o1, 0b1, 1l (int), 1u (uint32), 1L (int64), 1UL (uint64), 1s (int16), 1y (sbyte), 1uy (byte), 1.0 (float), 1.0f (float32), 1.0m (decimal), 1I (BigInteger)."
+
+    [<Theory>]
+    [<InlineData("1.0E28M")>]
+    [<InlineData("1.0E-28M")>]
+    let ``Valid Numeric Literals`` literal =
+        // Regressiont test for FSharp1.0: 2543 - Decimal literals do not support exponents
+
+        CompilerAssert.Pass ("let x = " + literal)
---
+++ tests/fsharp/Compiler/Conformance/BasicGrammarElements/DecimalConstants.fs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.UnitTests
+
+open NUnit.Framework
+
+[<TestFixture>]
+module ``Decimal Constants`` =
+
+    [<Test>]
+    let ``Product of decimal constants``() =
+        let oneOfOneMiDec = 1.0E-6M
+        let oneMiDec      = 1.0E+6M
+
+        Assert.areEqual 1.0M (oneOfOneMiDec * oneMiDec)
+
+    [<Test>]
+    let ``Sum of decimal constants``() =
+        let x = 
+            1.0E0M 
+            + 2.0E1M
+            + 3.0E2M
+            + 4.0E3M
+            + 5.0E4M
+            + 6.0E5M
+            + 7.0E6M 
+            + 8.0E7M
+            + 9.0E8M 
+            + 1.0E-1M
+            + 2.0E-2M
+            + 3.0E-3M 
+            + 4.0E-4M
+            + 5.0E-5M 
+            + 6.0E-6M 
+            + 7.0E-7M 
+            + 8.0E-8M 
+            + 9.0E-9M
+
+        Assert.areEqual 987654321.123456789M x
+
+    [<Test>]
+    let ``Sum of decimal literals with leading zero in exponential``() =
+        let x = 1.0E00M + 2.0E01M + 3.E02M + 1.E-01M + 2.0E-02M
+
+        Assert.areEqual 321.12M x
