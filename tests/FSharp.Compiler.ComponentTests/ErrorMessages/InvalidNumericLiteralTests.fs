7c6296db54c3ca9def8d5080d9f7ec365eb3dde4
Thorsten (Gerbrunn) <th.reichert@gmx.net>
2020-07-05 19:01:36 +0200

    Migrated E_BasicConstantsBigNum01.fs test case

--- tests/FSharp.Compiler.ComponentTests/ErrorMessages/InvalidNumericLiteralTests.fs
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/InvalidNumericLiteralTests.fs
@@ -19,6 +19,18 @@ let foo = 1up // int
             (2, 11, 2, 14)
             "This is not a valid numeric literal. Valid numeric literals include 1, 0x1, 0o1, 0b1, 1l (int), 1u (uint32), 1L (int64), 1UL (uint64), 1s (int16), 1y (sbyte), 1uy (byte), 1.0 (float), 1.0f (float32), 1.0m (decimal), 1I (BigInteger)."
 
+    [<Fact>]
+    let ``1N is invalid numeric literal``() =
+        CompilerAssert.TypeCheckSingleError
+            """
+let x = 1N
+            """
+            FSharpErrorSeverity.Error
+            0784
+            (2, 9, 2, 11)
+            "This numeric literal requires that a module 'NumericLiteralN' defining functions FromZero, FromOne, FromInt32, FromInt64 and FromString be in scope"
+
+
     [<Theory>]
     [<InlineData("1.0E28M")>]
     [<InlineData("1.0E-28M")>]
