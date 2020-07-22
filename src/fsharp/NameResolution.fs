2dd0fe3ff3b9896a8e7de31adb9b80cfc381d5d4
TIHan <lol.tihan@gmail.com>
2020-07-21 19:03:45 -0700

    Minor rename

--- src/fsharp/NameResolution.fs
+++ src/fsharp/NameResolution.fs
@@ -2392,7 +2392,8 @@ let GetRecordLabelsForType g nenv ty =
           result.Add k |> ignore
     result
 
-let ResolveNestedTypes (ncenv: NameResolver) (resInfo: ResolutionInfo) ad nm (typeNameResInfo: TypeNameResolutionInfo) m ty =
+/// Get the nested types of the given type and check the nested types based on the type name resolution info.
+let CheckForNestedTypesOfType (ncenv: NameResolver) (resInfo: ResolutionInfo) ad nm (typeNameResInfo: TypeNameResolutionInfo) m ty =
     let tinstEnclosing, tcrefsNested = GetNestedTyconRefsOfType ncenv.InfoReader ncenv.amap (ad, Some nm, typeNameResInfo.StaticArgsInfo, true, m) ty
     let tcrefsNested = tcrefsNested |> List.map (fun tcrefNested -> (resInfo, tcrefNested))
     let tcrefsNested = CheckForTypeLegitimacyAndMultipleGenericTypeAmbiguities (tcrefsNested, typeNameResInfo, PermitDirectReferenceToGeneratedType.No, m)
@@ -2473,7 +2474,7 @@ let rec ResolveLongIdentInTypePrim (ncenv: NameResolver) nenv lookupKind (resInf
     let nestedSearchAccessible =
         match rest with
         | [] ->
-            let nestedTypes = ResolveNestedTypes ncenv resInfo ad nm typeNameResInfo m ty
+            let nestedTypes = CheckForNestedTypesOfType ncenv resInfo ad nm typeNameResInfo m ty
             if isNil nestedTypes then
                 NoResultsOrUsefulErrors
             else
@@ -2485,7 +2486,7 @@ let rec ResolveLongIdentInTypePrim (ncenv: NameResolver) nenv lookupKind (resInf
                 | ResolveTypeNamesToTypeRefs ->
                     OneSuccess (resInfo, Item.Types (nm, nestedTypes), rest)
         | id2 :: rest2 ->
-            let nestedTypes = ResolveNestedTypes ncenv resInfo ad nm (TypeNameResolutionInfo.ResolveToTypeRefs TypeNameResolutionStaticArgsInfo.Indefinite) m ty
+            let nestedTypes = CheckForNestedTypesOfType ncenv resInfo ad nm (TypeNameResolutionInfo.ResolveToTypeRefs TypeNameResolutionStaticArgsInfo.Indefinite) m ty
             ResolveLongIdentInNestedTypes ncenv nenv lookupKind resInfo (depth+1) id m ad id2 rest2 findFlag typeNameResInfo nestedTypes
 
     match nestedSearchAccessible with
