95a328ffe2db8605cdb9ae3f8e29a924951cfd76
Don Syme <dsyme@microsoft.com>
2020-07-27 21:09:27 +0100

    'fix test'

--- tests/fsharpqa/Source/Conformance/Expressions/DataExpressions/NameOf/E_NameOfAdditionExpr.fs
+++
@@ -1,6 +0,0 @@
-// #Regression #Conformance #DataExpressions 
-// Verify that nameof doesn't work on const string
-//<Expects id="FS3250" span="(6,16)" status="error">Expression does not have a name.</Expects>
-let x = nameof(1+2)
-
-exit 0
