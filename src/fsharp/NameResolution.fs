aa288fe20c59f881476ba5d032691225dec48dba
TIHan <lol.tihan@gmail.com>
2020-07-31 09:44:06 -0700

    Adding union case support

--- src/fsharp/NameResolution.fs
+++ src/fsharp/NameResolution.fs
@@ -1072,17 +1072,29 @@ let ChooseEventInfosForNameEnv g ty (einfos: EventInfo list) =
 /// Rules:
 ///     1. Add nested types.
 ///     2. Add C# style extension members.
-///     3. Add extention methods.
-///     4. Add extension properties.
-///     5. Add events.
-///     6. Add fields.
-///     7. Add properies.
-///     8. Add methods.
+///     3. Add union cases.
+///     4. Add extention methods.
+///     5. Add extension properties.
+///     6. Add events.
+///     7. Add fields.
+///     8. Add properies.
+///     9. Add methods.
 let rec AddContentOfTypeToNameEnv (g:TcGlobals) (amap: Import.ImportMap) ad m (nenv: NameResolutionEnv) (ty: TType) =
     let infoReader = InfoReader(g,amap)
 
     let nenv = AddNestedTypesOfTypeToNameEnv infoReader amap ad m nenv ty
     let nenv = AddCSharpStyleExtensionMembersOfTypeToNameEnv amap m nenv ty
+    let nenv =
+        match tryTcrefOfAppTy g ty with
+        | ValueSome tcref when not tcref.IsILTycon ->
+            let ucrefs = tcref.UnionCasesAsList |> List.map tcref.MakeNestedUnionCaseRef
+            let unqualifiedItems = AddUnionCases2 BulkAdd.Yes nenv.eUnqualifiedItems ucrefs
+            let patItems = AddUnionCases1 nenv.ePatItems ucrefs
+            { nenv with
+                eUnqualifiedItems = unqualifiedItems
+                ePatItems = patItems }
+        | _ ->
+            nenv
 
     // The order of items matter such as intrinsic members will always be favored over extension members of the same name.
     // Extension property members will always be favored over extenion methods of the same name.
@@ -1230,8 +1242,8 @@ and private AddPartsOfTyconRefToNameEnv bulkAddMode ownDefinition (g: TcGlobals)
     let nenv = 
         if amap.g.langVersion.SupportsFeature LanguageFeature.OpenTypeDeclaration &&
            not isIL &&
-           TryFindFSharpBoolAttribute g g.attrib_AutoOpenAttribute tcref.Attribs = Some true then
-            if tcref.Typars(m).Length > 0 then failwith "nope" // TODO proper error
+           TryFindFSharpBoolAttribute g g.attrib_AutoOpenAttribute tcref.Attribs = Some true &&
+           tcref.Typars(m).Length = 0 then
             let ty = generalizedTyconRef tcref
             AddContentOfTypeToNameEnv g amap ad m nenv ty
         else
