a02b53fc18cb56d0041582965e3819c8c9505e90
Abel Braaksma <abel.online@xs4all.nl>
2020-08-04 18:27:19 +0200

    Fix line-no in E_MustBeIdent02.fs fsharpqa test

--- tests/fsharpqa/Source/Conformance/LexicalAnalysis/ConditionalCompilation/E_MustBeIdent02.fs
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/ConditionalCompilation/E_MustBeIdent02.fs
@@ -1,7 +1,7 @@
 // #Regression #Conformance #LexicalAnalysis
 // Regression test for FSHARP1.0:1419
-//<Expects id="FS0039" span="(6,14-7,17)" status="error">The type 'if_' is not defined.</Expects>
-//<Expects id="FS0039" span="(7,14-8,20)" status="error">The type 'endif_' is not defined.</Expects>
+//<Expects id="FS0039" span="(6,14-6,17)" status="error">The type 'if_' is not defined.</Expects>
+//<Expects id="FS0039" span="(7,14-7,20)" status="error">The type 'endif_' is not defined.</Expects>
 #light
 let t8 (x : #if_) = ()
 let t7 (x : #endif_) = ()
