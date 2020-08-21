a9b062411f3414faede8ff007fdb9207614e5ea5
Paul Blasucci <pblasucci@gmail.com>
2020-08-21 14:19:13 +0200

    finally learning how the QA suite checks expected failures

--- tests/fsharpqa/Source/Conformance/UnitsOfMeasure/Diagnostics/E_UnsupportedType01.fs
+++ tests/fsharpqa/Source/Conformance/UnitsOfMeasure/Diagnostics/E_UnsupportedType01.fs
@@ -1,6 +1,6 @@
 // #Regression #Conformance #UnitsOfMeasure #Diagnostics 
 // Regression test for FSHARP1.0:2345
-//<Expects id="FS0636" span="(9,9-12,16)" status="error">Units-of-measure supported only on float, float32, decimal, signed integer types, and unsigned integer types</Expects>
+//<Expects id="FS0636" span="(9,9-11,15)" status="error">Units-of-measure supported only on float, float32, decimal, signed integer types, and unsigned integer types</Expects>
 
 [<Measure>] type Kg
 
