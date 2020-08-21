ae13b6caa284ccb8ddf8d59c3d2ca10e9c769aa8
Paul Blasucci <pblasucci@gmail.com>
2020-08-21 11:43:37 +0200

    one more failing test fixed

--- tests/fsharpqa/Source/Conformance/UnitsOfMeasure/Diagnostics/E_UnsupportedType01.fs
+++ tests/fsharpqa/Source/Conformance/UnitsOfMeasure/Diagnostics/E_UnsupportedType01.fs
@@ -1,9 +1,9 @@
 // #Regression #Conformance #UnitsOfMeasure #Diagnostics 
 // Regression test for FSHARP1.0:2345
-//<Expects id="FS0636" span="(12,9-12,16)" status="error">Units-of-measure supported only on float, float32, decimal, signed integer types, and unsigned integer types</Expects>
+//<Expects id="FS0636" span="(9,9-12,16)" status="error">Units-of-measure supported only on float, float32, decimal, signed integer types, and unsigned integer types</Expects>
 
 [<Measure>] type Kg
 
 let x = 1<Kg>    // ok
 
-let a = 1I<Kg> // BigInt
+let a = 1<Kg> // BigInt
