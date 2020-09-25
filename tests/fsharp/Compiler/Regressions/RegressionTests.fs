378707966eb0ca903b48c98d24a1b014819bfd6a
Will Smith <lol.tihan@gmail.com>
2020-09-25 11:01:05 -0700

    Able to remap DecisionTreeTest.Error

---
+++ tests/fsharp/Compiler/Regressions/RegressionTests.fs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.UnitTests
+
+open NUnit.Framework
+open FSharp.Test.Utilities.Compiler
+open FSharp.Tests
+
+[<TestFixture>]
+module RegressionTests =
+
+    [<Test>]
+    let ``No internal errors should be raised``() =
+        FSharp """
+namespace FSharpBug
+
+type TestItemSeq = 
+    static member Test1 item = item
+    static member Test2 item = match item with Typo2 x -> x
+        """
+        |> compile
+        |> withErrorCodes [39]
+        |> ignore
