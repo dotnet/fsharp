66216771b87b9efa64ca2f7a03499f58ca9fcac8
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2020-10-26 16:41:11 +0100

    Fixed a bunch of lst files (fixed Expect -> Expects), casing. Added some helper functions to test framework.

--- tests/fsharpqa/Source/InteractiveSession/Misc/TimeToggles.fsx
+++ tests/fsharpqa/Source/InteractiveSession/Misc/TimeToggles.fsx
@@ -1,7 +1,7 @@
 // #Regression #NoMT #FSI #RequiresENU 
 // Regression test for FSharp1.0:4164 - FSI emit "please report to fsbugs" error on malformed usage of .. operator
-//<Expect status="success"> Timing now on</Expects>
-//<Expect status="success"> Timing now off</Expects>
+//<Expects status="success"> Timing now on</Expects>
+//<Expects status="success"> Timing now off</Expects>
 
 #time "on"
 #time "off"
