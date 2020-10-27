a8e289bc16a9a660ee365a6c8f9c65f51c6aaf2e
cartermp <pcarter@fastmail.com>
2020-10-27 16:17:44 -0700

    QuickInfo is scoped out + surface area

--- tests/FSharp.Compiler.Service.Tests/SurfaceArea.netstandard.fs
+++ tests/FSharp.Compiler.Service.Tests/SurfaceArea.netstandard.fs
@@ -22502,8 +22502,8 @@ FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: FSharp.Compile
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Int32 GetEffectivelySameAsHash()
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Int32 GetHashCode()
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Internal.Utilities.StructuredFormat.Layout FormatLayout(FSharp.Compiler.SourceCodeServices.FSharpDisplayContext)
-FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption1[Internal.Utilities.StructuredFormat.Layout] GetReturnTypeLayout(FSharp.Compiler.SourceCodeServices.FSharpDisplayContext)
-FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption1[Internal.Utilities.StructuredFormat.Layout]
+FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption`1[Internal.Utilities.StructuredFormat.Layout]
+FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption`1[Internal.Utilities.StructuredFormat.Layout] GetReturnTypeLayout(FSharp.Compiler.SourceCodeServices.FSharpDisplayContext)
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption`1[FSharp.Compiler.Range+range] DeclarationLocation
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption`1[FSharp.Compiler.Range+range] ImplementationLocation
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption`1[FSharp.Compiler.Range+range] SignatureLocation
@@ -22525,8 +22525,8 @@ FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: System.Collect
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: System.Collections.Generic.IList`1[FSharp.Compiler.SourceCodeServices.FSharpGenericParameter] get_GenericParameters()
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: System.Collections.Generic.IList`1[System.Collections.Generic.IList`1[FSharp.Compiler.SourceCodeServices.FSharpParameter]] CurriedParameterGroups
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: System.Collections.Generic.IList`1[System.Collections.Generic.IList`1[FSharp.Compiler.SourceCodeServices.FSharpParameter]] get_CurriedParameterGroups()
-FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption1[Microsoft.FSharp.Collections.FSharpList1[System.String]] PossibleArgumentList
-FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption1[Microsoft.FSharp.Collections.FSharpList1[System.String]] get_PossibleArgumentList()
+FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption`1[Microsoft.FSharp.Collections.FSharpList1[System.String]] PossibleArgumentList
+FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: Microsoft.FSharp.Core.FSharpOption`1[Microsoft.FSharp.Collections.FSharpList1[System.String]] get_PossibleArgumentList()
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: System.Collections.Generic.IList`1[System.String] ElaboratedXmlDoc
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: System.Collections.Generic.IList`1[System.String] XmlDoc
 FSharp.Compiler.SourceCodeServices.FSharpMemberOrFunctionOrValue: System.Collections.Generic.IList`1[System.String] get_ElaboratedXmlDoc()
@@ -22744,7 +22744,7 @@ FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: FSharp.Compiler.Sourc
 FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: Microsoft.FSharp.Core.FSharpOption`1[FSharp.Compiler.Range+range] ValidateBreakpointLocation(pos)
 FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: Microsoft.FSharp.Core.FSharpOption`1[FSharp.Compiler.SourceCodeServices.FSharpNoteworthyParamInfoLocations] FindNoteworthyParamInfoLocations(pos)
 FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: Boolean IsTypeAnnotationGiven(pos)
-FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: Microsoft.FSharp.Core.FSharpOption1[Microsoft.FSharp.Collections.FSharpList`1[FSharp.Compiler.Range+range]] GetAllArgumentsForFunctionApplication(pos)
+FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: Microsoft.FSharp.Core.FSharpOption`1[Microsoft.FSharp.Collections.FSharpList`1[FSharp.Compiler.Range+range]] GetAllArgumentsForFunctionApplication(pos)
 FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: Microsoft.FSharp.Core.FSharpOption`1[FSharp.Compiler.SyntaxTree+ParsedInput] ParseTree
 FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: Microsoft.FSharp.Core.FSharpOption`1[FSharp.Compiler.SyntaxTree+ParsedInput] get_ParseTree()
 FSharp.Compiler.SourceCodeServices.FSharpParseFileResults: System.String FileName
--- vsintegration/src/FSharp.Editor/InlineHints/InlineHints.fs
+++ vsintegration/src/FSharp.Editor/InlineHints/InlineHints.fs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
 namespace Microsoft.VisualStudio.FSharp.Editor
 
-open Microsoft.VisualStudio.Shell
-
-open System
 open System.Collections.Immutable
 open System.Threading
 open System.ComponentModel.Composition
@@ -20,7 +17,6 @@ type internal FSharpInlineHintsService
     [<ImportingConstructor>]
     (
         checkerProvider: FSharpCheckerProvider,
-        [<Import(typeof<SVsServiceProvider>)>] serviceProvider: IServiceProvider,
         projectInfoManager: FSharpProjectOptionsManager
     ) =
 
@@ -31,8 +27,7 @@ type internal FSharpInlineHintsService
             asyncMaybe {
                 do! Option.guard (not (isSignatureFile document.FilePath))
 
-                let! textVersion = document.GetTextVersionAsync(cancellationToken)
-                let! parsingOptions, projectOptions = projectInfoManager.TryGetOptionsForEditingDocumentOrProject(document, cancellationToken, userOpName)
+                let! _, projectOptions = projectInfoManager.TryGetOptionsForEditingDocumentOrProject(document, cancellationToken, userOpName)
                 let! sourceText = document.GetTextAsync(cancellationToken)
                 let! parseFileResults, _, checkFileResults = checkerProvider.Checker.ParseAndCheckDocument(document, projectOptions, userOpName)
                 let range = RoslynHelpers.TextSpanToFSharpRange(document.FilePath, textSpan, sourceText)
@@ -69,44 +64,9 @@ type internal FSharpInlineHintsService
                         for tt in typeInfo do
                             displayParts.Add(TaggedText(RoslynHelpers.roslynTag tt.Tag, tt.Text))
 
-                        // TODO - this is not actually correct
-                        // We need to get QuickInfo for the actual type we pull out, not the value
-                        // But it does demonstrate a possible way to do this
-                        let callBack position =
-                            fun _ _ ->
-                                asyncMaybe {
-                                    let! quickInfo =
-                                        FSharpAsyncQuickInfoSource.ProvideQuickInfo(
-                                            checkerProvider.Checker,
-                                            document.Id,
-                                            sourceText,
-                                            document.FilePath,
-                                            position,
-                                            parsingOptions,
-                                            projectOptions,
-                                            textVersion.GetHashCode(),
-                                            document.FSharpOptions.LanguageServicePerformance)
-
-                                    let documentationBuilder = XmlDocumentation.CreateDocumentationBuilder(serviceProvider.XMLMemberIndexService)
-                                    let mainDesc, docs  = FSharpAsyncQuickInfoSource.BuildSingleQuickInfoItem documentationBuilder quickInfo
-
-                                    let descriptionParts = ImmutableArray.CreateBuilder()
-
-                                    for tt in mainDesc do
-                                        descriptionParts.Add(TaggedText(RoslynHelpers.roslynTag tt.Tag, tt.Text))
-
-                                    for tt in docs do
-                                        descriptionParts.Add(TaggedText(RoslynHelpers.roslynTag tt.Tag, tt.Text))
-
-                                    return (descriptionParts.ToImmutableArray())
-                                }
-                                |> Async.map (Option.defaultValue ImmutableArray<_>.Empty)
-                                |> RoslynHelpers.StartAsyncAsTask(cancellationToken)
-
-                        let getDescriptionAsync position = Func<Document, CancellationToken, _>(callBack position)
                         let symbolSpan = RoslynHelpers.FSharpRangeToTextSpan(sourceText, symbolUse.RangeAlternate)
 
-                        let hint = FSharpInlineHint(TextSpan(symbolSpan.End, 0), displayParts.ToImmutableArray(), getDescriptionAsync symbolSpan.Start)
+                        let hint = FSharpInlineHint(TextSpan(symbolSpan.End, 0), displayParts.ToImmutableArray())
                         typeHints.Add(hint)
 
                     | :? FSharpMemberOrFunctionOrValue as func when func.IsFunction && not symbolUse.IsFromDefinition ->
