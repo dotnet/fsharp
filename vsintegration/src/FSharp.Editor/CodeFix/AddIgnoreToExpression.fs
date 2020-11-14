bd3e62afed9db3430ffd36abc38706d0edf6084d
cartermp <pcarter@fastmail.com>
2020-11-13 18:42:38 -0800

    Add basic quick fixes to ignore values or add let bindings

---
+++ vsintegration/src/FSharp.Editor/CodeFix/AddIgnoreToExpression.fs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace Microsoft.VisualStudio.FSharp.Editor
+
+open System.Composition
+open System.Collections.Immutable
+open System.Threading
+open System.Threading.Tasks
+
+open Microsoft.CodeAnalysis.Text
+open Microsoft.CodeAnalysis.CodeFixes
+open Microsoft.CodeAnalysis.CodeActions
+
+[<ExportCodeFixProvider(FSharpConstants.FSharpLanguageName, Name = "AddIgnoreToExpression"); Shared>]
+type internal FSharpAddIgnoreKeywordCodeFixProvider() =
+    inherit CodeFixProvider()
+
+    static let fixableDiagnosticIds = set ["FS0020"; "FS0001" ]
+
+    override __.FixableDiagnosticIds = fixableDiagnosticIds.ToImmutableArray()
+
+    override this.RegisterCodeFixesAsync context : Task =
+        async {
+            let title = SR.IgnoreExpression()
+
+            let diagnostics =
+                context.Diagnostics
+                |> Seq.filter (fun x -> fixableDiagnosticIds |> Set.contains x.Id)
+                |> Seq.toImmutableArray
+
+            let codeFix =
+                CodeFixHelpers.createTextChangeCodeFix(
+                    title,
+                    context,
+                    (fun () -> asyncMaybe.Return [| TextChange(TextSpan(context.Span.End, 0), " |> ignore") |]))
+            
+            context.RegisterCodeFix(codeFix, diagnostics)
+        } |> RoslynHelpers.StartAsyncUnitAsTask(context.CancellationToken)
+ 
---
+++ vsintegration/src/FSharp.Editor/CodeFix/AddLetBindingToExpression.fs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace Microsoft.VisualStudio.FSharp.Editor
+
+open System.Composition
+open System.Collections.Immutable
+open System.Threading
+open System.Threading.Tasks
+
+open Microsoft.CodeAnalysis.Text
+open Microsoft.CodeAnalysis.CodeFixes
+open Microsoft.CodeAnalysis.CodeActions
+
+[<ExportCodeFixProvider(FSharpConstants.FSharpLanguageName, Name = "AddLetBindingToExpression"); Shared>]
+type internal FSharpAddLetBindingCodeFixProvider() =
+    inherit CodeFixProvider()
+
+    static let fixableDiagnosticIds = set ["FS0020" ]
+
+    override __.FixableDiagnosticIds = fixableDiagnosticIds.ToImmutableArray()
+
+    override this.RegisterCodeFixesAsync context : Task =
+        async {
+            let title = SR.AddLetBindingToExpression()
+
+            let diagnostics =
+                context.Diagnostics
+                |> Seq.filter (fun x -> fixableDiagnosticIds |> Set.contains x.Id)
+                |> Seq.toImmutableArray
+
+            let codeFix =
+                CodeFixHelpers.createTextChangeCodeFix(
+                    title,
+                    context,
+                    (fun () -> asyncMaybe.Return [| TextChange(TextSpan(context.Span.Start, 0), "let x = ") |]))
+            
+            context.RegisterCodeFix(codeFix, diagnostics)
+        } |> RoslynHelpers.StartAsyncUnitAsTask(context.CancellationToken)
+ 
