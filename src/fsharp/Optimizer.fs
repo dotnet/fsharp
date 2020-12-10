540ad0d9fc98610a9b56f48952d3dbded06a6b79
Don Syme <dsyme@users.noreply.github.com>
2020-12-10 21:22:23 +0000

    FCS testing and fix for witnesses for unit of measure code (#10668)
    * add expression testing for unitized operators
    
    * fix witnesses for primitive operators with units of measure
    
    * first check test cases fail for primitive operators with units of measure
    
    * fix witnesses for primitive operators with units of measure
    
    * fix witnesses
    
    Co-authored-by: Don Syme <donsyme@fastmail.com>

--- src/fsharp/Optimizer.fs
+++ src/fsharp/Optimizer.fs
@@ -2456,7 +2456,7 @@ and OptimizeWhileLoop cenv env (spWhile, marker, e1, e2, m) =
 and OptimizeTraitCall cenv env (traitInfo, args, m) =
 
     // Resolve the static overloading early (during the compulsory rewrite phase) so we can inline. 
-    match ConstraintSolver.CodegenWitnessForTraitConstraint cenv.TcVal cenv.g cenv.amap m traitInfo args with
+    match ConstraintSolver.CodegenWitnessExprForTraitConstraint cenv.TcVal cenv.g cenv.amap m traitInfo args with
 
     | OkResult (_, Some expr) -> OptimizeExpr cenv env expr
 
