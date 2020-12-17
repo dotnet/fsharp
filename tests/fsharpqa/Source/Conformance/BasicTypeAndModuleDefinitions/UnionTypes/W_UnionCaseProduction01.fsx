3c8c650c53372ec7cb2061468dcde9f692033073
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-12-17 11:41:38 +0300

    Add second warning

--- tests/fsharpqa/Source/Conformance/BasicTypeAndModuleDefinitions/UnionTypes/W_UnionCaseProduction01.fsx
+++ tests/fsharpqa/Source/Conformance/BasicTypeAndModuleDefinitions/UnionTypes/W_UnionCaseProduction01.fsx
@@ -5,6 +5,7 @@
 //  | id of type * ... * type	-- n-ary union case
 //  | id : sig-spec	-- n-ary union case
 //<Expects id="FS0042" span="(10,12-12,24)" status="warning">This construct is deprecated: it is only for use in the F# library</Expects>
+//<Expects id="FS0042" span="(10,12-12,24)" status="warning">This construct is deprecated: it is only for use in the F# library</Expects>
 #light
 
 type T = | D : int -> T
