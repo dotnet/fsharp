e38ba1c36be12d2278f7c970df6ccad80bf9aef4
Don Syme <donsyme@fastmail.com>
2020-12-22 15:32:02 +0000

    fix test

--- src/fsharp/service/QuickParse.fs
+++ src/fsharp/service/QuickParse.fs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
 
-namespace FSharp.Compiler
+namespace FSharp.Compiler.SourceCodeServices
 
 open System
+open FSharp.Compiler
 open FSharp.Compiler.AbstractIL.Internal.Library
-open FSharp.Compiler.SourceCodeServices
 
 /// Qualified long name.
 type PartialLongName =
@@ -19,7 +19,8 @@ type PartialLongName =
       EndColumn: int
 
       /// Position of the last dot.
-      LastDotPos: int option }
+      LastDotPos: int option
+    }
     
     /// Empty partial long name.
     static member Empty(endColumn: int) = { QualifyingIdents = []; PartialIdent = ""; EndColumn = endColumn; LastDotPos = None }
