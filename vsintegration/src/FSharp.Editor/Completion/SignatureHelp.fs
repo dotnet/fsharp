38669100dff209695b8fac299db16bdd915834c6
Phillip Carter <pcarter@fastmail.com>
2020-12-22 13:07:12 -0800

    Generate parameter name in sighelp when there is no public parameter name (#10761)

--- vsintegration/src/FSharp.Editor/Completion/SignatureHelp.fs
+++ vsintegration/src/FSharp.Editor/Completion/SignatureHelp.fs
@@ -360,17 +360,23 @@ type internal FSharpSignatureHelpProvider
                     let displayArgs = ResizeArray()
 
                     // Offset by 1 here until we support reverse indexes in this codebase
-                    for argument in definedArgs.[.. definedArgs.Length - 1 - numArgsAlreadyApplied] do
+                    definedArgs.[.. definedArgs.Length - 1 - numArgsAlreadyApplied] |> Array.iteri (fun index argument ->
                         let taggedText = ResizeArray()
                         let tt = ResizeArray()
                         let layout = argument.Type.FormatLayout symbolUse.DisplayContext
                         LayoutRender.emitL taggedText.Add layout |> ignore
                         for part in taggedText do
                             RoslynHelpers.CollectTaggedText tt part
+                            
+                        let name =
+                            if String.IsNullOrWhiteSpace(argument.DisplayName) then
+                                "arg" + string index
+                            else
+                                argument.DisplayName
 
                         let display =
                             [|
-                                RoslynTaggedText(TextTags.Local, argument.DisplayName)
+                                RoslynTaggedText(TextTags.Local, name)
                                 RoslynTaggedText(TextTags.Punctuation, ":")
                                 RoslynTaggedText(TextTags.Space, " ")
                             |]
@@ -385,13 +391,14 @@ type internal FSharpSignatureHelpProvider
                             display.Add(RoslynTaggedText(TextTags.Punctuation, ")"))
 
                         let info =
-                            { ParameterName = argument.DisplayName
+                            { ParameterName = name
                               IsOptional = false
-                              CanonicalTypeTextForSorting = argument.FullName
+                              // No need to do anything different here, as this field is only relevant for overloaded parameter names in methods.
+                              CanonicalTypeTextForSorting = name
                               Documentation = ResizeArray()
                               DisplayParts = display }
 
-                        displayArgs.Add(info)
+                        displayArgs.Add(info))
 
                     do! Option.guard (displayArgs.Count > 0)
 
