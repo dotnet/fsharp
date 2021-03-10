42a30843207901040a29b4c3945f99c39f3b02ba
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2021-03-10 18:23:08 +0100

    Switch expected error

--- tests/fsharpqa/Source/Conformance/ObjectOrientedTypeDefinitions/TypeExtensions/optional/E_TypeExtensionWithInterface01.fs
+++ tests/fsharpqa/Source/Conformance/ObjectOrientedTypeDefinitions/TypeExtensions/optional/E_TypeExtensionWithInterface01.fs
@@ -1,6 +1,6 @@
 // #Conformance #ObjectOrientedTypes #TypeExtensions #Regression
 // Regression for 909525, previously there was no error here
-// <Expects status="error" id="FS0909" span="(5,15-5,33)">All implemented interfaces should be declared on the initial declaration of the type</Expects>
+// <Expects status="error" id="FS0090" span="(5,15-5,33)">Interface implementations should be given on the initial declaration of a type.</Expects>
 type System.Random with
     interface System.IComparable 
     static member Factory() = 1
