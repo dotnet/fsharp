754d23d5d0a220723e6371ebe01162b7a2684242
Don Syme <donsyme@fastmail.com>
2021-03-22 19:12:49 +0000

    patch

--- src/fsharp/service/IncrementalBuild.fs
+++ src/fsharp/service/IncrementalBuild.fs
@@ -1164,6 +1164,10 @@ type IncrementalBuilder(tcGlobals, frameworkTcImports, nonFrameworkAssemblyInput
     
     member builder.AreCheckResultsBeforeFileInProjectReady filename = 
         let slotOfFile = builder.GetSlotOfFileName filename
+        let cache = TimeStampCache defaultTimeStamp
+        let ctok = AssumeCompilationThreadWithoutEvidence()
+        computeStampedReferencedAssemblies cache ctok
+        computeStampedFileNames cache ctok
         match tryGetBeforeSlot slotOfFile with
         | Some _ -> true
         | _ -> false
