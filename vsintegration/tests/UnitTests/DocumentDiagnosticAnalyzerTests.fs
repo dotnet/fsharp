c9e7a4703dbb4beaa63b8b7f4bd8e610dd6c2607
Will Smith <lol.tihan@gmail.com>
2021-03-22 13:57:57 -0700

    Make projects unique in testing diagnostics

--- vsintegration/tests/UnitTests/DocumentDiagnosticAnalyzerTests.fs
+++ vsintegration/tests/UnitTests/DocumentDiagnosticAnalyzerTests.fs
@@ -40,8 +40,9 @@ type DocumentDiagnosticAnalyzerTests()  =
 
     let getDiagnostics (fileContents: string) = 
         async {
+            let projectOptions = { projectOptions with ProjectId = Some(Guid.NewGuid().ToString()) }
             let document, _ = RoslynTestHelpers.CreateDocument(filePath, fileContents)
-            let parsingOptions, _ = checker.GetParsingOptionsFromProjectOptions { projectOptions with ProjectId = Some(Guid.NewGuid().ToString()) }
+            let parsingOptions, _ = checker.GetParsingOptionsFromProjectOptions projectOptions
             let! syntacticDiagnostics = FSharpDocumentDiagnosticAnalyzer.GetDiagnostics(checker, document, parsingOptions, projectOptions, DiagnosticsType.Syntax) 
             let! semanticDiagnostics = FSharpDocumentDiagnosticAnalyzer.GetDiagnostics(checker, document, parsingOptions, projectOptions, DiagnosticsType.Semantic) 
             return syntacticDiagnostics.AddRange(semanticDiagnostics)
