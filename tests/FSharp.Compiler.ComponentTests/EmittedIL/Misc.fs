dba45677652c040e5cc5679c341e619a2c587ce6
kerams <kerams@users.noreply.github.com>
2021-03-31 12:38:24 +0200

    Fix tests pls

--- tests/FSharp.Compiler.ComponentTests/EmittedIL/Misc.fs
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/Misc.fs
@@ -23,21 +23,19 @@ let zGeneric<'a> (): 'a[] = [||]
          |> compileForNetCore
          |> shouldSucceed
          |> verifyIL ["""
-.method public static valuetype [runtime]System.Int32[] 
-              zInt() cil managed
+.method public static valuetype int32[] zInt() cil managed
 {
 
   .maxstack  8
-  IL_0000:  call       !!0[] [runtime]System.Array::Empty<valuetype [runtime]System.Int32>()
+  IL_0000:  call       !!0[] [runtime]System.Array::Empty<int32>()
   IL_0005:  ret
 } 
 
-.method public static class [runtime]System.String[] 
-              zString() cil managed
+.method public static class string[] zString() cil managed
 {
 
   .maxstack  8
-  IL_0000:  call       !!0[] [runtime]System.Array::Empty<class [runtime]System.String>()
+  IL_0000:  call       !!0[] [runtime]System.Array::Empty<string>()
   IL_0005:  ret
 }
 
