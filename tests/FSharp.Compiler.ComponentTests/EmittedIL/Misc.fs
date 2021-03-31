b2639a607768bf6262e2a5faca5a44c6e2503d32
kerams <kerams@users.noreply.github.com>
2021-03-31 12:13:49 +0200

    Add test

---
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/Misc.fs
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.ComponentTests.EmittedIL
+
+open Xunit
+open FSharp.Test.Utilities.Compiler
+
+module ``Misc`` =
+    let private compileForNetCore opts =
+        opts |> ignoreWarnings |> withOptions ["-g"; "--optimize+"; "--targetprofile:netcore"] |> compile
+
+    [<Fact>]
+    let ``Empty array construct compiles to System.Array.Empty<_>()``() =
+        FSharp """
+module Misc
+
+let zInt (): int[] = [||]
+
+let zString (): string[] = [||]
+
+let zGeneric<'a> (): 'a[] = [||]
+         """
+         |> compileForNetCore
+         |> shouldSucceed
+         |> verifyIL ["""
+.method public static valuetype [runtime]System.Int32[] 
+              zInt() cil managed
+{
+
+  .maxstack  8
+  IL_0000:  call       !!0[] [runtime]System.Array::Empty<valuetype [runtime]System.Int32>()
+  IL_0005:  ret
+} 
+
+.method public static class [runtime]System.String[] 
+              zString() cil managed
+{
+
+  .maxstack  8
+  IL_0000:  call       !!0[] [runtime]System.Array::Empty<class [runtime]System.String>()
+  IL_0005:  ret
+}
+
+.method public static !!a[]  zGeneric<a>() cil managed
+{
+
+  .maxstack  8
+  IL_0000:  call       !!0[] [runtime]System.Array::Empty<!!0>()
+  IL_0005:  ret
+}"""]
