f79f5d63dd3cc9dac4e986d446b480256c136390
Don Syme <donsyme@fastmail.com>
2021-04-08 13:47:34 +0100

    fix test

--- tests/fsharpqa/Source/Conformance/TypesAndTypeConstraints/CheckingSyntacticTypes/ByRef04.fsx
+++ tests/fsharpqa/Source/Conformance/TypesAndTypeConstraints/CheckingSyntacticTypes/ByRef04.fsx
@@ -1,6 +1,6 @@
 // #ByRef #Regression #inline
 // Regression test for DevDiv:122445 ("Internal compiler error when evaluating code with inline/byref")
-//<Expects status="success">val inline f :</Expects>
+//<Expects status="success">val f :</Expects>
 //<Expects status="success">  x:string -> y:nativeptr< \^a> -> bool</Expects>
 //<Expects status="success">    when  \^a : unmanaged and</Expects>
 //<Expects status="success">          \^a : \(static member TryParse : string \* nativeptr< \^a> -> bool\)</Expects>
