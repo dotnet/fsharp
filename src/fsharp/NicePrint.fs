60b00a7587007a5ca76794c56364380690161fa1
Phillip Carter <pcarter@fastmail.com>
2021-04-12 12:00:12 -0700

    Use indentation=4 for printing signatures in external Go to Def to match F# style guide (#11403)

--- src/fsharp/NicePrint.fs
+++ src/fsharp/NicePrint.fs
@@ -1928,7 +1928,7 @@ module private TastDefinitionPrinting =
                         valsL
                     ]
             else
-                headerL @@- (aboveL entitiesL valsL)
+                headerL @@---- entitiesL @@ valsL
 
     and layoutEntity (denv: DisplayEnv) (infoReader: InfoReader) ad m (entity: Entity) =
         if entity.IsModuleOrNamespace then
