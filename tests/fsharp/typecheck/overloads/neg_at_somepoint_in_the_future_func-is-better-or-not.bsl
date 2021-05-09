bf87fedb93b07626327a88496b532af31a44f5c3
Gauthier Segay <gauthier.segay@gmail.com>
2021-05-09 22:39:00 +0200

    more tests

--- tests/fsharp/typecheck/overloads/neg_at_somepoint_in_the_future_func-is-better-or-not.bsl
+++ tests/fsharp/typecheck/overloads/neg_at_somepoint_in_the_future_func-is-better-or-not.bsl
@@ -1,2 +1,16 @@
 
-neg_at_somepoint_in_the_future_func-is-better-or-not.fsx(11,1,11,17): typecheck error FS0193: func is better on the way out, you'll need type annotation on the lambda literal to remove ambiguity
+neg_at_somepoint_in_the_future_func-is-better-or-not.fsx(14,1,14,17): typecheck error FS0193: The method 'M' has overload with 'System.Func' delegate in the signature, to fix the warning, you need to specify the type explicitly.
+
+neg_at_somepoint_in_the_future_func-is-better-or-not.fsx(16,1,16,19): typecheck error FS0193: The method 'M2' has overload with 'System.Func' delegate in the signature, to fix the warning, you need to specify the type explicitly.
+
+neg_at_somepoint_in_the_future_func-is-better-or-not.fsx(25,1,25,37): typecheck error FS0041: A unique overload for method 'M3' could not be determined based on type information prior to this program point. A type annotation may be needed.
+
+Known types of arguments: (unit -> unit) * (unit -> unit)
+
+Candidates:
+ - member C.M3 : [<ParamArray>] funcs:Action array -> unit
+ - member C.M3 : [<ParamArray>] funcs:Func<unit,unit> array -> unit
+
+neg_at_somepoint_in_the_future_func-is-better-or-not.fsx(38,5,38,63): typecheck error FS0193: The method 'Min' has overload with 'System.Func' delegate in the signature, to fix the warning, you need to specify the type explicitly.
+
+neg_at_somepoint_in_the_future_func-is-better-or-not.fsx(40,5,40,74): typecheck error FS0193: The method 'Min' has overload with 'System.Func' delegate in the signature, to fix the warning, you need to specify the type explicitly.
--- tests/fsharp/typecheck/overloads/neg_at_somepoint_in_the_future_func-is-better-or-not.fsx
+++ tests/fsharp/typecheck/overloads/neg_at_somepoint_in_the_future_func-is-better-or-not.fsx
@@ -1,11 +1,41 @@
 open System
 
 type C() =
-    member _.M(a: Action<int>) = printfn "action"
-    member _.M(f: Func<int, unit>) = printfn "func"
-    
+    member _.M(a: Action<int>) = printfn "M(action)"
+    member _.M(f: Func<int, unit>) = printfn "M(func)"
+    member _.M2(f: Func<unit, unit>) = printfn "M2(func())"
+    member _.M2(f: Action) = printfn "M2(action())"
+    member _.M3([<ParamArray>] funcs: Func<unit,unit> array) = printfn "M3(funcs)"
+    member _.M3([<ParamArray>] funcs: Action array) = printfn "M3(actions)"
 let c = C()
 
 // https://github.com/dotnet/fsharp/issues/11534
 // we may want a warning here, or maybe an error proper at some point
 c.M(fun _ -> ())
+
+c.M2(fun () -> ())
+
+c.M3(Action(fun () -> ()), (fun () -> ()))
+c.M3(Action id, id, id, id)
+c.M3(Func<_,_> id, id, id, id)
+c.M3(Func<_,_>(fun () -> ()), (fun () -> ()))
+c.M3((fun () -> ()), Func<_,_>(fun () -> ()))
+
+// this one doesn't resolve, although without the param array, it would
+c.M3((fun () -> ()), (fun () -> ()))
+
+
+open System.Collections.Generic
+open System.Linq
+
+// here we verify different style of calling BCL overloaded methods
+type Query =
+  static member MinBy_ExplicitDelegate<'T, 'Q, 'Key when 'Key: equality and 'Key: comparison> (source: IEnumerable<'T>, valueSelector: 'T -> 'Key) =
+    Enumerable.Min(source, Func<'T, 'Key>(valueSelector))
+  static member MinBy_ExplicitDelegateWithArgName<'T, 'Q, 'Key when 'Key: equality and 'Key: comparison> (source: IEnumerable<'T>, valueSelector: 'T -> 'Key) =
+    Enumerable.Min(source, selector = Func<'T, 'Key>(valueSelector))
+  static member MinBy_Lambda<'T, 'Q, 'Key when 'Key: equality and 'Key: comparison> (source: IEnumerable<'T>, valueSelector: 'T -> 'Key) =
+    Enumerable.Min(source, (fun (t: 'T) -> (valueSelector t)))
+  static member MinBy_LambdaWithArgName<'T, 'Q, 'Key when 'Key: equality and 'Key: comparison> (source: IEnumerable<'T>, valueSelector: 'T -> 'Key) =
+    Enumerable.Min(source, selector = (fun (t: 'T) -> (valueSelector t)))
+
