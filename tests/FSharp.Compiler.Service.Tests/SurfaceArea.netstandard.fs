1d2bdf02c2cb369cf52764f02a3e2689ed4f7924
Will Smith <lol.tihan@gmail.com>
2021-05-18 12:26:14 -0700

    Update surface area

--- tests/FSharp.Compiler.Service.Tests/SurfaceArea.netstandard.fs
+++ tests/FSharp.Compiler.Service.Tests/SurfaceArea.netstandard.fs
@@ -10420,7 +10420,5 @@ FSharp.Compiler.Xml.XmlDoc: System.String GetXmlText()
 FSharp.Compiler.Xml.XmlDoc: System.String[] GetElaboratedXmlLines()
 FSharp.Compiler.Xml.XmlDoc: System.String[] UnprocessedLines
 FSharp.Compiler.Xml.XmlDoc: System.String[] get_UnprocessedLines()
-FSharp.Compiler.Xml.XmlDoc: Void .ctor(System.String[], FSharp.Compiler.Text.Range)oc: System.String[] UnprocessedLines
-FSharp.Compiler.Xml.XmlDoc: System.String[] get_UnprocessedLines()
 FSharp.Compiler.Xml.XmlDoc: Void .ctor(System.String[], FSharp.Compiler.Text.Range)"
         SurfaceArea.verify expected "netstandard" (System.IO.Path.Combine(__SOURCE_DIRECTORY__,__SOURCE_FILE__))
