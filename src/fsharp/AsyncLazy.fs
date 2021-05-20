78c5be8c36c60aa3170c2ab5e7c6009cb64a739d
Will Smith <lol.tihan@gmail.com>
2021-05-20 08:43:16 -0700

    Trying to fix tests

--- src/fsharp/AsyncLazy.fs
+++ src/fsharp/AsyncLazy.fs
@@ -65,6 +65,7 @@ type AsyncLazy<'T> (computation: Async<'T>) =
                                 // This computation can only be canceled if the requestCount reaches zero.
                                 let! result = computation
                                 cachedResult <- ValueSome result
+                                cachedResultAsync <- ValueSome (async { return result })
                                 computation <- Unchecked.defaultof<_>
                                 if not ct.IsCancellationRequested then
                                     replyChannel.Reply (Ok result)
@@ -83,50 +84,49 @@ type AsyncLazy<'T> (computation: Async<'T>) =
         match cachedResultAsync with
         | ValueSome resultAsync -> resultAsync
         | _ ->
-            match cachedResult with
-            | ValueSome result -> 
-                let resultAsync = async { return result }
-                cachedResultAsync <- ValueSome resultAsync
-                resultAsync
-            | _ ->
-                async {
-                   match cachedResult with
-                   | ValueSome result -> return result
-                   | _ ->
-                       let action =
-                            lock gate <| fun () ->
-                                // We try to get the cached result after the lock so we don't spin up a new mailbox processor.
-                                match cachedResult with
-                                | ValueSome result -> AgentAction<'T>.CachedValue result
+            async {
+                match cachedResult with
+                | ValueSome result -> return result
+                | _ ->
+                    let action =
+                        lock gate <| fun () ->
+                            // We try to get the cached result after the lock so we don't spin up a new mailbox processor.
+                            match cachedResult with
+                            | ValueSome result -> AgentAction<'T>.CachedValue result
+                            | _ ->
+                                requestCount <- requestCount + 1
+                                match agentInstance with
+                                | Some agentInstance -> AgentAction<'T>.GetValue agentInstance
                                 | _ ->
-                                    requestCount <- requestCount + 1
-                                    match agentInstance with
-                                    | Some agentInstance -> AgentAction<'T>.GetValue agentInstance
-                                    | _ ->
+                                    try
                                         let cts = new CancellationTokenSource ()
                                         let agent = new MailboxProcessor<AsyncLazyWeakMessage<'T>> (loop, cancellationToken = cts.Token)
                                         let newAgentInstance = (agent, cts)
                                         agentInstance <- Some newAgentInstance
                                         agent.Start ()
                                         AgentAction<'T>.GetValue newAgentInstance
+                                    with
+                                    | ex ->
+                                        agentInstance <- None
+                                        raise ex
 
-                       match action with
-                       | AgentAction.CachedValue result -> return result
-                       | AgentAction.GetValue (agent, cts) ->                       
-                           try
-                               let! ct = Async.CancellationToken
-                               match! agent.PostAndAsyncReply (fun replyChannel -> GetValue(replyChannel, ct)) with
-                               | Ok result -> return result
-                               | Error ex -> return raise ex
-                            finally
-                                lock gate <| fun () ->
-                                    requestCount <- requestCount - 1
-                                    if requestCount = 0 then
-                                         cts.Cancel () // cancel computation when all requests are cancelled
-                                         try (agent :> IDisposable).Dispose () with | _ -> ()
-                                         cts.Dispose ()
-                                         agentInstance <- None
-               }
+                    match action with
+                    | AgentAction.CachedValue result -> return result
+                    | AgentAction.GetValue (agent, cts) ->                       
+                        try
+                            let! ct = Async.CancellationToken
+                            match! agent.PostAndAsyncReply (fun replyChannel -> GetValue(replyChannel, ct)) with
+                            | Ok result -> return result
+                            | Error ex -> return raise ex                                 
+                        finally
+                            lock gate <| fun () ->
+                                requestCount <- requestCount - 1
+                                if requestCount = 0 then
+                                        cts.Cancel () // cancel computation when all requests are cancelled
+                                        try (agent :> IDisposable).Dispose () with | _ -> ()
+                                        cts.Dispose ()
+                                        agentInstance <- None
+            }
 
        member _.TryGetValue() = cachedResult
 
