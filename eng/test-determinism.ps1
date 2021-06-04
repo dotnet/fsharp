2e4f679b77c59879d603202b657b4e5262ca6fab
Will Smith <lol.tihan@gmail.com>
2021-06-03 17:12:38 -0700

    Adding job

--- eng/test-determinism.ps1
+++ eng/test-determinism.ps1
@@ -90,7 +90,7 @@ function Get-FilesToProcess([string]$rootDir) {
     }
 
     $fileId = $filePath.Substring($objDir.Length).Replace("\", ".").TrimStart(".")
-    $fileHash = (Get-FileHash $filePath -algorithm SHA512).Hash
+    $fileHash = (Get-FileHash $filePath -algorithm SHA1).Hash
 
     $data = @{}
     $data.Hash = $fileHash
