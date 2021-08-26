270c97063e73951e792fd9966f57f5d1766fac26
KevinRansom <codecutter@hotmail.com>
2021-08-25 20:40:28 -0700

    update versions

--- src/fsharp/LanguageFeatures.fs
+++ src/fsharp/LanguageFeatures.fs
@@ -52,13 +52,14 @@ type LanguageVersion (specifiedVersionAsString) =
     static let languageVersion46 = 4.6m
     static let languageVersion47 = 4.7m
     static let languageVersion50 = 5.0m
+    static let languageVersion60 = 6.0m
     static let previewVersion = 9999m                   // Language version when preview specified
-    static let defaultVersion = languageVersion50       // Language version when default specified
+    static let defaultVersion = languageVersion60       // Language version when default specified
     static let latestVersion = defaultVersion           // Language version when latest specified
-    static let latestMajorVersion = languageVersion50   // Language version when latestmajor specified
+    static let latestMajorVersion = languageVersion60   // Language version when latestmajor specified
 
     static let validOptions = [| "preview"; "default"; "latest"; "latestmajor" |]
-    static let languageVersions = set [| languageVersion46; languageVersion47 ; languageVersion50 |]
+    static let languageVersions = set [| languageVersion46; languageVersion47; languageVersion50; languageVersion60 |]
 
     static let features =
         dict [
@@ -81,8 +82,10 @@ type LanguageVersion (specifiedVersionAsString) =
             LanguageFeature.NameOf, languageVersion50
             LanguageFeature.StringInterpolation, languageVersion50
 
+            // F# 6.0
+
             // F# preview
-            LanguageFeature.AdditionalTypeDirectedConversions, previewVersion
+            LanguageFeature.AdditionalTypeDirectedConversions, languageVersion60
             LanguageFeature.RelaxWhitespace2, previewVersion
             LanguageFeature.OverloadsForCustomOperations, previewVersion
             LanguageFeature.ExpandedMeasurables, previewVersion
@@ -107,6 +110,7 @@ type LanguageVersion (specifiedVersionAsString) =
         | "4.6" -> languageVersion46
         | "4.7" -> languageVersion47
         | "5.0" -> languageVersion50
+        | "6.0" -> languageVersion60
         | _ -> 0m
 
     let versionToString v =
