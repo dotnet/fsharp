6d5a1a689ed91e3d85eafe4e752283a9ab6597f0
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2021-08-31 16:10:21 +0300

    Parser: better tree for unfinished paren patterns

--- tests/service/ParserTests.fs
+++ tests/service/ParserTests.fs
@@ -102,3 +102,30 @@ match () with
     | SynExpr.Match (_, _, [ SynMatchClause (_, _, _, SynExpr.ArbitraryAfterError _, _, _)
                              SynMatchClause (_, _, _, SynExpr.Const _, _, _) ], _) -> ()
     | _ -> failwith "Unexpected tree"
+
+[<Test>]
+let ``Let - Parameter - Paren 01`` () =
+    let parseResults = getParseResults """
+let f (x
+"""
+
+    match getSingleDeclInModule parseResults with
+    | SynModuleDecl.Let (_, [ SynBinding (headPat = headPat) ], _) ->
+        match headPat with
+        | SynPat.LongIdent (_, _, _, SynArgPats.Pats [ SynPat.FromParseError (SynPat.Paren (SynPat.Named _, _), _) ], _, _) -> ()
+        | _ -> failwith "Unexpected tree"
+    | _ -> failwith "Unexpected tree"
+
+[<Test>]
+let ``Let - Parameter - Paren 02 - Tuple`` () =
+    let parseResults = getParseResults """
+let f (x, y
+"""
+
+    match getSingleDeclInModule parseResults with
+    | SynModuleDecl.Let (_, [ SynBinding (headPat = headPat) ], _) ->
+        match headPat with
+        | SynPat.LongIdent (_, _, _, SynArgPats.Pats [ SynPat.FromParseError (SynPat.Paren (SynPat.Tuple _, _), _) ], _, _) -> ()
+        | _ -> failwith "Unexpected tree"
+    | _ -> failwith "Unexpected tree"
+
