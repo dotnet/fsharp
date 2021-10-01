70b6a7e6c4ac05bb4f104e419f420dde8253a2d0
Don Syme <donsyme@fastmail.fm>
2021-10-01 12:57:03 +0100

    alternative fix, non-breaking

--- tests/fsharp/typecheck/sigs/neg131.fs
+++ tests/fsharp/typecheck/sigs/neg131.fs
@@ -5,8 +5,9 @@ open System
 module TestOverloadsWithSrtpThatDontResolve1 =
 
     type OverloadsWithSrtp() =
+        [<FSharp.Core.CompilerServices.NoEagerConstraintApplication>]
         static member inline SomeMethod< ^T when ^T : (member Length: int) > (x: ^T, f: ^T -> int) = 1
-        static member inline SomeMethod(x: 'T list, f: 'T list -> int) = 2
+        static member  SomeMethod(x: 'T list, f: 'T list -> int) = 2
 
     // Here, 'x' doesn't contain any type information so the overload doesn't resolve
     // The second overload is generic so is not preferred
