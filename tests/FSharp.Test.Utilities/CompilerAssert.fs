85b4a72c85aa3a63b52e2874d426b745abf36cc6
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2021-10-08 18:16:51 +0200

    Fix for #12011

--- tests/FSharp.Test.Utilities/CompilerAssert.fs
+++ tests/FSharp.Test.Utilities/CompilerAssert.fs
@@ -692,13 +692,13 @@ Updated automatically, please check diffs in your pull request, changes must be 
     static member RunScript source expectedErrorMessages =
         CompilerAssert.RunScriptWithOptions [||] source expectedErrorMessages
 
-    static member Parse (source: string, ?langVersion: string) =
+    static member Parse (source: string, ?langVersion: string, ?fileName: string) =
         let langVersion = defaultArg langVersion "default"
-        let sourceFileName = "test.fs"
+        let sourceFileName = defaultArg fileName "test.fsx"
         let parsingOptions =
             { FSharpParsingOptions.Default with
                 SourceFiles = [| sourceFileName |]
-                LangVersionText=langVersion }
+                LangVersionText = langVersion }
         checker.ParseFile(sourceFileName, SourceText.ofString source, parsingOptions) |> Async.RunImmediate
 
     static member ParseWithErrors (source: string, ?langVersion: string) = fun expectedParseErrors -> 
