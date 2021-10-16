bb17ba6ee91b48a285cdf4e8164662900c86ac50
KevinRansom <codecutter@hotmail.com>
2021-10-15 17:14:17 -0700

    @xmldoctests@

--- tests/service/XmlDocTests.fs
+++ tests/service/XmlDocTests.fs
@@ -569,7 +569,6 @@ module ///M2
 """
         (checkXml "M" [|"M1"|])
         (checkParsingErrors [|
-            Error 3350, Line 5, Col 7, Line 5, Col 10, "Feature 'attributes to the right of the 'module' keyword' is not available in F# 5.0. Please use language version 'preview' or greater."
             Error 3520, Line 3, Col 7, Line 3, Col 12, "XML comment is not placed on a valid language element."
          |])
 
