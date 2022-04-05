ec4115d76334a09a25792f32ede6c8f711d7a07e
KevinRansom <codecutter@hotmail.com>
2022-04-05 01:42:57 -0700

    Delete files

--- tests/fsharpqa/Source/CodeGen/EmittedIL/SeqExpressionTailCalls/SeqExpressionTailCalls01.fs
+++
@@ -1,4 +0,0 @@
-// #NoMono #NoMT #CodeGen #EmittedIL #Sequences #TailCalls   
-module SeqExpressionTailCalls01 // Regression test for FSHARP1.0:4135
-let rec rwalk x = seq { yield x; yield! rwalk (x+1) }
-
