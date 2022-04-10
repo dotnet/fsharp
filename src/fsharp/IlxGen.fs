d9924a17a362a8bf026a1f311434a9960d20d8ad
kerams <kerams@users.noreply.github.com>
2022-04-10 10:14:43 +0200

    Fix GenWhileLoop when the stack is not empty, add 2 more tests

--- src/fsharp/IlxGen.fs
+++ src/fsharp/IlxGen.fs
@@ -4108,18 +4108,44 @@ and GenIntegerForLoop cenv cgbuf eenv (spFor, spTo, v, e1, dir, e2, loopBody, m)
 and GenWhileLoop cenv cgbuf eenv (spWhile, condExpr, bodyExpr, m) sequel =
     let eenv = SetIsInLoop true eenv
 
-    let startTest = CG.GenerateDelayMark cgbuf "startTest"
-    CG.EmitInstr cgbuf (pop 0) Push0 (I_br startTest.CodeLabel)
+    // jmp test; body; test; if testPassed then jmp body else finish
+    // 
+    // This is a pattern recognized by the JIT and it results in the most efficient assembly.
+    if cgbuf.GetCurrentStack().IsEmpty then
+        let startTest = CG.GenerateDelayMark cgbuf "startTest"
+        CG.EmitInstr cgbuf (pop 0) Push0 (I_br startTest.CodeLabel)
 
-    let startBody = CG.GenerateMark cgbuf "startBody"
-    GenExpr cenv cgbuf eenv bodyExpr discard
+        let startBody = CG.GenerateMark cgbuf "startBody"
+        GenExpr cenv cgbuf eenv bodyExpr discard
 
-    match spWhile with
-    | DebugPointAtWhile.Yes spStart -> CG.EmitDebugPoint cgbuf spStart
-    | DebugPointAtWhile.No -> ()
+        match spWhile with
+        | DebugPointAtWhile.Yes spStart -> CG.EmitDebugPoint cgbuf spStart
+        | DebugPointAtWhile.No -> ()
 
-    CG.SetMarkToHere cgbuf startTest
-    GenExpr cenv cgbuf eenv condExpr (CmpThenBrOrContinue (pop 1, [ I_brcmp (BI_brtrue, startBody.CodeLabel) ]))
+        CG.SetMarkToHere cgbuf startTest
+        GenExpr cenv cgbuf eenv condExpr (CmpThenBrOrContinue (pop 1, [ I_brcmp (BI_brtrue, startBody.CodeLabel) ]))
+
+    // In the rare cases when there is something already on the stack, e.g.
+    //
+    // let f() =
+    //     callSomething firstArgument ((while .... do ...); secondArgument)
+    //
+    // we emit
+    //
+    // test; if not testPassed jmp finish; body; jmp test; finish
+    else
+        let finish = CG.GenerateDelayMark cgbuf "while_finish"
+
+        match spWhile with
+        | DebugPointAtWhile.Yes spStart -> CG.EmitDebugPoint cgbuf spStart
+        | DebugPointAtWhile.No -> ()
+
+        let startTest = CG.GenerateMark cgbuf "startTest"
+
+        GenExpr cenv cgbuf eenv condExpr (CmpThenBrOrContinue (pop 1, [ I_brcmp (BI_brfalse, finish.CodeLabel) ]))
+
+        GenExpr cenv cgbuf eenv bodyExpr (DiscardThen (Br startTest))
+        CG.SetMarkToHere cgbuf finish
 
     GenUnitThenSequel cenv eenv m eenv.cloc cgbuf sequel
 
