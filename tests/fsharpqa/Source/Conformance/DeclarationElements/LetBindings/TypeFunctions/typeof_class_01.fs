c44b81120593c8d8f6e0d578862827fbcdc4df09
Kevin Ransom (msft) <codecutter@hotmail.com>
2022-04-19 08:37:42 -0700

    Conformance\DeclarationElements\LetBindings (#13008)

--- tests/fsharpqa/Source/Conformance/DeclarationElements/LetBindings/TypeFunctions/typeof_class_01.fs
+++
@@ -1,9 +0,0 @@
-// #Regression #Conformance #DeclarationElements #LetBindings #TypeTests 
-// Regression test for FSHARP1.0:2320
-// A is a class
-#light
-
-type A = class
-         end
-
-let _ = typeof<A>
