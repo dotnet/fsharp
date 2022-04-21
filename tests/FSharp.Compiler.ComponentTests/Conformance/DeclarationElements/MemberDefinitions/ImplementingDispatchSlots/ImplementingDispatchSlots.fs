34fb4ca28da7f189a4c92e6a34db5915146dd31c
Kevin Ransom (msft) <codecutter@hotmail.com>
2022-04-21 04:07:56 -0700

    Move Fsharpqa (#13027)
    * Source/Conformance/DeclarationElements/Events
    
    * Conformance/DeclarationElements/FieldMembers
    
    * Conformance\DeclarationElements\ImportDeclarations
    
    * Conformance/DeclarationElements/InterfaceSpecificationsAndImplementations
    
    * Conformance/DeclarationElements/MemberDefinitions/ImplementingDispatchSlots
    
    * Conformance/DeclarationElements/MemberDefinitions/MethodsAndProperties
    
    * typo
    
    * /Conformance/DeclarationElements/MemberDefinitions/NamedArguments
    
    * Conformance/DeclarationElements/MemberDefinitions/OptionalArguments
    
    * Conformance/DeclarationElements/MemberDefinitions/OptionalDefaultParamArgs
    
    * delete
    
    * Conformance/DeclarationElements/MemberDefinitions/OverloadingMembers
    
    * typo
    
    * typo

---
+++ tests/FSharp.Compiler.ComponentTests/Conformance/DeclarationElements/MemberDefinitions/ImplementingDispatchSlots/ImplementingDispatchSlots.fs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.ComponentTests.Conformance.DeclarationElements.MemberDefinitions
+
+open Xunit
+open FSharp.Test
+open FSharp.Test.Compiler
+
+module ImplementingDispatchSlots =
+
+    let verifyCompile compilation =
+        compilation
+        |> asExe
+        |> withOptions ["--nowarn:988"]
+        |> compile
+
+    let verifyCompileAndRun compilation =
+        compilation
+        |> asExe
+        |> withOptions ["--nowarn:988"]
+        |> compileAndRun
+
+    // SOURCE=SanityCheck.fs
+    [<Theory; Directory(__SOURCE_DIRECTORY__, Includes=[|"SanityCheck.fs"|])>]
+    let ``SanityCheck_fs`` compilation =
+        compilation
+        |> verifyCompileAndRun
+        |> shouldSucceed
+
+
---
+++ tests/FSharp.Compiler.ComponentTests/Conformance/DeclarationElements/MemberDefinitions/MethodsAndProperties/E_IndexerArityMismatch02.fs
@@ -0,0 +1,14 @@
+// #Regression #Conformance #DeclarationElements #MemberDefinitions #MethodsAndProperties 
+// Regression test for FSHARP1.0:1407 (Poor error message when arguments do not match signature of overloaded operator. Also incorrect span.)
+
+//<Expects id="FS0001" span="(11,24-11,35)" status="error">This expression was expected to have type.    ''a\[,,\]'    .but here has type.    'int\[\]'</Expects>
+//<Expects id="FS0001" span="(12,25-12,32)" status="error">This expression was expected to have type.    ''a\[\]'    .but here has type.    'int\[,\]'</Expects>
+//<Expects id="FS0001" span="(13,27-13,38)" status="error">This expression was expected to have type.    ''a\[,,\]'    .but here has type.    'int\[,,,\]'</Expects>
+//<Expects id="FS0001" span="(14,27-14,36)" status="error">This expression was expected to have type.    ''a\[,\]'    .but here has type.    'int\[,,,]'</Expects>
+
+let foo (arr : int[,]) = arr.[1,2] // ok
+
+let b1 (arr : int[]) = arr.[1,2,3]
+let b2 (arr : int[,]) = arr.[1]
+let b3 (arr : int[,,,]) = arr.[1,2,3]
+let b4 (arr : int[,,,]) = arr.[1,2]
