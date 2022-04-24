5bc84b7b2f2c097b1483260684d4dec48c1d0613
nojaf <florian.verdonck@outlook.com>
2022-04-24 21:00:50 +0200

    Update error message in E_type_id_equal_pipe.fsx

--- tests/fsharpqa/Source/InteractiveSession/Misc/E_type_id_equal_pipe.fsx
+++ tests/fsharpqa/Source/InteractiveSession/Misc/E_type_id_equal_pipe.fsx
@@ -1,5 +1,5 @@
 // #Regression #NoMT #FSI 
 // Regression test for FSHARP1.0:5629
-//<Expects status="error" span="(4,12)" id="FS0010">Incomplete structured construct at or before this point in union case\. Expected identifier, '\(', '\(\*\)' or other token\.$</Expects>
+//<Expects status="error" span="(4,12)" id="FS0010">Incomplete structured construct at or before this point in union case\. Expected identifier, '\(' or other token\.$</Expects>
 type R = | ;;
 exit 1;;
