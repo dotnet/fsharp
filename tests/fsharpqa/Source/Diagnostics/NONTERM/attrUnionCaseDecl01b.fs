9b9ae3cf4ccdda373494e30981b4a987f80c8b45
nojaf <florian.verdonck@outlook.com>
2022-04-25 13:04:03 +0200

    Update error message for attrUnionCaseDecl01b.fs.

--- tests/fsharpqa/Source/Diagnostics/NONTERM/attrUnionCaseDecl01b.fs
+++ tests/fsharpqa/Source/Diagnostics/NONTERM/attrUnionCaseDecl01b.fs
@@ -1,7 +1,7 @@
 // #Regression #Diagnostics 
 // Regression test for FSharp1.0:3702
 //<Expects status="notin">NONTERM</Expects>
-//<Expects id="FS0010" status="error" span="(12,5-12,9)">Incomplete structured construct at or before this point in union case\. Expected identifier, '\(', '\(\*\)' or other token\.$</Expects>
+//<Expects id="FS0010" status="error" span="(12,5-12,9)">Incomplete structured construct at or before this point in union case\. Expected identifier, '\(' or other token\.$</Expects>
 
 
            
