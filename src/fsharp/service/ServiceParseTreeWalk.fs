665f675fcc7ac85ac8ba4b8655a6f3c9fd0ec008
kerams <kerams@users.noreply.github.com>
2022-04-29 16:10:42 +0200

    Filter type test completion list (#12930)

--- src/fsharp/service/ServiceParseTreeWalk.fs
+++ src/fsharp/service/ServiceParseTreeWalk.fs
@@ -634,7 +634,8 @@ module SyntaxTraversal =
                 let path = SyntaxNode.SynPat p :: origPath
                 match p with
                 | SynPat.Paren (p, _) -> traversePat path p
-                | SynPat.Or (p1, p2, _, _) -> [ p1; p2] |> List.tryPick (traversePat path)
+                | SynPat.As (p1, p2, _)
+                | SynPat.Or (p1, p2, _, _) -> [ p1; p2 ] |> List.tryPick (traversePat path)
                 | SynPat.Ands (ps, _)
                 | SynPat.Tuple (_, ps, _)
                 | SynPat.ArrayOrList (_, ps, _) -> ps |> List.tryPick (traversePat path)
