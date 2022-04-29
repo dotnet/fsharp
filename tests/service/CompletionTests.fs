1ac6d1361264df1cc314ba76c2eb57f817114025
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2022-04-29 19:14:37 +0300

    Completion: empty record fixes (#12872)
    Co-authored-by: Vlad Zarytovskii <vzaritovsky@hotmail.com>

--- tests/service/CompletionTests.fs
+++ tests/service/CompletionTests.fs
@@ -1,18 +1,59 @@
 ﻿module FSharp.Compiler.Service.Tests.CompletionTests
 
 open FSharp.Compiler.EditorServices
-open FsUnit
 open NUnit.Framework
+
+let getCompletionInfo lineText (line, column) source =
+    let parseResults, checkResults = getParseAndCheckResults source
+    let plid = QuickParse.GetPartialLongNameEx(lineText, column)
+    checkResults.GetDeclarationListInfo(Some parseResults, line, lineText, plid)
+
+let getCompletionItemNames (completionInfo: DeclarationListInfo) =
+    completionInfo.Items |> Array.map (fun item -> item.Name)
+
+let assertHasItemWithNames names (completionInfo: DeclarationListInfo) =
+    let itemNames = getCompletionItemNames completionInfo |> set
+
+    for name in names do
+        Assert.That(Set.contains name itemNames, name)
+
 
 [<Test>]
 let ``Expr - record - field 01 - anon module`` () =
-    let parseResults, checkResults = getParseAndCheckResults """
-type Record = { Field: int}
+    let info = getCompletionInfo "{ Fi }" (4, 3)  """
+type Record = { Field: int }
 
 { Fi }
 """
-    let lineText = "{ Fi }"
-    let plid = QuickParse.GetPartialLongNameEx(lineText, 3)
-    let info = checkResults.GetDeclarationListInfo(Some parseResults, 4, lineText, plid)
+    assertHasItemWithNames ["Field"] info
 
-    info.Items |> Array.exists (fun item -> item.Name = "Field") |> shouldEqual true
+[<Test>]
+let ``Expr - record - field 02 - anon module`` () =
+    let info = getCompletionInfo "{ Fi }" (6, 3)  """
+type Record = { Field: int }
+
+let record = { Field = 1 }
+
+{ Fi }
+"""
+    assertHasItemWithNames ["Field"] info
+
+[<Test>]
+let ``Expr - record - empty 01`` () =
+    let info = getCompletionInfo "{  }" (4, 2) """
+type Record = { Field: int }
+
+{  }
+"""
+    assertHasItemWithNames ["Field"] info
+
+[<Test>]
+let ``Expr - record - empty 02`` () =
+    let info = getCompletionInfo "{  }" (6, 2) """
+type Record = { Field: int }
+
+let record = { Field = 1 }
+
+{  }
+"""
+    assertHasItemWithNames ["Field"; "record"] info
