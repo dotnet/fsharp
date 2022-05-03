d31eae100fa976cc146099080e45915b5df7db36
Don Syme <dsyme@users.noreply.github.com>
2022-05-03 01:12:08 +0100

    fix 13043 (#13065)
    * Fix https://github.com/dotnet/fsharp/issues/13043
    
    * with debug
    
    * remove labels
    
    * rename
    
    * Remove extra line
    
    * fix 13043
    
    * baselines
    
    * temp
    
    * Disable il test for now
    
    * opt/non-opt
    
    * proper no_opt
    
    * proper optimize+
    
    * merge main
    
    * strip lambdas correctly during codegen
    
    * proper treatment of typed expressions
    
    * update baselines
    
    * baseline
    
    * simplify
    
    Co-authored-by: Kevin Ransom <codecutter@hotmail.com>

--- src/fsharp/CheckExpressions.fs
+++ src/fsharp/CheckExpressions.fs
@@ -5850,7 +5850,7 @@ and TcNonControlFlowExpr (env: TcEnv) f =
         | NotedSourceConstruct.Combine
         | NotedSourceConstruct.With
         | NotedSourceConstruct.While
-        | NotedSourceConstruct.DelayOrQuoteOrRun -> 
+        | NotedSourceConstruct.DelayOrQuoteOrRun ->
             res, tpenv
         | NotedSourceConstruct.None ->
             // Skip outer debug point for "e1 && e2" and "e1 || e2"
@@ -5929,7 +5929,6 @@ and TcExprUndelayed cenv (overallTy: OverallTy) env tpenv (synExpr: SynExpr) =
 
     // e: ty
     | SynExpr.Typed (synBodyExpr, synType, m) ->
-        TcNonControlFlowExpr env <| fun env ->
         TcExprTypeAnnotated cenv overallTy env tpenv (synBodyExpr, synType, m)
 
     // e :? ty
