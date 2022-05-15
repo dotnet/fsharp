20aca60c71c2697797f94c6eb54f25cadd7133f2
Adam <me@example.com>
2022-05-16 01:34:37 +0200

    Add another unit test - Update and copy syntax

--- tests/FSharp.Compiler.ComponentTests/Conformance/RecordTypes/AnonymousRecords.fs
+++ tests/FSharp.Compiler.ComponentTests/Conformance/RecordTypes/AnonymousRecords.fs
@@ -19,3 +19,15 @@ module AnonRecd =
         |> compile
         |> shouldFail
         |> withErrorCode 3522
+
+    [<Fact>]
+    let ``Anonymous Records with duplicate labels - Update and copy syntax`` () =
+        FSharp """
+namespace FSharpTest
+
+module AnonRecd =
+    let v = {| {| y = 3 |} with y = 2; y = 4 |}
+"""
+        |> compile
+        |> shouldFail
+        |> withErrorCode 3522
