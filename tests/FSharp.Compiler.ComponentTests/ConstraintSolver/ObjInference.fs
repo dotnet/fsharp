ef5090f539b677a58c706b2e5daddadc148ae4a1
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-06-14 23:14:50 +0100

    Add more tests, some of which are failing

--- tests/FSharp.Compiler.ComponentTests/ConstraintSolver/ObjInference.fs
+++ tests/FSharp.Compiler.ComponentTests/ConstraintSolver/ObjInference.fs
@@ -5,12 +5,56 @@ open FSharp.Test.Compiler
 
 module ObjInference =
 
-    [<Fact>]
-    let ``Inference of obj``() =
-        FSharp """
-let f() = ([] = [])
-        """
+    let failureCases =
+        [
+            "let f() = ([] = [])", 1, 17, 1, 19
+        ]
+        |> List.map (fun (str, line1, col1, line2, col2) -> [| box str ; line1 ; col1 ; line2 ; col2 |])
+
+    [<Theory>]
+    [<MemberData(nameof(failureCases))>]
+    let ``Inference of obj``(code: string, line1: int, col1: int, line2: int, col2: int) =
+        FSharp code
         |> withErrorRanges
         |> typecheck
         |> shouldFail
-        |> withSingleDiagnostic (Warning 3524, Line 2, Col 17, Line 2, Col 19, "A type was not refined away from `obj`, which may be unintended. Consider adding explicit type annotations.")
+        |> withSingleDiagnostic (Warning 3524, Line line1, Col col1, Line line2, Col col2, "A type was not refined away from `obj`, which may be unintended. Consider adding explicit type annotations.")
+
+    let nullSuccessCases =
+        [
+            """System.Object.ReferenceEquals("hello", null)"""
+            """System.Object.ReferenceEquals("hello", (null: string))"""
+            """System.Object.ReferenceEquals(null, "hello")"""
+            """System.Object.ReferenceEquals((null: string), "hello")"""
+        ]
+        |> List.map Array.singleton
+
+    [<Theory>]
+    [<MemberData(nameof(nullSuccessCases))>]
+    let ``Don't warn on an explicit null``(expr: string) =
+        sprintf "%s |> ignore" expr
+        |> FSharp
+        |> typecheck
+        |> shouldSucceed
+
+    [<Theory>]
+    [<MemberData(nameof(nullSuccessCases))>]
+    let ``Don't warn inside quotations, explicit nulls``(expr: string) =
+        sprintf "<@ %s @> |> ignore" expr
+        |> FSharp
+        |> typecheck
+        |> shouldSucceed
+
+    let quotationSuccessCases =
+        [
+            "<@ List.map ignore [1;2;3] @>"
+        ]
+        |> List.map Array.singleton
+
+    [<Theory>]
+    [<MemberData(nameof(quotationSuccessCases))>]
+    let ``Don't warn inside quotations``(expr: string) =
+        sprintf "%s |> ignore" expr
+        |> FSharp
+        |> typecheck
+        |> shouldSucceed
