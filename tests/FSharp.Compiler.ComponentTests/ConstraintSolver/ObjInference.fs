9e1c8c2839d418fd9e38daa3cb885fa0fa7f31ad
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-06-14 23:36:44 +0100

    A couple more tests
    This incorporates the motivating examples from
    https://github.com/fsharp/fslang-suggestions/issues/885 .

--- tests/FSharp.Compiler.ComponentTests/ConstraintSolver/ObjInference.fs
+++ tests/FSharp.Compiler.ComponentTests/ConstraintSolver/ObjInference.fs
@@ -11,14 +11,30 @@ module ObjInference =
         ]
         |> List.map (fun (str, line1, col1, line2, col2) -> [| box str ; line1 ; col1 ; line2 ; col2 |])
 
+    let successCases =
+        [
+            "let add x y = x + y" // inferred as int
+            "let f x = string x" // inferred as generic 'a -> string
+            "let f() = ([] = ([] : obj list))" // obj is inferred, but is annotated
+            "let f() = (([] : obj list) = [])" // obj is inferred, but is annotated
+        ]
+        |> List.map Array.singleton
+
     [<Theory>]
     [<MemberData(nameof(failureCases))>]
-    let ``Inference of obj``(code: string, line1: int, col1: int, line2: int, col2: int) =
+    let ``Inference of obj is warned``(code: string, line1: int, col1: int, line2: int, col2: int) =
         FSharp code
         |> withErrorRanges
         |> typecheck
         |> shouldFail
         |> withSingleDiagnostic (Warning 3524, Line line1, Col col1, Line line2, Col col2, "A type was not refined away from `obj`, which may be unintended. Consider adding explicit type annotations.")
+
+    [<Theory>]
+    [<MemberData(nameof(successCases))>]
+    let ``Negative cases: inference of non-obj``(code: string) =
+        FSharp code
+        |> typecheck
+        |> shouldSucceed
 
     let nullSuccessCases =
         [
