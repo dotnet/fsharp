4f0390c047ac6ffda8a18e80f4f7859d3558af7f
Smaug123 <patrick+github@patrickstevens.co.uk>
2022-06-15 21:33:24 +0100

    Add test for simpler quotation case

--- tests/FSharp.Compiler.ComponentTests/ConstraintSolver/ObjInference.fs
+++ tests/FSharp.Compiler.ComponentTests/ConstraintSolver/ObjInference.fs
@@ -11,27 +11,29 @@ module ObjInference =
         ]
         |> List.map (fun (str, line1, col1, line2, col2) -> [| box str ; line1 ; col1 ; line2 ; col2 |])
 
-    let successCases =
-        [
-            "let add x y = x + y" // inferred as int
-            "let f x = string x" // inferred as generic 'a -> string
-            "let f() = ([] = ([] : obj list))" // obj is inferred, but is annotated
-            "let f() = (([] : obj list) = [])" // obj is inferred, but is annotated
-        ]
-        |> List.map Array.singleton
-
     [<Theory>]
     [<MemberData(nameof(failureCases))>]
-    let ``Inference of obj is warned``(code: string, line1: int, col1: int, line2: int, col2: int) =
+    let ``Warning is emitted when top type Obj is inferred``(code: string, line1: int, col1: int, line2: int, col2: int) =
         FSharp code
         |> withErrorRanges
         |> typecheck
         |> shouldFail
         |> withSingleDiagnostic (Warning 3524, Line line1, Col col1, Line line2, Col col2, "A type was not refined away from `obj`, which may be unintended. Consider adding explicit type annotations.")
 
+    let successCases =
+        [
+            "let add x y = x + y" // inferred as int
+            "let f x = string x" // inferred as generic 'a -> string
+            "let f() = ([] = ([] : obj list))" // obj is inferred, but is annotated
+            "let f() = (([] : obj list) = [])" // obj is inferred, but is annotated
+            """let x<[<Measure>]'m> : int<'m> = failwith ""
+let f () = x = x |> ignore""" // measure is inferred as 1, but that's not covered by this warning
+        ]
+        |> List.map Array.singleton
+
     [<Theory>]
     [<MemberData(nameof(successCases))>]
-    let ``Negative cases: inference of non-obj``(code: string) =
+    let ``Warning does not fire unless required``(code: string) =
         FSharp code
         |> typecheck
         |> shouldSucceed
@@ -55,7 +57,7 @@ module ObjInference =
 
     [<Theory>]
     [<MemberData(nameof(nullSuccessCases))>]
-    let ``Don't warn inside quotations, explicit nulls``(expr: string) =
+    let ``Don't warn on an explicit null, inside quotations``(expr: string) =
         sprintf "<@ %s @> |> ignore" expr
         |> FSharp
         |> typecheck
@@ -69,8 +71,16 @@ module ObjInference =
 
     [<Theory>]
     [<MemberData(nameof(quotationSuccessCases))>]
-    let ``Don't warn inside quotations``(expr: string) =
+    let ``Don't warn inside quotations of acceptable code``(expr: string) =
         sprintf "%s |> ignore" expr
         |> FSharp
         |> typecheck
         |> shouldSucceed
+
+    [<Fact>]
+    let ``Warn when the error appears inside a quotation``() =
+        sprintf "<@ [] = [] @> |> ignore"
+        |> FSharp
+        |> typecheck
+        |> shouldFail
+        |> withSingleDiagnostic (Warning 3524, Line 1, Col 9, Line 1, Col 11, "A type was not refined away from `obj`, which may be unintended. Consider adding explicit type annotations.")
