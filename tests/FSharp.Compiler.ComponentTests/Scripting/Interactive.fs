aa9e41467d1144b5dac924072dada31e66da123b
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2022-06-18 19:00:30 -1100

    Improve error reporting using [<EntryPoint>] in a script

--- tests/FSharp.Compiler.ComponentTests/Scripting/Interactive.fs
+++ tests/FSharp.Compiler.ComponentTests/Scripting/Interactive.fs
@@ -14,6 +14,30 @@ module ``Interactive tests`` =
         |> withEvalTypeEquals typeof<int>
         |> withEvalValueEquals 2
 
+    [<Fact>]
+    let ``Using the EntryPoint attribute in FSI should produce a compiler warning indicating that it won't be invoked automatically.`` () =
+        Fsx """
+[<EntryPoint>]
+let main argv =
+    printfn "Hello, world!"   
+    0
+        """
+        |> eval
+        |> shouldFail
+        |> withDiagnostics [
+            (Warning 2304, Line 3, Col 5, Line 3, Col 9, "Function with '[<EntryPoint>]' are not invoked in FSI. 'main' was not invoked. Execute '['main' <args>]' in order to invoke 'main' with the appropriate string array of command line arguments.")
+        ]
+
+    [<Fact>]
+    let ``Using the EntryPoint attribute in FSI should won't produce a compiler warning indicating that it won't be invoked automatically.`` () =
+        Fsx """
+let main argv =
+    printfn "Hello, world!"   
+    0
+        """
+        |> eval
+        |> shouldSucceed
+
 
 module ``External FSI tests`` =
     [<Fact>]
@@ -24,6 +48,6 @@ module ``External FSI tests`` =
 
     [<Fact>]
     let ``Invalid expression should fail``() =
-            Fsx "1+a"
-            |> runFsi
-            |> shouldFail
+        Fsx "1+a"
+        |> runFsi
+        |> shouldFail
