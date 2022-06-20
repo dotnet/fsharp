70ad2e5f2a9923bfbab519106d02dfa97347f58e
Jan Dryk <me@uxsoft.cz>
2022-06-20 10:55:11 +0200

    RFC FS-1123 Result module parity with Options (#13326)

--- src/FSharp.Core/result.fs
+++ src/FSharp.Core/result.fs
@@ -22,3 +22,99 @@ module Result =
         match result with
         | Error e -> Error e
         | Ok x -> binder x
+
+    [<CompiledName("IsOk")>]
+    let inline isOk result =
+        match result with
+        | Ok _ -> true
+        | Error _ -> false
+
+    [<CompiledName("IsError")>]
+    let inline isError result =
+        match result with
+        | Ok _ -> false
+        | Error _ -> true
+
+    [<CompiledName("DefaultValue")>]
+    let defaultValue value result =
+        match result with
+        | Error _ -> value
+        | Ok v -> v
+
+    [<CompiledName("DefaultWith")>]
+    let defaultWith defThunk result =
+        match result with
+        | Error _ -> defThunk ()
+        | Ok v -> v
+
+    [<CompiledName("Count")>]
+    let count result =
+        match result with
+        | Error _ -> 0
+        | Ok _ -> 1
+
+    [<CompiledName("Fold")>]
+    let fold<'T, 'Error, 'State> folder (state: 'State) (result: Result<'T, 'Error>) =
+        match result with
+        | Error _ -> state
+        | Ok x -> folder state x
+
+    [<CompiledName("FoldBack")>]
+    let foldBack<'T, 'Error, 'State> folder (result: Result<'T, 'Error>) (state: 'State) =
+        match result with
+        | Error _ -> state
+        | Ok x -> folder x state
+
+    [<CompiledName("Exists")>]
+    let exists predicate result =
+        match result with
+        | Error _ -> false
+        | Ok x -> predicate x
+
+    [<CompiledName("ForAll")>]
+    let forall predicate result =
+        match result with
+        | Error _ -> true
+        | Ok x -> predicate x
+
+    [<CompiledName("Contains")>]
+    let inline contains value result =
+        match result with
+        | Error _ -> false
+        | Ok v -> v = value
+
+    [<CompiledName("Iterate")>]
+    let iter action result =
+        match result with
+        | Error _ -> ()
+        | Ok x -> action x
+
+    [<CompiledName("ToArray")>]
+    let toArray result =
+        match result with
+        | Error _ -> [||]
+        | Ok x -> [| x |]
+
+    [<CompiledName("ToList")>]
+    let toList result =
+        match result with
+        | Error _ -> []
+        | Ok x -> [ x ]
+
+    [<CompiledName("ToSeq")>]
+    let toSeq result =
+        match result with
+        | Error _ -> []
+        | Ok x -> [ x ]
+
+    [<CompiledName("ToOption")>]
+    let toOption result =
+        match result with
+        | Error _ -> None
+        | Ok x -> Some x
+
+    [<CompiledName("ToValueOption")>]
+    let toValueOption result =
+        match result with
+        | Error _ -> ValueNone
+        | Ok x -> ValueSome x
