8b93a2449430d2402809047d32b23f06312bb7b5
Don Syme <donsyme@fastmail.fm>
2022-07-12 22:45:01 +0100

    fix tests

--- tests/fsharpqa/Source/Conformance/Expressions/Type-relatedExpressions/E_RigidTypeAnnotation02.fsx
+++ tests/fsharpqa/Source/Conformance/Expressions/Type-relatedExpressions/E_RigidTypeAnnotation02.fsx
@@ -15,12 +15,11 @@ let f3 x = (x : float32<Kg>) + 1M<Kg>
 let f4 x = (x : decimal<N*s*s/m>) + ""
 
 exit 1
-s
+
 //<Expects id="FS0001" span="(11,27-11,28)" status="error">The types 'string, int' do not support the operator</Expects>
 //<Expects id="FS0043" span="(11,25-11,26)" status="error">The types 'string, int' do not support the operator</Expects>
 //<Expects id="FS0001" span="(12,18-12,26)" status="error">The types 'string, byte' do not support the operator</Expects>
 //<Expects id="FS0043" span="(12,15-12,16)" status="error">The types 'string, byte' do not support the operator</Expects>
-//<Expects id="FS0001" span="(13,19-13,20)" status="error">This expression was expected to have type.    'float'    .but here has type.    'int'</Expects>
 //<Expects id="FS0001" span="(13,32-13,33)" status="error">The types 'float, int' do not support the operator</Expects>
 //<Expects id="FS0043" span="(13,30-13,31)" status="error">The types 'float, int' do not support the operator</Expects>
 //<Expects status="error" span="(14,32-14,38)" id="FS0001">The types 'float32<Kg>, decimal<Kg>' do not support the operator</Expects>
