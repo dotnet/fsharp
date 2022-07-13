25c2257f02506e4045a63fb97fbee0e5326c875b
nojaf <florian.verdonck@outlook.com>
2022-07-13 08:18:35 +0200

    Use trivia in SynLongIdent to print operator name.

---
+++ tests/FSharp.Compiler.ComponentTests/Language/NameofTests.fs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.ComponentTests.Language
+
+open Xunit
+open FSharp.Test.Compiler
+
+module NameofTests =
+
+    [<Theory>]
+    [<InlineData("+")>]
+    [<InlineData("-")>]
+    [<InlineData("/")>]
+    [<InlineData("*")>]
+    let ``nameof() with operator should return demangled name`` operator =
+        let source = $"""
+let expected = "{operator}"
+let actual = nameof({operator})
+if actual <> expected then failwith $"Expected nameof({{expected}}) to be '{{expected}}', but got '{{actual}}'"
+        """
+        Fsx source
+        |> asExe
+        |> withLangVersion50
+        |> compileAndRun
+        |> shouldSucceed
