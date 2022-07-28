86de1fd22744a39f37761dbe02f4a5be1c45813f
albert-du <52804499+albert-du@users.noreply.github.com>
2022-07-27 23:10:55 -0700

    add benchmark

---
+++ tests/benchmarks/CompiledCodeBenchmarks/MicroPerf/ForLoops.fs
@@ -0,0 +1,24 @@
+﻿module ForLoops
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Diagnosers
+open System.Runtime.CompilerServices
+
+[<DisassemblyDiagnoser>]
+[<MarkdownExporterAttribute.GitHub>]
+type ForLoopBenchmark() =
+
+    [<Params(100)>]
+    member val Start = 0 with get, set
+
+    [<Params(1000)>]
+    member val Finish = 0 with get, set
+
+    [<Params(10)>]
+    member val Step = 0 with get, set
+
+    [<Benchmark>]
+    member this.Benchmark() =
+        let mutable x = 0
+        for i in this.Start .. this.Finish .. this.Step do
+            x <- i
