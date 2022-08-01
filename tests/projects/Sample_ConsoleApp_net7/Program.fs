5c404c0fd267fd4b248f7246ba2aa707a3e56cce
Petr Pokorny <petrpokorny@microsoft.com>
2022-08-01 18:07:51 +0200

    Sample project for .NET 7

---
+++ tests/projects/Sample_ConsoleApp_net7/Program.fs
@@ -0,0 +1,10 @@
+﻿// SDK version 7.0.100-preview.6 or newer has to be installed for this to work
+open System.Numerics
+
+[<Measure>] type potato
+
+let f (x: 'T when IMultiplyOperators<'T,'T,'T>) = x 
+
+// f 7.0<potato> 
+
+printfn $"Hello from F# {f 7.0}!"
