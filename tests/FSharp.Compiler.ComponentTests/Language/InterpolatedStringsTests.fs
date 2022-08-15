b4b0c01be9929683f0779706b81b7748c9e7f4d0
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2022-08-15 19:14:47 +0200

    Fix 13509

---
+++ tests/FSharp.Compiler.ComponentTests/Language/InterpolatedStringsTests.fs
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.ComponentTests.Language
+
+open System
+open Xunit
+open FSharp.Test.Compiler
+
+module InterpolatedStringsTests =
+
+    [<Fact>]
+    let ``Regression: Empty Interpolated String properly typechecks with explicit type on binding`` () =
+        Fsx """ let a:byte = $"string" """
+        |> compile
+        |> shouldFail
+        |> withSingleDiagnostic (Error 1, Line 1, Col 15, Line 1, Col 24, "This expression was expected to have type" + Environment.NewLine + "    'byte'    " + Environment.NewLine + "but here has type" + Environment.NewLine + "    'string'    ")
+
+    [<Fact>]
+    let ``Interpolated String with hole properly typechecks with explicit type on binding`` () =
+        Fsx """ let a:byte = $"string" """
+        |> compile
+        |> shouldFail
+        |> withSingleDiagnostic (Error 1, Line 1, Col 15, Line 1, Col 24, "This expression was expected to have type" + Environment.NewLine + "    'byte'    " + Environment.NewLine + "but here has type" + Environment.NewLine + "    'string'    ")
