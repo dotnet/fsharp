0b936437fe1bd9be7d6cd568cedf976a9be54d03
albert-du <52804499+albert-du@users.noreply.github.com>
2022-08-22 21:59:51 -0700

    new benchmarks

--- tests/benchmarks/CompiledCodeBenchmarks/MicroPerf/ForLoops.fs
+++ tests/benchmarks/CompiledCodeBenchmarks/MicroPerf/ForLoops.fs
@@ -11,14 +11,20 @@ type ForLoopBenchmark() =
     [<Params(100)>]
     member val Start = 0 with get, set
 
-    [<Params(1000)>]
+    [<Params(1000, 1000000)>]
     member val Finish = 0 with get, set
 
     [<Params(10)>]
     member val Step = 0 with get, set
 
     [<Benchmark>]
-    member this.Benchmark() =
+    member this.VariableStep() =
         let mutable x = 0
-        for i in this.Start .. this.Finish .. this.Step do
+        for i in this.Start .. this.Step .. this.Finish do
+            x <- i
+
+    [<Benchmark>]
+    member this.ConstantStep() =
+        let mutable x = 0
+        for i in this.Start .. 10 .. this.Finish do
             x <- i
