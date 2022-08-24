3f8bb8b94b3637b573c7737a9d092701c2f6b982
Petr <psfinaki@users.noreply.github.com>
2022-08-24 14:46:43 +0200

    Update CompilerOptions.fs

--- src/Compiler/Driver/CompilerOptions.fs
+++ src/Compiler/Driver/CompilerOptions.fs
@@ -111,7 +111,9 @@ let compilerOptionUsage (CompilerOption (s, tag, spec, _, _)) =
         else
             sprintf "%s:%s" s tag (* still being decided *)
 
-let PrintCompilerOption print (CompilerOption (_s, _tag, _spec, _, help) as compilerOption) =
+let nl = Environment.NewLine
+
+let PrintCompilerOption printer (CompilerOption (_s, _tag, _spec, _, help) as compilerOption) =
     let flagWidth = 42 // fixed width for printing of flags, e.g. --debug:{full|pdbonly|portable|embedded}
     let defaultLineWidth = 80 // the fallback width
 
@@ -130,18 +132,18 @@ let PrintCompilerOption print (CompilerOption (_s, _tag, _spec, _, help) as comp
     //   flagWidth chars - for flags description or padding on continuation lines.
     //   single space    - space.
     //   description     - words upto but excluding the final character of the line.
-    print $"{compilerOptionUsage compilerOption, -40}"
+    printer $"{compilerOptionUsage compilerOption, -40}"
 
     let printWord column (word: string) =
         // Have printed upto column.
         // Now print the next word including any preceding whitespace.
         // Returns the column printed to (suited to folding).
         if column + 1 (*space*) + word.Length >= lineWidth then // NOTE: "equality" ensures final character of the line is never printed
-            print "\n"
-            print $"{String.Empty, -40} {word}"
+            printer $"{nl}"
+            printer $"{String.Empty, -40} {word}"
             flagWidth + 1 + word.Length
         else
-            print $" {word}"
+            printer $" {word}"
             column + 1 + word.Length
 
     let words =
@@ -150,16 +152,16 @@ let PrintCompilerOption print (CompilerOption (_s, _tag, _spec, _, help) as comp
         | Some s -> s.Split [| ' ' |]
 
     let _finalColumn = Array.fold printWord flagWidth words
-    print "\n"
+    printer $"{nl}"
 
-let PrintPublicOptions (heading, opts) print =
+let PrintPublicOptions (heading, opts) printer =
     if not (isNil opts) then
-        print "\n"
-        print "\n"
-        print $"\t\t{heading}\n"
-        List.iter (PrintCompilerOption print) opts
+        printer $"{nl}"
+        printer $"{nl}"
+        printer $"\t\t{heading}{nl}"
+        List.iter (PrintCompilerOption printer) opts
 
-let PrintCompilerOptionBlocks blocks print =
+let PrintCompilerOptionBlocks blocks printer =
     let publicBlocks =
         blocks
         |> List.choose (function
@@ -173,7 +175,7 @@ let PrintCompilerOptionBlocks blocks print =
             let headingOptions =
                 publicBlocks |> List.filter (fun (h2, _) -> heading = h2) |> List.collect snd
 
-            PrintPublicOptions (heading, headingOptions) print
+            PrintPublicOptions (heading, headingOptions) printer
             Set.add heading doneHeadings
 
     List.fold consider Set.empty publicBlocks |> ignore<Set<string>>
@@ -1974,8 +1976,8 @@ let deprecatedFlagsFsc tcConfigB =
 
 let DisplayBannerText tcConfigB printer =
     if tcConfigB.showBanner then
-        (printer $"{tcConfigB.productNameForBannerText}\n"
-         printer $"{FSComp.SR.optsCopyright ()}\n")
+        (printer $"{tcConfigB.productNameForBannerText}{nl}"
+         printer $"{FSComp.SR.optsCopyright ()}{nl}")
 
 /// FSC only help. (FSI has it's own help function).
 let DisplayHelpFsc printer exiter tcConfigB (blocks: CompilerOptionBlock list) =
@@ -1984,7 +1986,7 @@ let DisplayHelpFsc printer exiter tcConfigB (blocks: CompilerOptionBlock list) =
     exiter ()
 
 let DisplayVersion printer exiter tcConfigB =
-    printer $"{tcConfigB.productNameForBannerText}\n"
+    printer $"{tcConfigB.productNameForBannerText}{nl}"
     exiter ()
 
 let displayHelpFsc = DisplayHelpFsc Console.Write (fun () -> exit 0)
