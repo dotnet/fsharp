266ac9e8b8e833debc6be1a03b3ca754c3f5dc9a
nojaf <florian.verdonck@outlook.com>
2022-09-01 10:05:42 +0200

    Refactor SignatureTests.fs to ArrayTests.fs.

--- tests/FSharp.Compiler.ComponentTests/Signatures/ArrayTests.fs
+++ tests/FSharp.Compiler.ComponentTests/Signatures/ArrayTests.fs
@@ -1,12 +1,7 @@
 ﻿module FSharp.Compiler.ComponentTests.Signatures.SignatureTests
 
 open Xunit
-open FSharp.Test.Compiler
-
-[<Fact>]
-let ``Very basic signature test`` () =
-    FSharp "let a = 0"
-    |> signaturesShouldContain "val a: int"
+open FSharp.Compiler.ComponentTests.Signatures.TestHelpers
 
 [<Theory>]
 [<InlineData("let a : int[] = [| 1 |]",
@@ -16,23 +11,25 @@ let ``Very basic signature test`` () =
 [<InlineData("let c: array<int> = [| 3 |]",
              "val c: int array")>]
 let ``Value with int array return type`` implementation expectedSignature =
-    FSharp implementation
-    |> signaturesShouldContain expectedSignature
+    assertSingleSignatureBinding implementation expectedSignature
 
 [<Fact>]
 let ``2 dimensional array`` () =
-    FSharp "let a : int[,] = failwith \"todo\""
-    |> signaturesShouldContain "val a: int array2d"
+    assertSingleSignatureBinding
+        "let a : int[,] = failwith \"todo\""
+        "val a: int array2d"
 
 [<Fact>]
 let ``3 dimensional array`` () =
-    FSharp "let a : int[,,] = failwith \"todo\""
-    |> signaturesShouldContain "val a: int array3d"
+    assertSingleSignatureBinding
+        "let a : int[,,] = failwith \"todo\""
+        "val a: int array3d"
 
 [<Fact>]
 let ``4 dimensional array`` () =
-    FSharp "let a : int[,,,] = failwith \"todo\""
-    |> signaturesShouldContain "val a: int array4d"
+    assertSingleSignatureBinding
+        "let a : int[,,,] = failwith \"todo\""
+        "val a: int array4d"
 
 [<Fact>]
 let ``5 till 32 dimensional array`` () =
@@ -42,11 +39,13 @@ let ``5 till 32 dimensional array`` () =
             [ 1 .. idx ]
             |> List.fold (fun acc _ -> $"array<{acc}>") "int"
 
-        FSharp $"let a : {arrayType} = failwith \"todo\""
-        |> signaturesShouldContain $"val a: int array{idx}d"
+        assertSingleSignatureBinding
+            $"let a : {arrayType} = failwith \"todo\""
+            $"val a: int array{idx}d"
     )
 
 [<Fact>]
 let ``Use array2d syntax in implementation`` () =
-    FSharp "let y : int array2d = Array2D.init 0 0 (fun _ _ -> 0)"
-    |> signaturesShouldContain "val y: int array2d"
+    assertSingleSignatureBinding
+        "let y : int array2d = Array2D.init 0 0 (fun _ _ -> 0)"
+        "val y: int array2d"
