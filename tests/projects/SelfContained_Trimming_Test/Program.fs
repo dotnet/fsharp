44b158bd45952df477745f361cde019fdaed4276
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2022-09-12 18:07:05 +0200

    Added a very basic test project to test trimming

---
+++ tests/projects/SelfContained_Trimming_Test/Program.fs
@@ -0,0 +1,5 @@
+﻿// TODO: Needs more FSharp.Core functions to be used:
+[<EntryPoint>]
+let main _ =
+    printfn "Hello, trimmed world!"
+    0
---
+++ tests/projects/SelfContained_Trimming_Test/check.ps1
@@ -0,0 +1,23 @@
+$output = .\bin\Release\net7.0\win-x64\publish\SelfContained_Trimming_Test.exe
+
+# Checking that it is actually running.
+if (-not ($LASTEXITCODE -eq 0))
+{
+    Write-Host "Test failed with exit code ${LASTEXITCODE}" -ErrorAction Stop
+}
+
+# Checking that the output is as expected.
+$expected = "Hello, trimmed world!"
+if (-not ($output -eq $expected))
+{
+    Write-Host "Test failed with unexpected output:`nExpected:`n`t${expected}`nActual`n`t${output}" -ErrorAction Stop
+}
+
+# Checking that FSharp.Core binary is of expected size (needs adjustments if test is updated).
+$expected_len = 300000 # 300 KB
+$file = Get-Item .\bin\Release\net7.0\win-x64\publish\FSharp.Core.dll
+$file_len = $file.Length
+if ($file_len -le $expected_len)
+{
+    Write-Host "Test failed with unexpected FSharp.Core length:`nExpected:`n`t<=${expected_len}B`nActual`n`t  ${file_len}B" -ErrorAction Stop
+}
