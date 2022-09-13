2135010b2160cfa1d3cebe60f32371e8ffea43af
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2022-09-13 15:57:11 +0200

    Added a very basic test project to test trimming(10)

--- tests/projects/SelfContained_Trimming_Test/check.ps1
+++ tests/projects/SelfContained_Trimming_Test/check.ps1
@@ -14,7 +14,7 @@ if (-not ($output -eq $expected))
 }
 
 # Checking that FSharp.Core binary is of expected size (needs adjustments if test is updated).
-$expected_len = 2452481 # In bytes
+$expected_len = 245248 # In bytes
 $file = Get-Item .\bin\Release\net7.0\win-x64\publish\FSharp.Core.dll
 $file_len = $file.Length
 if (-not ($file_len -eq $expected_len))
