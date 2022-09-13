a90e98bee0169bdec8e1ccfc5be4b16ab914476f
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2022-09-13 19:29:18 +0200

    Added a very basic test project to test trimming(12)

--- tests/projects/SelfContained_Trimming_Test/check.ps1
+++ tests/projects/SelfContained_Trimming_Test/check.ps1
@@ -14,10 +14,10 @@ if (-not ($output -eq $expected))
 }
 
 # Checking that FSharp.Core binary is of expected size (needs adjustments if test is updated).
-$expected_len = 245248 # In bytes
+$expected_len = 2181120 # In bytes
 $file = Get-Item .\bin\Release\net7.0\win-x64\publish\FSharp.Core.dll
 $file_len = $file.Length
-if (-not ($file_len -eq $expected_len))
+if ($file_len -ge $expected_len)
 {
     Write-Error "Test failed with unexpected FSharp.Core length:`nExpected:`n`t${expected_len} Bytes`nActual:`n`t${file_len} Bytes" -ErrorAction Stop
 }
