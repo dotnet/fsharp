1cde708c6b625e5b36e06d20cdfca7643b1e68fc
Thomas Boby <th.boby@gmail.com>
2022-09-21 15:11:54 +0100

    Fixup tests

--- tests/fsharpqa/Source/Conformance/Expressions/Type-relatedExpressions/E_StaticCoercion_hole_as_left_arg.fsx
+++ tests/fsharpqa/Source/Conformance/Expressions/Type-relatedExpressions/E_StaticCoercion_hole_as_left_arg.fsx
@@ -2,11 +2,10 @@
 // Negative tests on :>
 // Using _ as left argument
 
-//<Expects status="error" span="(13,18-13,19)" id="FS0010">Unexpected symbol '_' in expression$</Expects>
-//<Expects status="error" span="(13,9-13,10)" id="FS0583">Unmatched '\('$</Expects>
-//<Expects status="error" span="(14,11-14,12)" id="FS0010">Unexpected symbol '_' in binding$</Expects>
+//<Expects status="error" span="(13,20-13,21)" id="FS0010">Unexpected symbol ')' in expression. Expected '.' or other token.$</Expects>
+//<Expects status="error" span="(14,13-14,14)" id="FS0010">Unexpected symbol ':>' in expression. Expected '.' or other token.$</Expects>
 //<Expects status="error" span="(14,9-14,10)" id="FS0583">Unmatched '\('$</Expects>
-//<Expects status="error" span="(15,11-15,12)" id="FS0010">Unexpected symbol '_' in binding$</Expects>
+//<Expects status="error" span="(15,13-15,14)" id="FS0010">Unexpected symbol ':>' in expression. Expected '.' or other token.$</Expects>
 //<Expects status="error" span="(15,9-15,10)" id="FS0583">Unmatched '\('$</Expects>
 
 
