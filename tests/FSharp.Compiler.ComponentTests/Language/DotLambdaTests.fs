6dbd7494a223fd904e557e2bcaf553b8cdcaff55
Thomas Boby <th.boby@gmail.com>
2022-09-22 15:19:51 +0100

    Add a simple test

---
+++ tests/FSharp.Compiler.ComponentTests/Language/DotLambdaTests.fs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.ComponentTests.Language
+
+open Xunit
+open FSharp.Test.Compiler
+
+module DotLambdaTests =
+
+    [<Fact>]
+    let ``Script: _.ToString()`` () =
+        Fsx "_.ToString()"
+        |> compile
+        |> shouldSucceed
+        
+    let ``Simple anonymous unary function shorthands compile`` () =
+        FSharp """
+module One
+let a1 : {| Foo : {| Bar : string |}|} -> string = _.Foo.Bar
+let a2 : {| Foo : int array |} -> int = _.Foo.[5]
+let a3 : {| Foo : int array |} -> int = _.Foo[5]
+let a4 : {| Foo : unit -> string |} -> string = _.Foo()
+let a5 : {| Foo : int -> {| X : string |} |} -> string = _.Foo(5).X
+
+open System
+let a6 = [1] |> List.map _.ToString()
+        """
+        |> compile
+        |> shouldSucceed
