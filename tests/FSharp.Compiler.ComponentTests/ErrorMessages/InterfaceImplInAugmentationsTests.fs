96868cea5084415cf13e5de6b8dfd10a60f35a40
Tomas Grosup <tomasgrosup@microsoft.com>
2022-10-05 12:40:01 +0200

    Covering test cases also for augmented exception types

--- tests/FSharp.Compiler.ComponentTests/ErrorMessages/InterfaceImplInAugmentationsTests.fs
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/InterfaceImplInAugmentationsTests.fs
@@ -23,7 +23,23 @@ type MyCustomType<'T> with
         |> withSingleDiagnostic (Warning 69, Line 8, Col 15, Line 8, Col 33,
                                  """Interface implementations should normally be given on the initial declaration of a type. Interface implementations in augmentations may lead to accessing static bindings before they are initialized, though only if the interface implementation is invoked during initialization of the static data, and in turn access the static data.  You may remove this warning using #nowarn "69" if you have checked this is not the case.""")
 
-  
+    [<Fact>]
+    let ``Exception type in non-recursive namespace show give a warning when augmented externally``() =
+        FSharp """
+namespace AugmentationsTest
+type MyCustomExcType<'T>() = 
+    inherit System.Exception()   
+    interface System.IDisposable
+
+type MyCustomExcType<'T> with
+    interface System.IDisposable with
+                member x.Dispose() = printfn "Finish"   """
+        |> typecheck
+        |> shouldFail
+        |> withSingleDiagnostic (Warning 69, Line 8, Col 15, Line 8, Col 33,
+                                 """Interface implementations should normally be given on the initial declaration of a type. Interface implementations in augmentations may lead to accessing static bindings before they are initialized, though only if the interface implementation is invoked during initialization of the static data, and in turn access the static data.  You may remove this warning using #nowarn "69" if you have checked this is not the case.""")
+
+
     [<Fact>]
     let ``Type in a recursive namespace can be augmented without warning``() =
         FSharp """
@@ -35,6 +51,21 @@ type MyCustomType<'T> =
 type MyCustomType<'T> with
     interface System.IDisposable with
                 member x.Dispose() = printfn "Finish"   """
+        |> typecheck
+        |> shouldSucceed
+        |> withWarnings []
+
+    [<Fact>]
+    let ``Exception in a recursive namespace can be augmented without warning``() =
+        FSharp """
+namespace rec AugmentationsTest
+type MyCustomExcType<'T>() = 
+    inherit System.Exception()   
+    interface System.IDisposable
+
+type MyCustomExcType<'T> with
+    interface System.IDisposable with
+                member x.Dispose() = printfn "Finish"    """
         |> typecheck
         |> shouldSucceed
         |> withWarnings []
