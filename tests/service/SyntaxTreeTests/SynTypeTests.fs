8fe3be8d8c95376e17b80fbdb0b1291f647f9099
nojaf <florian.verdonck@outlook.com>
2022-10-10 14:17:14 +0200

    Add SynType.Or.

--- tests/service/SyntaxTreeTests/SynTypeTests.fs
+++ tests/service/SyntaxTreeTests/SynTypeTests.fs
@@ -49,3 +49,65 @@ type T =
       ])) ->
         assertRange (3, 14) (3, 56) mTuple
     | _ -> Assert.Fail $"Could not get valid AST, got {parseResults}"
+
+[<Test>]
+let ``SynType.Or inside SynExpr.TraitCall`` () =
+    let parseResults =
+        getParseResults
+             """
+let inline (!!) (x: ^a) : ^b = ((^a or ^b): (static member op_Implicit: ^a -> ^b) x)
+ """
+
+    match parseResults with
+    | ParsedInput.ImplFile (ParsedImplFileInput (contents = [ SynModuleOrNamespace.SynModuleOrNamespace(decls = [
+        SynModuleDecl.Let(bindings = [
+            SynBinding(expr =
+                SynExpr.Typed(expr =
+                    SynExpr.Paren(expr =
+                        SynExpr.TraitCall(supportTys =
+                            SynType.Paren(
+                                SynType.Or(
+                                    SynType.Var(range = mVar1),
+                                    SynType.Var(range = mVar2),
+                                    mOrType,
+                                    { OrKeyword = mOrWord }),
+                            mParen))
+                    )))
+        ]) ]) ])) ->
+        assertRange (2, 33) (2, 35) mVar1
+        assertRange (2, 36) (2, 38) mOrWord
+        assertRange (2, 39) (2, 41) mVar2
+        assertRange (2, 33) (2, 41) mOrType
+        assertRange (2, 32) (2, 42) mParen
+    | _ -> Assert.Fail $"Could not get valid AST, got {parseResults}"
+
+[<Test>]
+let ``SynType.Or inside SynTypeConstraint.WhereTyparSupportsMember`` () =
+    let parseResults =
+        getParseResults
+             """
+let inline f_StaticMethod<'T1, 'T2 when ('T1 or 'T2) : (static member StaticMethod: int -> int) >() : int = 
+    ()
+ """
+
+    match parseResults with
+    | ParsedInput.ImplFile (ParsedImplFileInput (contents = [ SynModuleOrNamespace.SynModuleOrNamespace(decls = [
+        SynModuleDecl.Let(bindings = [
+            SynBinding(headPat =
+                SynPat.LongIdent(typarDecls = Some (SynValTyparDecls(typars = Some (SynTyparDecls.PostfixList(constraints = [
+                    SynTypeConstraint.WhereTyparSupportsMember(typars = SynType.Paren(
+                        SynType.Or(
+                            SynType.Var(range = mVar1),
+                            SynType.Var(range = mVar2),
+                            mOrType,
+                            { OrKeyword = mOrWord }),
+                        mParen
+                    ))
+                ]))))))
+        ]) ]) ])) ->
+        assertRange (2, 41) (2, 44) mVar1
+        assertRange (2, 45) (2, 47) mOrWord
+        assertRange (2, 48) (2, 51) mVar2
+        assertRange (2, 41) (2, 51) mOrType
+        assertRange (2, 40) (2, 52) mParen
+    | _ -> Assert.Fail $"Could not get valid AST, got {parseResults}"
