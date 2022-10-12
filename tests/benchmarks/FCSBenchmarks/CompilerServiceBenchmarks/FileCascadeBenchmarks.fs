8a8499c294a06a6060f5a5b22b03d40751985e67
Tomas Grosup <tomasgrosup@microsoft.com>
2022-10-12 17:25:27 +0200

    Benchmark attributes added

--- tests/benchmarks/FCSBenchmarks/CompilerServiceBenchmarks/FileCascadeBenchmarks.fs
+++ tests/benchmarks/FCSBenchmarks/CompilerServiceBenchmarks/FileCascadeBenchmarks.fs
@@ -12,9 +12,11 @@ open FSharp.Compiler.AbstractIL.ILBinaryReader
 open BenchmarkDotNet.Attributes
 open FSharp.Compiler.Benchmarks
 open Microsoft.CodeAnalysis.Text
+open BenchmarkDotNet.Order
+open BenchmarkDotNet.Mathematics
 
 [<AutoOpen>]
-module private Helpers =
+module private CascadeProjectHelpers =
         
     let createProject files projectFilename =       
         {
@@ -41,35 +43,76 @@ let myFunc0 () = 5"""
         $"""
 module Benchmark%i{number}
 open Benchmark%i{number-1}
-let myFunc%i{number} () = myFunc%i{number-1}()"""
+let myFunc%i{number} () = myFunc%i{number-1}()
+//$COMMENTAREA$"""
 
 [<MemoryDiagnoser>]
+[<LongRunJob>]
+[<Orderer(SummaryOrderPolicy.FastestToSlowest)>]
+[<RankColumn(NumeralSystem.Roman)>]
 type FileCascadeBenchmarks() = 
-        let mutable project : FSharpProjectOptions option = None
+    let mutable project : FSharpProjectOptions option = None
 
-        let filesToCreate = 100
+    let getProject() = project.Value
+    
+    let checker = FSharpChecker.Create(projectCacheSize = 5, enableParallelCheckingWithSignatureFiles = true, parallelReferenceResolution = true)
+    let filesToCreate = 64
+
+    let mutable finalFileContents = SourceText.ofString ""
         
-        [<GlobalSetup>]
-        member _.Setup() =
-                match project with
-                | Some p -> p
-                | None ->
-                    let projectFolder = Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(),"CascadeBenchmarkProject"))                    
-                    if(projectFolder.Exists) then
-                        do projectFolder.Delete(recursive=true)
-                    do Directory.CreateDirectory(projectFolder.FullName) |> ignore
+    [<GlobalSetup>]
+    member _.Setup() =
+        let projectFolder = Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(),"CascadeBenchmarkProject"))                    
+        if(projectFolder.Exists) then
+            do projectFolder.Delete(recursive=true)
+        do Directory.CreateDirectory(projectFolder.FullName) |> ignore
 
-                    let inProjectFolder fileName = Path.Combine(projectFolder.FullName,fileName)
+        let inProjectFolder fileName = Path.Combine(projectFolder.FullName,fileName)
 
-                    File.WriteAllText(inProjectFolder "Benchmark0.fs",baselineModule)
-                    for i = 1 to filesToCreate do
-                        File.WriteAllText(inProjectFolder $"Benchmark%i{i}.fs", generateSourceCode i)
+        File.WriteAllText(inProjectFolder "Benchmark0.fs",baselineModule)
+        for i = 1 to filesToCreate do
+            File.WriteAllText(inProjectFolder $"Benchmark%i{i}.fs", generateSourceCode i)
 
-                    let dllFileName = inProjectFolder "CascadingBenchMark.dll"
-                    let allSourceCodeFiles = [| for i in 0 .. 100 -> inProjectFolder $"Benchmark%i{i}.fs"|]
-                    let x = createProject allSourceCodeFiles dllFileName
-                    project <- Some x
-                    x
-                
+        let dllFileName = inProjectFolder "CascadingBenchMark.dll"
+        let allSourceCodeFiles = [| for i in 0 .. filesToCreate -> inProjectFolder $"Benchmark%i{i}.fs"|]
+        let x = createProject allSourceCodeFiles dllFileName
+        project <- Some x
+        finalFileContents <- generateSourceCode filesToCreate |> SourceText.ofString
 
 
+    member x.ChangeFile(fileIndex:int, action) = 
+        let project = getProject()
+        let fileName = project.SourceFiles.[fileIndex]
+        let fullOriginalSource = File.ReadAllText(fileName)
+        try
+            File.WriteAllText(fileName, fullOriginalSource.Replace("$COMMENTAREA$","$FILEMODIFIED"))
+            action()
+        finally
+            File.WriteAllText(fileName,fullOriginalSource)
+
+    member x.CheckFinalFile () =
+        let project = getProject()
+        let lastFile = project.SourceFiles |> Array.last
+        checker.ParseAndCheckFileInProject(lastFile,999,finalFileContents,project)
+               
+    [<Benchmark>]
+    member x.ParseProjectAsIs() =
+        x.CheckFinalFile()
+
+    [<Benchmark(Baseline=true)>]
+    member x.ParseProjectWithFullCacheClear() =
+        checker.ClearCache([getProject()])
+        checker.InvalidateConfiguration(getProject())
+        x.CheckFinalFile()
+
+    [<Benchmark>]
+    member x.ParseProjectWithChangingFirstFile() =
+        x.ChangeFile(fileIndex=0, action= fun () -> x.CheckFinalFile())  
+
+    [<Benchmark>]
+    member x.ParseProjectWithChanging25thPercentileFile() =
+        x.ChangeFile(fileIndex=filesToCreate/4, action= fun () -> x.CheckFinalFile())  
+
+    [<Benchmark>]
+    member x.ParseProjectWithChangingMiddleFile() =
+        x.ChangeFile(fileIndex=filesToCreate/2, action= fun () -> x.CheckFinalFile())  
