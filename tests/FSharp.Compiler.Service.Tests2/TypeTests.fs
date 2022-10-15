4e5593b334157658dcffe5649fc3778c2b20a6c3
janusz <janusz.wrobel.2@gmail.com>
2022-10-15 14:48:58 +0100

    WIP

--- tests/FSharp.Compiler.Service.Tests2/TypeTests.fs
+++ tests/FSharp.Compiler.Service.Tests2/TypeTests.fs
@@ -36,6 +36,119 @@ let rec visitSynModuleDecl (decl : SynModuleDecl) : Stuff =
             yield! Seq.collect visitSynModuleDecl synModuleDecls
         }
 
+and visitSynIdent (x : SynIdent) : Stuff =
+    [] // TODO Correct?
+
+and visitSynType (x : SynType) : Stuff =
+    match x with
+    | SynType.Anon range ->
+        []
+    | SynType.App(typeName, rangeOption, typeArgs, commaRanges, greaterRange, isPostfix, range) ->
+        failwith unsupported
+    | SynType.Array(rank, elementType, range) ->
+        failwith unsupported
+    | SynType.Fun(argType, returnType, range, synTypeFunTrivia) ->
+        failwith unsupported
+    | SynType.Or(lhsType, rhsType, range, synTypeOrTrivia) ->
+        failwith unsupported
+    | SynType.Paren(innerType, range) ->
+        failwith unsupported
+    | SynType.Tuple(isStruct, synTupleTypeSegments, range) ->
+        failwith unsupported
+    | SynType.Var(synTypar, range) ->
+        failwith unsupported
+    | SynType.AnonRecd(isStruct, fields, range) ->
+        failwith unsupported
+    | SynType.HashConstraint(innerType, range) ->
+        failwith unsupported
+    | SynType.LongIdent synLongIdent ->
+        failwith unsupported
+    | SynType.MeasureDivide(synType, divisor, range) ->
+        failwith unsupported
+    | SynType.MeasurePower(baseMeasure, synRationalConst, range) ->
+        failwith unsupported
+    | SynType.SignatureParameter(synAttributeLists, optional, identOption, usedType, range) ->
+        failwith unsupported
+    | SynType.StaticConstant(synConst, range) ->
+        failwith unsupported
+    | SynType.LongIdentApp(typeName, synLongIdent, rangeOption, typeArgs, commaRanges, greaterRange, range) ->
+        failwith unsupported
+    | SynType.StaticConstantExpr(synExpr, range) ->
+        failwith unsupported
+    | SynType.StaticConstantNamed(synType, value, range) ->
+        failwith unsupported
+    | SynType.WithGlobalConstraints(typeName, synTypeConstraints, range) ->
+        failwith unsupported
+
+and visitPreXmlDoc (doc : FSharp.Compiler.Xml.PreXmlDoc) : Stuff =
+    [] // TODO Check
+
+and visitSynAccess (x : SynAccess) : Stuff =
+    failwith unsupported
+
+and visitSynField (x : SynField) : Stuff =
+    match x with
+    | SynField.SynField(synAttributeLists, isStatic, identOption, fieldType, isMutable, preXmlDoc, synAccessOption, range, synFieldTrivia) ->
+        seq {
+            yield! visitSynAttributeLists synAttributeLists
+            yield! visitSynType fieldType
+            yield! visitPreXmlDoc preXmlDoc
+            match synAccessOption with | Some access -> yield! visitSynAccess access | None -> ()
+        }
+
+and visitSynFields (x : SynField list) : Stuff =
+    Seq.collect visitSynField x
+
+and visitSynUnionCaseKind (x : SynUnionCaseKind) : Stuff =
+    match x with
+    | SynUnionCaseKind.Fields synFields ->
+        
+        [] // TODO
+    | SynUnionCaseKind.FullType(fullType, fullTypeInfo) ->
+        [] // TODO
+
+and visitSynUnionCase (x : SynUnionCase) : Stuff =
+    match x with
+    | SynUnionCase.SynUnionCase(synAttributeLists, synIdent, synUnionCaseKind, preXmlDoc, synAccessOption, range, synUnionCaseTrivia) ->
+        seq {
+            yield! visitSynAttributeLists synAttributeLists
+            yield! visitSynIdent synIdent
+            yield! visitSynUnionCaseKind synUnionCaseKind
+        }
+
+and visitSynExceptionDefnRepr (x : SynExceptionDefnRepr) : Stuff =
+    match x with
+    | SynExceptionDefnRepr.SynExceptionDefnRepr(synAttributeLists, synUnionCase, identsOption, preXmlDoc, synAccessOption, range) ->
+        seq {
+            yield! visitSynAttributeLists synAttributeLists
+            yield! visitSynUnionCase synUnionCase
+            // TODO
+        }
+
+and visitSynTypeDefnRepr (x : SynTypeDefnRepr) : Stuff =
+    match x with
+    | SynTypeDefnRepr.Exception synExceptionDefnRepr ->
+        visitSynExceptionDefnRepr synExceptionDefnRepr
+
+and visitSynMemberDefn (defn : SynMemberDefn) : Stuff =
+    [] // TODO
+
+and visitSynMemberDefns (defns : SynMemberDefn list) : Stuff =
+    Seq.collect visitSynMemberDefn defns
+
+and visitSynTypeDefn (defn : SynTypeDefn) : Stuff =
+    match defn with
+    | SynTypeDefn.SynTypeDefn(synComponentInfo, synTypeDefnRepr, synMemberDefns, synMemberDefnOption, range, synTypeDefnTrivia) ->
+        seq {
+            yield! visitSynComponentInfo synComponentInfo
+            yield! visitSynTypeDefnRepr synTypeDefnRepr
+            yield! visitSynMemberDefns synMemberDefns
+            match synMemberDefnOption with Some defn -> yield! visitSynMemberDefn defn | None -> ()
+        }
+
+and visitSynTypeDefns (defns : SynTypeDefn list) : Stuff =
+    Seq.collect visitSynTypeDefn defns
+
 and visitSynExceptionDefn (defn : SynExceptionDefn) : Stuff =
     []
 
