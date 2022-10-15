7de1d91194b9218b9edf94e5c0500c3fb44d1ed3
janusz <janusz.wrobel.2@gmail.com>
2022-10-15 14:15:19 +0100

    Start implementing AST visitor

---
+++ tests/FSharp.Compiler.Service.Tests2/TypeTests.fs
@@ -0,0 +1,108 @@
+﻿module FSharp.Compiler.Service.Tests2.SyntaxTreeTests.TypeTests
+
+open FSharp.Compiler.CodeAnalysis
+open FSharp.Compiler.Service.Tests.Common
+open FSharp.Compiler.Syntax
+open FSharp.Compiler.SyntaxTrivia
+open NUnit.Framework
+
+let unsupported = "unsupported"
+type Stuff = SynLongIdent seq
+
+let rec visitSynModuleDecl (decl : SynModuleDecl) : Stuff =
+    // TODO
+    match decl with
+    | SynModuleDecl.Attributes(synAttributeLists, range) ->
+        visitSynAttributeLists synAttributeLists
+    | SynModuleDecl.Exception(synExceptionDefn, range) ->
+        visitSynExceptionDefn synExceptionDefn
+    | SynModuleDecl.Expr(synExpr, range) ->
+        visitSynExpr synExpr
+    | SynModuleDecl.HashDirective(parsedHashDirective, range) ->
+        []
+    | SynModuleDecl.Let(isRecursive, synBindings, range) ->
+        visitSynBindings synBindings
+    | SynModuleDecl.Open(synOpenDeclTarget, range) -> 
+        visitSynOpenDeclTarget synOpenDeclTarget
+    | SynModuleDecl.Types(synTypeDefns, range) ->
+        visitSynTypeDefns synTypeDefns
+    | SynModuleDecl.ModuleAbbrev(ident, longId, range) ->
+        visitLongIdent longId
+    | SynModuleDecl.NamespaceFragment synModuleOrNamespace ->
+        visitSynModuleOrNamespace synModuleOrNamespace
+    | SynModuleDecl.NestedModule(synComponentInfo, isRecursive, synModuleDecls, isContinuing, range, synModuleDeclNestedModuleTrivia) ->
+        seq {
+            yield! visitSynComponentInfo synComponentInfo
+            yield! Seq.collect visitSynModuleDecl synModuleDecls
+        }
+
+and visitSynExceptionDefn (defn : SynExceptionDefn) : Stuff =
+    []
+
+and visitSynBinding (binding : SynBinding) : Stuff =
+    [] // TODO
+
+and visitSynBindings (bindings : SynBinding list) : Stuff =
+    Seq.collect visitSynBinding bindings
+
+and visitSynOpenDeclTarget (target : SynOpenDeclTarget) : Stuff =
+    [] // TODO
+
+and visitSynComponentInfo (info : SynComponentInfo) : Stuff =
+    [] // TODO
+
+and visitLongIdent (ident : LongIdent) : Stuff =
+    [] // TODO
+    
+and visitSynLongIdent (ident : SynLongIdent) : Stuff  =
+    [ident]
+    
+and visitSynExpr (expr : SynExpr) =
+    // TODO
+    match expr with
+    | _ -> []
+    
+and visitSynAttribute (attribute : SynAttribute) : Stuff  =
+    seq {
+        yield! visitSynLongIdent attribute.TypeName
+        yield! visitSynExpr attribute.ArgExpr
+    }
+                
+and visitSynAttributeList (attributeList : SynAttributeList) : Stuff  =
+    attributeList.Attributes
+    |> Seq.collect visitSynAttribute
+
+and visitSynAttributeLists (attributeLists : SynAttributeList list) : Stuff  =
+    Seq.collect visitSynAttributeList attributeLists
+
+and visitSynModuleOrNamespace (x : SynModuleOrNamespace) : Stuff  =
+    match x with
+    | SynModuleOrNamespace.SynModuleOrNamespace(longId, isRecursive, synModuleOrNamespaceKind, synModuleDecls, preXmlDoc, synAttributeLists, synAccessOption, range, synModuleOrNamespaceTrivia) ->
+        seq {
+            yield! synModuleDecls |> Seq.collect visitSynModuleDecl
+            yield! visitSynAttributeLists synAttributeLists 
+        }
+
+and visit (input : ParsedInput) : Stuff  =
+    match input with
+    | ParsedInput.SigFile _ -> failwith unsupported
+    | ParsedInput.ImplFile(ParsedImplFileInput(fileName, isScript, qualifiedNameOfFile, scopedPragmas, parsedHashDirectives, synModuleOrNamespaces, flags, parsedImplFileInputTrivia)) ->
+        synModuleOrNamespaces
+        |> Seq.collect visitSynModuleOrNamespace
+
+[<Test>]
+let ``Single SynEnumCase contains range of constant`` () =
+    let parseResults = 
+        getParseResults
+            """
+type Foo = One = 0x00000001
+"""
+
+    match parseResults with
+    | ParsedInput.ImplFile (ParsedImplFileInput (contents = [ SynModuleOrNamespace.SynModuleOrNamespace(decls = [
+        SynModuleDecl.Types(typeDefns = [
+            SynTypeDefn.SynTypeDefn(typeRepr =
+                SynTypeDefnRepr.Simple(simpleRepr = SynTypeDefnSimpleRepr.Enum(cases = [ SynEnumCase.SynEnumCase(valueRange = r) ])))])
+    ]) ])) ->
+        assertRange (2, 17) (2, 27) r
+    | _ -> Assert.Fail "Could not get valid AST"
