58dcee66516d70127f4c7c52ecb738fb271fc2eb
janusz <janusz.wrobel.2@gmail.com>
2022-10-30 17:07:14 +0000

    changes

---
+++ tests/DiamondTest/Program.fs
@@ -0,0 +1,8 @@
+﻿module DiamondTest.Program
+
+printfn $"A: {DiamondTest.A.a}"
+printfn $"B1: {DiamondTest.B1.b1}"
+printfn $"B2: {DiamondTest.B2.b2}"
+printfn $"C1: {DiamondTest.C1.c1}"
+printfn $"C2: {DiamondTest.C2.c2}"
+printfn $"D: {DiamondTest.D.d}"
