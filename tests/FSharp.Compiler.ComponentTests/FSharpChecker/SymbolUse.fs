59e2d3458ed9ec26c60e4c545a40e2cf39c4525d
Petr Pokorny <petrpokorny@microsoft.com>
2022-11-09 14:30:41 +0100

    More tests

--- tests/FSharp.Compiler.ComponentTests/FSharpChecker/SymbolUse.fs
+++ tests/FSharp.Compiler.ComponentTests/FSharpChecker/SymbolUse.fs
@@ -49,3 +49,31 @@ val f: x: 'a -> TFirstV_1<'a>
                 let symbolUse = typeCheckResult.GetSymbolUseAtLocation(5, 6, "let f2 x = x + 1", ["f2"]) |> Option.defaultWith (fun () -> failwith "no symbol use found")
                 Assert.True(symbolUse.IsPrivateToFile))
         }
+
+    [<Fact>]
+    let ``Function parameter, no signature file`` () =
+        SyntheticProject.Create(sourceFile "First" []).Workflow {
+            checkFile "First" (fun (typeCheckResult: FSharpCheckFileResults) ->
+                let symbolUse = typeCheckResult.GetSymbolUseAtLocation(5, 8, "let f2 x = x + 1", ["x"]) |> Option.defaultWith (fun () -> failwith "no symbol use found")
+                Assert.True(symbolUse.IsPrivateToFile))
+        }
+
+    /// This is a bug: https://github.com/dotnet/fsharp/issues/14277
+    [<Fact>]
+    let ``Function parameter, with signature file`` () =
+        SyntheticProject.Create(sourceFile "First" [] |> addSignatureFile).Workflow {
+            checkFile "First" (fun (typeCheckResult: FSharpCheckFileResults) ->
+                let symbolUse = typeCheckResult.GetSymbolUseAtLocation(5, 8, "let f2 x = x + 1", ["x"]) |> Option.defaultWith (fun () -> failwith "no symbol use found")
+                // This should be false, because it's also in the signature file
+                Assert.True(symbolUse.IsPrivateToFile))
+        }
+
+    [<Fact>]
+    let ``Private function, with signature file`` () =
+        SyntheticProject.Create(
+            { sourceFile "First" [] with ExtraSource = "let private f3 x = x + 1" } 
+            |> addSignatureFile).Workflow {
+            checkFile "First" (fun (typeCheckResult: FSharpCheckFileResults) ->
+                let symbolUse = typeCheckResult.GetSymbolUseAtLocation(6, 14, "let private f3 x = x + 1", ["f3"]) |> Option.defaultWith (fun () -> failwith "no symbol use found")
+                Assert.False(symbolUse.IsPrivateToFile))
+        }
