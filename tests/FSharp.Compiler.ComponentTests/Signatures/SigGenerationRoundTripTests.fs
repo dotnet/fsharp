6f1bed57ef4bbbfe346f1bb830ae7b210636bd00
Tomas Grosup <tomasgrosup@microsoft.com>
2022-11-14 13:19:46 +0100

    Component test for signature roundtrip

---
+++ tests/FSharp.Compiler.ComponentTests/Signatures/SigGenerationRoundTripTests.fs
@@ -0,0 +1,32 @@
+﻿module FSharp.Compiler.ComponentTests.Signatures.SigGenerationRoundTripTests
+
+open Xunit
+open FSharp.Test
+open FSharp.Test.Compiler
+open System.IO
+
+let testCasesDir = Path.Combine(__SOURCE_DIRECTORY__,"TestCasesForGenerationRoundTrip")
+let allTestCases = 
+    Directory.EnumerateFiles(testCasesDir) 
+    |> Seq.toArray 
+    |> Array.map Path.GetFileName
+    |> Array.map (fun f -> [|f :> obj|])
+
+[<Theory>]
+[<MemberData(nameof(allTestCases))>]
+let ``Generate and compile`` implFileName =    
+    let implContents = File.ReadAllText (Path.Combine(testCasesDir,implFileName))
+
+    let generatedSignature = 
+        Fs implContents     
+        |> withDefines ["TESTS_AS_APP"]
+        |> printSignatures   
+
+    Fsi generatedSignature    
+    |> withAdditionalSourceFile (FsSource implContents)
+    |> withDefines ["TESTS_AS_APP"]
+    |> ignoreWarnings
+    |> asExe
+    |> compile
+    |> shouldSucceed
+
