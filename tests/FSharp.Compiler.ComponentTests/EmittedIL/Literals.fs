6fa39bccdc31fde21318518da715e2970e135c1d
kerams <kerams@users.noreply.github.com>
2022-11-22 21:08:37 +0100

    Fix test

--- tests/FSharp.Compiler.ComponentTests/EmittedIL/Literals.fs
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/Literals.fs
@@ -144,12 +144,6 @@ let [<Literal>] x = 1 + System.DateTime.Now.Hour
         |> compile
         |> shouldFail
         |> withResults [
-            { Error = Warning 52
-              Range = { StartLine = 4
-                        StartColumn = 25
-                        EndLine = 4
-                        EndColumn = 49 }
-              Message = "The value has been copied to ensure the original is not mutated by this operation or because the copy is implicit when returning a struct from a member and another member is then accessed" }
             { Error = Error 267
               Range = { StartLine = 4
                         StartColumn = 25
