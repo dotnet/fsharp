9787da2e5fdb30091a9367b253faf1b60a21ae50
KevinRansom <codecutter@hotmail.com>
2022-11-30 15:02:55 -0800

    refonly validation

--- src/Compiler/Driver/CompilerOptions.fs
+++ src/Compiler/Driver/CompilerOptions.fs
@@ -581,7 +581,7 @@ let SetDeterministicSwitch (tcConfigB: TcConfigBuilder) switch =
 
 let SetReferenceAssemblyOnlySwitch (tcConfigB: TcConfigBuilder) switch =
     match tcConfigB.emitMetadataAssembly with
-    | MetadataAssemblyGeneration.None ->
+    | MetadataAssemblyGeneration.None when tcConfigB.standalone = false && tcConfigB.extraStaticLinkRoots.IsEmpty ->
         tcConfigB.emitMetadataAssembly <-
             if (switch = OptionSwitch.On) then
                 MetadataAssemblyGeneration.ReferenceOnly
@@ -591,7 +591,7 @@ let SetReferenceAssemblyOnlySwitch (tcConfigB: TcConfigBuilder) switch =
 
 let SetReferenceAssemblyOutSwitch (tcConfigB: TcConfigBuilder) outputPath =
     match tcConfigB.emitMetadataAssembly with
-    | MetadataAssemblyGeneration.None ->
+    | MetadataAssemblyGeneration.None when tcConfigB.standalone = false &&  tcConfigB.extraStaticLinkRoots.IsEmpty ->
         if FileSystem.IsInvalidPathShim outputPath then
             error (Error(FSComp.SR.optsInvalidRefOut (), rangeCmdArgs))
         else
@@ -1304,9 +1304,12 @@ let advancedFlagsFsc tcConfigB =
             "standalone",
             tagNone,
             OptionUnit(fun _ ->
-                tcConfigB.openDebugInformationForLaterStaticLinking <- true
-                tcConfigB.standalone <- true
-                tcConfigB.implicitlyResolveAssemblies <- true),
+                match tcConfigB.emitMetadataAssembly with
+                | MetadataAssemblyGeneration.None ->
+                    tcConfigB.openDebugInformationForLaterStaticLinking <- true
+                    tcConfigB.standalone <- true
+                    tcConfigB.implicitlyResolveAssemblies <- true
+                | _ -> error (Error(FSComp.SR.optsInvalidRefAssembly (), rangeCmdArgs))),
             None,
             Some(FSComp.SR.optsStandalone ())
         )
@@ -1315,8 +1318,11 @@ let advancedFlagsFsc tcConfigB =
             "staticlink",
             tagFile,
             OptionString(fun s ->
-                tcConfigB.extraStaticLinkRoots <- tcConfigB.extraStaticLinkRoots @ [ s ]
-                tcConfigB.implicitlyResolveAssemblies <- true),
+                match tcConfigB.emitMetadataAssembly with
+                | MetadataAssemblyGeneration.None ->
+                    tcConfigB.extraStaticLinkRoots <- tcConfigB.extraStaticLinkRoots @ [ s ]
+                    tcConfigB.implicitlyResolveAssemblies <- true
+                | _ -> error (Error(FSComp.SR.optsInvalidRefAssembly (), rangeCmdArgs))),
             None,
             Some(FSComp.SR.optsStaticlink ())
         )
