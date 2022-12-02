c6f21aac317da1afb8f737dd330e48c5262d2685
KevinRansom <codecutter@hotmail.com>
2022-12-02 15:51:13 -0800

    Fix nested type error messages

--- tests/FSharp.Compiler.ComponentTests/Interop/DeeplyNestedCSharpClasses.fs
+++ tests/FSharp.Compiler.ComponentTests/Interop/DeeplyNestedCSharpClasses.fs
@@ -11,22 +11,19 @@ module ``Deeply nested CSharpClasses`` =
 //Expect error similar to:
 //(8,29): error FS0039: The value, constructor, namespace or type 'somefunctoin' is not defined. Maybe you want one of the following:   somefunction
 
-    [<Fact>]
-    let ``Missing function generates good message and range`` () =
-
-        let cslib = 
-            CSharp """
+    let cslib =
+        CSharp """
 using System;
 
-namespace TorchSharp
+namespace MyNamespace
 {
-    public class torch
+    public class OuterClass
     {
 
-        public class nn
+        public class InnerClass
         {
 
-            public class functional
+            public class MoreInnerClass
             {
                 public static void somefunction() { }
             }
@@ -34,15 +31,59 @@ namespace TorchSharp
     }
 }"""
 
+    [<Fact>]
+    let ``Missing type outerclass generates good message and range`` () =
+
         let fsharpSource =
             """
-open TorchSharp
-
-let loss2 = torch.nn.functional.somefunctoin()   //Note the miss-typed functionname we expect a good error message
+let loss2 = MyNamespace.OoterClass.InnerClass.MoreInnerClass.somefunction()   //Note the miss-typed functionname we expect a good error message
 """
         FSharp fsharpSource
         |> asExe
         |> withReferences [cslib]
-        |> compileAndRun
-        |> shouldSucceed
+        |> compile
+        |> withSingleDiagnostic (Error 39, Line 2, Col 25, Line 2, Col 35, "The value, constructor, namespace or type 'OoterClass' is not defined. Maybe you want one of the following:
+   OuterClass")
 
+    [<Fact>]
+    let ``Missing type nested type innerclass generates good message and range`` () =
+
+        let fsharpSource =
+            """
+let loss2 = MyNamespace.OuterClass.InerClass.MoreInnerClass.somefunction()   //Note the miss-typed InnerClass name we expect a good error message
+"""
+        FSharp fsharpSource
+        |> asExe
+        |> withReferences [cslib]
+        |> compile
+        |> shouldFail
+        |> withSingleDiagnostic (Error 39, Line 2, Col 36, Line 2, Col 45, "The type 'OuterClass' does not define the field, constructor or member 'InerClass'.")
+
+    [<Fact>]
+    let ``Missing type nested type moreinnerclass generates good message and range`` () =
+
+        let fsharpSource =
+            """
+let loss2 = MyNamespace.OuterClass.InnerClass.MoareInnerClass.somefunction()   //Note the miss-typed MoreInnerClass we expect a good error message
+"""
+        FSharp fsharpSource
+        |> asExe
+        |> withReferences [cslib]
+        |> compile
+        |> shouldFail
+        |> withSingleDiagnostic (Error 39, Line 2, Col 47, Line 2, Col 62, "The type 'InnerClass' does not define the field, constructor or member 'MoareInnerClass'.")
+
+    [<Fact>]
+    let ``Missing function generates good message and range`` () =
+
+        let fsharpSource =
+            """
+let loss2 = MyNamespace.OuterClass.InnerClass.MoreInnerClass.somefunctoion()   //Note the miss-typed somefunction we expect a good error message
+"""
+        FSharp fsharpSource
+        |> asExe
+        |> withReferences [cslib]
+        |> compile
+        |> shouldFail
+        |> withSingleDiagnostic ((Error 39, Line 2, Col 62, Line 2, Col 75, """The type 'MoreInnerClass' does not define the field, constructor or member 'somefunctoion'. Maybe you want one of the following:
+   somefunction"""))
