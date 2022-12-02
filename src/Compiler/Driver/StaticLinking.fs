1fc23fbe3e99db638f9ee57572d02efe1c5bf5cb
Kevin Ransom (msft) <codecutter@hotmail.com>
2022-12-02 04:42:47 -0800

    refonly validation (#14416)

--- src/Compiler/Driver/StaticLinking.fs
+++ src/Compiler/Driver/StaticLinking.fs
@@ -514,10 +514,6 @@ let StaticLink (ctok, tcConfig: TcConfig, tcImports: TcImports, ilGlobals: ILGlo
         id
     else
         (fun ilxMainModule ->
-            match tcConfig.emitMetadataAssembly with
-            | MetadataAssemblyGeneration.None -> ()
-            | _ -> error (Error(FSComp.SR.optsInvalidRefAssembly (), rangeCmdArgs))
-
             ReportTime tcConfig "Find assembly references"
 
             let dependentILModules =
