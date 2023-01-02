36d146937d4c897536505d502bac45f0ed394bef
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2023-01-02 18:28:32 +0100

    Reuse existing runimmediate via ResultCell<AsyncResult<_>> instead of TCS

--- src/FSharp.Core/async.fs
+++ src/FSharp.Core/async.fs
@@ -1188,24 +1188,6 @@ module AsyncPrimitives =
         |> unfake
 
     [<DebuggerHidden>]
-    let RunImmediateOnCurrentThread (cancellationToken: CancellationToken) (computation: Async<'T>) =
-        let tcs = TaskCompletionSource<'T>()
-        let task = tcs.Task
-
-        let cont k =
-            tcs.SetResult k
-
-        let econt (edi: ExceptionDispatchInfo) =
-            tcs.SetException(edi.GetAssociatedSourceException())
-
-        let ccont _ =
-            tcs.SetCanceled()
-
-        StartWithContinuations cancellationToken computation cont econt ccont
-
-        task.Result
-
-    [<DebuggerHidden>]
     let StartAsTask cancellationToken (computation: Async<'T>) taskCreationOptions =
         let taskCreationOptions = defaultArg taskCreationOptions TaskCreationOptions.None
         let tcs = TaskCompletionSource<_>(taskCreationOptions)
@@ -1521,7 +1503,7 @@ type Async =
 
     static member RunImmediate(computation: Async<'T>, ?cancellationToken: CancellationToken) =
         let cancellationToken = defaultArg cancellationToken Async.DefaultCancellationToken
-        AsyncPrimitives.RunImmediateOnCurrentThread cancellationToken computation
+        AsyncPrimitives.RunImmediate cancellationToken computation
 
     static member RunSynchronously(computation: Async<'T>, ?timeout, ?cancellationToken: CancellationToken) =
         let timeout, cancellationToken =
