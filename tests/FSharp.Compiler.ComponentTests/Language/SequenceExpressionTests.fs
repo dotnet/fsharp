e3dc32dbbf2d4cbf614cd90ca74067f19521453e
Tomas Grosup <tomasgrosup@microsoft.com>
2023-01-03 17:15:39 +0100

    More tailored failing tests added

--- tests/FSharp.Compiler.ComponentTests/Language/SequenceExpressionTests.fs
+++ tests/FSharp.Compiler.ComponentTests/Language/SequenceExpressionTests.fs
@@ -45,6 +45,24 @@ if sum <> 16 then
     |> shouldSucceed
 
 [<Fact>]
+let ``A sequence expression can fail later in try/with and still get caught``() =
+    Fsx """
+let sum =
+    seq {
+        try
+            yield 1   
+            yield (10 /  0)
+        with _ -> ()
+    }
+    |> Seq.sum
+if sum <> (1) then
+    failwith $"Sum was {sum} instead"
+    """
+    |> asExe
+    |> compileAndRun
+    |> shouldSucceed
+
+[<Fact>]
 let ``A sequence expression can do multiple yields from try/with clause``() =
     Fsx """
 let sum =
@@ -52,7 +70,8 @@ let sum =
         for x in [0;1] do       
             try
                 yield 1   // Should work both times before failure
-                yield! seq{ (10 /  x);2}  // Will crash for 0        
+                yield (10/x)  // Will crash for 0     
+                yield 2  // Will only get there for 1
             with _ ->
                 yield 100
                 yield 100
@@ -129,7 +148,7 @@ if sum <> 110 then
 [<InlineData("()","42","43")>]
 [<InlineData("41","()","43")>]
 [<InlineData("41","42","()")>]
-let ``Propper type matching in seq{try/with}``(valInTry,valInWith1,valInWith2) =
+let ``Propper type matching in seq{try/with} with implicit yield``(valInTry,valInWith1,valInWith2) =
     Fsx $"""
 let typedSeq =
     seq {{
@@ -143,6 +162,45 @@ let typedSeq =
     """
     |> typecheck
     |> shouldSucceed
+
+[<Theory>]
+[<InlineData("41","42","43")>]
+[<InlineData("yield 41","yield 42","yield 43")>]
+let ``seq{try/with} using yield or implicit must be consistent``(valInTry,valInWith1,valInWith2) =
+    Fsx $"""
+let typedSeq =
+    seq {{
+        for x in [0;1] do       
+            try
+                %s{valInTry}
+            with
+            |_ when x = 0 -> %s{valInWith1}
+            |_ when x = 0 -> %s{valInWith2}
+    }}
+    """
+    |> typecheck
+    |> shouldSucceed
+
+[<Theory>]
+[<InlineData("yield 41","42","43")>]
+[<InlineData("yield 41","42","yield 43")>]
+let ``seq{try/with} mismatch implicit vs. yield``(valInTry,valInWith1,valInWith2) =
+    Fsx $"""
+let typedSeq =
+    seq {{
+        for x in [0;1] do       
+            try
+                %s{valInTry}
+            with
+            |_ when x = 0 -> %s{valInWith1}
+            |_ when x = 0 -> %s{valInWith2}
+    }}
+    """
+    |> typecheck
+    |> shouldFail
+    |> withErrorCode 3221
+    |> withDiagnosticMessageMatches "This expression returns a value of type 'int' but is implicitly discarded."
+    |> withDiagnosticMessageMatches "If you intended to use the expression as a value in the sequence then use an explicit 'yield'."
 
 [<Theory>]
 [<InlineData("42","false","false")>]
