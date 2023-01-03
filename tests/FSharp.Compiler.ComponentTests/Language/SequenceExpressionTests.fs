2cff81fc59c97171a17047c59bf941a29a0d339a
Tomas Grosup <tomasgrosup@microsoft.com>
2023-01-03 16:55:48 +0100

    Failing test added - error at enumeration time not being caught!

--- tests/FSharp.Compiler.ComponentTests/Language/SequenceExpressionTests.fs
+++ tests/FSharp.Compiler.ComponentTests/Language/SequenceExpressionTests.fs
@@ -26,6 +26,46 @@ if sum <> 110 then
     |> shouldSucceed
 
 [<Fact>]
+let ``A sequence expression can yield! from with clause``() =
+    Fsx """
+let sum =
+    seq {
+        for x in [0;1] do       
+            try
+                yield (10 /  x)
+            with _ ->
+                yield! seq{1;2;3}
+    }
+    |> Seq.sum
+if sum <> 16 then
+    failwith $"Sum was {sum} instead"
+    """
+    |> asExe
+    |> compileAndRun
+    |> shouldSucceed
+
+[<Fact>]
+let ``A sequence expression can do multiple yields from try/with clause``() =
+    Fsx """
+let sum =
+    seq {
+        for x in [0;1] do       
+            try
+                yield 1   // Should work both times before failure
+                yield! seq{ (10 /  x);2}  // Will crash for 0        
+            with _ ->
+                yield 100
+                yield 100
+    }
+    |> Seq.sum
+if sum <> (1+100+100+1+10+2) then
+    failwith $"Sum was {sum} instead"
+    """
+    |> asExe
+    |> compileAndRun
+    |> shouldSucceed
+
+[<Fact>]
 let ``A sequence expression can yield from try and have empty with``() =
     Fsx """
 let sum =
@@ -85,15 +125,11 @@ if sum <> 110 then
     |> shouldSucceed
 
 [<Theory>]
-[<InlineData("42","false","false")>]
-[<InlineData("42","43","false")>]
-[<InlineData("42","false","43")>]
 [<InlineData("41","42","43")>]
 [<InlineData("()","42","43")>]
 [<InlineData("41","()","43")>]
 [<InlineData("41","42","()")>]
-[<InlineData("()","()","()")>]
-let ``Typecheck mismatch between try and match return types``(valInTry,valInWith1,valInWith2) =
+let ``Propper type matching in seq{try/with}``(valInTry,valInWith1,valInWith2) =
     Fsx $"""
 let typedSeq =
     seq {{
@@ -107,4 +143,49 @@ let typedSeq =
     """
     |> typecheck
     |> shouldSucceed
+
+[<Theory>]
+[<InlineData("42","false","false")>]
+[<InlineData("42","43","false")>]
+[<InlineData("42","false","43")>]
+let ``Type mismatch error in seq{try/with}``(valInTry,valInWith1,valInWith2) =
+    Fsx $"""
+let typedSeq =
+    seq {{
+        for x in [0;1] do       
+            try
+                %s{valInTry}
+            with
+            |_ when x = 0 -> %s{valInWith1}
+            |_ when x = 0 -> %s{valInWith2}
+    }}
+    """
+    |> typecheck
+    |> shouldFail
+    |> withErrorCode 193
+    |> withDiagnosticMessageMatches "Type constraint mismatch"
+
+
+[<Literal>]
+let printCode = """ printfn "Hello there" """
+
+[<Theory>]
+[<InlineData("()","()","()")>]
+[<InlineData(printCode,printCode,printCode)>]
+let ``Missing result type in seq{try/with}``(valInTry,valInWith1,valInWith2) =
+    Fsx $"""
+let typedSeq =
+    seq {{
+        for x in [0;1] do       
+            try
+                %s{valInTry}
+            with
+            |_ when x = 0 -> %s{valInWith1}
+            |_ when x = 0 -> %s{valInWith2}
+    }}
+    """
+    |> typecheck
+    |> shouldFail
+    |> withErrorCode 30
+    |> withDiagnosticMessageMatches "Value restriction. The value 'typedSeq' has been inferred to have generic type"
  
