79f487c38b2dd7df10e7e1116f2397d7e7d8f556
nojaf <florian.verdonck@outlook.com>
2023-01-12 15:42:24 +0100

    Move comment to type definition.

--- src/Compiler/Driver/ParseAndCheckInputs.fs
+++ src/Compiler/Driver/ParseAndCheckInputs.fs
@@ -1455,6 +1455,26 @@ open FSharp.Compiler.GraphChecking
 type State = TcState * bool
 type FinalFileResult = TcEnv * TopAttribs * CheckedImplFile option * ModuleOrNamespaceType
 
+/// Auxiliary type for re-using signature information in TcEnvFromImpls.
+///
+/// TcState has two typing environments: TcEnvFromSignatures && TcEnvFromImpls
+/// When type checking a file, depending on the type (implementation or signature), it will use one of these typing environments (TcEnv).
+/// Checking a file will populate the respective TcEnv.
+///
+/// When a file has a dependencies, the information of the signature file in case a pair (implementation file backed by a signature) will suffice to type-check that file.
+/// Example: if `B.fs` has a dependency on `A`, the information of `A.fsi` is enough for `B.fs` to type-check, on condition that information is available in the TcEnvFromImpls.
+/// We introduce a special ArtificialImplFile node in the graph to satisfy this. `B.fs -> [ A.fsi ]` becomes `B.fs -> [ ArtificialImplFile A ].
+/// The `ArtificialImplFile A` node will duplicate the signature information which A.fsi provided earlier.
+/// Processing a `ArtificialImplFile` node will add the information from the TcEnvFromSignatures to the TcEnvFromImpls.
+/// This means `A` will be known in both TcEnvs and therefor `B.fs` can be type-checked.
+/// By doing this, we can speed up the graph processing as type checking a signature file is less expensive than its implementation counterpart.
+///
+/// When we need to actually type-check an implementation file backed by a signature, we cannot have the duplicate information of the signature file present in TcEnvFromImpls.
+/// Example `A.fs -> [ A.fsi ]`. An implementation file always depends on its signature.
+/// Type-checking `A.fs` will add the actual information to TcEnvFromImpls and we do not depend on the `ArtificialImplFile A` for `A.fs`.
+///
+/// In order to deal correctly with the `ArtificialImplFile` logic, we need to transform the resolved graph to contain the additional pair nodes.
+/// After we have type-checked the graph, we exclude the ArtificialImplFile nodes as they are not actual physical files in our project.
 [<RequireQualifiedAccess>]
 type NodeToTypeCheck =
     /// A real physical file in the current project.
@@ -1652,24 +1672,6 @@ let CheckMultipleInputsUsingGraphMode
     let graph =
         DependencyResolution.mkGraph tcConfig.compilingFSharpCore filePairs sourceFiles
 
-    // TcState has two typing environments: TcEnvFromSignatures && TcEnvFromImpls
-    // When type checking a file, depending on the type (implementation or signature), it will use one of these typing environments (TcEnv).
-    // Checking a file will populate the respective TcEnv.
-    //
-    // When a file has a dependencies, the information of the signature file in case a pair (implementation file backed by a signature) will suffice to type-check that file.
-    // Example: if `B.fs` has a dependency on `A`, the information of `A.fsi` is enough for `B.fs` to type-check, on condition that information is available in the TcEnvFromImpls.
-    // We introduce a special ArtificialImplFile node in the graph to satisfy this. `B.fs -> [ A.fsi ]` becomes `B.fs -> [ ArtificialImplFile A ].
-    // The `ArtificialImplFile A` node will duplicate the signature information which A.fsi provided earlier.
-    // Processing a `ArtificialImplFile` node will add the information from the TcEnvFromSignatures to the TcEnvFromImpls.
-    // This means `A` will be known in both TcEnvs and therefor `B.fs` can be type-checked.
-    // By doing this, we can speed up the graph processing as type checking a signature file is less expensive than its implementation counterpart.
-    //
-    // When we need to actually type-check an implementation file backed by a signature, we cannot have the duplicate information of the signature file present in TcEnvFromImpls.
-    // Example `A.fs -> [ A.fsi ]`. An implementation file always depends on its signature.
-    // Type-checking `A.fs` will add the actual information to TcEnvFromImpls and we do not depend on the `ArtificialImplFile A` for `A.fs`.
-    //
-    // In order to deal correctly with the `ArtificialImplFile` logic, we need to transform the resolved graph to contain the additional pair nodes.
-    // After we have type-checked the graph, we exclude the ArtificialImplFile nodes as they are not actual physical files in our project.
     let nodeGraph =
         let mkArtificialImplFile n = NodeToTypeCheck.ArtificialImplFile n
         let mkPhysicalFile n = NodeToTypeCheck.PhysicalFile n
