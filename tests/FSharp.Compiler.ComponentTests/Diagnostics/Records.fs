ef57ea64e4cfc0c6a9d92aabbca3d6cc1ea6f004
kerams <kerams@users.noreply.github.com>
2023-01-29 13:57:08 +0100

    Warn when record update expression changes all fields

---
+++ tests/FSharp.Compiler.ComponentTests/Diagnostics/Records.fs
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+module FSharp.Compiler.ComponentTests.Diagnostics.Records
+
+open Xunit
+open FSharp.Test
+open FSharp.Test.Compiler
+
+[<Fact>]
+let ``Warning emitted when record update syntax changes all fields``() =
+    Fsx """
+module Records
+
+type R = { F1: int; F2: string }
+
+let updateOk r = { r with F1 = 1 }
+let updateWarn r = { r with F1 = 1; F2 = "" }
+    """
+    |> typecheck
+    |> shouldFail
+    |> withDiagnostics [
+        (Warning 3560, Line 7, Col 20, Line 7, Col 46, "This copy-and-update record expression changes all fields of record type 'Records.R'. Consider using the record construction syntax instead.")
+    ]
