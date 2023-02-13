d2ebfc147990aea0ad635b97154f417e4f1673b0
Tomas Grosup <tomasgrosup@microsoft.com>
2023-02-13 18:08:34 +0100

    If background testsjob does not succeed, stop entire execution

--- eng/Build.ps1
+++ eng/Build.ps1
@@ -553,7 +553,7 @@ try {
         
         # Collect output from  background jobs
         Wait-job $bgJob | out-null
-        Receive-Job $bgJob      
+        Receive-Job $bgJob -ErrorAction Stop     
     }
 
     if ($testFSharpQA) {
