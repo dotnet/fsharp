aa6e91fb640299ce571f372d919429d8291c574e
KevinRansom <codecutter@hotmail.com>
2023-03-09 00:40:17 -0800

    epilog

---
+++ src/FSharp.Core/prim-types-epilog.fs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace Microsoft.FSharp.Core
+
+// We can't do this in prim-types.fs, so it goes here instead.
+[<assembly: AutoOpen("Microsoft.FSharp")>]
+[<assembly: AutoOpen("Microsoft.FSharp.Core.LanguagePrimitives.IntrinsicOperators")>]
+[<assembly: AutoOpen("Microsoft.FSharp.Core")>]
+[<assembly: AutoOpen("Microsoft.FSharp.Collections")>]
+[<assembly: AutoOpen("Microsoft.FSharp.Control")>]
+do ()
