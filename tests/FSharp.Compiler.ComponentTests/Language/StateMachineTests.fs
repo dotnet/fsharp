a5b40798eacd6434b3c76058e3b8d5793ff09647
Petr <psfinaki@users.noreply.github.com>
2023-03-13 13:49:50 +0100

    Update StateMachineTests.fs

--- tests/FSharp.Compiler.ComponentTests/Language/StateMachineTests.fs
+++ tests/FSharp.Compiler.ComponentTests/Language/StateMachineTests.fs
@@ -7,37 +7,29 @@ open FSharp.Test.Compiler
 
 module StateMachineTests =
 
-    [<Fact>]
+    [<Fact>] // https://github.com/dotnet/fsharp/issues/13067
     let ``Declaring a local function with a flexible type``() = 
         let code = """
 task {
-    let m1 f s = Seq.map f s // doing this is fine
-
+    let m1 f s = Seq.map f s
     do! Async.Sleep 1
     do! System.Threading.Tasks.Task.Delay 1
 
-    let m2 f (s: #seq<_>) = Seq.map f s // doing this is not fine
-
-    try
-        do! Async.Sleep 1 // this crashes
-        printf "wow I made it\r\n"
-    with _ ->
-        printf "nope\r\n"
-
-    do! System.Threading.Tasks.Task.Delay 1 // this just never resumes
-    printf "wow I made it\r\n"
+    let m2 f (s: #seq<_>) = Seq.map f s
+    do! Async.Sleep 1
+    do! System.Threading.Tasks.Task.Delay 1
 
     return 1
 }
 |> fun f -> f.Wait()
 """
 
-        //Fsx code
-        //|> withNoOptimize
-        //|> compile
-        //|> shouldFail
-        //|> withWarningCode 3511
-        //|> ignore
+        Fsx code
+        |> withNoOptimize
+        |> compile
+        |> shouldFail
+        |> withWarningCode 3511
+        |> ignore
 
         Fsx code
         |> withNoOptimize
