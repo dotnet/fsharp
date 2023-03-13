9aa57ad15be3b8f8c5e7f159795cfdab08af390a
Petr <psfinaki@users.noreply.github.com>
2023-03-13 15:03:08 +0100

    Create AbstractEditorTest.cs

---
+++ vsintegration/tests/FSharp.Editor.IntegrationTests/AbstractEditorTest.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Testing;
+using System;
+using System.Threading.Tasks;
+
+namespace FSharp.Editor.IntegrationTests;
+
+public abstract class AbstractEditorTest : AbstractIntegrationTest
+{
+    private readonly string _solutionName;
+    private readonly string _projectName;
+    private readonly string _projectTemplate;
+
+    protected AbstractEditorTest(string solutionName, string projectTemplate, string projectName)
+    {
+        _solutionName = solutionName ?? throw new ArgumentNullException(nameof(solutionName));
+        _projectTemplate = projectTemplate ?? throw new ArgumentNullException(nameof(projectTemplate));
+        _projectName = projectName ?? throw new ArgumentNullException(nameof(projectName));
+    }
+
+    public override async Task InitializeAsync()
+    {
+        var solutionExplorer = TestServices.SolutionExplorer;
+        var token = HangMitigatingCancellationToken;
+
+        await base.InitializeAsync();
+
+        await solutionExplorer.CreateSolutionAsync(_solutionName, token);
+        await solutionExplorer.AddProjectAsync(_projectName, _projectTemplate, token);
+        await solutionExplorer.RestoreNuGetPackagesAsync(token);
+    }
+}
