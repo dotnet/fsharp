1ac0e0ef6e6df1aeb3533291b1bf3323014e605c
Tomas Grosup <tomasgrosup@microsoft.com>
2023-03-21 11:52:20 +0100

    asyncMaybe => background task

--- vsintegration/src/FSharp.Editor/TaskList/TaskListService.fs
+++ vsintegration/src/FSharp.Editor/TaskList/TaskListService.fs
@@ -21,17 +21,22 @@ type internal FSharpTaskListService
     (
     ) =
 
+    let getDefines (doc:Microsoft.CodeAnalysis.Document) = 
+        asyncMaybe { 
+            let! _, _, parsingOptions, _ = doc.GetFSharpCompilationOptionsAsync(nameof(FSharpTaskListService)) |> liftAsync 
+            return CompilerEnvironment.GetConditionalDefinesForEditing parsingOptions }
+        |> Async.map (Option.defaultValue [])
+
+
     interface IFSharpTaskListService with
         member _.GetTaskListItemsAsync(doc,desc,cancellationToken) = 
-            asyncMaybe{
+            backgroundTask{
                 let foundTaskItems = ImmutableArray.CreateBuilder()
-                let! sourceText = doc.GetTextAsync(cancellationToken)
-                let! _, _, parsingOptions, _ = doc.GetFSharpCompilationOptionsAsync(nameof(FSharpTaskListService)) |> liftAsync
-                let defines = CompilerEnvironment.GetConditionalDefinesForEditing parsingOptions
-                Debug.WriteLine($"{doc.FilePath} is having {sourceText.Lines.Count} lines")
-                for line in sourceText.Lines do                
-                    let lineTxt = line.ToString()
-                    Debug.WriteLine($"Text is = '{lineTxt}'")
+                let! sourceText = doc.GetTextAsync(cancellationToken)             
+                let! defines = doc |> getDefines
+               
+                for line in sourceText.Lines do  
+                 
                     let granularTokens = 
                         Tokenizer.tokenizeLine(doc.Id, sourceText, line.Span.Start, doc.FilePath, defines) 
                         |> Array.filter(fun t -> t.Kind = LexerSymbolKind.Comment)
@@ -45,21 +50,20 @@ type internal FSharpTaskListService
                             | head :: tail when token.Left-head.Right <= 1   -> {|token with Left = head.Left|} :: tail
                             | _  -> token :: acc )
 
-                    let hmmcontractedCommentTokens = contractedTokens |> List.map (fun x -> x.Left, x.Right, lineTxt.Substring(x.Left,1 + x.Right - x.Left))
-                    Debug.WriteLine($"AFTER %A{hmmcontractedCommentTokens}")
+                    for ct in contractedTokens do    
+                        let lineTxt = line.ToString()
+                        let tokenSize = 1+(ct.Right - ct.Left)
 
-                    for ct in contractedTokens do                        
-                        for d in desc do 
-                            let idx = lineTxt.IndexOf(value = d.Text,startIndex = ct.Left, count = 1+(ct.Right - ct.Left), comparisonType = StringComparison.OrdinalIgnoreCase)  
+                        for d in desc do                     
+                            let idx = lineTxt.IndexOf(d.Text, ct.Left, tokenSize, StringComparison.OrdinalIgnoreCase)  
                             
                             if idx > -1 then 
                                 let taskLength = 1+ct.Right-idx                
                                 // A descriptor followed by another letter is not a todocomment, like todoabc. But TODO, TODO2 or TODO: should be.
                                 if (idx+taskLength) >= lineTxt.Length || not (Char.IsLetter(lineTxt.[idx+taskLength])) then
-                                    foundTaskItems.Add(new FSharpTaskListItem(d, lineTxt.Substring(idx,taskLength).TrimEnd([|'*';')'|]), doc, new TextSpan(line.Span.Start+idx, taskLength)))
-                                else Debug.WriteLine($"Failed terminating criteria: idx= {idx}, len = {taskLength}, %A{ct}")
+                                    let taskText = lineTxt.Substring(idx,taskLength).TrimEnd([|'*';')'|])
+                                    let taskSpan = new TextSpan(line.Span.Start+idx, taskText.Length)
+                                    foundTaskItems.Add(new FSharpTaskListItem(d, taskText, doc, taskSpan))                          
                        
                 return foundTaskItems.ToImmutable()
             } 
-            |> Async.map (Option.defaultValue ImmutableArray.Empty)
-            |> RoslynHelpers.StartAsyncAsTask cancellationToken
