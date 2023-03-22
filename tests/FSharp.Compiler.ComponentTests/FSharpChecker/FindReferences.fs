cfc98745b3280b2ceb6aea42ee48d131c8d52dc1
majocha <majocha@outlook.com>
2023-03-22 19:27:19 +0100

    fix conflict

--- tests/FSharp.Compiler.ComponentTests/FSharpChecker/FindReferences.fs
+++ tests/FSharp.Compiler.ComponentTests/FSharpChecker/FindReferences.fs
@@ -201,12 +201,11 @@ let foo x = 5""" })
 [<Fact>]
 let ``We find values of a type that has been aliased`` () =
 
-    let project = SyntheticProject.Create("TypeAliasTest",
+    let project = SyntheticProject.Create(
         { sourceFile "First" [] with
             ExtraSource = "type MyInt = int32\n" +
                           "let myNum = 7"
-            SignatureFile = Custom ("module TypeAliasTest.ModuleFirst\n" +
-                                    "type MyInt = int32\n" +
+            SignatureFile = Custom ("type MyInt = int32\n" +
                                     "val myNum: MyInt") },
         { sourceFile "Second" [] with
             ExtraSource = "let goo x = ModuleFirst.myNum + x"})
@@ -250,12 +249,10 @@ type MyType() = class end
 
 let x = MyType()
 """
-    let project = SyntheticProject.Create(
+    SyntheticProject.Create(
         { sourceFile "Program" [] with
-            SignatureFile = Custom "module Moo"
-            Source = source } )
-
-    ProjectWorkflowBuilder(project) {
+            SignatureFile = Custom ""
+            Source = source } ).Workflow {
 
         placeCursor "Program" "MyType"
 
@@ -283,7 +280,7 @@ let x = MyType()
 """
     let project = SyntheticProject.Create(
         { sourceFile "Program" [] with
-            SignatureFile = Custom "module Moo"
+            SignatureFile = Custom ""
             Source = source } )
 
     ProjectWorkflowBuilder(project, useGetSource = true, useChangeNotifications = true) {
@@ -304,4 +301,3 @@ let x = MyType()
             "FileProgram.fs", 6, 8, 14
         ])
     }
-
