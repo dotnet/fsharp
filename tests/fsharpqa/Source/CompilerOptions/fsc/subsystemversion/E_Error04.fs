20d356ce06e9d5a22b7835683b5211e62959715d
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2023-03-27 14:16:16 +0200

    Cleanup

--- tests/fsharpqa/Source/CompilerOptions/fsc/subsystemversion/E_Error04.fs
+++ tests/fsharpqa/Source/CompilerOptions/fsc/subsystemversion/E_Error04.fs
@@ -1,4 +1,4 @@
-//<Expects status="error" id="FS1051">Invalid version '4,0' for '--subsystemversion'\. The version must be 4\.00 or greater\.$</Expects>
+//<Expects status="error" id="FS1051">Invalid version '4,0' for '--subsystemversion'\. The version must be 4\.00 or greater\.$</Expects>
 exit 0
 
 
