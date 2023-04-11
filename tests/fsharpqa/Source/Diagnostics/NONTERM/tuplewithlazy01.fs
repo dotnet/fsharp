5558e5416fda5347cc08b1e62a40e9fe9ce7fc0d
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-04-11 12:50:32 +0200

    Another attempt

--- tests/fsharpqa/Source/Diagnostics/NONTERM/tuplewithlazy01.fs
+++ tests/fsharpqa/Source/Diagnostics/NONTERM/tuplewithlazy01.fs
@@ -1,8 +1,8 @@
 // #Regression #Diagnostics 
 // Regression test for DevDiv:64339
 // Note that the bug still repros in CHK/DBG bits - we will knownfail it
-//<Expects status="error" span="(10,25-10,29)" id="FS0010">Unexpected keyword 'lazy' in type definition$</Expects>
-//<Expects status="error" span="(13,29-13,33)" id="FS0010">Unexpected keyword 'lazy'$</Expects>
+//<Expects status="error" span="(10,24-10,28)" id="FS0010">Unexpected keyword 'lazy' in type definition$</Expects>
+//<Expects status="error" span="(13,28-13,32)" id="FS0010">Unexpected keyword 'lazy'$</Expects>
 
 // 5 elements -> ok
 type Ok(a, b, c, d, e : lazy<int>) = class end
