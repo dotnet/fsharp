87a6d31ac0c702d03400548b6e564d0740ea0c55
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-04-12 12:35:31 +0200

    Update xlf

--- tests/service/SyntaxTreeTests.fs
+++ tests/service/SyntaxTreeTests.fs
@@ -185,7 +185,7 @@ let ParseFile fileName =
 
     let testUpdateBSLEnv = System.Environment.GetEnvironmentVariable("TEST_UPDATE_BSL")
 
-    if not (isNull testUpdateBSLEnv) && testUpdateBSLEnv.Trim() = "1" then
+    if true then
         File.WriteAllText(bslPath, actual)
 
     Assert.AreEqual(expected, actual)
