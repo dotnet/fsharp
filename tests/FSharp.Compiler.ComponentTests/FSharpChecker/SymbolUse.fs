18a5da64a73ba8342f1fb2d7616ed3550429f125
0101 <0101@innit.cz>
2023-04-14 16:48:56 +0200

    test adjustment

--- tests/FSharp.Compiler.ComponentTests/FSharpChecker/SymbolUse.fs
+++ tests/FSharp.Compiler.ComponentTests/FSharpChecker/SymbolUse.fs
@@ -19,10 +19,17 @@ module IsPrivateToFile =
     """
     let testFile = { sourceFile "Test" [] with Source = source }
 
+    let signature = $"""
+    let f: x:int -> int
+    let f2: {functionParameter}: int -> int
+    """
+
+    let testFileWithSignature = { testFile with SignatureFile = Custom signature }
+
     [<Fact>]
     let ``Function definition in signature file`` () =
         let project = SyntheticProject.Create(
-            testFile |> addSignatureFile,
+            testFileWithSignature,
             sourceFile "Second" [testFile.Id])
 
         project.Workflow {
@@ -66,14 +73,14 @@ module IsPrivateToFile =
 
     [<Fact>]
     let ``Function parameter, with signature file, part 1`` () =
-        SyntheticProject.Create(testFile |> addSignatureFile).Workflow {
+        SyntheticProject.Create(testFileWithSignature).Workflow {
             checkSymbolUse testFile.Id functionParameter (fun symbolUse ->
                 Assert.False(symbolUse.IsPrivateToFile))
         }
 
     [<Fact>]
     let ``Function parameter, with signature file, part 2`` () =
-        SyntheticProject.Create(testFile |> addSignatureFile).Workflow {
+        SyntheticProject.Create(testFileWithSignature).Workflow {
             checkSymbolUse testFile.Id functionParameter (fun symbolUse ->
                 Assert.True(symbolUse.IsPrivateToFileAndSignatureFile))
         }
@@ -82,83 +89,7 @@ module IsPrivateToFile =
     let ``Function parameter, with signature file, part 3`` () =
 
         for attempt in 1 .. 20 do
-            SyntheticProject.Create(testFile |> addSignatureFile).Workflow {
-                checkSymbolUse testFile.Id functionParameter (fun symbolUse ->
-
-                    let couldBeParameter, declarationLocation =
-                        match symbolUse.Symbol with
-                        | :? FSharpParameter as p -> true, Some p.DeclarationLocation
-                        | :? FSharpMemberOrFunctionOrValue as m when not m.IsModuleValueOrMember -> true, Some m.DeclarationLocation
-                        | _ -> false, None
-
-                    let thisIsSignature = SourceFileImpl.IsSignatureFile symbolUse.Range.FileName
-
-                    let argReprInfo =
-                        match symbolUse.Symbol.Item with
-                        | Item.Value v -> v.Deref.ArgReprInfoForDisplay
-                        | _ -> None
-
-                    let signatureLocation = argReprInfo |> Option.bind (fun a -> a.OtherRange)
-
-                    let diagnostics = $"Attempt: #{attempt} couldBeParameter: {couldBeParameter} \n declarationLocation: {declarationLocation} \n thisIsSignature: {thisIsSignature} \n signatureLocation: {signatureLocation} \n argReprInfo: {argReprInfo}"
-
-                    let result =
-                        couldBeParameter
-                        && (thisIsSignature
-                            || (signatureLocation.IsSome && signatureLocation <> declarationLocation))
-
-                    if not result then
-                        failwith diagnostics)
-            }
-            |> ignore
-
-    [<Fact>]
-    let ``Function parameter, with signature file, clear cache`` () =
-
-        for attempt in 1 .. 20 do
-            SyntheticProject.Create(testFile |> addSignatureFile).Workflow {
-
-                clearCache
-
-                checkSymbolUse testFile.Id functionParameter (fun symbolUse ->
-
-                    let couldBeParameter, declarationLocation =
-                        match symbolUse.Symbol with
-                        | :? FSharpParameter as p -> true, Some p.DeclarationLocation
-                        | :? FSharpMemberOrFunctionOrValue as m when not m.IsModuleValueOrMember -> true, Some m.DeclarationLocation
-                        | _ -> false, None
-
-                    let thisIsSignature = SourceFileImpl.IsSignatureFile symbolUse.Range.FileName
-
-                    let argReprInfo =
-                        match symbolUse.Symbol.Item with
-                        | Item.Value v -> v.Deref.ArgReprInfoForDisplay
-                        | _ -> None
-
-                    let signatureLocation = argReprInfo |> Option.bind (fun a -> a.OtherRange)
-
-                    let diagnostics = $"Attempt: #{attempt} couldBeParameter: {couldBeParameter} \n declarationLocation: {declarationLocation} \n thisIsSignature: {thisIsSignature} \n signatureLocation: {signatureLocation} \n argReprInfo: {argReprInfo}"
-
-                    let result =
-                        couldBeParameter
-                        && (thisIsSignature
-                            || (signatureLocation.IsSome && signatureLocation <> declarationLocation))
-
-                    if not result then
-                        failwith diagnostics)
-            }
-            |> ignore
-
-    [<Fact>]
-    let ``Function parameter, with signature file, don't auto-generate signature`` () =
-
-        let signature = $"""
-        val f: x: int -> int
-        val f2: {functionParameter}: int -> int
-        """
-        for attempt in 1 .. 20 do
-            SyntheticProject.Create({ testFile with SignatureFile = Custom signature }).Workflow {
-
+            SyntheticProject.Create(testFileWithSignature).Workflow {
                 checkSymbolUse testFile.Id functionParameter (fun symbolUse ->
 
                     let couldBeParameter, declarationLocation =
