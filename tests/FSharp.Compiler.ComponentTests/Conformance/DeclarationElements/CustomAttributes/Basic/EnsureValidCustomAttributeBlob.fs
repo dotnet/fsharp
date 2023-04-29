611f8f844a3c5a0b1d77d63d70c38d6e5c268326
KevinRansom <codecutter@hotmail.com>
2023-04-28 22:37:27 -0700

    testcase

--- tests/FSharp.Compiler.ComponentTests/Conformance/DeclarationElements/CustomAttributes/Basic/EnsureValidCustomAttributeBlob.fs
+++ tests/FSharp.Compiler.ComponentTests/Conformance/DeclarationElements/CustomAttributes/Basic/EnsureValidCustomAttributeBlob.fs
@@ -1,18 +1,6 @@
-// #Regression #Conformance #DeclarationElements #Attributes 
-#light
-
-// FSB 3565, verify typeof<_> and typedefof<_> can be used in
-// attributes. Also, verify that their behaivor is the same in
-// attributes as in normal usage.
-
 open System
 open System.Reflection
+open FSharp.Collections
 
-let runTest() = 
-    for a in typeof<List<int>>.GetConstructor().GetCustomAttributes() do
-        Console.WriteLine(a)
-    true
-
-// Actually run our test
-if runTest() <> true then failwith "Failed: 1"
-
+let attributes = typeof<List<int>>.GetConstructor(BindingFlags.NonPublic ||| BindingFlags.Instance, Unchecked.defaultof<Binder>, Type.EmptyTypes, [||]).GetCustomAttributes()
+for a in attributes do Console.WriteLine(a.ToString())
