02a490fa5a4d579b7271d2a17d114815ab2c3b79
nojaf <florian.verdonck@outlook.com>
2023-05-03 09:34:40 +0200

    Add static to SwallowResourceText signature member.

--- src/FSharp.Build/FSharpEmbedResourceText.fs
+++ src/FSharp.Build/FSharpEmbedResourceText.fs
@@ -370,7 +370,7 @@ open Printf
     let StringBoilerPlateSignature =
         "    // BEGIN BOILERPLATE
     /// If set to true, then all error messages will just return the filled 'holes' delimited by ',,,'s - this is for language-neutral testing (e.g. localization-invariant baselines).
-    member SwallowResourceText: bool with get, set
+    static member SwallowResourceText: bool with get, set
     // END BOILERPLATE"
 
     let generateResxAndSource (fileName: string) =
