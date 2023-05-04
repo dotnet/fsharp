b0321f8c706847cca54329ddac751433d7af7658
Adam Boniecki <adboniec@microsoft.com>
2023-05-04 12:17:58 +0200

    Fix more tests

--- tests/FSharp.Compiler.ComponentTests/CompilerOptions/fsc/checked/checked.fs
+++ tests/FSharp.Compiler.ComponentTests/CompilerOptions/fsc/checked/checked.fs
@@ -146,7 +146,7 @@ module Checked =
         |> compile
         |> shouldFail
         |> withDiagnostics [
-            (Error 243, Line 0, Col 1, Line 0, Col 1, "Unrecognized option: '--Checked'")
+            (Error 243, Line 0, Col 1, Line 0, Col 1, "Unrecognized option: '--Checked'. Use '--help' to learn about recognized command line options.")
         ]
 
     //  SOURCE=unrecogarg.fs  SCFLAGS="--checked*"  # fsc--checked*
