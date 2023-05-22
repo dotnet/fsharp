709563eeff0283c50677190ef8cfefeba2f5aba2
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2023-05-22 17:09:53 +0200

    Update CancellableTasks.fs

--- vsintegration/src/FSharp.Editor/Common/CancellableTasks.fs
+++ vsintegration/src/FSharp.Editor/Common/CancellableTasks.fs
@@ -1,5 +1,25 @@
 // Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+//
+// Implementation is taken from IcedTasks (https://github.com/TheAngryByrd/IcedTasks/blob/72638c8719014ae963f2662449c99f87090041d1/LICENSE.md?plain=1#L1-L21), under MIT license
+// Which was originally written in 2016 by Robert Peele (humbobst@gmail.com)
+// New operator-based overload resolution for F# 4.0 compatibility by Gustavo Leon in 2018.
+// Revised for insertion into FSharp.Core by Microsoft, 2019.
+// Revised to implement CancellationToken semantics
+//
+// Original notice:
+// To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights
+// to this software to the public domain worldwide. This software is distributed without any warranty.
+// IcedTasks MIT notice (https://github.com/TheAngryByrd/IcedTasks/blob/72638c8719014ae963f2662449c99f87090041d1/LICENSE.md?plain=1#L1-L21):
+// MIT License
 
+// Copyright (c) [year] [fullname]
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
 namespace Microsoft.VisualStudio.FSharp.Editor
 
 // Don't warn about the resumable code invocation
