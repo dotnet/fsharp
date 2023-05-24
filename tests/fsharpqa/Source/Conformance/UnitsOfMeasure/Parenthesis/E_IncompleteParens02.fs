ec4bba30d1f2630c9f3dabddc0cfbf59933fc483
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-05-24 12:42:04 +0200

    Try to update baselines

--- tests/fsharpqa/Source/Conformance/UnitsOfMeasure/Parenthesis/E_IncompleteParens02.fs
+++ tests/fsharpqa/Source/Conformance/UnitsOfMeasure/Parenthesis/E_IncompleteParens02.fs
@@ -1,7 +1,8 @@
 // #Regression #Conformance #UnitsOfMeasure 
 // Regression test for FSHARP1.0:2662
 // Make sure we can use ( and ) in Units of Measure
-//<Expects status="error" span="(10,26-10,27)" id="FS0010">Unexpected symbol '\)' in expression$</Expects>
+//<Expects status="error" span="(11,26-11,27)" id="FS0010">Unexpected symbol '\)' in binding$</Expects>
+//<Expects status="error" span="(11,24-11,25)" id="FS0010">Unexpected token '/' or incomplete expression</Expects>
 
 [<Measure>] type Kg
 [<Measure>] type m
