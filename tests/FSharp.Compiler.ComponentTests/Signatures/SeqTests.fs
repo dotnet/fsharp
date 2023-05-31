7c21316aa4de4015fce735194b0c39881345f7f1
nojaf <florian.verdonck@outlook.com>
2023-05-31 13:26:12 +0200

    Moar baselines

--- tests/FSharp.Compiler.ComponentTests/Signatures/SeqTests.fs
+++ tests/FSharp.Compiler.ComponentTests/Signatures/SeqTests.fs
@@ -8,3 +8,9 @@ let ``int seq`` () =
     assertSingleSignatureBinding
         "let s = seq { yield 1 }"
         "val s: int seq"
+
+[<Fact>]
+let ``tuple seq`` () =
+    assertSingleSignatureBinding
+        "let s = seq { yield (1, 'b', 2.) }"
+        "val s: (int * char * float) seq"
