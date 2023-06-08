63842852a7793fe6614d4f78fba111eb8e0d3bf9
Petr <psfinaki@users.noreply.github.com>
2023-06-08 17:56:20 +0200

    Democratizing code fixes - part one

---
+++ vsintegration/tests/FSharp.Editor.Tests/CodeFixes/CodeFixTestFramework.fs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+module FSharp.Editor.Tests.CodeFixes.CodeFixTestFramework
+
+open System.Threading
+
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.Text
+open Microsoft.VisualStudio.FSharp.Editor
+open Microsoft.VisualStudio.FSharp.Editor.CancellableTasks
+
+open FSharp.Editor.Tests.Helpers
+
+type TestCodeFix = { Title: string; FixedCode: string }
+
+let getRelevantDiagnostic (document: Document) errorNumber =
+    cancellableTask {
+        let! _, checkFileResults = document.GetFSharpParseAndCheckResultsAsync "test"
+
+        return
+            checkFileResults.Diagnostics
+            |> Seq.where (fun d -> d.ErrorNumber = errorNumber)
+            |> Seq.exactlyOne
+    }
+
+let fix (code: string) diagnostic (fixProvider: IFSharpCodeFix) =
+    cancellableTask {
+        let sourceText = SourceText.From code
+        let document = RoslynTestHelpers.GetFsDocument code
+
+        let! diagnostic = getRelevantDiagnostic document diagnostic
+
+        let diagnosticSpan =
+            RoslynHelpers.FSharpRangeToTextSpan(sourceText, diagnostic.Range)
+
+        let! title, changes = fixProvider.GetChangesAsync document diagnosticSpan
+        let fixedSourceText = sourceText.WithChanges changes
+
+        return
+            {
+                Title = title
+                FixedCode = fixedSourceText.ToString()
+            }
+    }
+    |> CancellableTask.start CancellationToken.None
+    |> fun task -> task.Result
