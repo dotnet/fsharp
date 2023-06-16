83cc0e4f5a958da008706f2448fe3ee3036df145
Tomas Grosup <tomasgrosup@microsoft.com>
2023-06-16 20:49:47 +0200

    internalerror with MakeValueAssign fixed?

--- tests/FSharp.Compiler.ComponentTests/Language/RegressionTests.fs
+++ tests/FSharp.Compiler.ComponentTests/Language/RegressionTests.fs
@@ -19,3 +19,13 @@ type TestItemSeq =
         |> compile
         |> withErrorCodes [39]
         |> ignore
+
+
+    [<Fact>]
+    let ``Member val regression - not allowed without primary constructor``  () = 
+        Fs """module Test
+    type Bad3 = 
+        member val X = 1 + 1   """     
+        |> typecheck
+        |> shouldFail
+        |> withDiagnostics []
