c3031c268b45b4e77ca5895844c07845a455510e
Petr <psfinaki@users.noreply.github.com>
2023-06-16 14:39:20 +0200

    add tests

---
+++ vsintegration/tests/FSharp.Editor.Tests/CodeFixes/ChangeEqualsInFieldTypeToColonTests.fs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+module FSharp.Editor.Tests.CodeFixes.ChangeEqualsInFieldTypeToColonTests
+
+open Microsoft.VisualStudio.FSharp.Editor
+open Xunit
+
+open CodeFixTestFramework
+
+let private codeFix : IFSharpCodeFixProvider = failwith "" // AddInstanceMemberParameterCodeFixProvider()
+let private diagnostic = 0010 // Unexpected symbol...
+
+[<Fact>]
+let ``Fixes FS0010`` () =
+    let code = 
+        """
+type Band = { Name = string }
+"""
+
+    let expected = 
+        Some 
+            {
+                Message = "Use ':' for type in field declaration"
+                FixedCode = """
+type Band = { Name : string }
+"""
+            }
+
+    let actual = codeFix |> tryFix code diagnostic
+
+    Assert.Equal(expected, actual)
