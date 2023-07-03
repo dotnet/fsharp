9e3b1e858593eb438788e8204eb6c1fb584ff44b
Petr <psfinaki@users.noreply.github.com>
2023-07-03 23:36:15 +0200

    Fixing code fixes, part 4

---
+++ vsintegration/tests/FSharp.Editor.Tests/CodeFixes/UseTripleQuotedInterpolationTests.fs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+module FSharp.Editor.Tests.CodeFixes.UseTripleQuotedInterpolationTests
+
+open Microsoft.VisualStudio.FSharp.Editor
+open Xunit
+
+open CodeFixTestFramework
+
+let private codeFix = UseTripleQuotedInterpolationCodeFixProvider()
+let private diagnostic = 3373 // ... invalid interpolated string ...
+
+[<Fact>]
+let ``Fixes FS3373`` () =
+    let code =
+        """
+let answer = $"{"42"}"
+"""
+
+    let expected =
+        Some
+            {
+                Message = "Use triple quoted string interpolation."
+                // yeah... because I can't understand how to
+                // put a triple-quoted string inside a triple-quoted string
+                // but this is what we need
+                FixedCode =
+                    """
+let answer = $"""
+                    + "\"\"\""
+                    + """{"42"}"""
+                    + "\"\"\""
+                    + """
+"""
+            }
+
+    let actual = codeFix |> tryFix code diagnostic
+
+    Assert.Equal(expected, actual)
