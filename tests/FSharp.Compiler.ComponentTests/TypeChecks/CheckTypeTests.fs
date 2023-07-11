db0f90121cdd9bfb62e64ceaa5aa1c714830705f
nojaf <florian.verdonck@outlook.com>
2023-07-11 17:45:11 +0200

    Initial recovery for function types in union case field.

---
+++ tests/FSharp.Compiler.ComponentTests/TypeChecks/CheckTypeTests.fs
@@ -0,0 +1,15 @@
+﻿module TypeChecks.CheckTypeTests
+
+open Xunit
+open FSharp.Test.Compiler
+
+[<Fact>]
+let ``Recursive type with attribute`` () =
+    FSharp """
+namespace Foo
+
+type Bar = | Bar of int -> int
+type Other = int
+"""
+    |> typecheck
+    |> withSingleDiagnostic (Error 3578, Line 4, Col 21, Line 4, Col 31, "Unexpected function type in union case field definition")
