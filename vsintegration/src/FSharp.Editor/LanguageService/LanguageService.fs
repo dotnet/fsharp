cb05a62da514fb4bfc76511f6d9751fedb895312
Tomas Grosup <tomasgrosup@microsoft.com>
2023-07-12 17:13:27 +0200

    Add in optional setting for solution-wide background analysis

--- vsintegration/src/FSharp.Editor/LanguageService/LanguageService.fs
+++ vsintegration/src/FSharp.Editor/LanguageService/LanguageService.fs
@@ -423,28 +423,23 @@ type internal FSharpLanguageService(package: FSharpPackage) =
     override _.Initialize() =
         base.Initialize()
 
-        let globalOptions =
-            package
-                .ComponentModel
-                .DefaultExportProvider
-                .GetExport<FSharpGlobalOptions>()
-                .Value
+        let exportProvider = package.ComponentModel.DefaultExportProvider
+        let globalOptions = exportProvider.GetExport<FSharpGlobalOptions>().Value
 
-        globalOptions.BlockForCompletionItems <- false
-        globalOptions.SetBackgroundAnalysisScope(openFilesOnly = true)
+        let workspace = package.ComponentModel.GetService<VisualStudioWorkspace>()
 
-        let globalOptions =
-            package
-                .ComponentModel
-                .DefaultExportProvider
-                .GetExport<FSharpGlobalOptions>()
-                .Value
+        let solutionAnalysis =
+            workspace
+                .Services
+                .GetService<EditorOptions>()
+                .Advanced
+                .SolutionBackgroundAnalysis
+
+        globalOptions.SetBackgroundAnalysisScope(openFilesOnly = not solutionAnalysis)
 
         globalOptions.BlockForCompletionItems <- false
 
-        let theme =
-            package.ComponentModel.DefaultExportProvider.GetExport<ISetThemeColors>().Value
-
+        let theme = exportProvider.GetExport<ISetThemeColors>().Value
         theme.SetColors()
 
     override _.ContentTypeName = FSharpConstants.FSharpContentTypeName
