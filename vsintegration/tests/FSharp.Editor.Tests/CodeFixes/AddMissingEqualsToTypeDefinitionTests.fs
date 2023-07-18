ce37105b935adadf26fc84db3b405407489c55fe
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-07-18 13:03:56 +0200

    Parser: more unfinished type recovery (#15585)
    * Parser: rework missing type body recovery
    
    * Update baselines
    
    * Adjust the code fix
    
    * nah baselines
    
    * Remove error declaration
    
    ---------
    
    Co-authored-by: Petr <psfinaki@users.noreply.github.com>

--- vsintegration/tests/FSharp.Editor.Tests/CodeFixes/AddMissingEqualsToTypeDefinitionTests.fs
+++ vsintegration/tests/FSharp.Editor.Tests/CodeFixes/AddMissingEqualsToTypeDefinitionTests.fs
@@ -8,10 +8,10 @@ open Xunit
 open CodeFixTestFramework
 
 let private codeFix = AddMissingEqualsToTypeDefinitionCodeFixProvider()
-let private diagnostic = 3360 // Unexpected token in type def...
+let private diagnostic = 0010 // Unexpected symbol...
 
 [<Fact>]
-let ``Fixes FS0360 for missing equals in type def - simple types`` () =
+let ``Fixes FS0010 for missing equals in type def - simple types`` () =
     let code =
         """
 type Song { Artist : string; Title : int }
@@ -32,7 +32,7 @@ type Song = { Artist : string; Title : int }
     Assert.Equal(expected, actual)
 
 [<Fact>]
-let ``Fixes FS0360 for missing equals in type def - records`` () =
+let ``Fixes FS0010 for missing equals in type def - records`` () =
     let code =
         """
 type Name Name of string
@@ -52,19 +52,10 @@ type Name = Name of string
 
     Assert.Equal(expected, actual)
 
-[<Fact>]
-// apparently this throws the same error hah
-let ``Doesn't fix FS0360 for invalid interface defs`` () =
-    let code =
-        """
-type IA<'b> = 
-    abstract Foo : int -> int
-
-type IB<'b> = 
-    inherit IA<'b>
-    inherit IA<int>
-"""
-
+[<Theory>]
+[<InlineData "type X = open">]
+[<InlineData "=">]
+let ``Doesn't fix FS0010 for random unexpected symbols`` code =
     let expected = None
 
     let actual = codeFix |> tryFix code diagnostic
