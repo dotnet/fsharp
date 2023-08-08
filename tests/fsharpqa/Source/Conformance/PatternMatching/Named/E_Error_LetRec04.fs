f8d93b0ef6e5e1ff761a5b060ed80b6d6e37975f
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2023-08-08 13:15:16 +0200

    Remove old tests

--- tests/fsharpqa/Source/Conformance/PatternMatching/Named/E_Error_LetRec04.fs
+++
@@ -1,5 +0,0 @@
-// #Regression #Conformance #PatternMatching #ActivePatterns 
-// Regression test for FSHARP1.0:4621
-//<Expects span="(4,9-4,29)" status="error" id="FS0001">This expression was expected to have type.    ''a option'    .but here has type.    'string'</Expects>
-let rec (|Foo3|_|) (a:int) x = "BAD DOG!"
-
--- tests/fsharpqa/Source/Conformance/PatternMatching/Named/activePatterns06.fs
+++
@@ -1,3 +0,0 @@
-// #Conformance #PatternMatching #ActivePatterns 
-#light
-
