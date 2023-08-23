0d7ba212e9f77dcc3eddd151f973dec530d7b3ff
Florian Verdonck <florian.verdonck@outlook.com>
2023-08-23 16:48:16 +0200

    Backport #15845 to 17.7

--- src/Compiler/Checking/CheckDeclarations.fs
+++ src/Compiler/Checking/CheckDeclarations.fs
@@ -3958,8 +3958,10 @@ module TcDeclarations =
                     if tcref.TyparsNoRange.Length = synTypars.Length then
                         (tcref.TyparsNoRange, synTypars)
                         ||> List.zip
-                        |> List.iter (fun (typar, SynTyparDecl.SynTyparDecl(_, SynTypar(ident = untypedIdent))) ->
-                            typar.SetIdent(untypedIdent)
+                        |> List.iter (fun (typar, SynTyparDecl.SynTyparDecl (_, typar = tp)) ->
+                            let (SynTypar(ident = untypedIdent)) = tp
+                            if typar.StaticReq = sr then
+                                typar.SetIdent(untypedIdent)
                         )
 
                     res
