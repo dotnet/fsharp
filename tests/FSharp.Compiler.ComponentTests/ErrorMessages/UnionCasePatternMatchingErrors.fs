247d130a98de369409acf56454c5daec6b9dea0f
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2023-08-27 11:26:05 +0200

    Fix regression for FS0725

--- tests/FSharp.Compiler.ComponentTests/ErrorMessages/UnionCasePatternMatchingErrors.fs
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/UnionCasePatternMatchingErrors.fs
@@ -85,9 +85,8 @@ let myVal =
     |> withLangVersionPreview
     |> typecheck
     |> shouldFail
-    |> withSingleDiagnostic (Warning 3548, Line 9, Col 7, Line 9, Col 10, "Pattern discard is not allowed for union case that takes no data.")
-    
-      
+    |> withSingleDiagnostic (Warning 3548, Line 9, Col 9, Line 9, Col 10, "Pattern discard is not allowed for union case that takes no data.")
+ 
 [<Fact>]
 let ``Union Pattern discard allowed for union case that takes no data with Lang version 7`` () =
      FSharp """
@@ -132,7 +131,7 @@ let myVal =
     |> withLangVersionPreview
     |> typecheck
     |> shouldFail
-    |> withSingleDiagnostic (Warning 3548, Line 9, Col 7, Line 9, Col 10, "Pattern discard is not allowed for union case that takes no data.")
+    |> withSingleDiagnostic (Warning 3548, Line 9, Col 9, Line 9, Col 10, "Pattern discard is not allowed for union case that takes no data.")
     
 [<Fact>]
 let ``Pattern discard not allowed for union case that takes no data with Lang preview`` () =
@@ -153,7 +152,7 @@ let myVal =
     |> withLangVersionPreview
     |> typecheck
     |> shouldFail
-    |> withSingleDiagnostic (Warning 3548, Line 12, Col 7, Line 12, Col 10, "Pattern discard is not allowed for union case that takes no data.")
+    |> withSingleDiagnostic (Warning 3548, Line 12, Col 9, Line 12, Col 10, "Pattern discard is not allowed for union case that takes no data.")
 
 [<Fact>]
 let ``Pattern function discard not allowed for union case that takes no data with Lang preview`` () =
@@ -174,7 +173,7 @@ let myVal =
     |> withLangVersionPreview
     |> typecheck
     |> shouldFail
-    |> withSingleDiagnostic (Warning 3548, Line 12, Col 7, Line 12, Col 10, "Pattern discard is not allowed for union case that takes no data.")
+    |> withSingleDiagnostic (Warning 3548, Line 12, Col 9, Line 12, Col 10, "Pattern discard is not allowed for union case that takes no data.")
     
 [<Fact>]
 let ``Pattern discard allowed for union case that takes no data with Lang version 7`` () =
@@ -215,7 +214,7 @@ let myVal =
     |> withLangVersionPreview
     |> typecheck
     |> shouldFail
-    |> withSingleDiagnostic (Warning 3548, Line 12, Col 7, Line 12, Col 10, "Pattern discard is not allowed for union case that takes no data.")
+    |> withSingleDiagnostic (Warning 3548, Line 12, Col 9, Line 12, Col 10, "Pattern discard is not allowed for union case that takes no data.")
 
 [<Fact>]
 let ``Multiple pattern discards not allowed for union case that takes no data with Lang preview`` () =
@@ -241,8 +240,8 @@ let myVal =
     |> typecheck
     |> shouldFail
     |> withDiagnostics [
-        (Warning 3548, Line 16, Col 7, Line 16, Col 10, "Pattern discard is not allowed for union case that takes no data.")
-        (Warning 3548, Line 17, Col 20, Line 17, Col 23, "Pattern discard is not allowed for union case that takes no data.")
+        (Warning 3548, Line 16, Col 9, Line 16, Col 10, "Pattern discard is not allowed for union case that takes no data.")
+        (Warning 3548, Line 17, Col 22, Line 17, Col 23, "Pattern discard is not allowed for union case that takes no data.")
     ]
     
 [<Fact>]
@@ -269,8 +268,8 @@ let myVal =
     |> typecheck
     |> shouldFail
     |> withDiagnostics [
-        (Warning 3548, Line 16, Col 7, Line 16, Col 10, "Pattern discard is not allowed for union case that takes no data.")
-        (Warning 3548, Line 17, Col 20, Line 17, Col 23, "Pattern discard is not allowed for union case that takes no data.")
+        (Warning 3548, Line 16, Col 9, Line 16, Col 10, "Pattern discard is not allowed for union case that takes no data.")
+        (Warning 3548, Line 17, Col 22, Line 17, Col 23, "Pattern discard is not allowed for union case that takes no data.")
     ]
     
 [<Fact>]
@@ -283,6 +282,18 @@ let myDiscardedArgFunc(A _) = 5+5"""
     |> withLangVersion70
     |> typecheck
     |> shouldSucceed
+    
+[<Fact>]
+let ``Pattern discard not allowed for single-case unions when using them as a deconstruct syntax in functions  with Lang preview`` () =
+     FSharp """
+module Tests
+type MyWrapper = A
+
+let myDiscardedArgFunc(A _) = 5+5"""
+    |> withLangVersionPreview
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Warning 3548, Line 5, Col 26, Line 5, Col 27, "Pattern discard is not allowed for union case that takes no data.")
 
 [<Fact>]
 let ``Pattern named not allowed for single-case unions when using them as a deconstruct syntax in functions  with Lang 7`` () =
@@ -294,9 +305,54 @@ let myFunc(A a) = 5+5"""
     |> withLangVersion70
     |> typecheck
     |> shouldFail
-    |> withDiagnostics [
-        (Error 725, Line 5, Col 12, Line 5, Col 15, "This union case does not take arguments")
-    ]
+    |> withSingleDiagnostic (Error 725, Line 5, Col 12, Line 5, Col 15, "This union case does not take arguments")
+    
+[<Fact>]
+let ``Pattern named not allowed for single-case unions when using them as a deconstruct syntax in functions with Lang preview`` () =
+     FSharp """
+module Tests
+type MyWrapper = A
+
+let myFunc(A a) = 5+5"""
+    |> withLangVersionPreview
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Warning 3548, Line 5, Col 14, Line 5, Col 15, "Pattern discard is not allowed for union case that takes no data.")
+    
+[<Fact>]
+let ``Pattern named not allowed for unions cases with no data and using them as a deconstruct syntax in functions Lang 7`` () =
+     FSharp """
+module Tests
+    let myDiscardedArgFunc(None x y) = None"""
+    |> withLangVersion70
+    |> withOptions ["--nowarn:25"]
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 3, Col 28, Line 3, Col 36, "This union case does not take arguments")
+
+[<Fact>]
+let ``Pattern named not allowed for unions cases with no data and using them as a deconstruct syntax in functions Lang preview`` () =
+     FSharp """
+module Tests
+    let myDiscardedArgFunc(None x y) = None"""
+    |> withLangVersionPreview
+    |> withOptions ["--nowarn:25"]
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 3, Col 28, Line 3, Col 36, "This union case does not take arguments")
+    
+[<Fact>]
+let ``Pattern discard or named are not allowed for single-case union case that takes no data with Lang 7`` () =
+     FSharp """
+module Tests
+type MyWrapper = A
+
+let myFunc(A a) = 5+5
+let myDiscardedArgFunc(A _) = 5+5"""
+    |> withLangVersion70
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 5, Col 12, Line 5, Col 15, "This union case does not take arguments")
 
 [<Fact>]
 let ``Pattern discard or named are not allowed for single-case union case that takes no data with Lang preview`` () =
@@ -310,6 +366,192 @@ let myDiscardedArgFunc(A _) = 5+5"""
     |> typecheck
     |> shouldFail
     |> withDiagnostics [
-        (Warning 3548, Line 5, Col 12, Line 5, Col 15, "Pattern discard is not allowed for union case that takes no data.")
-        (Warning 3548, Line 6, Col 24, Line 6, Col 27, "Pattern discard is not allowed for union case that takes no data.")
+        (Warning 3548, Line 5, Col 14, Line 5, Col 15, "Pattern discard is not allowed for union case that takes no data.")
+        (Warning 3548, Line 6, Col 26, Line 6, Col 27, "Pattern discard is not allowed for union case that takes no data.")
+    ]
+    
+[<Fact>]
+let ``Pattern discard not allowed union case does not take discarded arguments with Lang 7`` () =
+     FSharp """
+module Tests
+    type A = | A
+    let a = A
+    match a with
+    | A _ _ -> ()"""
+    |> withLangVersion70
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 6, Col 7, Line 6, Col 12, "This union case does not take arguments")
+    
+[<Fact>]
+let ``Pattern discard not allowed union case does not take discarded arguments with Lang preview`` () =
+     FSharp """
+module Tests
+    type A = | A
+    let a = A
+    match a with
+    | A _ _ -> ()"""
+    |> withLangVersionPreview
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 6, Col 7, Line 6, Col 12, "This union case does not take arguments")
+    
+[<Fact>]
+let ``Pattern named not allowed union case does not take arguments with Lang 7`` () =
+     FSharp """
+module Tests
+    match None with
+    | None x y -> ()
+    | Some _ -> ()"""
+    |> withLangVersion70
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 4, Col 7, Line 4, Col 15, "This union case does not take arguments")
+    
+[<Fact>]
+let ``Pattern named not allowed union case does not take arguments with Lang preview`` () =
+     FSharp """
+module Tests
+    match None with
+    | None x y -> ()
+    | Some _ -> ()"""
+    |> withLangVersionPreview
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 4, Col 7, Line 4, Col 15, "This union case does not take arguments")
+    
+[<Fact>]
+let ``Pattern named not allowed union case does not take arguments wrapped with parens with Lang preview`` () =
+     FSharp """
+module Tests
+    match None with
+    | None (x, y) -> ()
+    | Some _ -> ()"""
+    |> withLangVersionPreview
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 4, Col 7, Line 4, Col 18, "This union case does not take arguments")
+    
+[<Fact>]
+let ``Pattern named not allowed union case does not take arguments wrapped with parens with Lang 7`` () =
+     FSharp """
+module Tests
+    match None with
+    | None (x, y) -> ()
+    | Some _ -> ()"""
+    |> withLangVersion70
+    |> typecheck
+    |> shouldFail
+    |> withSingleDiagnostic (Error 725, Line 4, Col 7, Line 4, Col 18, "This union case does not take arguments")
+
+[<Fact>]
+let ``Pattern named not allowed union case does not take any arguments with Lang 7`` () =
+     FSharp """
+module Tests
+    type A = { X: int }
+    type B = B of int
+    match None with
+    | None 1 -> ()
+    
+    match None with
+    | None (1, 2) -> ()
+    
+    match None with
+    | None [] -> ()
+    
+    match None with
+    | None [||] -> ()
+    
+    match None with
+    | None { X = 1 } -> ()
+    
+    match None with
+    | None (B 1) -> ()
+    
+    match None with
+    | None (x, y) -> ()
+    
+    match None with
+    | None false -> ()
+    
+    match None with
+    | None _ -> ()
+    
+    type C = | C
+    
+    let myDiscardedArgFunc(C _) = ()
+    
+    let myDiscardedArgFunc2(C c) = ()
+    """
+    |> withLangVersion70
+    |> withOptions ["--nowarn:25"]
+    |> typecheck
+    |> shouldFail
+    |> withDiagnostics [
+        (Error 725, Line 6, Col 7, Line 6, Col 13, "This union case does not take arguments")
+        (Error 725, Line 9, Col 7, Line 9, Col 18, "This union case does not take arguments")
+        (Error 725, Line 12, Col 7, Line 12, Col 14, "This union case does not take arguments")
+        (Error 725, Line 15, Col 7, Line 15, Col 16, "This union case does not take arguments")
+        (Error 725, Line 18, Col 7, Line 18, Col 21, "This union case does not take arguments")
+        (Error 725, Line 21, Col 7, Line 21, Col 17, "This union case does not take arguments")
+        (Error 725, Line 24, Col 7, Line 24, Col 18, "This union case does not take arguments")
+        (Error 725, Line 27, Col 7, Line 27, Col 17, "This union case does not take arguments")
+        (Error 725, Line 36, Col 29, Line 36, Col 32, "This union case does not take arguments")
+    ]
+    
+[<Fact>]
+let ``Pattern named not allowed union case does not take any arguments with Lang preview`` () =
+     FSharp """
+module Tests
+    type A = { X: int }
+    type B = | B of int
+    match None with
+    | None 1 -> ()
+    
+    match None with
+    | None (1, 2) -> ()
+    
+    match None with
+    | None [] -> ()
+    
+    match None with
+    | None [||] -> ()
+    
+    match None with
+    | None { X = 1 } -> ()
+    
+    match None with
+    | None (B 1) -> ()
+    
+    match None with
+    | None (x, y) -> ()
+    
+    match None with
+    | None false -> ()
+    
+    match None with
+    | None _ -> ()
+    
+    type C = | C
+    
+    let myDiscardedArgFunc(C _) = ()
+    
+    let myDiscardedArgFunc2(C c) = ()
+    """
+    |> withLangVersionPreview
+    |> withOptions ["--nowarn:25"]
+    |> typecheck
+    |> shouldFail
+    |> withDiagnostics [
+        (Error 725, Line 6, Col 7, Line 6, Col 13, "This union case does not take arguments")
+        (Error 725, Line 9, Col 7, Line 9, Col 18, "This union case does not take arguments")
+        (Error 725, Line 12, Col 7, Line 12, Col 14, "This union case does not take arguments")
+        (Error 725, Line 15, Col 7, Line 15, Col 16, "This union case does not take arguments")
+        (Error 725, Line 18, Col 7, Line 18, Col 21, "This union case does not take arguments")
+        (Error 725, Line 21, Col 7, Line 21, Col 17, "This union case does not take arguments")
+        (Error 725, Line 24, Col 7, Line 24, Col 18, "This union case does not take arguments")
+        (Error 725, Line 27, Col 7, Line 27, Col 17, "This union case does not take arguments")
+        (Warning 3548, Line 30, Col 12, Line 30, Col 13, "Pattern discard is not allowed for union case that takes no data.")
+        (Warning 3548, Line 34, Col 30, Line 34, Col 31, "Pattern discard is not allowed for union case that takes no data.")
+        (Warning 3548, Line 36, Col 31, Line 36, Col 32, "Pattern discard is not allowed for union case that takes no data.")
     ]
