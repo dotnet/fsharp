c5fa3bda18626bcd96f38893d89501f3498973c2
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2023-09-13 15:09:52 +0200

    Update prebuilts + trimming tests

--- tests/AheadOfTime/Trimming/check.ps1
+++ tests/AheadOfTime/Trimming/check.ps1
@@ -37,7 +37,7 @@ function CheckTrim($root, $tfm, $outputfile, $expected_len) {
 
 
 # Check net7.0 trimmed assemblies
-CheckTrim -root "SelfContained_Trimming_Test" -tfm "net7.0" -outputfile "FSharp.Core.dll" -expected_len 287744
+CheckTrim -root "SelfContained_Trimming_Test" -tfm "ne8.0" -outputfile "FSharp.Core.dll" -expected_len 287744
 
 # Check net7.0 trimmed assemblies
-CheckTrim -root "StaticLinkedFSharpCore_Trimming_Test" -tfm "net7.0" -outputfile "StaticLinkedFSharpCore_Trimming_Test.dll" -expected_len 8821248
+CheckTrim -root "StaticLinkedFSharpCore_Trimming_Test" -tfm "net8.0" -outputfile "StaticLinkedFSharpCore_Trimming_Test.dll" -expected_len 8821248
