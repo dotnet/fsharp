1009d6f656179016f56f7a58f5b8c34e82476275
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2023-10-08 16:39:48 +0200

    Adjust test: Error msg now contains number of invalid chars

--- tests/fsharpqa/Source/Conformance/LexicalAnalysis/StringsAndCharacters/E_ByteStrUnicodeChar01.fs
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/StringsAndCharacters/E_ByteStrUnicodeChar01.fs
@@ -4,7 +4,7 @@
 // Verify error when trying to take the byte string of 
 // a string literal with unicode characters
 
-//<Expects id="FS1140" status="error">This byte array literal contains characters that do not encode as a single byte</Expects>
+//<Expects id="FS1140" status="error">This byte array literal contains 1 characters that do not encode as a single byte</Expects>
 
 let _ = "\u2660"B
 
