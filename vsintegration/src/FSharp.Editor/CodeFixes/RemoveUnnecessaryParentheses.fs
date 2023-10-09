591c5df89c2dd1a1fe2def53548a27478eebbb90
Brian Rourke Boll <brianrourkeboll@gmail.com>
2023-10-09 10:22:45 -0400

    Don't need that here

--- vsintegration/src/FSharp.Editor/CodeFixes/RemoveUnnecessaryParentheses.fs
+++ vsintegration/src/FSharp.Editor/CodeFixes/RemoveUnnecessaryParentheses.fs
@@ -32,19 +32,6 @@ type ReadOnlySpanExtensions =
                 i <- i + 1
 
         if found then i else -1
-
-    [<Extension>]
-    static member IndexOfAnyExcept(span: ReadOnlySpan<char>, values: ReadOnlySpan<char>) =
-        let mutable i = 0
-        let mutable found = false
-
-        while not found && i < span.Length do
-            if values.IndexOf span[i] < 0 then
-                found <- true
-            else
-                i <- i + 1
-
-        if found then i else -1
 #endif
 
 [<AutoOpen>]
