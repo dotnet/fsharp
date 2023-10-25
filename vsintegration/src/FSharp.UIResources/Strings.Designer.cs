be6352d1b6325ca93edd8487b19b333a81545201
Petr Pokorny <petrpokorny@microsoft.com>
2023-10-25 14:20:40 +0200

    Enable Live Buffers by default (#16176)

--- vsintegration/src/FSharp.UIResources/Strings.Designer.cs
+++ vsintegration/src/FSharp.UIResources/Strings.Designer.cs
@@ -178,7 +178,7 @@ namespace Microsoft.VisualStudio.FSharp.UIResources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Use live (unsaved) buffers for checking (restart required).
+        ///   Looks up a localized string similar to Use live (unsaved) buffers for analysis (restart required).
         /// </summary>
         public static string Enable_Live_Buffers {
             get {
@@ -331,7 +331,7 @@ namespace Microsoft.VisualStudio.FSharp.UIResources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Live Buffers (experimental).
+        ///   Looks up a localized string similar to Live Buffers.
         /// </summary>
         public static string LiveBuffers {
             get {
