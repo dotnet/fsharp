eb73c06d6a30b838084677db76c91e4e23690d73
dawe <dawedawe@posteo.de>
2023-11-03 18:15:37 +0100

    format

--- tests/FSharp.Compiler.ComponentTests/ErrorMessages/IndexingSyntax.fs
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/IndexingSyntax.fs
@@ -22,15 +22,21 @@ namespace N
         |> withLangVersion70
         |> compile
         |> shouldFail
-        |> withResults [
-            { Error = Information 3365
-              Range = { StartLine = 10
-                        StartColumn = 20
-                        EndLine = 10
-                        EndColumn = 24 }
-              Message =
-               "The syntax 'expr1[expr2]' is used for indexing. Consider adding a type annotation to enable indexing, or if calling a function add a space, e.g. 'expr1 [expr2]'." }
-        ]
+        |> withResults
+            [
+                {
+                    Error = Information 3365
+                    Range =
+                        {
+                            StartLine = 10
+                            StartColumn = 20
+                            EndLine = 10
+                            EndColumn = 24
+                        }
+                    Message =
+                        "The syntax 'expr1[expr2]' is used for indexing. Consider adding a type annotation to enable indexing, or if calling a function add a space, e.g. 'expr1 [expr2]'."
+                }
+            ]
 
     [<FSharp.Test.FactForNETCOREAPP>]
     let ``Warn successfully for SynExpr.LongIdent app`` () =
@@ -51,12 +57,18 @@ namespace N
         |> withLangVersion70
         |> compile
         |> shouldFail
-        |> withResults [
-            { Error = Information 3365
-              Range = { StartLine = 12
-                        StartColumn = 13
-                        EndLine = 12
-                        EndColumn = 25 }
-              Message =
-               "The syntax 'expr1[expr2]' is used for indexing. Consider adding a type annotation to enable indexing, or if calling a function add a space, e.g. 'expr1 [expr2]'." }
-        ]
+        |> withResults
+            [
+                {
+                    Error = Information 3365
+                    Range =
+                        {
+                            StartLine = 12
+                            StartColumn = 13
+                            EndLine = 12
+                            EndColumn = 25
+                        }
+                    Message =
+                        "The syntax 'expr1[expr2]' is used for indexing. Consider adding a type annotation to enable indexing, or if calling a function add a space, e.g. 'expr1 [expr2]'."
+                }
+            ]
