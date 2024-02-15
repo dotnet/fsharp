d405abd7908f853fa4ca1ab30b06c7986b6bd9d4
Jakub Majocha <majocha@outlook.com>
2024-02-15 09:13:49 +0100

    fix some tests

--- src/Compiler/Service/service.fs
+++ src/Compiler/Service/service.fs
@@ -345,7 +345,7 @@ type FSharpChecker
         async {
             let ctok = CompilationThreadToken()
             return CompileHelpers.compileFromArgs (ctok, argv, legacyReferenceResolver, None, None)
-        }
+        } |> InitGlobalDiagnostics
 
     /// This function is called when the entire environment is known to have changed for reasons not encoded in the ProjectOptions of any project/compilation.
     /// For example, the type provider approvals file may have changed.
