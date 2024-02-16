4e6022882c5d69a3479fa3bd6485cebb3fe251c4
Jakub Majocha <majocha@outlook.com>
2024-02-16 10:02:33 +0100

    noop for now

--- src/Compiler/Facilities/DiagnosticsLogger.fs
+++ src/Compiler/Facilities/DiagnosticsLogger.fs
@@ -419,8 +419,8 @@ type internal DiagnosticsThreadStatics =
         | _ when box ts |> isNull -> ()
         | al, ts when al = ts -> ()
         | _ ->
-            Debugger.Break()
-            // failwith $"DiagnosticsLogger diverged. AsyncLocal: <{dlName al}>, ThreadStatic: <{dlName ts}>, tid: {Thread.CurrentThread.ManagedThreadId}."
+            // Debugger.Break()
+            failwith $"DiagnosticsLogger diverged. AsyncLocal: <{dlName al}>, ThreadStatic: <{dlName ts}>, tid: {Thread.CurrentThread.ManagedThreadId}."
 
     [<ThreadStatic; DefaultValue>]
     static val mutable private buildPhase: BuildPhase
@@ -952,8 +952,7 @@ type StackGuard(maxDepth: int, name: string) =
         GetEnvInteger ("FSHARP_" + name + "StackGuardDepth") StackGuard.DefaultDepth
 
 
-///
-let PreserveAsyncScope computation = computation : Async<'T>
+let PreserveAsyncScope computation =  computation : Async<'T>
     //async {
     //    let diagnosticsLogger = DiagnosticsThreadStatics.DiagnosticsLogger
     //    let buildPhase = DiagnosticsThreadStatics.BuildPhase
