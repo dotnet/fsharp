7dd79e5a97f18c99884755705862a47c39f37929
Brian Rourke Boll <brianrourkeboll@gmail.com>
2024-03-01 11:45:54 -0500

    Missed in merge

--- tests/FSharp.Compiler.ComponentTests/EmittedIL/SeqExpressionStepping/SeqExpressionStepping.fs
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/SeqExpressionStepping/SeqExpressionStepping.fs
@@ -107,6 +107,7 @@ module SeqExpressionStepping =
     let ``SeqExpressionSteppingTest07_RealInternalSignatureOn_fs`` compilation =
         compilation
         |> withRealInternalSignatureOn
+        |> withLangVersionPreview // TODO https://github.com/dotnet/fsharp/issues/16739: Remove this when LanguageFeature.LowerIntegralRangesToFastLoops is out of preview.
         |> verifyCompilation
 
     // SOURCE=SeqExpressionSteppingTest07.fs SCFLAGS="-g --test:EmitFeeFeeAs100001 --optimize-" COMPILE_ONLY=1 POSTCMD="..\\CompareIL.cmd SeqExpressionSteppingTest7.exe"	# SeqExpressionSteppingTest7.fs -
