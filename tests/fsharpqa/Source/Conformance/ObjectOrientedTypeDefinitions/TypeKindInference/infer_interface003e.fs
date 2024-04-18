7cafa3e995b6ad96dfa65c0870fb3e45a972dd9e
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2024-04-18 22:15:46 +0100

    update tests

--- tests/fsharpqa/Source/Conformance/ObjectOrientedTypeDefinitions/TypeKindInference/infer_interface003e.fs
+++ tests/fsharpqa/Source/Conformance/ObjectOrientedTypeDefinitions/TypeKindInference/infer_interface003e.fs
@@ -3,7 +3,7 @@
 // attribute must match inferred type
 
 //<Expects status="error" span="(15,6)" id="FS0365">No implementation was given for 'abstract TK_I_005\.M: unit -> unit'$</Expects>
-//<Expects status="error" span="(15,6)" id="FS0054">This type is 'abstract' since some abstract members have not been given an implementation\. If this is intentional then add the '\[<AbstractClass>\]' attribute to your type\.$</Expects>
+//<Expects status="error" span="(15,6)" id="FS0054">Classes cannot contain abstract members\. If this is intentional then provide a default member implementation or add the '\[<AbstractClass>\]' attribute to your type\.$</Expects>
 //<Expects status="error" span="(19,6)" id="FS0365">No implementation was given for 'abstract TK_C_000\.M: int -> int'$</Expects>
 //<Expects status="error" span="(19,6)" id="FS0054">Classes cannot contain abstract members\. If this is intentional then provide a default member implementation or add the '\[<AbstractClass>\]' attribute to your type\.$</Expects>
 
