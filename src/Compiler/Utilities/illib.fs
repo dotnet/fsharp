82208bd5edba4593ee2ba5a702c94cf9020d0e69
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2024-04-25 23:08:22 +0300

    Add volatile to valueFactory

--- src/Compiler/Utilities/illib.fs
+++ src/Compiler/Utilities/illib.fs
@@ -14,7 +14,7 @@ open System.Runtime.CompilerServices
 [<Class>]
 type InterruptibleLazy<'T> private (value, valueFactory: unit -> 'T) =
     let syncObj = obj ()
-    let mutable valueFactory = valueFactory
+    let [<VolatileField>] mutable valueFactory = valueFactory
     let mutable value = value
 
     new(valueFactory: unit -> 'T) = InterruptibleLazy(Unchecked.defaultof<_>, valueFactory)
@@ -28,9 +28,7 @@ type InterruptibleLazy<'T> private (value, valueFactory: unit -> 'T) =
         match box valueFactory with
         | null -> value
         | _ ->
-
             Monitor.Enter(syncObj)
-
             try
                 match box valueFactory with
                 | null -> ()
