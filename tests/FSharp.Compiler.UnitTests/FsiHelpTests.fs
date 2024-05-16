cbc2d993e6ff5cc29e0fc206796271c95e33f934
dawe <dawedawe@posteo.de>
2024-05-16 23:15:44 +0200

    add some tests

---
+++ tests/FSharp.Compiler.UnitTests/FsiHelpTests.fs
@@ -0,0 +1,30 @@
+﻿namespace FSharp.Compiler.UnitTests
+
+open FSharp.Test.Assert
+open Xunit
+
+[<CollectionDefinition("FsiHelpTests", DisableParallelization = false)>]
+module FsiHelpTests =
+
+    [<Fact>]
+    let ``Can get help for FSharp.Compiler.Xml.PreXmlDoc.Create`` () =
+        match FSharp.Compiler.Interactive.FsiHelp.Logic.Quoted.tryGetDocumentation <@ FSharp.Compiler.Xml.PreXmlDoc.Create @> with
+        | ValueSome h ->
+            h.Assembly |> shouldBe "FSharp.Compiler.Service.dll"
+            h.FullName |> shouldBe "FSharp.Compiler.Xml.PreXmlDoc.Create"
+
+            h.Summary
+            |> shouldBe "Create a PreXmlDoc from a collection of unprocessed lines"
+        | ValueNone -> Assert.True(false, "No xml documentation found")
+
+    [<Fact>]
+    let ``Can get help for FSharp.Compiler.Syntax.SyntaxNodes.tryPickLast`` () =
+        match FSharp.Compiler.Interactive.FsiHelp.Logic.Quoted.tryGetDocumentation <@ FSharp.Compiler.Syntax.SyntaxNodes.tryPickLast @> with
+        | ValueSome h ->
+            h.Assembly |> shouldBe "FSharp.Compiler.Service.dll"
+            h.FullName |> shouldBe "FSharp.Compiler.Syntax.SyntaxNodesModule.tryPickLast"
+            Assert.StartsWith("Applies the given function to each node of the AST and ", h.Summary)
+            h.Parameters |> shouldNotBeEmpty
+            h.Returns.IsSome |> shouldBeTrue
+            h.Examples |> shouldNotBeEmpty
+        | ValueNone -> Assert.True(false, "No xml documentation found")
