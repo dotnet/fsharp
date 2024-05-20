eb5b5ecfaae59d201b3b97ef08b32d59badbb17f
Tomas Grosup <tomasgrosup@microsoft.com>
2024-05-20 12:01:47 +0200

    update micro tests

--- tests/adhoc/nullness/validate.cmd
+++ tests/adhoc/nullness/validate.cmd
@@ -1,9 +1,9 @@
 REM devenv FSharp.sln /ProjectConfig Proto
-
+set "fsiLocation="C:\Program Files\dotnet\sdk\8.0.300\FSharp\fsc.dll""
 REM dotnet build src\fsc\fscProject -f net472 -c Proto -p ProtoDebug=true
 
-fsc.exe tests\adhoc\nullness\micro.fs -i
-fsc.exe tests\adhoc\nullness\micro.fsi tests\adhoc\nullness\micro.fs -i
+dotnet %fsiLocation% tests\adhoc\nullness\micro.fs -i
+dotnet %fsiLocation% tests\adhoc\nullness\micro.fsi tests\adhoc\nullness\micro.fs -i
 
 artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\micro.fs -i
 artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\micro.fsi tests\adhoc\nullness\micro.fs -i
@@ -17,12 +17,12 @@ artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\micro.fsi tests\adho
 REM -------------
 
 
-fsc.exe tests\adhoc\nullness\existing\positive.fs 2> tests\adhoc\nullness\existing\positive.previous.bsl & type tests\adhoc\nullness\existing\positive.previous.bsl 
+dotnet %fsiLocation% tests\adhoc\nullness\existing\positive.fs 2> tests\adhoc\nullness\existing\positive.previous.bsl & type tests\adhoc\nullness\existing\positive.previous.bsl 
 artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\existing\positive.fs 2>  tests\adhoc\nullness\existing\positive.next.bsl & type tests\adhoc\nullness\existing\positive.next.bsl
 artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\existing\positive.fs --langversion:preview 2>  tests\adhoc\nullness\existing\positive.next.enabled.bsl & type tests\adhoc\nullness\existing\positive.next.enabled.bsl
 artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\existing\positive.fs --langversion:preview --checknulls 2>  tests\adhoc\nullness\existing\positive.next.enabled.checknulls.bsl & type tests\adhoc\nullness\existing\positive.next.enabled.checknulls.bsl
 
-fsc.exe tests\adhoc\nullness\existing\negative.fs 2> tests\adhoc\nullness\existing\negative.previous.bsl & type tests\adhoc\nullness\existing\negative.previous.bsl
+dotnet %fsiLocation% tests\adhoc\nullness\existing\negative.fs 2> tests\adhoc\nullness\existing\negative.previous.bsl & type tests\adhoc\nullness\existing\negative.previous.bsl
 artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\existing\negative.fs 2>  tests\adhoc\nullness\existing\negative.next.bsl & type tests\adhoc\nullness\existing\negative.next.bsl
 artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\existing\negative.fs --langversion:preview 2>  tests\adhoc\nullness\existing\negative.next.enabled.bsl & type tests\adhoc\nullness\existing\negative.next.enabled.bsl
 artifacts\bin\fsc\Proto\net472\fsc.exe tests\adhoc\nullness\existing\negative.fs --langversion:preview --checknulls 2>  tests\adhoc\nullness\existing\negative.next.enabled.checknulls.bsl & type tests\adhoc\nullness\existing\negative.next.enabled.checknulls.bsl
