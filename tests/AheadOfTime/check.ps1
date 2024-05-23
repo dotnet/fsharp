0eb503e75afb66186d799e1e0797d0eb999bd67a
Kevin Ransom <kevinr@microsoft.com>
2024-05-23 13:28:12 -0700

    fix aot

--- tests/AheadOfTime/check.ps1
+++ tests/AheadOfTime/check.ps1
@@ -1,4 +1,10 @@
 Write-Host "AheadOfTime: check1.ps1"
 
+$savedNUGET_PACKAGES=$env:NUGET_PACKAGES
+
+$env:NUGET_PACKAGES=Join-Path $PSScriptRoot "../../artifacts/nuget/AOT/"
+dotnet nuget locals global-packages --clear
+
 Equality\check.ps1
 Trimming\check.ps1
+$env:NUGET_PACKAGES=$savedNUGET_PACKAGES
