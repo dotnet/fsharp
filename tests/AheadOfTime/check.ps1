01f0341b83a594df4d3841c3a9d22fc470f9b70c
Petr <psfinaki@users.noreply.github.com>
2024-05-27 12:25:03 +0200

    Update check.ps1

--- tests/AheadOfTime/check.ps1
+++ tests/AheadOfTime/check.ps1
@@ -1,6 +1,6 @@
 Write-Host "AheadOfTime: check1.ps1"
 
-# the NUGET_PACKAGES environment vriable tells dotnet nuget where the global package is
+# the NUGET_PACKAGES environment variable tells dotnet nuget where the global package is
 # So save the current setting, we'll reset it after the tests are complete
 # Then clear the global cache so that we can grab the FSharp.Core nuget we built earlier
 $savedNUGET_PACKAGES=$env:NUGET_PACKAGES
