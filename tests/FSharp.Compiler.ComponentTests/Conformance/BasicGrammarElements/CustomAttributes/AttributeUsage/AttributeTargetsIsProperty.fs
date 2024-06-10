3ea80b3dae979bcfe5d8f208761eb7fb297f685f
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2024-06-10 13:12:46 +0200

    update tests

--- tests/FSharp.Compiler.ComponentTests/Conformance/BasicGrammarElements/CustomAttributes/AttributeUsage/AttributeTargetsIsProperty.fs
+++ tests/FSharp.Compiler.ComponentTests/Conformance/BasicGrammarElements/CustomAttributes/AttributeUsage/AttributeTargetsIsProperty.fs
@@ -8,16 +8,3 @@ type PropertyLevelAttribute() =
 type U =
     | [<PropertyLevel>] A
     | [<PropertyLevel>] B
-
-[<AttributeUsage(AttributeTargets.Property)>]
-type Name(x: string) =
-    inherit Attribute()
-    member _.value = x
-
-type User =
-    { [<Name("id")>]
-      Id: int
-      [<Name("email")>]
-      Email: string
-      [<Name("organization_id")>]
-      OrganizationId: option<string> }
