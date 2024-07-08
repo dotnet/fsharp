bf1a1b7f8915c2886287de24e8827173dec21afb
Brian Rourke Boll <brianrourkeboll@gmail.com>
2024-07-08 13:55:52 -0400

    Update trimmed size

--- tests/AheadOfTime/Trimming/check.ps1
+++ tests/AheadOfTime/Trimming/check.ps1
@@ -42,7 +42,7 @@ function CheckTrim($root, $tfm, $outputfile, $expected_len) {
 # error NETSDK1124: Trimming assemblies requires .NET Core 3.0 or higher.
 
 # Check net7.0 trimmed assemblies
-CheckTrim -root "SelfContained_Trimming_Test" -tfm "net8.0" -outputfile "FSharp.Core.dll" -expected_len 284672
+CheckTrim -root "SelfContained_Trimming_Test" -tfm "net8.0" -outputfile "FSharp.Core.dll" -expected_len 285184
 
 # Check net8.0 trimmed assemblies
 CheckTrim -root "StaticLinkedFSharpCore_Trimming_Test" -tfm "net8.0" -outputfile "StaticLinkedFSharpCore_Trimming_Test.dll" -expected_len 8818176
