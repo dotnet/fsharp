bbd02996a9a07f0c530d1f8310b06f5cc3126c04
KevinRansom <codecutter@hotmail.com>
2024-07-28 16:54:32 -0700

    rework tests

--- tests/FSharp.Compiler.ComponentTests/EmittedIL/GenericComparison/CrossAssembly.fs
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/GenericComparison/CrossAssembly.fs
@@ -7,43 +7,26 @@ open FSharp.Test.Compiler
 
 module GenericComparisonCrossAssembly =
 
-    [<Fact>]
-    let ``fslib``() =
+    [<InlineData(true)>]        // RealSig
+    [<InlineData(false)>]       // Regular
+    [<Theory>]
+    let ``fslib``(realsig) =
         FSharpWithFileName "Program.fs"
             """
 ValueSome (1, 2) = ValueSome (2, 3) |> ignore"""
+        |> withRealInternalSignature realsig
         |> withOptimize
         |> compileExeAndRun
         |> shouldSucceed
         |> verifyIL [ """
-  .method assembly specialname static void staticInitialization@() cil managed
-  {
-    
-    .maxstack  8
-    IL_0000:  ldc.i4.1
-    IL_0001:  ldc.i4.2
-    IL_0002:  newobj     instance void class [runtime]System.Tuple`2<int32,int32>::.ctor(!0,
-                                                                                          !1)
-    IL_0007:  call       valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<!0> valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<class [runtime]System.Tuple`2<int32,int32>>::NewValueSome(!0)
-    IL_000c:  stsfld     valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<class [runtime]System.Tuple`2<int32,int32>> Program::x@1
-    IL_0011:  ldc.i4.2
-    IL_0012:  ldc.i4.3
-    IL_0013:  newobj     instance void class [runtime]System.Tuple`2<int32,int32>::.ctor(!0,
-                                                                                          !1)
-    IL_0018:  call       valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<!0> valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<class [runtime]System.Tuple`2<int32,int32>>::NewValueSome(!0)
-    IL_001d:  stsfld     valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<class [runtime]System.Tuple`2<int32,int32>> Program::y@1
-    IL_0022:  ldsflda    valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<class [runtime]System.Tuple`2<int32,int32>> Program::x@1
-    IL_0027:  call       valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<class [runtime]System.Tuple`2<int32,int32>> Program::get_y@1()
     IL_002c:  call       class [runtime]System.Collections.IEqualityComparer [FSharp.Core]Microsoft.FSharp.Core.LanguagePrimitives::get_GenericEqualityComparer()
     IL_0031:  call       instance bool valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<class [runtime]System.Tuple`2<int32,int32>>::Equals(valuetype [FSharp.Core]Microsoft.FSharp.Core.FSharpValueOption`1<!0>,
-                                                                                                                                                             class [runtime]System.Collections.IEqualityComparer)
-    IL_0036:  stsfld     bool Program::arg@1
-    IL_003b:  ret
-  } 
 """ ]
 
-    [<Fact>]
-    let ``Another assembly``() =
+    [<InlineData(true)>]        // RealSig
+    [<InlineData(false)>]       // Regular
+    [<Theory>]
+    let ``Another assembly``(realsig) =
         let module1 =
             FSharpWithFileName "Module1.fs"
                 """
@@ -53,6 +36,7 @@ module Module1
     type Struct(v: int, u: int) =
         member _.V = v
         member _.U = u """
+            |> withRealInternalSignature realsig
             |> withOptimize
             |> asLibrary
             |> withName "module1"
@@ -64,33 +48,66 @@ Module1.Struct(1, 2) = Module1.Struct(2, 3) |> ignore"""
 
         module2
         |> withReferences [module1]
+        |> withRealInternalSignature realsig
         |> withOptimize
         |> compileExeAndRun
         |> shouldSucceed
-        |> verifyIL [ """
-  .method assembly specialname static void staticInitialization@() cil managed
-  {
-    
-    .maxstack  8
-    IL_0000:  ldc.i4.1
-    IL_0001:  ldc.i4.2
-    IL_0002:  newobj     instance void [module1]Module1/Struct::.ctor(int32,
-                                                                      int32)
-    IL_0007:  stsfld     valuetype [module1]Module1/Struct Program::x@1
-    IL_000c:  ldc.i4.2
-    IL_000d:  ldc.i4.3
-    IL_000e:  newobj     instance void [module1]Module1/Struct::.ctor(int32,
-                                                                      int32)
-    IL_0013:  stsfld     valuetype [module1]Module1/Struct Program::y@1
-    IL_0018:  ldsflda    valuetype [module1]Module1/Struct Program::x@1
-    IL_001d:  call       valuetype [module1]Module1/Struct Program::get_y@1()
-    IL_0022:  call       class [runtime]System.Collections.IEqualityComparer [FSharp.Core]Microsoft.FSharp.Core.LanguagePrimitives::get_GenericEqualityComparer()
-    IL_0027:  call       instance bool [module1]Module1/Struct::Equals(valuetype [module1]Module1/Struct,
-                                                                       class [runtime]System.Collections.IEqualityComparer)
-    IL_002c:  stsfld     bool Program::arg@1
-    IL_0031:  ret
-  } 
+        |> withILContains [ """
+IL_0022: call class [mscorlib]System.Collections.IEqualityComparer [FSharp.Core]Microsoft.FSharp.Core.LanguagePrimitives::get_GenericEqualityComparer()
+IL_0027: call instance bool [module1]Module1/Struct::Equals(valuetype [module1]Module1/Struct, class [mscorlib]System.Collections.IEqualityComparer)
 """ ]
+
+    [<InlineData(true)>]        // RealSig
+    [<InlineData(false)>]       // Regular
+    [<Theory>]
+    let ``Single assembly``(realsig) =
+        let mainModule = 
+            FSharpWithFileName "Program.fs"
+                """
+module Module1 =
+
+    [<Struct>]
+    type Struct(v: int, u: int) =
+        member _.V = v
+        member _.U = u
+
+module Module2 =
+    Module1.Struct(1, 2) = Module1.Struct(2, 3) |> ignore
+"""
+        mainModule
+        |> withRealInternalSignature realsig
+        |> withOptimize
+        |> compileExeAndRun
+        |> shouldSucceed
+        |> verifyIL [
+            """
+      .method public hidebysig virtual final instance bool  Equals(valuetype Program/Module1/Struct obj) cil managed
+      {
+        .custom instance void [runtime]System.Runtime.CompilerServices.CompilerGeneratedAttribute::.ctor() = ( 01 00 00 00 ) 
+        
+        .maxstack  8
+        IL_0000:  ldarg.0
+        IL_0001:  ldfld      int32 Program/Module1/Struct::v
+        IL_0006:  ldarga.s   obj
+        IL_0008:  ldfld      int32 Program/Module1/Struct::v
+        IL_000d:  bne.un.s   IL_001f
+
+        IL_000f:  ldarg.0
+        IL_0010:  ldfld      int32 Program/Module1/Struct::u
+        IL_0015:  ldarga.s   obj
+        IL_0017:  ldfld      int32 Program/Module1/Struct::u
+        IL_001c:  ceq
+        IL_001e:  ret
+
+        IL_001f:  ldc.i4.0
+        IL_0020:  ret
+      } """
+            """
+    IL_0022:  call       class [runtime]System.Collections.IEqualityComparer [FSharp.Core]Microsoft.FSharp.Core.LanguagePrimitives::get_GenericEqualityComparer()
+    IL_0027:  call       instance bool Program/Module1/Struct::Equals(valuetype Program/Module1/Struct,
+                                                                      class [runtime]System.Collections.IEqualityComparer)
+""" ]
+
 
     [<InlineData(true)>]        // RealSig
     [<InlineData(false)>]       // Regular
@@ -118,14 +135,18 @@ open Module1
 
 Value.Zero = Value.Create 0 |> ignore"""
 
+            |> withRealInternalSignature realsig
             |> withReferences [module1]
             |> withOptimize
 
         module1
+        |> withRealInternalSignature realsig
         |> compile
         |> shouldSucceed
         |> withILContains ["""
-    .method public hidebysig instance bool Equals(class Module1/Value obj, class [runtime]System.Collections.IEqualityComparer comp) cil managed
+    .method public hidebysig virtual final 
+            instance bool  Equals(class Module1/Value obj,
+                                  class [runtime]System.Collections.IEqualityComparer comp) cil managed
     {
       .custom instance void [runtime]System.Runtime.CompilerServices.CompilerGeneratedAttribute::.ctor() = ( 01 00 00 00 ) 
       
@@ -157,24 +178,72 @@ Value.Zero = Value.Create 0 |> ignore"""
         |> ignore
         
         module2
+        |> withRealInternalSignature realsig
         |> compileExeAndRun
         |> shouldSucceed
-        |> verifyIL ["""
-  .method assembly specialname static void staticInitialization@() cil managed
-  {
-    
-    .maxstack  8
-    IL_0000:  call       class [module1]Module1/Value [module1]Module1/Value::get_Zero()
-    IL_0005:  stsfld     class [module1]Module1/Value Program::x@1
-    IL_000a:  ldc.i4.0
-    IL_000b:  call       class [module1]Module1/Value [module1]Module1/Value::Create(int32)
-    IL_0010:  stsfld     class [module1]Module1/Value Program::y@1
-    IL_0015:  call       class [module1]Module1/Value Program::get_x@1()
-    IL_001a:  call       class [module1]Module1/Value Program::get_y@1()
-    IL_001f:  call       class [runtime]System.Collections.IEqualityComparer [FSharp.Core]Microsoft.FSharp.Core.LanguagePrimitives::get_GenericEqualityComparer()
-    IL_0024:  callvirt   instance bool [module1]Module1/Value::Equals(class [module1]Module1/Value,
-                                                                      class [runtime]System.Collections.IEqualityComparer)
-    IL_0029:  stsfld     bool Program::arg@1
-    IL_002e:  ret
-  } 
+        |> withILContains ["""
+IL_001f:  call       class [runtime]System.Collections.IEqualityComparer [FSharp.Core]Microsoft.FSharp.Core.LanguagePrimitives::get_GenericEqualityComparer()
+IL_0024:  callvirt   instance bool [module1]Module1/Value::Equals(class [module1]Module1/Value,
+                                                                  class [runtime]System.Collections.IEqualityComparer)
         """]
+
+    [<InlineData(true)>]        // RealSig
+    [<InlineData(false)>]       // Regular
+    [<Theory>]
+    let ``Single Assembly with private type`` (realsig) =
+
+        let mainModule = 
+            FSharpWithFileName "Program.fs"
+                """
+module Module1 =
+
+    type Value =
+        private { value: uint32 }
+
+        static member Zero = { value = 0u }
+        static member Create(value: int) = { value = uint value }
+
+module Module2 =
+    open Module1
+
+    Value.Zero = Value.Create 0 |> ignore"""
+
+        mainModule
+        |> withRealInternalSignature realsig
+        |> compileExeAndRun
+        |> shouldSucceed
+        |> verifyIL [
+            """
+      .method public hidebysig virtual final instance bool  Equals(class Program/Module1/Value obj) cil managed
+      {
+        .custom instance void [runtime]System.Runtime.CompilerServices.CompilerGeneratedAttribute::.ctor() = ( 01 00 00 00 ) 
+        
+        .maxstack  8
+        IL_0000:  ldarg.0
+        IL_0001:  brfalse.s  IL_0017
+
+        IL_0003:  ldarg.1
+        IL_0004:  brfalse.s  IL_0015
+
+        IL_0006:  ldarg.0
+        IL_0007:  ldfld      uint32 Program/Module1/Value::value@
+        IL_000c:  ldarg.1
+        IL_000d:  ldfld      uint32 Program/Module1/Value::value@
+        IL_0012:  ceq
+        IL_0014:  ret
+
+        IL_0015:  ldc.i4.0
+        IL_0016:  ret
+
+        IL_0017:  ldarg.1
+        IL_0018:  ldnull
+        IL_0019:  cgt.un
+        IL_001b:  ldc.i4.0
+        IL_001c:  ceq
+        IL_001e:  ret
+      } """
+            """
+    IL_0020:  call       class [runtime]System.Collections.IEqualityComparer [FSharp.Core]Microsoft.FSharp.Core.LanguagePrimitives::get_GenericEqualityComparer()
+    IL_0025:  callvirt   instance bool Program/Module1/Value::Equals(class Program/Module1/Value,
+                                                                     class [runtime]System.Collections.IEqualityComparer)
+        """ ]
