358d2961c9ab7a0bd2ab54e712a54c8bced61dbc
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-08-05 16:03:20 +0100

    More test cases

--- tests/FSharp.Compiler.ComponentTests/Conformance/BasicGrammarElements/MemberDefinitions/OverloadingMembers/OverloadResolutionUsingFunction.fs
+++ tests/FSharp.Compiler.ComponentTests/Conformance/BasicGrammarElements/MemberDefinitions/OverloadingMembers/OverloadResolutionUsingFunction.fs
@@ -18,3 +18,11 @@ ae.Handle(
         | :? OperationCanceledException -> true
         | _ -> false        
     ))
+
+let m () =
+    fun x ->(function x -> function y -> function z -> <@ x + y + z @>)
+
+let f () = 
+    (function
+        | None -> fun x -> x + 2
+        | Some f -> f)
