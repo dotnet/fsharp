008f03b789cfb8bb7a080e5a50cf4b5763f2bd8e
Jakub Majocha <1760221+majocha@users.noreply.github.com>
2024-09-02 20:40:54 +0200

    restore cast tests exact behavior

--- tests/fsharp/Compiler/Libraries/Core/Operators/CastTests.fs
+++ tests/fsharp/Compiler/Libraries/Core/Operators/CastTests.fs
@@ -9,11 +9,14 @@ open System
 
 module ``Cast Tests`` =
 
+    let inline isTypeOf<'T> x =
+        Assert.True(typeof<'T>.IsInstanceOfType(x))
+
     [<Fact>]
     let ``Cast precedence over expression forms``() =
         // Regression test for FSHARP1.0:1247
         // Precedence of type annotations :> and :?> over preceeding expression forms, e.g. if-then-else etc.
 
-        Assert.IsAssignableFrom<Object> (2 :> Object) |> ignore
-        Assert.IsAssignableFrom<Object list> [(2 :> Object)] |> ignore
-        Assert.IsAssignableFrom<Object list> [2 :> Object] |> ignore
+        isTypeOf<Object> (2 :> Object)
+        isTypeOf<Object list> [(2 :> Object)]
+        isTypeOf<Object list> [2 :> Object]
