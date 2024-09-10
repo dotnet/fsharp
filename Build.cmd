f754c518b2999599a30d8525c7feb19d2c94df14
Kevin Ransom <kevinr@microsoft.com>
2024-09-09 17:04:50 -0700

    bump

--- Build.cmd
+++ Build.cmd
@@ -1,2 +1,2 @@
-@echo off 
+@echo off
 powershell -ExecutionPolicy ByPass -NoProfile -command "Measure-Command { & """%~dp0eng\build.ps1""" -build -restore %* } | Select-Object TotalMinutes, TotalSeconds"
