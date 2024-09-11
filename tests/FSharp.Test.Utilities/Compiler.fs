4fda023dd54e3dce2262e51ca375b5069cb8915e
Jakub Majocha <1760221+majocha@users.noreply.github.com>
2024-09-11 18:57:29 +0200

    reusing fsi sessions

--- tests/FSharp.Test.Utilities/Compiler.fs
+++ tests/FSharp.Test.Utilities/Compiler.fs
@@ -1041,7 +1041,11 @@ module rec Compiler =
             evalFSharp fs script
         | _ -> failwith "Script evaluation is only supported for F#."
 
-    let getSessionForEval args version = new FSharpScript(additionalArgs=args,quiet=false,langVersion=version)
+    let getSessionForEval =
+        let cache = Collections.Concurrent.ConcurrentDictionary<_, FSharpScript>()
+        let factory(args, version) = new FSharpScript(additionalArgs=args,quiet=false,langVersion=version)
+        fun args version ->
+            cache.GetOrAdd((args, version), factory)
 
     let evalInSharedSession (script:FSharpScript) (cUnit: CompilationUnit)  : CompilationResult =
         match cUnit with
