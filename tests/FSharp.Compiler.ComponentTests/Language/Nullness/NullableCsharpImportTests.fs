9961e4508a005771b2a10126498acd40ae8c39d3
Tomas Grosup <tomasgrosup@microsoft.com>
2024-09-12 14:55:58 +0200

    repro added

--- tests/FSharp.Compiler.ComponentTests/Language/Nullness/NullableCsharpImportTests.fs
+++ tests/FSharp.Compiler.ComponentTests/Language/Nullness/NullableCsharpImportTests.fs
@@ -225,4 +225,39 @@ let ``Consumption of nullable C# - no generics, just strings in methods and fiel
             Error 3261, Line 25, Col 85, Line 25, Col 97, "Nullness warning: The types 'string' and 'string | null' do not have equivalent nullability."
             Error 3261, Line 28, Col 99, Line 28, Col 111, "Nullness warning: The types 'string' and 'string | null' do not have equivalent nullability."
             Error 3261, Line 30, Col 97, Line 30, Col 109, "Nullness warning: The types 'string' and 'string | null' do not have equivalent nullability."]
+    
+    
+[<FactForNETCOREAPP>]
+let ``Regression 17701 - Nullable value type with nested generics`` () =
+    let csharpLib =
+        CSharp """
+using System;
+using System.Collections.Immutable;
+
+namespace Nullables;
+public class NullableClass {
+    public static ImmutableArray<string>? nullableImmArrayOfStrings;
+}""" |> withName "csNullableLib"
+     |> withCSharpLanguageVersionPreview
+
+    FSharp """module FSNullable
+open Nullables
+
+let nullablestrNoParams = NullableClass.nullableImmArrayOfStrings
+let toOption = NullableClass.nullableImmArrayOfStrings |> Option.ofNullable
+let lengthOfFirstString = (NullableClass.nullableImmArrayOfStrings.Value |> Seq.head).Length
+    """        
+    |> asLibrary
+    |> withReferences [csharpLib]
+    |> withStrictNullness
+    |> compile
+    |> shouldFail
+    |> withDiagnostics [
+            Error 3261, Line 5, Col 40, Line 5, Col 85, "Nullness warning: The types 'string' and 'string | null' do not have compatible nullability."
+            Error 3261, Line 5, Col 40, Line 5, Col 85, "Nullness warning: The types 'string' and 'string | null' do not have equivalent nullability."
+            Error 3261, Line 14, Col 34, Line 14, Col 62, "Nullness warning: The types 'string' and 'string | null' do not have equivalent nullability."
+            Error 3261, Line 16, Col 35, Line 16, Col 39, "Nullness warning: The type 'string' does not support 'null'."
+            Error 3261, Line 25, Col 85, Line 25, Col 97, "Nullness warning: The types 'string' and 'string | null' do not have equivalent nullability."
+            Error 3261, Line 28, Col 99, Line 28, Col 111, "Nullness warning: The types 'string' and 'string | null' do not have equivalent nullability."
+            Error 3261, Line 30, Col 97, Line 30, Col 109, "Nullness warning: The types 'string' and 'string | null' do not have equivalent nullability."]
             
