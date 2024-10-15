14ca0c02232f7272caaf4b36a5e7c79745f5f87e
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-10-15 13:41:36 +0100

    Better `inherit` error reporting (#17879)
    * Better `inherit` error reporting
    
    * update baselines
    
    * format code
    
    * Update tests
    
    * release notes
    
    * update tests

--- src/Compiler/Service/FSharpParseFileResults.fs
+++ src/Compiler/Service/FSharpParseFileResults.fs
@@ -814,7 +814,7 @@ type FSharpParseFileResults(diagnostics: FSharpDiagnostic[], input: ParsedInput,
                         | SynMemberDefn.Interface(members = Some membs) ->
                             for m in membs do
                                 yield! walkMember m
-                        | SynMemberDefn.Inherit(_, _, m) ->
+                        | SynMemberDefn.Inherit(range = m) ->
                             // can break on the "inherit" clause
                             yield! checkRange m
                         | SynMemberDefn.ImplicitInherit(_, arg, _, m) ->
