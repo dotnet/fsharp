fea45d578d8d8e40b1b221be20cdb886800a520c
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-10-17 09:16:10 +0100

    comment id.idText.Length >= 3  to see what breaks

--- src/Compiler/Checking/NameResolution.fs
+++ src/Compiler/Checking/NameResolution.fs
@@ -3394,7 +3394,7 @@ let rec ResolvePatternLongIdentPrim sink (ncenv: NameResolver) fullyQualified wa
                     && warnOnUpper = WarnOnUpperCase
                     // HACK: This is an historical hack that seems to related the use country and language codes, which are very common in codebases
                     // Removing this check will cause a lot of new warnings/errors in existing codebases.
-                    && id.idText.Length >= 3
+                    // && id.idText.Length >= 3
                     && System.Char.ToLowerInvariant id.idText[0] <> id.idText[0]
                 then
                     warning(UpperCaseIdentifierInPattern m)
