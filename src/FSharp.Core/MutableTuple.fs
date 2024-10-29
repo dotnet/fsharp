cd0521c68bfd2977ef7248766bafe1cd6336fb40
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-10-29 18:53:28 +0000

    revert changes from MutableTuple

--- src/FSharp.Core/MutableTuple.fs
+++ src/FSharp.Core/MutableTuple.fs
@@ -28,7 +28,7 @@ type AnonymousObject<'T1> =
     val private item1: 'T1
     member x.Item1 = x.item1
 
-    new(item1) = { item1 = item1 }
+    new(Item1) = { item1 = Item1 }
 
 /// <summary>This type shouldn't be used directly from user code.</summary>
 /// <exclude />
@@ -39,7 +39,7 @@ type AnonymousObject<'T1, 'T2> =
     val private item2: 'T2
     member x.Item2 = x.item2
 
-    new(item1, item2) = { item1 = item1; item2 = item2 }
+    new(Item1, Item2) = { item1 = Item1; item2 = Item2 }
 
 /// <summary>This type shouldn't be used directly from user code.</summary>
 /// <exclude />
@@ -53,11 +53,11 @@ type AnonymousObject<'T1, 'T2, 'T3> =
     val private item3: 'T3
     member x.Item3 = x.item3
 
-    new(item1, item2, item3) =
+    new(Item1, Item2, Item3) =
         {
-            item1 = item1
-            item2 = item2
-            item3 = item3
+            item1 = Item1
+            item2 = Item2
+            item3 = Item3
         }
 
 /// <summary>This type shouldn't be used directly from user code.</summary>
@@ -75,12 +75,12 @@ type AnonymousObject<'T1, 'T2, 'T3, 'T4> =
     val private item4: 'T4
     member x.Item4 = x.item4
 
-    new(item1, item2, item3, item4) =
+    new(Item1, Item2, Item3, Item4) =
         {
-            item1 = item1
-            item2 = item2
-            item3 = item3
-            item4 = item4
+            item1 = Item1
+            item2 = Item2
+            item3 = Item3
+            item4 = Item4
         }
 
 /// <summary>This type shouldn't be used directly from user code.</summary>
@@ -101,13 +101,13 @@ type AnonymousObject<'T1, 'T2, 'T3, 'T4, 'T5> =
     val private item5: 'T5
     member x.Item5 = x.item5
 
-    new(item1, item2, item3, item4, item5) =
+    new(Item1, Item2, Item3, Item4, Item5) =
         {
-            item1 = item1
-            item2 = item2
-            item3 = item3
-            item4 = item4
-            item5 = item5
+            item1 = Item1
+            item2 = Item2
+            item3 = Item3
+            item4 = Item4
+            item5 = Item5
         }
 
 /// <summary>This type shouldn't be used directly from user code.</summary>
@@ -131,14 +131,14 @@ type AnonymousObject<'T1, 'T2, 'T3, 'T4, 'T5, 'T6> =
     val private item6: 'T6
     member x.Item6 = x.item6
 
-    new(item1, item2, item3, item4, item5, item6) =
+    new(Item1, Item2, Item3, Item4, Item5, Item6) =
         {
-            item1 = item1
-            item2 = item2
-            item3 = item3
-            item4 = item4
-            item5 = item5
-            item6 = item6
+            item1 = Item1
+            item2 = Item2
+            item3 = Item3
+            item4 = Item4
+            item5 = Item5
+            item6 = Item6
         }
 
 /// <summary>This type shouldn't be used directly from user code.</summary>
@@ -165,15 +165,15 @@ type AnonymousObject<'T1, 'T2, 'T3, 'T4, 'T5, 'T6, 'T7> =
     val private item7: 'T7
     member x.Item7 = x.item7
 
-    new(item1, item2, item3, item4, item5, item6, item7) =
+    new(Item1, Item2, Item3, Item4, Item5, Item6, Item7) =
         {
-            item1 = item1
-            item2 = item2
-            item3 = item3
-            item4 = item4
-            item5 = item5
-            item6 = item6
-            item7 = item7
+            item1 = Item1
+            item2 = Item2
+            item3 = Item3
+            item4 = Item4
+            item5 = Item5
+            item6 = Item6
+            item7 = Item7
         }
 
 /// <summary>This type shouldn't be used directly from user code.</summary>
@@ -203,14 +203,14 @@ type AnonymousObject<'T1, 'T2, 'T3, 'T4, 'T5, 'T6, 'T7, 'T8> =
     val private item8: 'T8
     member x.Item8 = x.item8
 
-    new(item1, item2, item3, item4, item5, item6, item7, item8) =
+    new(Item1, Item2, Item3, Item4, Item5, Item6, Item7, Item8) =
         {
-            item1 = item1
-            item2 = item2
-            item3 = item3
-            item4 = item4
-            item5 = item5
-            item6 = item6
-            item7 = item7
-            item8 = item8
+            item1 = Item1
+            item2 = Item2
+            item3 = Item3
+            item4 = Item4
+            item5 = Item5
+            item6 = Item6
+            item7 = Item7
+            item8 = Item8
         }
