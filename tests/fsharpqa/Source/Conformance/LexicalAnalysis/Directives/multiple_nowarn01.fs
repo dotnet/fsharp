1ec5c871b0e5fe2cefe93ed01cb98f66cc0682d0
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-10-31 12:11:07 +0000

    Revert "update fsharpqa tests"
    This reverts commit 2f5d8789a66b0691ea0c47b8ab26ffbb7bfe0cbc.

--- tests/fsharpqa/Source/Conformance/LexicalAnalysis/Directives/multiple_nowarn01.fs
+++ tests/fsharpqa/Source/Conformance/LexicalAnalysis/Directives/multiple_nowarn01.fs
@@ -1,7 +1,7 @@
 // #Regression #Conformance #LexicalAnalysis #ReqNOMT 
 // Regression test for FSHARP1.0:6044
 
-#nowarn "44" "3874"
+#nowarn "44" "49"
 
 [<System.Obsolete("Dummy attribute to trigger a warning")>]
 let obsoleteIdentifier = 12
