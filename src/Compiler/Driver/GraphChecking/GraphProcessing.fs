20e7d19169accc0bb446ac07afe861a824119c0c
Jakub Majocha <1760221+majocha@users.noreply.github.com>
2024-11-05 14:23:45 +0100

    this change was not guilty after all

--- src/Compiler/Driver/GraphChecking/GraphProcessing.fs
+++ src/Compiler/Driver/GraphChecking/GraphProcessing.fs
@@ -2,8 +2,6 @@
 
 open System.Threading
 open FSharp.Compiler.GraphChecking
-open System.Threading.Tasks
-open System
 
 /// Information about the node in a graph, describing its relation with other nodes.
 type NodeInfo<'Item> =
@@ -176,15 +174,6 @@ let processGraphAsync<'Item, 'Result when 'Item: equality and 'Item: comparison>
     async {
         let transitiveDeps = graph |> Graph.transitive
         let dependents = graph |> Graph.reverse
-        // Cancellation source used to signal either an exception in one of the items or end of processing.
-        let! parentCt = Async.CancellationToken
-        use localCts = new CancellationTokenSource()
-
-        let completionSignal = TaskCompletionSource()
-
-        use _ = parentCt.Register(fun () -> completionSignal.TrySetCanceled() |> ignore)
-
-        use cts = CancellationTokenSource.CreateLinkedTokenSource(parentCt, localCts.Token)
 
         let makeNode (item: 'Item) : GraphNode<'Item, 'Result> =
             let info =
@@ -228,42 +217,8 @@ let processGraphAsync<'Item, 'Result when 'Item: equality and 'Item: comparison>
                     |> Option.defaultWith (fun () -> failwith $"Results for item '{node.Info.Item}' are not yet available")
             }
 
-        let processedCount = IncrementableInt(0)
-
-        let handleExn (item, ex: exn) =
-            try
-                localCts.Cancel()
-            with :? ObjectDisposedException ->
-                // If it's disposed already, it means that the processing has already finished, most likely due to cancellation or failure in another node.
-                ()
-
-            match ex with
-            | :? OperationCanceledException -> completionSignal.TrySetCanceled()
-            | _ ->
-                completionSignal.TrySetException(
-                    GraphProcessingException($"[*] Encountered exception when processing item '{item}': {ex.Message}", ex)
-                )
-            |> ignore
-
-        let incrementProcessedNodesCount () =
-            if processedCount.Increment() = nodes.Count then
-                completionSignal.TrySetResult() |> ignore
-
-        let rec queueNode node =
-            Async.Start(
-                async {
-                    let! res = processNode node |> Async.Catch
-
-                    match res with
-                    | Choice1Of2() -> ()
-                    | Choice2Of2 ex -> handleExn (node.Info.Item, ex)
-                },
-                cts.Token
-            )
-
-        and processNode (node: GraphNode<'Item, 'Result>) : Async<unit> =
+        let rec processNode (node: GraphNode<'Item, 'Result>) =
             async {
-
                 let info = node.Info
 
                 let! singleRes = work getItemPublicNode info
@@ -279,14 +234,10 @@ let processGraphAsync<'Item, 'Result when 'Item: equality and 'Item: comparison>
                         // Note: We cannot read 'dependent.ProcessedDepsCount' again to avoid returning the same item multiple times.
                         pdc = dependent.Info.Deps.Length)
 
-                unblockedDependents |> Array.iter queueNode
-                incrementProcessedNodesCount ()
+                do! unblockedDependents |> Array.map processNode |> Async.Parallel |> Async.Ignore
             }
 
-        leaves |> Array.iter queueNode
-
-        // Wait for end of processing, an exception, or an external cancellation request.
-        do! completionSignal.Task |> Async.AwaitTask
+        do! leaves |> Array.map processNode |> Async.Parallel |> Async.Ignore
 
         // All calculations succeeded - extract the results and sort in input order.
         return
