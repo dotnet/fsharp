8730a03ff08fb302342d51cbd113617c7a365762
Brian Rourke Boll <brianrourkeboll@gmail.com>
2024-12-28 21:02:55 -0500

    Reorder

--- vsintegration/tests/FSharp.Editor.Tests/CodeFixes/AddNewKeywordToDisposableConstructorInvocationTests.fs
+++ vsintegration/tests/FSharp.Editor.Tests/CodeFixes/AddNewKeywordToDisposableConstructorInvocationTests.fs
@@ -52,29 +52,6 @@ let _ = new System.Threading.Tasks.Task<int>(fun _ -> 3)
     Assert.Equal(expected, actual)
 
 [<Fact>]
-let ``Fixes FS0760 — adds parentheses when needed`` () =
-    let code =
-        """
-let path = "test.txt"
-let sr = System.IO.StreamReader path
-"""
-
-    let expected =
-        Some
-            {
-                Message = "Add 'new' keyword"
-                FixedCode =
-                    """
-let path = "test.txt"
-let sr = new System.IO.StreamReader(path)
-"""
-            }
-
-    let actual = codeFix |> tryFix code Auto
-
-    Assert.Equal(expected, actual)
-
-[<Fact>]
 let ``Fixes FS0760 — keeps space`` () =
     let code =
         """
@@ -109,6 +86,29 @@ let stream = System.IO.MemoryStream()
                 FixedCode =
                     """
 let stream = new System.IO.MemoryStream()
+"""
+            }
+
+    let actual = codeFix |> tryFix code Auto
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Fixes FS0760 — adds parentheses when needed`` () =
+    let code =
+        """
+let path = "test.txt"
+let sr = System.IO.StreamReader path
+"""
+
+    let expected =
+        Some
+            {
+                Message = "Add 'new' keyword"
+                FixedCode =
+                    """
+let path = "test.txt"
+let sr = new System.IO.StreamReader(path)
 """
             }
 
