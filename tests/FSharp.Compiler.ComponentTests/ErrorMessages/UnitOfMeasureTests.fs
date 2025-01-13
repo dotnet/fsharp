80ef33721adc4b8ec3266e9cc1856d089d7ef0d1
Edgar Gonzalez <egonzalez@totallymoney.com>
2025-01-13 21:33:24 +0000

    Fix internal error when missing Measure attribute in a unsolved measure typar

---
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/UnitOfMeasureTests.fs
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+module ErrorMessages.UnitOfMeasureTests
+
+open Xunit
+open FSharp.Test.Compiler
+
+[<Fact>]
+let ``Error - Expected unit-of-measure type parameter must be marked with the [<Measure>] attribute.`` () =
+    Fsx """
+type A<[<Measure>]'u>(x : int<'u>) =
+    member this.X = x
+
+type B<'u>(x: 'u) =
+    member this.X = x
+
+module M =
+    type A<'u> with // Note the missing Measure attribute
+        member this.Y = this.X
+
+    type B<'u> with
+        member this.Y = this.X
+        """
+        |> typecheck
+        |> shouldFail
+        |> withDiagnostics [
+            (Error 3874, Line 9, Col 12, Line 9, Col 14, "Expected unit-of-measure type parameter must be marked with the [<Measure>] attribute.")
+        ]
+
+[<Fact>]
+let ``Expected unit-of-measure type parameter must be marked with the [<Measure>] attribute.`` () =
+        Fsx """
+type A<[<Measure>]'u>(x : int<'u>) =
+    member this.X = x
+
+module M =
+    type A<[<Measure>] 'u> with // Note the Measure attribute
+        member this.Y = this.X
+        """
+        |> typecheck
+        |> shouldSucceed
+
