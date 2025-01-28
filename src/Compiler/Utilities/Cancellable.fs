ca2d8c94740e92a4b70af179497954282a1d13ed
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2025-01-28 06:21:49 +0100

    Cancellable: only cancel on OCE with own token

--- src/Compiler/Utilities/Cancellable.fs
+++ src/Compiler/Utilities/Cancellable.fs
@@ -68,8 +68,11 @@ module Cancellable =
             try
                 use _ = Cancellable.UsingToken(ct)
                 oper ct
-            with :? OperationCanceledException as e ->
-                ValueOrCancelled.Cancelled(OperationCanceledException e.CancellationToken)
+            with
+                | :? OperationCanceledException as e when e.CancellationToken = ct ->
+                    ValueOrCancelled.Cancelled e
+                | :? OperationCanceledException as e ->
+                    InvalidOperationException("Wrong cancellation token", e) |> raise
 
     let fold f acc seq =
         Cancellable(fun ct ->
