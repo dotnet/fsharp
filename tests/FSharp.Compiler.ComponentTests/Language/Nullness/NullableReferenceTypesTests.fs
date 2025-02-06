9057d521aebb38b9e577715e217faaa7948b1fbf
Tomas Grosup <tomasgrosup@microsoft.com>
2025-02-06 18:23:33 +0100

    Set propper test assert

--- tests/FSharp.Compiler.ComponentTests/Language/Nullness/NullableReferenceTypesTests.fs
+++ tests/FSharp.Compiler.ComponentTests/Language/Nullness/NullableReferenceTypesTests.fs
@@ -1373,4 +1373,4 @@ let main _ =
     |> compile
     //|> verifyIL ["abc"]
     |> run
-    |> verifyOutputContains [|"StringOption => Nullable / Nullable"|]
+    |> verifyOutputContains [|"Test true;,1 true,2 true,3 true,4 true,5 true,6 false,7 true,8 false,9 false,10 false,11 false,12 true"|]
