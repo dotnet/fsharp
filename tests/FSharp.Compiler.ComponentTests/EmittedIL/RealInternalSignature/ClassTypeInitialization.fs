4513b21740983019afefa765ad44b41c7110ecf3
KevinRansom <codecutter@hotmail.com>
2025-02-28 10:11:54 -0800

    temp

--- tests/FSharp.Compiler.ComponentTests/EmittedIL/RealInternalSignature/ClassTypeInitialization.fs
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/RealInternalSignature/ClassTypeInitialization.fs
@@ -1146,3 +1146,103 @@ namespace GenericInterfaceTest
         |> withOutputContainsAllInOrderWithWildcards [
             "All Classes and Methods in*MembersBasicRoundtrip.dll Verified."
             ]
+
+    [<InlineData(true, true)>]          // RealSig Optimize
+    [<InlineData(true, false)>]         // RealSig NoOptimize
+    [<InlineData(false, true)>]         // Regular Optimize
+    [<InlineData(false, false)>]        // Regular NoOptimize
+    [<Theory>]
+    let ``UtilsStrings.SR`` (realSig, optimize) =
+
+        FSharp """
+// This is a generated file; the original input is 'Facilities\UtilsStrings.txt'
+namespace UtilsStrings
+
+open Microsoft.FSharp.Core.LanguagePrimitives.IntrinsicOperators
+open Microsoft.FSharp.Reflection
+open System.Reflection
+
+type internal SR private() =
+
+    // BEGIN BOILERPLATE
+
+    static let getCurrentAssembly () = System.Reflection.Assembly.GetExecutingAssembly()
+
+    static let getTypeInfo (t: System.Type) = t
+
+    static let resources = lazy (new System.Resources.ResourceManager("UtilsStrings", getCurrentAssembly()))
+
+    static let GetString(name:string) =
+        let s = resources.Value.GetString(name, System.Globalization.CultureInfo.CurrentUICulture)
+        s
+
+    static let mkFunctionValue (tys: System.Type[]) (impl:objnull->objnull) =
+        FSharpValue.MakeFunction(FSharpType.MakeFunctionType(tys.[0],tys.[1]), impl)
+
+    static let funTyC = typeof<(obj -> obj)>.GetGenericTypeDefinition()
+
+    static let isNamedType(ty:System.Type) = not (ty.IsArray ||  ty.IsByRef ||  ty.IsPointer)
+    static let isFunctionType (ty1:System.Type)  =
+        isNamedType(ty1) && getTypeInfo(ty1).IsGenericType && System.Type.op_Equality(ty1.GetGenericTypeDefinition(), funTyC)
+
+    static let rec destFunTy (ty:System.Type) =
+        if isFunctionType ty then
+            ty, ty.GetGenericArguments()
+        else
+            match getTypeInfo(ty).BaseType with
+            | null -> failwith "destFunTy: not a function type"
+            | b -> destFunTy b
+
+    static let buildFunctionForOneArgPat (ty: System.Type) impl =
+        let _,tys = destFunTy ty
+        let rty = tys.[1]
+        // PERF: this technique is a bit slow (e.g. in simple cases, like 'sprintf "%x"')
+        mkFunctionValue tys (fun inp -> impl rty inp)
+
+    static let capture1 (fmt:string) i args ty (go: objnull list -> System.Type -> int -> obj) : obj =
+        match fmt.[i] with
+        | '%' -> go args ty (i+1)
+        | 'd'
+        | 'f'
+        | 's' -> buildFunctionForOneArgPat ty (fun rty n -> go (n :: args) rty (i+1))
+        | _ -> failwith "bad format specifier"
+
+    // newlines and tabs get converted to strings when read from a resource file
+    // this will preserve their original intention
+    static let postProcessString (s: string) =
+        s.Replace("\\n","\n").Replace("\\t","\t").Replace("\\r","\r").Replace("\\\"", "\"")
+
+    static let createMessageString (messageString: string) (fmt: Printf.StringFormat<'T>) : 'T =
+        let fmt = fmt.Value // here, we use the actual error string, as opposed to the one stored as fmt
+        let len = fmt.Length
+
+        /// Function to capture the arguments and then run.
+        let rec capture args ty i =
+            if i >= len ||  (fmt.[i] = '%' && i+1 >= len) then
+                let b = new System.Text.StringBuilder()
+                b.AppendFormat(messageString, [| for x in List.rev args -> x |]) |> ignore
+                box(b.ToString()) |> Unchecked.nonNull
+            elif System.Char.IsSurrogatePair(fmt,i) then
+                capture args ty (i+2)
+            else
+                match fmt.[i] with
+                | '%' ->
+                    let i = i+1
+                    capture1 fmt i args ty capture
+                | _ ->
+                    capture args ty (i+1)
+
+        (unbox (capture [] (typeof<'T>) 0) : 'T)
+    // END BOILERPLATE
+    """
+        |> withName "UtilsStringsSR"
+        |> asLibrary
+        |> withRealInternalSignature realSig
+        |> withOptimization optimize
+        |> compile
+        |> shouldSucceed
+        |> verifyPEFileWithSystemDlls
+        |> withOutputContainsAllInOrderWithWildcards [
+            "All Classes and Methods in*UtilsStringsSR.dll Verified."
+            ]
+
