7e69d3ca5bd3d7a339269df2b4ef8059b536e8db
KevinRansom <codecutter@hotmail.com>
2025-03-25 20:15:09 -0700

    Update nuget handling in fsi

--- tests/FSharp.Compiler.Private.Scripting.UnitTests/FSharpScriptTests.fs
+++ tests/FSharp.Compiler.Private.Scripting.UnitTests/FSharpScriptTests.fs
@@ -513,3 +513,21 @@ let add (col:IServiceCollection) =
         let _value,diag = script.Eval(code)
         Assert.Empty(diag)
 
+    [<Theory>]
+    [<InlineData("""#r "nuget:envdte,usepackagetargets=true" """, true, "")>]
+    [<InlineData("""#r "nuget:envdte,usepackagetargets=false" """, true, "")>]
+    [<InlineData("""#r "nuget:envdte,usepackagetargets=invalidvalue" """, false, "input.fsx (1,1)-(1,49) interactive error Invalid value for boolean 'usepackagetargets', valid values: true or false")>]
+    [<InlineData("""#r "nuget:envdte,usepackagetargets=" """, false, "input.fsx (1,1)-(1,37) interactive error Invalid value for boolean 'usepackagetargets', valid values: true or false")>]
+    member _.``Eval script with usepackagetargets options``(code, shouldSucceed, error) =
+        use script = new FSharpScript()
+        let result, errors = script.Eval(code)
+        match shouldSucceed with
+        | true ->
+            Assert.Empty(errors)
+            match result with
+            | Ok(_) -> ()
+            | Error(ex) -> Assert.True(false, "expected no failures")
+        | false ->
+            Assert.NotEmpty(errors)
+            Assert.Equal(1, errors.Length)
+            Assert.Equal(error, errors.[0].ToString())
