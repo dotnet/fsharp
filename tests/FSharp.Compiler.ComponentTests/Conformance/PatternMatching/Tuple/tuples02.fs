07a88e26eaff94476f71b29b5e19e496adef6c2f
Edgar Gonzalez <edgargonzalez@MacBookPro.communityfibre.co.uk>
2025-03-31 16:13:25 +0100

    update test

--- tests/FSharp.Compiler.ComponentTests/Conformance/PatternMatching/Tuple/tuples02.fs
+++ tests/FSharp.Compiler.ComponentTests/Conformance/PatternMatching/Tuple/tuples02.fs
@@ -5,3 +5,11 @@ module PM =
     let b, c =
         { new PT with member __.A = 1 }
         , { new PT with member __.A = 1 }
+
+module private PM2 = 
+    type PT = 
+        abstract A : int
+    let a = { new PT with member __.A = 1 }
+    let b, c =
+        { new PT with member __.A = 1 }
+        , { new PT with member __.A = 1 }
