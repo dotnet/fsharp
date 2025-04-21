27e201257efd8b333848440d8d0c509d170d0018
Edgar Gonzalez <edgargonzalez.info@gmail.com>
2025-04-21 17:20:50 +0100

    Improve tests

---
+++ tests/FSharp.Compiler.ComponentTests/Conformance/BasicGrammarElements/UseBindings/UseBang01.fs
@@ -0,0 +1,55 @@
+﻿open System
+
+type Disposable() =
+    [<DefaultValue>] static val mutable private disposedTimes: int
+    [<DefaultValue>] static val mutable private constructedTimes: int
+
+    do Disposable.constructedTimes <- Disposable.constructedTimes + 1
+
+    static member DisposeCallCount() = Disposable.disposedTimes
+    static member ConstructorCallCount() = Disposable.constructedTimes
+
+    interface IDisposable with
+        member _.Dispose() =
+            Disposable.disposedTimes <- Disposable.disposedTimes + 1
+
+type DisposableBuilder() =
+    member _.Using(resource: #IDisposable, f) =
+        async {
+            use res = resource
+            return! f res
+        }
+        
+    member _.Bind(disposable: Disposable, f) =
+        async {
+            let c = disposable
+            return! f c
+        }
+    
+    member _.Return(x) = async.Return x
+    
+    member _.ReturnFrom(x) = x
+    
+    member _.Bind(task, f) = async.Bind(task, f)
+
+let counterDisposable = DisposableBuilder()
+
+let example() =
+    counterDisposable {
+        use! res = new Disposable()
+        use! __ = new Disposable()
+        use! (res1) = new Disposable()
+        use! (___) = new Disposable()
+        use! _ = new Disposable()
+        return ()
+    }
+
+example()
+|> Async.RunSynchronously
+
+let disposeCalls = Disposable.DisposeCallCount()
+if disposeCalls <> 5 then
+    failwithf $"unexpected dispose call count: %i{disposeCalls}"
+let ctorCalls = Disposable.ConstructorCallCount()
+if ctorCalls <> 5 then
+    failwithf $"unexpected constructor call count: %i{ctorCalls}"
---
+++ tests/FSharp.Compiler.ComponentTests/Conformance/BasicGrammarElements/UseBindings/UseBangBindings.fs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace Language
+
+open FSharp.Test
+open Xunit
+open FSharp.Test.Compiler
+
+module UseBangBindingsVersion9 =
+    [<Theory; Directory(__SOURCE_DIRECTORY__, Includes=[|"UseBang01.fs"|])>]
+    let ``UseBangBindings - UseBang01_fs - Current LangVersion`` compilation =
+        compilation
+        |> asFsx
+        |> withLangVersion90
+        |> typecheck
+        |> shouldFail
+        |> withDiagnostics [
+            (Error 1228, Line 43, Col 14, Line 43, Col 15, "'use!' bindings must be of the form 'use! <var> = <expr>'")
+        ]
+
+module UseBangBindingsPreview =
+    [<Theory; Directory(__SOURCE_DIRECTORY__, Includes=[|"UseBang01.fs"|])>]
+    let ``UseBangBindings - UseBang01_fs - Preview LangVersion`` compilation =
+        compilation
+        |> asExe
+        |> withLangVersionPreview
+        |> compileAndRun
+        |> shouldSucceed
