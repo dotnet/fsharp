d9d7fa0f9a9c2ecc0c13b99779bb94051e360bc9
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-05-16 17:40:46 +0300

    wip

--- src/Compiler/SyntaxTree/SyntaxTreeOps.fs
+++ src/Compiler/SyntaxTree/SyntaxTreeOps.fs
@@ -103,8 +103,7 @@ let rec pushUnaryArg expr arg =
         SynExpr.TypeApp(innerExpr, mLess, tyargs, mCommas, mGreater, mTypars, m)
     | SynExpr.ArbitraryAfterError(_, m) when m.Start = m.End ->
         SynExpr.DiscardAfterMissingQualificationAfterDot(SynExpr.Ident arg, m.StartRange, unionRanges arg.idRange m)
-    | SynExpr.DiscardAfterMissingQualificationAfterDot(synExpr, _, _) ->
-        pushUnaryArg synExpr arg
+    | SynExpr.DiscardAfterMissingQualificationAfterDot(synExpr, _, _) -> pushUnaryArg synExpr arg
     | _ ->
         errorR (Error(FSComp.SR.tcDotLambdaAtNotSupportedExpression (), expr.Range))
         expr
