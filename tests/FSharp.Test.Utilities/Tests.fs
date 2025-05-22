d092d8f055756c80282d52cdcae8429dc1b08fc6
Jakub Majocha <1760221+majocha@users.noreply.github.com>
2025-05-22 22:47:02 +0200

    add failing test to check that the task works

--- tests/FSharp.Test.Utilities/Tests.fs
+++ tests/FSharp.Test.Utilities/Tests.fs
@@ -43,3 +43,9 @@ let ``TestConsole captures output`` () =
 
     let results = Seq.init 10 task |> Async.Parallel |> Async.RunSynchronously
     Assert.Equal(expected, results)
+
+
+// Dummy failing test
+[<Fact>]
+let ``Dummy test`` () =
+    Assert.True(false, "This test is a dummy and should fail")
