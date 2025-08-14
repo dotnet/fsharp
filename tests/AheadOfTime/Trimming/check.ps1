c21bdb5d7c759823b3a5b85b0e46c97221771f8b
Tomas Grosup <tomasgrosup@microsoft.com>
2025-08-14 10:47:13 +0200

    adjust tests that need illink,
    Right now fails with:
    
     You must install or update .NET to run this application.      App: C:\Users\tomasgrosup\.nuget\packages\microsoft.net.illink.tasks\10.0.0-preview.7.25380.108\tools\net\illink.dll   Architecture: x64   Framework: 'Microsoft.NETCore.App', version '10.0.0-preview.7.25380.108' (x64)

--- tests/AheadOfTime/Trimming/check.ps1
+++ tests/AheadOfTime/Trimming/check.ps1
@@ -51,12 +51,12 @@ function CheckTrim($root, $tfm, $outputfile, $expected_len) {
 # NOTE: Trimming now errors out on desktop TFMs, as shown below:
 # error NETSDK1124: Trimming assemblies requires .NET Core 3.0 or higher.
 
-# Check net10.0 trimmed assemblies
-CheckTrim -root "SelfContained_Trimming_Test" -tfm "net10.0" -outputfile "FSharp.Core.dll" -expected_len 300032
+# Check net9.0 trimmed assemblies
+CheckTrim -root "SelfContained_Trimming_Test" -tfm "net9.0" -outputfile "FSharp.Core.dll" -expected_len 300032
 
-# Check net10.0 trimmed assemblies with static linked FSharpCore
-CheckTrim -root "StaticLinkedFSharpCore_Trimming_Test" -tfm "net10.0" -outputfile "StaticLinkedFSharpCore_Trimming_Test.dll" -expected_len 9154048
+# Check net9.0 trimmed assemblies with static linked FSharpCore
+CheckTrim -root "StaticLinkedFSharpCore_Trimming_Test" -tfm "net9.0" -outputfile "StaticLinkedFSharpCore_Trimming_Test.dll" -expected_len 9154048
 
-# Check net10.0 trimmed assemblies with F# metadata resources removed
-CheckTrim -root "FSharpMetadataResource_Trimming_Test" -tfm "net10.0" -outputfile "FSharpMetadataResource_Trimming_Test.dll" -expected_len 7601152
+# Check net9.0 trimmed assemblies with F# metadata resources removed
+CheckTrim -root "FSharpMetadataResource_Trimming_Test" -tfm "net9.0" -outputfile "FSharpMetadataResource_Trimming_Test.dll" -expected_len 7601152
 
