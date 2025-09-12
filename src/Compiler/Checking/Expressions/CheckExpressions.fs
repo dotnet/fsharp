9bd11e8d18c9da27bfc78edd006c973b3c9d256c
edgargonzalez <egonzalez@totallymoney.com>
2025-09-12 11:54:45 +0100

    Parser: Capture multiple BlockSeparator

--- src/Compiler/Checking/Expressions/CheckExpressions.fs
+++ src/Compiler/Checking/Expressions/CheckExpressions.fs
@@ -7736,7 +7736,7 @@ and TcRecdExpr cenv overallTy env tpenv (inherits, withExprOpt, synRecdFields, m
     let withExprOptChecked, tpenv =
         match withExprOpt with
         | None -> None, tpenv
-        | Some (origExpr, _) ->
+        | Some (origExpr, _blockSep) ->
             match inherits with
             | Some (_, _, mInherits, _, _) -> error(Error(FSComp.SR.tcInvalidRecordConstruction(), mInherits))
             | None ->
@@ -7757,7 +7757,7 @@ and TcRecdExpr cenv overallTy env tpenv (inherits, withExprOpt, synRecdFields, m
 
                 match withExprOpt, synLongId.LongIdent, exprBeingAssigned with
                 | _, [ id ], _ -> ([], id), exprBeingAssigned
-                | Some withExpr, lid, Some exprBeingAssigned -> TransformAstForNestedUpdates cenv env overallTy lid exprBeingAssigned withExpr
+                | Some (origExpr, blockSep), lid, Some exprBeingAssigned -> TransformAstForNestedUpdates cenv env overallTy lid exprBeingAssigned (origExpr, blockSep)
                 | _ -> List.frontAndBack synLongId.LongIdent, exprBeingAssigned)
 
         let flds = if hasOrigExpr then GroupUpdatesToNestedFields flds else flds
