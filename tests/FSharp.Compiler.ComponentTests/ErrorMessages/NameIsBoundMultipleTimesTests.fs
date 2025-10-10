3a1028b06c838b13336847b9c438baff00fadb49
edgargonzalez <egonzalez@totallymoney.com>
2025-10-10 17:12:19 +0100

    Another one

--- tests/FSharp.Compiler.ComponentTests/ErrorMessages/NameIsBoundMultipleTimesTests.fs
+++ tests/FSharp.Compiler.ComponentTests/ErrorMessages/NameIsBoundMultipleTimesTests.fs
@@ -162,3 +162,18 @@ let f (unitVar, ()) = ()
 """
         |> typecheck
         |> shouldSucceed
+
+    [<Fact>]
+    let ``Name is bound multiple times is reported for combined snippet f1,f2,f3``() =
+        Fsx """
+let f1 a a = ()
+let f2 (a, b as c) c = ()
+let f3 (a, b as c) a = ()
+"""
+        |> typecheck
+        |> shouldFail
+        |> withDiagnostics [
+            (Error 38, Line 2, Col 10, Line 2, Col 11, "'a' is bound twice in this pattern");
+            (Error 38, Line 3, Col 20, Line 3, Col 21, "'c' is bound twice in this pattern");
+            (Error 38, Line 4, Col 20, Line 4, Col 21, "'a' is bound twice in this pattern")
+        ]
