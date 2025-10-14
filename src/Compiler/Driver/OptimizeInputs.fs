0cbe37b4b8c698d2c6e3ab441c958adf6639df24
Jakub Majocha <1760221+majocha@users.noreply.github.com>
2025-10-14 13:06:15 +0200

    merge optimization

--- src/Compiler/Driver/OptimizeInputs.fs
+++ src/Compiler/Driver/OptimizeInputs.fs
@@ -510,15 +510,14 @@ let ApplyAllOptimizations
     let phases = phases.ToArray()
 
     let results, optEnvFirstLoop =
-        match tcConfig.optSettings.processingMode with
         // Parallel optimization breaks determinism - turn it off in deterministic builds.
-        | Optimizer.OptimizationProcessingMode.Parallel when (not tcConfig.deterministic) ->
+        if tcConfig.deterministic then
+            optimizeFilesSequentially optEnv phases implFiles
+        else
             let results, optEnvFirstPhase =
                 ParallelOptimization.optimizeFilesInParallel optEnv phases implFiles
 
             results |> Array.toList, optEnvFirstPhase
-        | Optimizer.OptimizationProcessingMode.Parallel
-        | Optimizer.OptimizationProcessingMode.Sequential -> optimizeFilesSequentially optEnv phases implFiles
 
 #if DEBUG
     if tcConfig.showOptimizationData then
@@ -578,7 +577,7 @@ let GenerateIlxCode
             isInteractive = tcConfig.isInteractive
             isInteractiveItExpr = isInteractiveItExpr
             alwaysCallVirt = tcConfig.alwaysCallVirt
-            parallelIlxGenEnabled = tcConfig.parallelIlxGen && not tcConfig.deterministic
+            parallelIlxGenEnabled = not tcConfig.deterministic
         }
 
     ilxGenerator.GenerateCode(ilxGenOpts, optimizedImpls, topAttrs.assemblyAttrs, topAttrs.netModuleAttrs)
