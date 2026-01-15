e474dc8c746a28855d8571731de40cba4e997b21
Tomas Grosup <Tomas.Grosup@gmail.com>
2026-01-15 10:18:47 +0100

    Migrate noframework and nologo tests to ComponentTests

--- tests/FSharp.Compiler.ComponentTests/CompilerOptions/fsc/noframework/noframework.fs
+++ tests/FSharp.Compiler.ComponentTests/CompilerOptions/fsc/noframework/noframework.fs
@@ -8,8 +8,29 @@ open FSharp.Test.Compiler
 
 module Noframework =
 
-    // This test was automatically generated (moved from FSharpQA suite - CompilerOptions/fsc/noframework)
-    //<Expects status="success"></Expects>
+    // Migrated from FSharpQA suite - CompilerOptions/fsc/noframework
+    // Regression test for FSHARP1.0:5976
+    // System.Func<...> is in System.Core.dll - testing that it's available by default
+
+    [<Theory; FileInlineData("noframework01.fs")>]
+    let ``noframework - noframework01_fs - default references`` compilation =
+        compilation
+        |> getCompilation
+        |> asExe
+        |> compileAndRun
+        |> shouldSucceed
+        |> ignore
+
+    [<Theory; FileInlineData("noframework01.fs")>]
+    let ``noframework - noframework01_fs - fsi`` compilation =
+        compilation
+        |> getCompilation
+        |> asFsx
+        |> compile
+        |> shouldSucceed
+        |> ignore
+
+    // See FSHARP1.0:6181 - it is no longer an error to specify --noframework and not specify -r to mscorlib/fscore
     [<Theory; FileInlineData("noframework02.fs")>]
     let ``noframework - noframework02_fs - --noframework`` compilation =
         compilation
