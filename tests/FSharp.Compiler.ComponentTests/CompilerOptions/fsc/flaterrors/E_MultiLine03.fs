935021564f4341700a6cdf2f35e51685e3b006aa
Tomas Grosup <Tomas.Grosup@gmail.com>
2026-01-15 10:38:49 +0100

    Migrate flaterrors and optimize tests from fsharpqa to ComponentTests
    - Git-move flaterrors source files to resources/tests/CompilerOptions/fsc/flaterrors/
    - Rename flaterrors.fs to Flaterrors.fs (capital F)
    - Git-move optimize source files to resources/tests/CompilerOptions/fsc/optimize/
    - Create Optimize.fs with tests for --optimize, --optimize+, -O, -O+, -O-, --optimize-
    - Update FSharp.Compiler.ComponentTests.fsproj with new paths
    - Comment out flaterrors and optimize in test.lst
    - Delete fsharpqa/Source/CompilerOptions/fsc/flaterrors and optimize folders

--- tests/FSharp.Compiler.ComponentTests/CompilerOptions/fsc/flaterrors/E_MultiLine03.fs
+++
@@ -1,5 +0,0 @@
-// #Regression #NoMT #CompilerOptions 
-// Test that using [--flaterrors] does not make an impact on regular single-line error messages
-
-
-let a = b
--- tests/fsharpqa/Source/CompilerOptions/fsc/flaterrors/E_MultiLine04.fs
+++
@@ -1,5 +0,0 @@
-// #Regression #NoMT #CompilerOptions 
-// Used by various [--flaterrors] tests
-//<Expects status="error" id="FS0243">Unrecognized option: '.+'</Expects>
-
-exit 0
