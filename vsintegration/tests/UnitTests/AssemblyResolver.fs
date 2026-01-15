6664f43d1c0a08bf69ffc4e3b63a0baf39294d55
Adam Boniecki <20281641+abonie@users.noreply.github.com>
2026-01-15 14:51:28 +0100

    Ensure legacy project tests are not parallel

--- vsintegration/tests/UnitTests/AssemblyResolver.fs
+++ vsintegration/tests/UnitTests/AssemblyResolver.fs
@@ -1,5 +1,14 @@
 namespace Microsoft.VisualStudio.FSharp
 
+open Xunit
+
+// Disable parallel test execution for this assembly because tests share
+// MSBuild's BuildManager.DefaultBuildManager which is a singleton and
+// throws "The operation cannot be completed because a build is already in progress"
+// when accessed concurrently from multiple tests.
+[<assembly: CollectionBehavior(DisableTestParallelization = true)>]
+do ()
+
 module AssemblyResolver =
     open FSharp.Test.VSAssemblyResolver
 
