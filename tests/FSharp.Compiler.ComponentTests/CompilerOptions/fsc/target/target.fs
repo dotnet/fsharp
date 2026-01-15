db0d451985a2d7c27703667024a5fc80733c0c56
Tomas Grosup <Tomas.Grosup@gmail.com>
2026-01-15 10:01:21 +0100

    Migrate small fsc CompilerOptions folders from fsharpqa to ComponentTests
    Migrated folders:
    - dumpAllCommandLineOptions (2 tests)
    - gccerrors (1 test)
    - standalone (1 test - simplified from original multi-file test)
    - staticlink (1 test - simplified from original multi-file test)
    - tailcalls (6 tests)
    - target (2 tests)
    - tokenize (2 tests)
    
    The nologo folder was skipped as it requires complex baseline comparison.
    
    Tests now use the FileInlineData pattern with source files in the same
    directory as the test files. Updated test.lst to comment out migrated
    folders.

---
+++ tests/FSharp.Compiler.ComponentTests/CompilerOptions/fsc/target/target.fs
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace CompilerOptions.Fsc
+
+open Xunit
+open FSharp.Test
+open FSharp.Test.Compiler
+
+module Target =
+
+    // Migrated from FSharpQA suite - CompilerOptions/fsc/target
+    // Error cases for target option
+
+    // error01.fs: Unrecognized option: '--a'
+    [<Theory; FileInlineData("error01.fs")>]
+    let ``target - error01_fs - --a`` compilation =
+        compilation
+        |> getCompilation
+        |> asExe
+        |> withOptions ["--a"]
+        |> compile
+        |> shouldFail
+        |> withErrorCode 243
+        |> withDiagnosticMessageMatches "Unrecognized option: '--a'"
+        |> ignore
+
+    // error02.fs: The file extension of '/a' is not recognized
+    [<Theory; FileInlineData("error02.fs")>]
+    let ``target - error02_fs - //a`` compilation =
+        compilation
+        |> getCompilation
+        |> asExe
+        |> withOptions ["//a"]
+        |> compile
+        |> shouldFail
+        |> withErrorCode 226
+        |> withDiagnosticMessageMatches @"The file extension of '/a' is not recognized\. Source files must have extension \.fs, \.fsi, \.fsx or \.fsscript"
+        |> ignore
