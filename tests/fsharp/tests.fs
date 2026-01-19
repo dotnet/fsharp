87ba15f37e8f659cadf4ca012ed13fd9d4920665
Adam Boniecki <20281641+abonie@users.noreply.github.com>
2026-01-20 00:40:59 +0100

    Disable parallel execution of fsi tests

--- tests/fsharp/tests.fs
+++ tests/fsharp/tests.fs
@@ -29,6 +29,9 @@ let FSI = FSI_NETFX
 
 let log = printfn
 
+// Disable parallel execution for CoreTests because the printing and FSI tests
+// spawn external FSI processes with stdin redirection that can interfere with each other
+[<Collection(nameof NotThreadSafeResourceCollection)>]
 module CoreTests =
 
 
