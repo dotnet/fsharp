1ce8e10a06109dd96ae3301540cc5b5c459f4cd1
Tomas Grosup <Tomas.Grosup@gmail.com>
2026-02-12 23:43:27 +0100

    Re-trigger CI with sprint 1-5 fixes

--- tests/fsharp/Compiler/CodeGen/EmittedIL/TaskGeneratedCode.fs
+++ tests/fsharp/Compiler/CodeGen/EmittedIL/TaskGeneratedCode.fs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+// Copyright (c) Microsoft Corporation. All Rights Reserved. See License.txt in the project root for license information.
 
 namespace FSharp.Compiler.UnitTests.CodeGen.EmittedIL
 
