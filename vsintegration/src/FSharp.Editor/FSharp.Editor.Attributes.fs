1c49d44aa3921d48a1f1c723cde6900e3d9a43d3
0xmagnus <216443774+0xmagnus91@users.noreply.github.com>
2026-02-11 23:14:00 -0500

    docs: minor formatting cleanup in compiler and editor

--- vsintegration/src/FSharp.Editor/FSharp.Editor.Attributes.fs
+++ vsintegration/src/FSharp.Editor/FSharp.Editor.Attributes.fs
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
-
+// Minor formatting cleanup
 namespace Microsoft.VisualStudio.FSharp.Editor
 
 //--------------------------------------------------------------------------------------
