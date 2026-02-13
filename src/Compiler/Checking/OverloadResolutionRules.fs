f97b0188591cdd98151dcd60171626b53106db8f
Tomas Grosup <Tomas.Grosup@gmail.com>
2026-02-13 15:15:00 +0100

    Remove Description field from TiebreakRule record
    Move documentation strings from runtime Description field to XML doc
    comments on TiebreakRuleId enum cases. The Description field was never
    read at runtime (not in diagnostics, logging, or anywhere), so this
    eliminates unnecessary string allocations on every rule instance.

--- src/Compiler/Checking/OverloadResolutionRules.fs
+++ src/Compiler/Checking/OverloadResolutionRules.fs
@@ -34,27 +34,41 @@ type OverloadResolutionContext =
 /// Rules are evaluated in ascending order by their integer value.
 [<RequireQualifiedAccess>]
 type TiebreakRuleId =
+    /// Prefer methods that don't use type-directed conversion
     | NoTDC = 1
+    /// Prefer methods that need less type-directed conversion
     | LessTDC = 2
+    /// Prefer methods that only have nullable type-directed conversions
     | NullableTDC = 3
+    /// Prefer methods that don't give 'this code is less generic' warnings
     | NoWarnings = 4
+    /// Prefer methods that don't use param array arg
     | NoParamArray = 5
+    /// Prefer methods with more precise param array arg type
     | PreciseParamArray = 6
+    /// Prefer methods that don't use out args
     | NoOutArgs = 7
+    /// Prefer methods that don't use optional args
     | NoOptionalArgs = 8
+    /// Compare regular unnamed args using subsumption ordering
     | UnnamedArgs = 9
+    /// Prefer non-extension methods over extension methods
     | PreferNonExtension = 10
+    /// Between extension methods, prefer most recently opened
     | ExtensionPriority = 11
+    /// Prefer non-generic methods over generic methods
     | PreferNonGeneric = 12
+    /// Prefer more concrete type instantiations over more generic ones
     | MoreConcrete = 13
+    /// F# 5.0 rule - compare all arguments including optional and named
     | NullableOptionalInterop = 14
+    /// For properties, prefer more derived type (partial override support)
     | PropertyOverride = 15
 
 /// Rules are ordered by their TiebreakRuleId (lower value = higher priority).
 type TiebreakRule =
     {
         Id: TiebreakRuleId
-        Description: string
         /// Optional LanguageFeature required for this rule to be active.
         /// If Some, the rule is skipped when the feature is not supported.
         RequiredFeature: LanguageFeature option
@@ -315,7 +329,6 @@ let private compareArgLists ctx (args1: CalledArg list) (args2: CalledArg list) 
 let private noTDCRule: TiebreakRule =
     {
         Id = TiebreakRuleId.NoTDC
-        Description = "Prefer methods that don't use type-directed conversion"
         RequiredFeature = None
         Compare =
             fun _ (struct (_, usesTDC1, _)) (struct (_, usesTDC2, _)) ->
@@ -331,7 +344,6 @@ let private noTDCRule: TiebreakRule =
 let private lessTDCRule: TiebreakRule =
     {
         Id = TiebreakRuleId.LessTDC
-        Description = "Prefer methods that need less type-directed conversion"
         RequiredFeature = None
         Compare =
             fun _ (struct (_, usesTDC1, _)) (struct (_, usesTDC2, _)) ->
@@ -347,7 +359,6 @@ let private lessTDCRule: TiebreakRule =
 let private nullableTDCRule: TiebreakRule =
     {
         Id = TiebreakRuleId.NullableTDC
-        Description = "Prefer methods that only have nullable type-directed conversions"
         RequiredFeature = None
         Compare =
             fun _ (struct (_, usesTDC1, _)) (struct (_, usesTDC2, _)) ->
@@ -363,7 +374,6 @@ let private nullableTDCRule: TiebreakRule =
 let private noWarningsRule: TiebreakRule =
     {
         Id = TiebreakRuleId.NoWarnings
-        Description = "Prefer methods that don't give 'this code is less generic' warnings"
         RequiredFeature = None
         Compare = fun _ (struct (_, _, warnCount1)) (struct (_, _, warnCount2)) -> compare (warnCount1 = 0) (warnCount2 = 0)
     }
@@ -371,7 +381,6 @@ let private noWarningsRule: TiebreakRule =
 let private noParamArrayRule: TiebreakRule =
     {
         Id = TiebreakRuleId.NoParamArray
-        Description = "Prefer methods that don't use param array arg"
         RequiredFeature = None
         Compare =
             fun _ (struct (candidate, _, _)) (struct (other, _, _)) ->
@@ -381,7 +390,6 @@ let private noParamArrayRule: TiebreakRule =
 let private preciseParamArrayRule: TiebreakRule =
     {
         Id = TiebreakRuleId.PreciseParamArray
-        Description = "Prefer methods with more precise param array arg type"
         RequiredFeature = None
         Compare =
             fun ctx (struct (candidate, _, _)) (struct (other, _, _)) ->
@@ -394,7 +402,6 @@ let private preciseParamArrayRule: TiebreakRule =
 let private noOutArgsRule: TiebreakRule =
     {
         Id = TiebreakRuleId.NoOutArgs
-        Description = "Prefer methods that don't use out args"
         RequiredFeature = None
         Compare = fun _ (struct (candidate, _, _)) (struct (other, _, _)) -> compare (not candidate.HasOutArgs) (not other.HasOutArgs)
     }
@@ -402,7 +409,6 @@ let private noOutArgsRule: TiebreakRule =
 let private noOptionalArgsRule: TiebreakRule =
     {
         Id = TiebreakRuleId.NoOptionalArgs
-        Description = "Prefer methods that don't use optional args"
         RequiredFeature = None
         Compare =
             fun _ (struct (candidate, _, _)) (struct (other, _, _)) -> compare (not candidate.HasOptionalArgs) (not other.HasOptionalArgs)
@@ -411,7 +417,6 @@ let private noOptionalArgsRule: TiebreakRule =
 let private unnamedArgsRule: TiebreakRule =
     {
         Id = TiebreakRuleId.UnnamedArgs
-        Description = "Compare regular unnamed args using subsumption ordering"
         RequiredFeature = None
         Compare =
             fun ctx (struct (candidate, _, _)) (struct (other, _, _)) ->
@@ -443,7 +448,6 @@ let private unnamedArgsRule: TiebreakRule =
 let private preferNonExtensionRule: TiebreakRule =
     {
         Id = TiebreakRuleId.PreferNonExtension
-        Description = "Prefer non-extension methods over extension methods"
         RequiredFeature = None
         Compare =
             fun _ (struct (candidate, _, _)) (struct (other, _, _)) ->
@@ -453,7 +457,6 @@ let private preferNonExtensionRule: TiebreakRule =
 let private extensionPriorityRule: TiebreakRule =
     {
         Id = TiebreakRuleId.ExtensionPriority
-        Description = "Between extension methods, prefer most recently opened"
         RequiredFeature = None
         Compare =
             fun _ (struct (candidate, _, _)) (struct (other, _, _)) ->
@@ -466,7 +469,6 @@ let private extensionPriorityRule: TiebreakRule =
 let private preferNonGenericRule: TiebreakRule =
     {
         Id = TiebreakRuleId.PreferNonGeneric
-        Description = "Prefer non-generic methods over generic methods"
         RequiredFeature = None
         Compare =
             fun _ (struct (candidate, _, _)) (struct (other, _, _)) -> compare candidate.CalledTyArgs.IsEmpty other.CalledTyArgs.IsEmpty
@@ -510,7 +512,6 @@ let private getCachedHasSRTP (ctx: OverloadResolutionContext) (meth: CalledMeth<
 let private moreConcreteRule: TiebreakRule =
     {
         Id = TiebreakRuleId.MoreConcrete
-        Description = "Prefer more concrete type instantiations over more generic ones"
         RequiredFeature = Some LanguageFeature.MoreConcreteTiebreaker
         Compare =
             fun ctx (struct (candidate, _, _)) (struct (other, _, _)) ->
@@ -536,7 +537,6 @@ let private moreConcreteRule: TiebreakRule =
 let private nullableOptionalInteropRule: TiebreakRule =
     {
         Id = TiebreakRuleId.NullableOptionalInterop
-        Description = "F# 5.0 rule - compare all arguments including optional and named"
         RequiredFeature = Some LanguageFeature.NullableOptionalInterop
         Compare =
             fun ctx (struct (candidate, _, _)) (struct (other, _, _)) ->
@@ -548,7 +548,6 @@ let private nullableOptionalInteropRule: TiebreakRule =
 let private propertyOverrideRule: TiebreakRule =
     {
         Id = TiebreakRuleId.PropertyOverride
-        Description = "For properties, prefer more derived type (partial override support)"
         RequiredFeature = None
         Compare =
             fun ctx (struct (candidate, _, _)) (struct (other, _, _)) ->
