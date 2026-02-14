c3f34de3159acf7033e11ef2d9762479124a9daf
Bozhidar Batsov <bozhidar@batsov.dev>
2026-02-14 16:59:40 +0200

    Add IL baseline tests for print and printn
    Verify that inline specialization produces direct calls to
    Int32.ToString, Double.ToString with InvariantCulture, and
    Console.Out.Write/WriteLine for the respective types.

---
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/PrintFunction.fs
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace EmittedIL
+
+open Xunit
+open FSharp.Test.Compiler
+
+module PrintFunction =
+
+    [<Fact>]
+    let ``print with int specializes to Int32 ToString with InvariantCulture``() =
+        FSharp """
+module PrintInt
+
+let printInt () = print 42
+         """
+         |> withOptimize
+         |> compile
+         |> shouldSucceed
+         |> verifyIL [
+             """call       class [netstandard]System.IO.TextWriter [netstandard]System.Console::get_Out()"""
+             """call       class [netstandard]System.Globalization.CultureInfo [netstandard]System.Globalization.CultureInfo::get_InvariantCulture()"""
+             """call       instance string [netstandard]System.Int32::ToString(string,"""
+             """callvirt   instance void [netstandard]System.IO.TextWriter::Write(string)"""]
+
+    [<Fact>]
+    let ``printn with int specializes to Int32 ToString with InvariantCulture``() =
+        FSharp """
+module PrintnInt
+
+let printnInt () = printn 42
+         """
+         |> withOptimize
+         |> compile
+         |> shouldSucceed
+         |> verifyIL [
+             """call       class [netstandard]System.IO.TextWriter [netstandard]System.Console::get_Out()"""
+             """call       class [netstandard]System.Globalization.CultureInfo [netstandard]System.Globalization.CultureInfo::get_InvariantCulture()"""
+             """call       instance string [netstandard]System.Int32::ToString(string,"""
+             """callvirt   instance void [netstandard]System.IO.TextWriter::WriteLine(string)"""]
+
+    [<Fact>]
+    let ``print with string writes to Console Out``() =
+        FSharp """
+module PrintString
+
+let printStr () = print "hello"
+         """
+         |> withOptimize
+         |> compile
+         |> shouldSucceed
+         |> verifyIL [
+             """call       class [netstandard]System.IO.TextWriter [netstandard]System.Console::get_Out()"""
+             """callvirt   instance void [netstandard]System.IO.TextWriter::Write(string)"""]
+
+    [<Fact>]
+    let ``printn with float specializes to Double ToString with InvariantCulture``() =
+        FSharp """
+module PrintnFloat
+
+let printnFloat () = printn 3.14
+         """
+         |> withOptimize
+         |> compile
+         |> shouldSucceed
+         |> verifyIL [
+             """call       class [netstandard]System.IO.TextWriter [netstandard]System.Console::get_Out()"""
+             """call       class [netstandard]System.Globalization.CultureInfo [netstandard]System.Globalization.CultureInfo::get_InvariantCulture()"""
+             """call       instance string [netstandard]System.Double::ToString(string,"""
+             """callvirt   instance void [netstandard]System.IO.TextWriter::WriteLine(string)"""]
