d2d60a482324f494dc1e79fe68480022a1ff82b4
Youssef Victor <youssefvictor00@gmail.com>
2026-02-20 10:32:48 +0100

    Update TestFramework.fs

--- tests/FSharp.Test.Utilities/TestFramework.fs
+++ tests/FSharp.Test.Utilities/TestFramework.fs
@@ -516,11 +516,7 @@ module Command =
     let exec dir envVars (redirect:RedirectInfo) path args =
 
 #if !NETCOREAPP
-        let ensureConsole () =
-            // Set UTF-8 encoding for console input/output to ensure FSI receives UTF-8 data.
-            // This is needed because on net472 ProcessStartInfo.StandardInputEncoding is unavailable,
-            // so the spawned process inherits the console's encoding settings.
-            Console.InputEncoding <- Text.UTF8Encoding(false)
+        let ensureConsole () = ()
 #else
         let ensureConsole () = ()
 #endif
