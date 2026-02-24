43a5063c4208d57d7a06af3d7be262baf5dcffc0
Youssef Victor <youssefvictor00@gmail.com>
2026-02-24 10:20:59 +0100

    Write input encoding

--- eng/Build.ps1
+++ eng/Build.ps1
@@ -370,7 +370,9 @@ function TestUsingMSBuild([string] $testProject, [string] $targetFramework, [str
 
     $testResultsDir = "$ArtifactsDir\TestResults\$configuration"
     $testBinLogPath = "$LogDir\${projectName}_$targetFramework.binlog"
-    
+
+    Write-Host [Console]::InputEncoding
+
     # MTP requires --solution flag for .sln files
     $testTarget = if ($testProject.EndsWith('.sln')) { "--solution ""$testProject""" } else { "--project ""$testProject""" }
     
