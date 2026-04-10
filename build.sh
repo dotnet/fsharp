dotnet/fsharp

commit 722fd6b375e5645e51a479b0441f931ee718e86b

Remove time exec from root build scripts (#19211)
Fixes https://github.com/dotnet/fsharp/issues/19209

build.sh
@@ -13,4 +13,4 @@ while [[ -h $source ]]; do
 done
 
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-time "$scriptroot/eng/build.sh" --build --restore "$@"
+"$scriptroot/eng/build.sh" --restore --build $@

Build.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "Measure-Command { & """%~dp0eng\build.ps1""" -build -restore %* } | Select-Object TotalMinutes, TotalSeconds"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\Build.ps1""" -restore -build %*"

commit 341d32c7d5a5c7e10caa2a24b70d0981a0f60b19

Fix quoting in build.sh
This allows it to accept arguments that contain spaces, such as
propreties like "/p:OfficialBuilder=Foo Bar".

Fixes the regression introduced in https://github.com/dotnet/fsharp/pull/19211

build.sh
@@ -13,4 +13,4 @@ while [[ -h $source ]]; do
 done
 
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-"$scriptroot/eng/build.sh" --restore --build $@
+"$scriptroot/eng/build.sh" --restore --build "$@"