9004c7cba0655c8f02a8c1098f89014a514c310f
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2026-05-11 12:24:13 +0200

    Tests/emitted IL: split some baselines into optimize+/optimize- (#19676)

--- tests/FSharp.Compiler.ComponentTests/EmittedIL/AsyncExpressionStepping/AsyncExpressionStepping.fs
+++ tests/FSharp.Compiler.ComponentTests/EmittedIL/AsyncExpressionStepping/AsyncExpressionStepping.fs
@@ -10,7 +10,6 @@ module AsyncExpressionStepping =
         compilation
         |> asFs
         |> withOptions [ "--test:EmitFeeFeeAs100001" ]
-        |> withNoOptimize
         |> withEmbeddedPdb
         |> withEmbedAllSource
         |> ignoreWarnings
@@ -18,37 +17,37 @@ module AsyncExpressionStepping =
         |> verifyBaseline
         |> verifyILBaseline
 
-    [<Theory; FileInlineData("AsyncExpressionSteppingTest1.fs", Realsig=BooleanOptions.Both)>]
+    [<Theory; FileInlineData("AsyncExpressionSteppingTest1.fs", Realsig = BooleanOptions.Both, Optimize = BooleanOptions.Both)>]
     let ``AsyncExpressionSteppingTest1_fs`` compilation =
         compilation
         |> getCompilation
         |> verifyCompilation
 
-    [<Theory; FileInlineData("AsyncExpressionSteppingTest2.fs", Realsig=BooleanOptions.Both)>]
+    [<Theory; FileInlineData("AsyncExpressionSteppingTest2.fs", Realsig=BooleanOptions.Both, Optimize = BooleanOptions.Both)>]
     let ``AsyncExpressionSteppingTest2`` compilation =
         compilation
         |> getCompilation
         |> verifyCompilation
 
-    [<Theory; FileInlineData("AsyncExpressionSteppingTest3.fs", Realsig=BooleanOptions.Both)>]
+    [<Theory; FileInlineData("AsyncExpressionSteppingTest3.fs", Realsig=BooleanOptions.Both, Optimize = BooleanOptions.Both)>]
     let ``AsyncExpressionSteppingTest3_fs`` compilation =
         compilation
         |> getCompilation
         |> verifyCompilation
 
-    [<Theory; FileInlineData("AsyncExpressionSteppingTest4.fs", Realsig=BooleanOptions.Both)>]
+    [<Theory; FileInlineData("AsyncExpressionSteppingTest4.fs", Realsig=BooleanOptions.Both, Optimize = BooleanOptions.Both)>]
     let ``AsyncExpressionSteppingTest4_fs`` compilation =
         compilation
         |> getCompilation
         |> verifyCompilation
 
-    [<Theory; FileInlineData("AsyncExpressionSteppingTest5.fs", Realsig=BooleanOptions.Both)>]
+    [<Theory; FileInlineData("AsyncExpressionSteppingTest5.fs", Realsig=BooleanOptions.Both, Optimize = BooleanOptions.Both)>]
     let ``AsyncExpressionSteppingTest5_fs`` compilation =
         compilation
         |> getCompilation
         |> verifyCompilation
 
-    [<Theory; FileInlineData("AsyncExpressionSteppingTest6.fs", Realsig=BooleanOptions.Both)>]
+    [<Theory; FileInlineData("AsyncExpressionSteppingTest6.fs", Realsig=BooleanOptions.Both, Optimize = BooleanOptions.Both)>]
     let ``AsyncExpressionSteppingTest6_fs`` compilation =
         compilation
         |> getCompilation
