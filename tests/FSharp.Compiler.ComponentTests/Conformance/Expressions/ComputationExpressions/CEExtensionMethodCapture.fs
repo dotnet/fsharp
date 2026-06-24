dotnet/fsharp

commit 7487bd1e51ab9f7fd930d2f013f53853d46741fc

Fix accessibility and type-matching for generic extension method lookups (#19536)

src/Compiler/Checking/NameResolution.fs
@@ -727,8 +727,11 @@ let SelectMethInfosFromExtMembers (infoReader: InfoReader) optFilter apparentTy 
     ]
 
 /// Query the available extension methods of a type (including extension methods for inherited types)
-let ExtensionMethInfosOfTypeInScope (collectionSettings: ResultCollectionSettings) (infoReader: InfoReader) (nenv: NameResolutionEnv) optFilter isInstanceFilter m ty =
-    let extMemsDangling = SelectMethInfosFromExtMembers  infoReader optFilter ty  m nenv.eUnindexedExtensionMembers
+let ExtensionMethInfosOfTypeInScope (collectionSettings: ResultCollectionSettings) (infoReader: InfoReader) (nenv: NameResolutionEnv) ad optFilter isInstanceFilter m ty =
+    let amap = infoReader.amap
+
+    let extMemsDangling = SelectMethInfosFromExtMembers infoReader optFilter ty m nenv.eUnindexedExtensionMembers
+
     if collectionSettings = ResultCollectionSettings.AtMostOneResult && not (isNil extMemsDangling) then 
         extMemsDangling
     else
@@ -743,6 +746,9 @@ let ExtensionMethInfosOfTypeInScope (collectionSettings: ResultCollectionSetting
                 | _ -> [])
         extMemsDangling @ extMemsFromHierarchy
     |> List.filter (fun minfo ->
+        let isAccesible = IsMethInfoAccessible amap m ad minfo
+
+        isAccesible &&
         match isInstanceFilter with
         | LookupIsInstance.Ambivalent -> true
         | LookupIsInstance.Yes -> minfo.IsInstance
@@ -754,7 +760,35 @@ let AllMethInfosOfTypeInScope collectionSettings infoReader nenv optFilter ad fi
     if collectionSettings = ResultCollectionSettings.AtMostOneResult && not (isNil intrinsic) then 
         intrinsic
     else
-        intrinsic @ ExtensionMethInfosOfTypeInScope collectionSettings infoReader nenv optFilter LookupIsInstance.Ambivalent m ty
+        intrinsic @ ExtensionMethInfosOfTypeInScope collectionSettings infoReader nenv ad optFilter LookupIsInstance.Ambivalent m ty
+
+let IsExtensionMethCompatibleWithTy (infoReader: InfoReader) m (ty: TType) (minfo: MethInfo) =
+    let g = infoReader.g
+    let amap = infoReader.amap
+
+    not minfo.IsExtensionMember ||
+    match minfo.GetObjArgTypes(amap, m, []) with
+    | thisTy :: _ ->
+        let ty1 = thisTy |> stripTyEqns g
+        let ty2 = ty |> stripTyEqns g
+
+        match ty1, ty2 with
+        | TType_var (tp1, _), _ ->
+            let coercesToConstraints =
+                tp1.Constraints |> List.choose (function
+                    | TyparConstraint.CoercesTo(targetCTy, _) -> Some targetCTy
+                    | _ -> None)
+            match coercesToConstraints with
+            | [] -> true  // No CoercesTo constraint means it could match anything
+            | constraints ->
+                constraints |> List.exists (fun targetCTy ->
+                    let cTy = targetCTy |> stripTyEqns g
+                    TypeRelations.TypeFeasiblySubsumesType 0 g amap m cTy TypeRelations.CanCoerce ty2)
+        | _, TType_var _ -> true
+        | _ ->
+            TypeRelations.TypeFeasiblySubsumesType 0 g amap m ty1 TypeRelations.CanCoerce ty2
+    | _ -> 
+        true
 
 //-------------------------------------------------------------------------
 // Helpers to do with building environments
@@ -1184,7 +1218,7 @@ let rec AddStaticContentOfTypeToNameEnv (g:TcGlobals) (amap: Import.ImportMap) a
         [| 
             // Extension methods
             yield! 
-                ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults infoReader nenv None LookupIsInstance.No m ty
+                ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults infoReader nenv ad None LookupIsInstance.No m ty
                 |> ChooseMethInfosForNameEnv g m ty
 
             // Extension properties
@@ -2827,7 +2861,7 @@ let rec ResolveLongIdentInTypePrim (ncenv: NameResolver) nenv lookupKind (resInf
                             | _ ->
                                 // lookup in-scope extension methods
                                 // to keep in sync with the same expression in `| Some(MethodItem msets) when isLookupExpr` below
-                                match ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv optFilter isInstanceFilter m ty with
+                                match ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv ad optFilter isInstanceFilter m ty with
                                 | [] -> success [resInfo, x, rest]
                                 | methods ->
                                     let extensionMethods = Item.MakeMethGroup(nm, methods)
@@ -2841,7 +2875,7 @@ let rec ResolveLongIdentInTypePrim (ncenv: NameResolver) nenv lookupKind (resInf
                 let minfos = msets |> ExcludeHiddenOfMethInfos g ncenv.amap m
 
                 // fold the available extension members into the overload resolution
-                let extensionMethInfos = ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv optFilter isInstanceFilter m ty
+                let extensionMethInfos = ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv ad optFilter isInstanceFilter m ty
 
                 success [resInfo, Item.MakeMethGroup (nm, minfos@extensionMethInfos), rest]
 
@@ -2860,7 +2894,7 @@ let rec ResolveLongIdentInTypePrim (ncenv: NameResolver) nenv lookupKind (resInf
 
             if not (isNil pinfos) && isLookUpExpr then OneResult(success (resInfo, Item.Property (nm, pinfos, None), rest)) else
 
-            let minfos = ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv optFilter isInstanceFilter m ty
+            let minfos = ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv ad optFilter isInstanceFilter m ty
 
             if not (isNil minfos) && isLookUpExpr then
                 success [resInfo, Item.MakeMethGroup (nm, minfos), rest]
@@ -2898,7 +2932,7 @@ let rec ResolveLongIdentInTypePrim (ncenv: NameResolver) nenv lookupKind (resInf
             for p in ExtensionPropInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv None LookupIsInstance.Ambivalent ad m ty do
                 addToBuffer p.PropertyName
 
-            for m in ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv None LookupIsInstance.Ambivalent m ty do
+            for m in ExtensionMethInfosOfTypeInScope ResultCollectionSettings.AllResults ncenv.InfoReader nenv ad None LookupIsInstance.Ambivalent m ty do
                 addToBuffer m.DisplayName
 
             for p in GetIntrinsicPropInfosOfType ncenv.InfoReader None ad AllowMultiIntfInstantiations.No findFlag m ty do

tests/FSharp.Compiler.ComponentTests/Conformance/Expressions/ComputationExpressions/CEExtensionMethodCapture.fs
@@ -1,0 +1,169 @@
+module Conformance.Expressions.CEExtensionMethodCapture
+
+open Xunit
+open FSharp.Test.Compiler
+
+[<Fact>]
+let ``CE doesn't capture an extension method beyond the access domain``() =
+    FSharp """
+        open System.Runtime.CompilerServices
+
+        type AsyncSeq<'T>(i: 'T) = 
+            class
+            let l = [i]
+            member this.Data = l
+            end
+
+        type AsyncSeqBuilder() =
+            member _.Yield(x: 'T) : AsyncSeq<'T> =
+                AsyncSeq(x)
+
+        [<Extension>]
+        type PrivateExtensions =
+            [<Extension>]
+            static member inline private Run(this: AsyncSeqBuilder) =
+                this
+
+        let asyncSeq = AsyncSeqBuilder()
+
+        let xs : AsyncSeq<int> =
+            asyncSeq {
+                yield 1
+            }
+    """
+    |> asExe
+    |> compile
+    |> shouldSucceed
+
+[<Fact>]
+let ``CE doesn't capture an extension method with generic type``() =
+    FSharp """
+        open System.Runtime.CompilerServices
+
+        type FooClass = class end
+
+        type AsyncSeq<'T>(i: 'T) = 
+            class
+            let l = [i]
+            member this.Data = l
+            end
+
+        type AsyncSeqBuilder() =
+            member _.Yield(x: 'T) : AsyncSeq<'T> =
+                AsyncSeq(x)
+
+        [<Extension>]
+        type PublicExtensions =
+            [<Extension>]
+            static member inline Run(this: #FooClass) =
+                this
+
+        let asyncSeq = AsyncSeqBuilder()
+
+        let xs : AsyncSeq<int> =
+            asyncSeq {
+                yield 1
+            }
+    """
+    |> asExe
+    |> compile
+    |> shouldSucceed
+
+// Deliberately trigger an error to ensure that a method is captured
+[<Fact>]
+let ``CE captures a public extension method and procudes an error due to invalid args``() =
+    FSharp """
+        open System.Runtime.CompilerServices
+
+        type AsyncSeq<'T>(i: 'T) = 
+            class
+            let l = [i]
+            member this.Data = l
+            end
+
+        type AsyncSeqBuilder() =
+            member _.Yield(x: 'T) : AsyncSeq<'T> =
+                AsyncSeq(x)
+
+        [<Extension>]
+        type PublicExtensions =
+            [<Extension>]
+            static member inline Run(this: AsyncSeqBuilder, invalidArg: string) =
+                this
+
+        let asyncSeq = AsyncSeqBuilder()
+
+        let xs : AsyncSeq<int> =
+            asyncSeq {
+                yield 1
+            }
+    """
+    |> asExe
+    |> compile
+    |> shouldFail
+
+// Deliberately trigger an error to ensure that a method is captured
+[<Fact>]
+let ``CE captures a public extension method with valid generic constrainted type and procudes an error due to invalid args``() =
+    FSharp """
+        open System.Runtime.CompilerServices
+
+        type AsyncSeq<'T>(i: 'T) = 
+            class
+            let l = [i]
+            member this.Data = l
+            end
+
+        type AsyncSeqBuilder() =
+            member _.Yield(x: 'T) : AsyncSeq<'T> =
+                AsyncSeq(x)
+
+        [<Extension>]
+        type PublicExtensions =
+            [<Extension>]
+            static member inline Run(this: #AsyncSeqBuilder, invalidArg: string) =
+                this
+
+        let asyncSeq = AsyncSeqBuilder()
+
+        let xs : AsyncSeq<int> =
+            asyncSeq {
+                yield 1
+            }
+    """
+    |> asExe
+    |> compile
+    |> shouldFail
+
+// Deliberately trigger an error to ensure that a method is captured
+[<Fact>]
+let ``CE captures a public extension method with generic type and procudes an error due to invalid args``() =
+    FSharp """
+        open System.Runtime.CompilerServices
+
+        type AsyncSeq<'T>(i: 'T) = 
+            class
+            let l = [i]
+            member this.Data = l
+            end
+
+        type AsyncSeqBuilder() =
+            member _.Yield(x: 'T) : AsyncSeq<'T> =
+                AsyncSeq(x)
+
+        [<Extension>]
+        type PublicExtensions =
+            [<Extension>]
+            static member Run(this: 'T, invalidArg: string) =
+                this
+
+        let asyncSeq = AsyncSeqBuilder()
+
+        let xs : AsyncSeq<int> =
+            asyncSeq {
+                yield 1
+            }
+    """
+    |> asExe
+    |> compile
+    |> shouldFail

src/Compiler/Checking/Expressions/CheckComputationExpressions.fs
@@ -67,6 +67,7 @@ let inline noTailCall ceenv = { ceenv with tailCall = false }
 
 let inline TryFindIntrinsicOrExtensionMethInfo collectionSettings (cenv: cenv) (env: TcEnv) m ad nm ty =
     AllMethInfosOfTypeInScope collectionSettings cenv.infoReader env.NameEnv (Some nm) ad IgnoreOverrides m ty
+    |> List.filter (IsExtensionMethCompatibleWithTy cenv.infoReader m ty)
 
 /// Ignores an attribute
 let inline IgnoreAttribute _ = None

src/Compiler/Checking/Expressions/CheckExpressions.fs
@@ -3126,6 +3126,7 @@ let BuildPossiblyConditionalMethodCall (cenv: cenv) env isMutable m isProp minfo
 
 let TryFindIntrinsicOrExtensionMethInfo collectionSettings (cenv: cenv) (env: TcEnv) m ad nm ty =
     AllMethInfosOfTypeInScope collectionSettings cenv.infoReader env.NameEnv (Some nm) ad IgnoreOverrides m ty
+    |> List.filter (IsExtensionMethCompatibleWithTy cenv.infoReader m ty)
 
 let TryFindFSharpSignatureInstanceGetterProperty (cenv: cenv) (env: TcEnv) m nm ty (sigTys: TType list) =
     let g = cenv.g

src/Compiler/Checking/NameResolution.fsi
@@ -689,6 +689,9 @@ val internal AllMethInfosOfTypeInScope:
     ty: TType ->
         MethInfo list
 
+/// Check whether the 'this' argument of an extension method is compatible with the target type
+val internal IsExtensionMethCompatibleWithTy: infoReader: InfoReader -> m: range -> ty: TType -> minfo: MethInfo -> bool
+
 /// Used to report an error condition where name resolution failed due to an indeterminate type
 exception internal IndeterminateType of range
 

tests/FSharp.Compiler.ComponentTests/Conformance/BasicGrammarElements/UseBindings/UseBindingsAndExtensionMembers.fs
@@ -1,0 +1,32 @@
+module Conformance.BasicGrammarElements.UseBindExtensionMethodCapture
+
+open Xunit
+open FSharp.Test.Compiler
+
+[<Fact>]
+let ``Use binding doesn't capture an extension method with generic type``() =
+    FSharp """
+        open System
+        open System.Runtime.CompilerServices
+
+        type FooClass() = class end
+
+        type Disposable() =
+            interface IDisposable with 
+                member _.Dispose() = ()
+
+        [<Extension>]
+        type PublicExtensions =
+            [<Extension>]
+            static member inline Dispose(this: #FooClass) =
+                this
+    
+        let foo() =
+            use a = new Disposable()
+            ()
+    
+        foo()
+    """
+    |> asExe
+    |> compile
+    |> shouldSucceed

tests/FSharp.Compiler.ComponentTests/FSharp.Compiler.ComponentTests.fsproj
@@ -76,6 +76,7 @@
     <Compile Include="Conformance\BasicGrammarElements\ValueRestriction\ValueRestriction.fs" />
     <Compile Include="Conformance\BasicGrammarElements\UseBindings\UseBindings.fs" />
     <Compile Include="Conformance\BasicGrammarElements\UseBindings\UseBangBindings.fs" />
+	<Compile Include="Conformance\BasicGrammarElements\UseBindings\UseBindingsAndExtensionMembers.fs" />
     <Compile Include="Conformance\Constraints\Unmanaged.fs" />
     <Compile Include="Conformance\GeneratedEqualityHashingComparison\Attributes\Diags\Attributes_Diags.fs" />
     <Compile Include="Conformance\GeneratedEqualityHashingComparison\Attributes\Legacy\Attributes_Legacy.fs" />
@@ -85,6 +86,7 @@
     <Compile Include="Conformance\Expressions\ApplicationExpressions\Ctor.fs" />
     <Compile Include="Conformance\Expressions\BindingExpressions\BindingExpressions.fs" />
     <Compile Include="Conformance\Expressions\ComputationExpressions\ComputationExpressions.fs" />
+	<Compile Include="Conformance\Expressions\ComputationExpressions\CEExtensionMethodCapture.fs" />
     <Compile Include="Conformance\Expressions\ObjectExpressions\ObjectExpressions.fs" />
     <Compile Include="Conformance\Expressions\ObjectExpressions\StructObjectExpression.fs" />
     <Compile Include="Conformance\Expressions\ControlFlowExpressions\PatternMatching\PatternMatching.fs" />

commit 69fa447ae9e0f828ad8ba16977066ffa23b8eeca

Fix FS1110 in task { let! } when generic IL extension is in scope (#19938)
* Add RED tests for FS1110 in task { let! } with generic IL extension in scope (#19936)

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

* Fix FS1110 in task { let! } when generic IL extension is in scope (#19936)

- Move IsExtensionMethCompatibleWithTy above ExtensionMethInfosOfTypeInScope.
- Pass FormalMethodInst (not []) to GetObjArgTypes so IL method-typar this-args
  import successfully. Wrap in narrow try/with that rethrows
  OperationCanceledException for cancellable IDE paths.
- Confine the AtMostOneResult short-circuit so dangling unindexed extensions
  only mask hierarchy-indexed candidates when at least one is compatible with
  the target type. AllResults branch preserved byte-identically.

Regression from PR #19536 (issue #19349 fix).

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

* Release notes: FS1110 fix in task { let! } with generic IL extension (#19936)

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

---------

Co-authored-by: Copilot <copilot@github.com>
Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

src/Compiler/Checking/NameResolution.fs
@@ -731,13 +731,54 @@ let SelectMethInfosFromExtMembers (infoReader: InfoReader) optFilter apparentTy 
                 | _ -> ()
     ]
 
+let IsExtensionMethCompatibleWithTy (infoReader: InfoReader) m (ty: TType) (minfo: MethInfo) =
+    let g = infoReader.g
+    let amap = infoReader.amap
+
+    not minfo.IsExtensionMember ||
+    let objArgTys =
+        try
+            Some (minfo.GetObjArgTypes(amap, m, minfo.FormalMethodInst))
+        with
+        | :? System.OperationCanceledException -> reraise ()
+        | _ -> None
+
+    match objArgTys with
+    | Some (thisTy :: _) ->
+        let ty1 = thisTy |> stripTyEqns g
+        let ty2 = ty |> stripTyEqns g
+
+        match ty1, ty2 with
+        | TType_var (tp1, _), _ ->
+            let coercesToConstraints =
+                tp1.Constraints |> List.choose (function
+                    | TyparConstraint.CoercesTo(targetCTy, _) -> Some targetCTy
+                    | _ -> None)
+            match coercesToConstraints with
+            | [] -> true
+            | constraints ->
+                constraints |> List.exists (fun targetCTy ->
+                    let cTy = targetCTy |> stripTyEqns g
+                    TypeRelations.TypeFeasiblySubsumesType 0 g amap m cTy TypeRelations.CanCoerce ty2)
+        | _, TType_var _ -> true
+        | _ ->
+            TypeRelations.TypeFeasiblySubsumesType 0 g amap m ty1 TypeRelations.CanCoerce ty2
+    | Some [] ->
+        true
+    | None ->
+        false
+
 /// Query the available extension methods of a type (including extension methods for inherited types)
 let ExtensionMethInfosOfTypeInScope (collectionSettings: ResultCollectionSettings) (infoReader: InfoReader) (nenv: NameResolutionEnv) ad optFilter isInstanceFilter m ty =
     let amap = infoReader.amap
 
     let extMemsDangling = SelectMethInfosFromExtMembers infoReader optFilter ty m nenv.eUnindexedExtensionMembers
 
-    if collectionSettings = ResultCollectionSettings.AtMostOneResult && not (isNil extMemsDangling) then 
+    let shortCircuit =
+        collectionSettings = ResultCollectionSettings.AtMostOneResult
+        && extMemsDangling |> List.exists (fun minfo -> IsExtensionMethCompatibleWithTy infoReader m ty minfo)
+
+    if shortCircuit then
         extMemsDangling
     else
         let extMemsFromHierarchy =
@@ -766,34 +807,6 @@ let AllMethInfosOfTypeInScope collectionSettings infoReader nenv optFilter ad fi
         intrinsic
     else
         intrinsic @ ExtensionMethInfosOfTypeInScope collectionSettings infoReader nenv ad optFilter LookupIsInstance.Ambivalent m ty
-
-let IsExtensionMethCompatibleWithTy (infoReader: InfoReader) m (ty: TType) (minfo: MethInfo) =
-    let g = infoReader.g
-    let amap = infoReader.amap
-
-    not minfo.IsExtensionMember ||
-    match minfo.GetObjArgTypes(amap, m, []) with
-    | thisTy :: _ ->
-        let ty1 = thisTy |> stripTyEqns g
-        let ty2 = ty |> stripTyEqns g
-
-        match ty1, ty2 with
-        | TType_var (tp1, _), _ ->
-            let coercesToConstraints =
-                tp1.Constraints |> List.choose (function
-                    | TyparConstraint.CoercesTo(targetCTy, _) -> Some targetCTy
-                    | _ -> None)
-            match coercesToConstraints with
-            | [] -> true  // No CoercesTo constraint means it could match anything
-            | constraints ->
-                constraints |> List.exists (fun targetCTy ->
-                    let cTy = targetCTy |> stripTyEqns g
-                    TypeRelations.TypeFeasiblySubsumesType 0 g amap m cTy TypeRelations.CanCoerce ty2)
-        | _, TType_var _ -> true
-        | _ ->
-            TypeRelations.TypeFeasiblySubsumesType 0 g amap m ty1 TypeRelations.CanCoerce ty2
-    | _ -> 
-        true
 
 //-------------------------------------------------------------------------
 // Helpers to do with building environments

tests/FSharp.Compiler.ComponentTests/Conformance/Expressions/ComputationExpressions/CEExtensionMethodCapture.fs
@@ -167,3 +167,218 @@ let ``CE captures a public extension method with generic type and procudes an er
     |> asExe
     |> compile
     |> shouldFail
+
+
+// ---------------------------------------------------------------------------
+// Regression tests for https://github.com/dotnet/fsharp/issues/19936
+// FS1110 internal compiler error in `task { let! }` when a generic IL
+// extension method (with a method-typar `this`-arg) is in scope via `open`.
+// Reproduction requires a C# library because F# extensions take the FSMeth
+// path which never crashed.
+// ---------------------------------------------------------------------------
+
+module Issue19936 =
+
+    /// Minimal C# library reproducing the ReactiveUI shape:
+    /// a static extension class with a generic method-typar `this`-arg
+    /// constrained by a generic interface that itself mentions another
+    /// method-typar. This shape is what triggers FS1110 on HEAD when the
+    /// extension is enumerated during CE method resolution.
+    let private csLib =
+        CSharp """
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ReactiveUIRepro
+{
+    public interface IViewFor<TModel> where TModel : class
+    {
+        TModel ViewModel { get; set; }
+    }
+
+    public static class PropertyBindingMixins
+    {
+        public static IDisposable Bind<TView, TModel, TValue>(this TView view, TModel viewModel, TValue value)
+            where TView : class, IViewFor<TModel>
+            where TModel : class
+        {
+            return null;
+        }
+    }
+}
+        """
+        |> withName "ReactiveUIRepro"
+
+    [<Fact>]
+    let ``Issue 19936 - task let! type-checks when generic IL extension method is in scope``() =
+        FSharp """
+module Repro
+open ReactiveUIRepro
+open System.Threading.Tasks
+
+let chooser () : Task<string> = Task.FromResult("file.txt")
+
+let result : Task<string> =
+    task {
+        let! file = chooser ()
+        return file
+    }
+        """
+        |> withReferences [ csLib ]
+        |> asExe
+        |> compile
+        |> shouldSucceed
+
+    [<Fact>]
+    let ``Issue 19936 - task return! type-checks when generic IL extension method is in scope``() =
+        let csLib2 =
+            CSharp """
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ReactiveUIRepro2
+{
+    public interface IViewFor<TModel> where TModel : class
+    {
+        TModel ViewModel { get; set; }
+    }
+
+    public static class ReturnFromMixins
+    {
+        public static IDisposable ReturnFrom<TView, TModel, TValue>(this TView view, TModel viewModel, TValue value)
+            where TView : class, IViewFor<TModel>
+            where TModel : class
+        {
+            return null;
+        }
+    }
+}
+            """
+            |> withName "ReactiveUIRepro2"
+
+        FSharp """
+module Repro
+open ReactiveUIRepro2
+open System.Threading.Tasks
+
+let chooser () : Task<string> = Task.FromResult("file.txt")
+
+let result : Task<string> =
+    task {
+        return! chooser ()
+    }
+        """
+        |> withReferences [ csLib2 ]
+        |> asExe
+        |> compile
+        |> shouldSucceed
+
+    [<Fact>]
+    let ``Issue 19936 - task use! type-checks when generic IL extension method is in scope``() =
+        FSharp """
+module Repro
+open ReactiveUIRepro
+open System
+open System.Threading.Tasks
+
+type Resource() =
+    interface IDisposable with
+        member _.Dispose() = ()
+
+let acquire () : Task<Resource> = Task.FromResult(new Resource())
+
+let result : Task<int> =
+    task {
+        use! r = acquire ()
+        return 1
+    }
+        """
+        |> withReferences [ csLib ]
+        |> asExe
+        |> compile
+        |> shouldSucceed
+
+    /// Regression guard: an in-scope C# generic-extension named `Bind` whose
+    /// `this`-typar is constrained by a generic *class* (instead of a generic
+    /// interface) - the same broken-import shape via a different metadata
+    /// channel - must not crash `task { let! }` resolution.
+    [<Fact>]
+    let ``Issue 19936 - task let! type-checks when class-constrained generic IL Bind extension is in scope``() =
+        let cs =
+            CSharp """
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ClassConstrainedRepro
+{
+    public abstract class BaseView<TModel> where TModel : class
+    {
+        public TModel ViewModel { get; set; }
+    }
+
+    public static class ClassBindMixins
+    {
+        public static int Bind<TView, TModel, TValue>(this TView view, TModel viewModel, TValue value)
+            where TView : BaseView<TModel>
+            where TModel : class
+        {
+            return 0;
+        }
+    }
+}
+            """
+            |> withName "ClassConstrainedRepro"
+
+        FSharp """
+module Repro
+open ClassConstrainedRepro
+open System.Threading.Tasks
+
+let chooser () : Task<int> = Task.FromResult(42)
+
+let result : Task<int> =
+    task {
+        let! v = chooser ()
+        return v + 1
+    }
+        """
+        |> withReferences [ cs ]
+        |> asExe
+        |> compile
+        |> shouldSucceed
+
+    /// Preservation guard for PR #19536's original intent (issue #19349):
+    /// the inaccessible private extension Run must STILL be skipped so the CE
+    /// compiles. (Mirrors the first existing test in this file, but pairs it
+    /// with the issue #19936 fix to confirm we did not regress the original fix.)
+    [<Fact>]
+    let ``Issue 19936 - PR 19536 fix is preserved: inaccessible private extension is still skipped``() =
+        FSharp """
+open System.Runtime.CompilerServices
+
+type AsyncSeq<'T>(i: 'T) =
+    class
+    let l = [i]
+    member this.Data = l
+    end
+
+type AsyncSeqBuilder() =
+    member _.Yield(x: 'T) : AsyncSeq<'T> =
+        AsyncSeq(x)
+
+[<Extension>]
+type PrivateExtensions =
+    [<Extension>]
+    static member inline private Run(this: AsyncSeqBuilder) =
+        this
+
+let asyncSeq = AsyncSeqBuilder()
+
+let xs : AsyncSeq<int> =
+    asyncSeq {
+        yield 1
+    }
+        """
+        |> asExe
+        |> compile
+        |> shouldSucceed