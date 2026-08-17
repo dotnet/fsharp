81fba9db0a7cb263d3fd8e9b96c62b8e4c98c910
Andrii Chebukin <XperiAndri@Outlook.com>
2026-08-17 13:03:28 +0200

    DocumentCache: preserve cancellation semantics

--- vsintegration/src/FSharp.Editor/Common/DocumentCache.fs
+++ vsintegration/src/FSharp.Editor/Common/DocumentCache.fs
@@ -19,29 +19,36 @@ type DocumentCache<'Value when 'Value: not struct>(name: string, ?cacheItemPolic
         defaultArg cacheItemPolicy (CacheItemPolicy(SlidingExpiration = (TimeSpan.FromSeconds defaultSlidingExpiration)))
 
     static let tryGetCachedValueAsync (doc: Document, cache: MemoryCache, ct: CancellationToken) =
-        task {
-            let! currentVersion = doc.GetTextVersionAsync ct
+        if ct.IsCancellationRequested then
+            Task.FromCanceled<'Value voption>(ct)
+        else
+            task {
+                let! currentVersion = doc.GetTextVersionAsync ct
 
-            match cache.Get(doc.Id.ToString()) with
-            | null -> return ValueNone
-            | :? (VersionStamp * 'Value) as value ->
-                if fst value = currentVersion then
-                    return ValueSome(snd value)
-                else
-                    return ValueNone
-            | _ -> return ValueNone
-        }
+                match cache.Get(doc.Id.ToString()) with
+                | null -> return ValueNone
+                | :? (VersionStamp * 'Value) as value ->
+                    if fst value = currentVersion then
+                        return ValueSome(snd value)
+                    else
+                        return ValueNone
+                | _ -> return ValueNone
+            }
 
     static let setCacheValueAsync (doc: Document, value: 'Value, cache: MemoryCache, policy: CacheItemPolicy, ct: CancellationToken) =
-        task {
-            let! currentVersion = doc.GetTextVersionAsync ct
-            do cache.Set(doc.Id.ToString(), (currentVersion, value), policy)
-        }
+        if ct.IsCancellationRequested then
+            Task.FromCanceled<unit>(ct)
+        else
+            task {
+                let! currentVersion = doc.GetTextVersionAsync ct
+                do cache.Set(doc.Id.ToString(), (currentVersion, value), policy)
+            }
 
     new(name: string, slidingExpirationSeconds: float) =
         new DocumentCache<'Value>(name, CacheItemPolicy(SlidingExpiration = (TimeSpan.FromSeconds slidingExpirationSeconds)))
 
-    member _.TryGetValueAsync(doc: Document) : CancellableTask<'Value voption> = fun ct -> tryGetCachedValueAsync (doc, cache, ct)
+    member _.TryGetValueAsync(doc: Document) : CancellableTask<'Value voption> =
+        fun ct -> tryGetCachedValueAsync (doc, cache, ct)
 
     member _.SetAsync(doc: Document, value: 'Value) : CancellableTask<unit> =
         fun ct -> setCacheValueAsync (doc, value, cache, policy, ct)
