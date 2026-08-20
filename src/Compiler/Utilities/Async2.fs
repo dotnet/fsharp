82d03b5676dc1cdcd8ae6f328130aa54d69efee3
Jakub Majocha <1760221+majocha@users.noreply.github.com>
2026-08-20 12:57:36 +0200

    StartAsTask in threadpool

--- src/Compiler/Utilities/Async2.fs
+++ src/Compiler/Utilities/Async2.fs
@@ -540,7 +540,7 @@ type Async2 =
 
     static member StartAsTask(computation: Async2<_>, ?cancellationToken: CancellationToken) : Task<_> =
         let ct = defaultArg cancellationToken CancellationToken.None
-        Async2.startImmediate ct computation
+        Async2.startInThreadPool ct computation
 
     static member RunImmediate(computation: Async2<'T>, ?cancellationToken: CancellationToken) : 'T =
         let ct = defaultArg cancellationToken CancellationToken.None
