11efdeab179d56ce7123dd24d6c5ef5f040f6457
Andrii Chebukin <XperiAndri@Outlook.com>
2026-08-20 23:48:45 +0200

    Optimize list creation in `IlxGen.fs`

--- src/Compiler/CodeGen/IlxGen.fs
+++ src/Compiler/CodeGen/IlxGen.fs
@@ -1972,7 +1972,7 @@ let GenPossibleILDebugRange (cenv: cenv) m =
 //--------------------------------------------------------------------------
 
 let HashRangeSorted (ht: IDictionary<_, int * _>) =
-    [ for KeyValue(_k, v) in ht -> v ] |> List.sortBy fst |> List.map snd
+    seq { for KeyValue(_k, v) in ht -> v } |> Seq.sortBy fst |> Seq.map snd
 
 let MergeOptions m o1 o2 =
     match o1, o2 with
@@ -2075,9 +2075,9 @@ type TypeDefBuilder(tdef: ILTypeDef, tdefDiscards) =
         tdef.With(
             methods = mkILMethods (sortByKey gmethods),
             fields = mkILFields (sortByKey gfields),
-            properties = mkILProperties (tdef.Properties.AsList() @ HashRangeSorted gproperties),
+            properties = mkILProperties [ yield! tdef.Properties.AsList(); yield! HashRangeSorted gproperties ],
             events = mkILEvents (sortByKey gevents),
-            nestedTypes = mkILTypeDefs (tdef.NestedTypes.AsList() @ gnested.Close(g)),
+            nestedTypes = mkILTypeDefs [ yield! tdef.NestedTypes.AsList(); yield! gnested.Close(g) ],
             customAttrs = storeILCustomAttrs attrs
         )
 
@@ -12291,8 +12291,16 @@ and GenTypeDef cenv mgbuf lazyInitInfo eenv m (tycon: Tycon) : ILTypeRef option 
                     | _ -> ()
                 ]
 
-            let ilMethods = methodDefs @ augmentOverrideMethodDefs @ abstractMethodDefs
-            let ilProperties = mkILProperties (ilPropertyDefsForFields @ abstractPropDefs)
+            let ilMethods =
+                [
+                    yield! methodDefs
+                    yield! augmentOverrideMethodDefs
+                    yield! abstractMethodDefs
+                ]
+
+            let ilProperties =
+                mkILProperties [ yield! ilPropertyDefsForFields; yield! abstractPropDefs ]
+
             let ilEvents = mkILEvents abstractEventDefs
             let ilFields = mkILFields ilFieldDefs
 
